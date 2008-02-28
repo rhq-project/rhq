45cd70aa31c6c08f8d8bd2ab539d6bb921095efe
Heiko Rupp <hwr@redhat.com>
2008-02-28 12:46:36 +0000

    Fix package declaration from last svn move.

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/legacy/action/resource/common/events/EventDetailsAction.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/legacy/action/resource/common/events/EventDetailsAction.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-package org.rhq.enterprise.gui.legacy.action.resource.common.monitor.visibility;
+package org.rhq.enterprise.gui.legacy.action.resource.common.events;
 
 import java.util.List;
 import java.util.Map;

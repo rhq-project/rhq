a53b4ee3142f7812769f38128adb3a649ed21879
Mark Spritzler <mspritzl@redhat.com>
2008-03-12 22:12:20 +0000

    Refactored, moved ApplicationServerComponent from the Tomcat plugin to the pluginapi

---
+++ modules/core/plugin-api/src/main/java/org/rhq/core/pluginapi/inventory/ApplicationServerComponent.java
@@ -0,0 +1,36 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.core.pluginapi.inventory;
+
+import java.io.File;
+
+/**
+ * Interface marking a resource component as a server for web applications (EARs, WARs).
+ *
+ * @author Jason Dobies
+ */
+public interface ApplicationServerComponent {
+    /**
+     * Returns the working path of the application server. This is not necessarily just the installation path, but takes
+     * into account multiple configurations (such as JBoss' default profile).
+     *
+     * @return full filesystem path to the working configuration path
+     */
+    File getConfigurationPath();
+}

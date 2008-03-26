2c89ffd3cd1527b0724cef4f677669e866b424d3
Ian Springer <ispringe@redhat.com>
2008-03-26 21:18:35 +0000

    minor cleanup

--- etc/m2/deploy-pom.cmd
+++ etc/m2/deploy-pom.cmd
@@ -4,13 +4,10 @@ if "%1" == "" goto USAGE
 
 setlocal
 set POM_FILE=%1
-set POM_ARGS=-DgeneratePom=false -DpomFile=%POM_FILE%
-set PACKAGING=pom
 if "%RHQ_M2_REPO%" == "" set RHQ_M2_REPO=%USERPROFILE%\.m2\repository
 
-echo mvn deploy:deploy-file -Dfile=%POM_FILE% -Dpackaging=%PACKAGING% %POM_ARGS% -Durl="file://%RHQ_M2_REPO%" -DrepositoryId=local
-
-mvn deploy:deploy-file -Dfile=%POM_FILE% -Dpackaging=%PACKAGING% %POM_ARGS% -Durl="file://%RHQ_M2_REPO%" -DrepositoryId=local
+echo mvn deploy:deploy-file -Dfile="%POM_FILE%" -Dpackaging=pom -DgeneratePom=false -DpomFile="%POM_FILE%" -Durl="file://%RHQ_M2_REPO%" -DrepositoryId=local
+mvn deploy:deploy-file -Dfile="%POM_FILE%" -Dpackaging=pom -DgeneratePom=false -DpomFile="%POM_FILE%" -Durl="file://%RHQ_M2_REPO%" -DrepositoryId=local
 
 endlocal
 exit /b 0

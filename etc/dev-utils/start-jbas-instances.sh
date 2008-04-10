e95cebb1d77eb2ba5438250b8b208f6aae5d4887
Ian Springer <ispringe@redhat.com>
2008-04-10 20:14:39 +0000

    shell script that starts up n JBossAS instances

---
+++ etc/dev-utils/start-jbas-instances.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# set this to the number of instances you want to start
+INSTANCES=20
+
+export JAVA_HOME=${HOME}/jdk1.5.0_15
+export JBOSS_HOME=${HOME}/jboss-4.0.5.GA
+
+for ((  i = 0 ;  i <= ${INSTANCES};  i++  )); do
+  if [ ! -d ${JBOSS_HOME}/server/config${i} ]; then
+    echo "Creating new config dir '${JBOSS_HOME}/server/config${i}'..."
+    cp -pr ${JBOSS_HOME}/server/default ${JBOSS_HOME}/server/config${i}
+  fi
+  echo "Starting instance #${i} bound to 127.0.0.${i}..."
+  nohup ./run.sh -c config${i} -b 127.0.0.${i} >config${i}.log 2>&1 &
+done

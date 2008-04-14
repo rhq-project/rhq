75f86cb45d8a22ea38dd8e4c584a7e6beb8ede96
Ian Springer <ispringe@redhat.com>
2008-04-14 21:34:42 +0000

    reverted changes from r627 as per Charles's request (see (http://jira.rhq-project.org/browse/RHQ-92)

--- modules/core/gui/src/main/java/org/rhq/core/gui/configuration/ConfigUIComponent.java
+++ modules/core/gui/src/main/java/org/rhq/core/gui/configuration/ConfigUIComponent.java
@@ -43,11 +43,10 @@ public class ConfigUIComponent extends UIComponentBase implements FacesComponent
     private static final String NULL_CONFIGURATION_STYLE_ATTRIBUTE = "nullConfigurationStyle";
     private static final String LIST_NAME_ATTRIBUTE = "listName";
     private static final String LIST_INDEX_ATTRIBUTE = "listIndex";
-    private static final String READ_ONLY_ATTRIBUTE = "readOnly";
 
     private ConfigurationDefinition configurationDefinition;
     private Configuration configuration;
-    private Boolean readOnly;
+    private boolean readOnly;
     private String listName;
     private Integer listIndex;
     private String nullConfigurationDefinitionMessage;
@@ -94,11 +93,7 @@ public class ConfigUIComponent extends UIComponentBase implements FacesComponent
     }
 
     public boolean isReadOnly() {
-        if (this.readOnly == null) {
-            this.readOnly = FacesComponentUtility.getExpressionAttribute(this, READ_ONLY_ATTRIBUTE, Boolean.class);
-        }
-        
-        return (this.readOnly != null) ? this.readOnly : true;
+        return readOnly;
     }
 
     public void setReadOnly(boolean readOnly) {
@@ -110,7 +105,7 @@ public class ConfigUIComponent extends UIComponentBase implements FacesComponent
             this.listName = FacesComponentUtility.getExpressionAttribute(this, LIST_NAME_ATTRIBUTE, String.class);
         }
 
-        return this.listName;
+        return listName;
     }
 
     public void setListName(String listName) {
@@ -122,7 +117,7 @@ public class ConfigUIComponent extends UIComponentBase implements FacesComponent
             this.listIndex = FacesComponentUtility.getExpressionAttribute(this, LIST_INDEX_ATTRIBUTE, Integer.class);
         }
 
-        return this.listIndex;
+        return listIndex;
     }
 
     public void setListIndex(Integer listIndex) {

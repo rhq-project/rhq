9b1272693f5aad54af1157b0be248361b3cf6290
Jason Dobies <jdobies@redhat.com>
2008-06-05 18:34:24 +0000

    First pass at the RHQ plugin maven archetype.

---
+++ etc/rhq-plugin-archetype/src/main/resources/archetype-resources/src/main/java/Holder.java
@@ -0,0 +1,8 @@
+package ${package};
+
+/**
+ * Maven archetypes cannot create empty directories, so this class simply functions to get the
+ * requested package structure created.
+ */
+public class Holder {
+}

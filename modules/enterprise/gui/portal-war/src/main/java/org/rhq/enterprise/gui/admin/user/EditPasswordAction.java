22c1b4c9140b2d2fc91a1a0a11f969671ee277a6
Heiko Rupp <hwr@redhat.com>
2008-06-06 15:33:02 +0000

    Allow users to change their own passwords (again). Bassically when the user was changing his own password he was invalidating this current session by the login/logout for verifying his password. RHQ-528

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/admin/user/EditPasswordAction.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/admin/user/EditPasswordAction.java
@@ -21,16 +21,19 @@ package org.rhq.enterprise.gui.admin.user;
 import javax.security.auth.login.LoginException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+
 import org.rhq.core.domain.auth.Subject;
 import org.rhq.core.domain.authz.Permission;
-import org.rhq.enterprise.gui.legacy.Constants;
+import org.rhq.enterprise.gui.legacy.ParamConstants;
 import org.rhq.enterprise.gui.legacy.action.BaseAction;
 import org.rhq.enterprise.gui.legacy.util.RequestUtils;
+import org.rhq.enterprise.gui.util.WebUtility;
 import org.rhq.enterprise.server.auth.SubjectManagerLocal;
 import org.rhq.enterprise.server.util.LookupUtil;
 
@@ -42,41 +45,56 @@ public class EditPasswordAction extends BaseAction {
      * @see BaseAction#execute(org.apache.struts.action.ActionMapping,org.apache.struts.action.ActionForm,
      *      javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
+    @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
         HttpServletResponse response) throws Exception {
         Log log = LogFactory.getLog(NewAction.class.getName());
 
         EditPasswordForm pForm = (EditPasswordForm) form;
-        ActionForward forward = checkSubmit(request, mapping, form, Constants.USER_PARAM, pForm.getId());
+        ActionForward forward = checkSubmit(request, mapping, form, ParamConstants.USER_PARAM, pForm.getId());
 
         if (forward != null) {
             return forward;
         }
 
         SubjectManagerLocal subjectManager = LookupUtil.getSubjectManager();
-        Subject subject = RequestUtils.getSubject(request);
+        Subject subject = WebUtility.getSubject(request);
+        int subjectSession = subject.getSessionId();
+
         Subject userToBeModified = subjectManager.findSubjectById(pForm.getId());
 
-        log.debug("Editing password for user [" + userToBeModified.getName() + "]");
+        String userName = userToBeModified.getName();
+        log.debug("Editing password for user [" + userName + "]");
 
         boolean admin = LookupUtil.getAuthorizationManager().hasGlobalPermission(subject, Permission.MANAGE_SECURITY);
+        boolean isSameUser = subject.getName().equals(userName);
 
         // if this user cannot administer other user's passwords, make sure he gave the old password as confirmation
         if (!admin) {
             try {
-                int dummySession = subjectManager.login(userToBeModified.getName(), pForm.getCurrentPassword())
-                    .getSessionId();
+                int dummySession = subjectManager.login(userName, pForm.getCurrentPassword()).getSessionId();
                 subjectManager.logout(dummySession);
+
+                // The above killed our session for subject if subject == userToBeModified
+                if (isSameUser) {
+                    subject = subjectManager.login(userName, pForm.getCurrentPassword());
+                }
             } catch (LoginException e) {
                 RequestUtils.setError(request, "admin.user.error.WrongPassword", "currentPassword");
-                return returnFailure(request, mapping, Constants.USER_PARAM, pForm.getId());
+                return returnFailure(request, mapping, ParamConstants.USER_PARAM, pForm.getId());
             }
         }
 
-        subjectManager.changePassword(RequestUtils.getSubject(request), userToBeModified.getName(), pForm
-            .getNewPassword());
+        String newPassword = pForm.getNewPassword();
+        subjectManager.changePassword(subject, userName, newPassword);
 
-        return returnSuccess(request, mapping, Constants.USER_PARAM,
+        // when we have arrived here, the password is changed.
+        // If this was a change of our own password, we need to re-login now
+        if (isSameUser) {
+            subjectManager.logout(subject.getSessionId());
+        }
+
+        return returnSuccess(request, mapping, ParamConstants.USER_PARAM,
 
         pForm.getId());
     }

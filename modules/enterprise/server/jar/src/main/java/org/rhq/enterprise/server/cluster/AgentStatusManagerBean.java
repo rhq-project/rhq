3e5898fe436501e8b7d5ae178e2808fa10668de9
Joseph Marques <jmarques@redhat.com>
2008-08-08 05:56:01 +0000

    add author tags and copyright message;

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cluster/AgentStatusManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cluster/AgentStatusManagerBean.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.enterprise.server.cluster;
 
 import java.util.List;
@@ -17,6 +35,10 @@ import org.rhq.core.domain.resource.Agent;
 import org.rhq.core.domain.resource.Resource;
 import org.rhq.enterprise.server.RHQConstants;
 import org.rhq.enterprise.server.core.AgentManagerLocal;
+
+/**
+ * @author Joseph Marques
+ */
 
 @Stateless
 public class AgentStatusManagerBean implements AgentStatusManagerLocal {
--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cluster/instance/ServerManagerLocal.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cluster/instance/ServerManagerLocal.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.enterprise.server.cluster.instance;
 
 import java.util.List;
@@ -6,6 +24,10 @@ import javax.ejb.Local;
 
 import org.rhq.core.domain.cluster.Server;
 import org.rhq.core.domain.resource.Agent;
+
+/**
+ * @author Joseph Marques
+ */
 
 @Local
 public interface ServerManagerLocal {

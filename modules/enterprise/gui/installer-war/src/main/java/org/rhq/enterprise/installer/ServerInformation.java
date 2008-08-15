b39ae7a13a14253ece325680a438bd638499f528
Jay Shaughnessy <jshaughn@redhat.com>
2008-08-15 13:55:51 +0000

    updated rhq_server.mode to rhq_server.operation_mode to handle column name change

--- modules/enterprise/gui/installer-war/src/main/java/org/rhq/enterprise/installer/ServerInformation.java
+++ modules/enterprise/gui/installer-war/src/main/java/org/rhq/enterprise/installer/ServerInformation.java
@@ -1082,9 +1082,9 @@ public class ServerInformation {
             if (0 == stm.executeUpdate()) {
                 stm.close();
 
-                // set all new servers to mode=NORMAL
+                // set all new servers to operation_mode=NORMAL
                 stm = conn
-                    .prepareStatement("INSERT INTO rhq_server ( id, name, address, port, secure_port, ctime, mode ) VALUES ( ?, ?, ?, ?, ?, ?, 'NORMAL' )");
+                    .prepareStatement("INSERT INTO rhq_server ( id, name, address, port, secure_port, ctime, operation_mode ) VALUES ( ?, ?, ?, ?, ?, ?, 'NORMAL' )");
                 stm.setInt(1, db.getNextSequenceValue(conn, "rhq_server", "id"));
                 stm.setString(2, server.name);
                 stm.setString(3, server.endpointAddress);

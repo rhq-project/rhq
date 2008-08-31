5a34cf09c542eb17d59bf520af8aee8446758460
Joseph Marques <jmarques@redhat.com>
2008-08-31 00:58:56 +0000

    add copyright information and author tags;

--- modules/core/domain/src/main/java/org/rhq/core/domain/cluster/composite/FailoverListComposite.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/cluster/composite/FailoverListComposite.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.core.domain.cluster.composite;
 
 import java.io.IOException;
@@ -10,6 +28,9 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * @author jmarques
+ */
 public class FailoverListComposite implements Iterator<FailoverListComposite.ServerEntry>, Serializable {
 
     private static final long serialVersionUID = 1L;

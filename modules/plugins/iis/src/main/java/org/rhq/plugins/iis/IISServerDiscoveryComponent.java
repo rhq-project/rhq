a6b602c34043b25a2bc7d0c196a46cf8155800f6
Joseph Marques <jmarques@redhat.com>
2008-09-07 00:32:41 +0000

    RHQ-427 - suppress all exceptions coming from missing registry keys and values;

--- modules/plugins/iis/src/main/java/org/rhq/plugins/iis/IISServerDiscoveryComponent.java
+++ modules/plugins/iis/src/main/java/org/rhq/plugins/iis/IISServerDiscoveryComponent.java
@@ -5,26 +5,27 @@
 
 package org.rhq.plugins.iis;
 
-import org.hyperic.sigar.win32.RegistryKey;
-import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
-import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
-import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
-import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
-import org.rhq.core.domain.configuration.Configuration;
-import org.rhq.core.system.OperatingSystemType;
+import java.io.File;
+import java.util.Collections;
+import java.util.Set;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hyperic.sigar.win32.RegistryKey;
+import org.hyperic.sigar.win32.Win32Exception;
 
-import java.util.Set;
-import java.util.Collections;
-import java.io.File;
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
+import org.rhq.core.system.OperatingSystemType;
 
 /**
  *
  * @author Greg Hinkle
  */
 public class IISServerDiscoveryComponent implements ResourceDiscoveryComponent {
-
 
     private static final String REG_INET_SERVICE = "SYSTEM\\CurrentControlSet\\Services\\W3SVC";
     private static final String REG_INET = "SOFTWARE\\Microsoft\\InetStp";
@@ -33,49 +34,41 @@ public class IISServerDiscoveryComponent implements ResourceDiscoveryComponent {
 
     private Log log = LogFactory.getLog(IISServerDiscoveryComponent.class);
 
-    
-    public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext discoveryContext) throws InvalidPluginConfigurationException, Exception {
+    public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext discoveryContext)
+        throws InvalidPluginConfigurationException, Exception {
 
         if (discoveryContext.getSystemInformation().getOperatingSystemType() != OperatingSystemType.WINDOWS.WINDOWS)
             return null;
 
-        RegistryKey w3svcKey = RegistryKey.LocalMachine.openSubKey(REG_INET_SERVICE);
+        String path = null;
+        String imagePath = null;
+        String version = null;
 
-        String imagePath = w3svcKey.getStringValue("ImagePath").trim();
+        try {
+            RegistryKey w3svcKey = RegistryKey.LocalMachine.openSubKey(REG_INET_SERVICE);
 
-        String path = imagePath.substring(0, imagePath.lastIndexOf(File.separator));
+            imagePath = w3svcKey.getStringValue("ImagePath").trim();
+            path = imagePath.substring(0, imagePath.lastIndexOf(File.separator));
 
-        RegistryKey versionInfo = RegistryKey.LocalMachine.openSubKey(REG_INET);
-        int majorVersion = versionInfo.getIntValue(REG_INET_MAJORVER);
-        int minorVersion = versionInfo.getIntValue(REG_INET_MINORVER);
+            RegistryKey versionInfo = RegistryKey.LocalMachine.openSubKey(REG_INET);
+            int majorVersion = versionInfo.getIntValue(REG_INET_MAJORVER);
+            int minorVersion = versionInfo.getIntValue(REG_INET_MINORVER);
 
-        String version = majorVersion + "." + minorVersion;
+            version = majorVersion + "." + minorVersion;
+        } catch (Win32Exception w32e) {
+            log.debug("Could not find a valid installation of IIS");
+            return null;
+        }
 
         log.debug("IIS installation found. Path: " + path + " Version: " + version);
 
         Configuration pluginConfig = discoveryContext.getDefaultPluginConfiguration();
-        DiscoveredResourceDetails details =
-                new DiscoveredResourceDetails(
-                        discoveryContext.getResourceType(),
-                        imagePath,
-                        discoveryContext.getSystemInformation().getHostname() + " IIS Server",
-                version,
-                        "IIS Server on " + discoveryContext.getSystemInformation().getHostname(),
-                        pluginConfig, null);
+        DiscoveredResourceDetails details = new DiscoveredResourceDetails(discoveryContext.getResourceType(),
+            imagePath, discoveryContext.getSystemInformation().getHostname() + " IIS Server", version, "IIS Server on "
+                + discoveryContext.getSystemInformation().getHostname(), pluginConfig, null);
 
         return Collections.singleton(details);
     }
-
-
-    /*
-    SVN Project - Server
-      SVN Tree - Trunk
-
-    RHQ Project                      [SVN Project]
-      trunk                          [SVN Tree]
-      branches/API-REFACTORING       [SVN Tree]
-      ghinkle                        [SVN User]
-    */
 
     public static void main(String[] args) throws Exception {
         new IISServerDiscoveryComponent().discoverResources(null);

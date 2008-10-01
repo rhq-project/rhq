d5d26216f078cd0b1baa4e7393a42c7a94fb954a
Joseph Marques <jmarques@redhat.com>
2008-10-01 20:01:56 +0000

    i don't know why, but eclipse insists on removing the ServiceContainerMetricsMBean cast;

--- modules/enterprise/agent/src/main/java/org/rhq/enterprise/agent/AgentManagement.java
+++ modules/enterprise/agent/src/main/java/org/rhq/enterprise/agent/AgentManagement.java
@@ -473,8 +473,8 @@ public class AgentManagement implements AgentManagementMBean, MBeanRegistration 
     private ServiceContainerMetricsMBean getServerSideMetrics() {
         try {
             MBeanServer mbs = m_agent.getServiceContainer().getMBeanServer();
-            return MBeanServerInvocationHandler.newProxyInstance(mbs, ServiceContainerMetricsMBean.OBJECTNAME_METRICS,
-                ServiceContainerMetricsMBean.class, false);
+            return (ServiceContainerMetricsMBean) MBeanServerInvocationHandler.newProxyInstance(mbs,
+                ServiceContainerMetricsMBean.OBJECTNAME_METRICS, ServiceContainerMetricsMBean.class, false);
         } catch (Exception e) {
             throw new IllegalStateException(e); // should never happen
         }

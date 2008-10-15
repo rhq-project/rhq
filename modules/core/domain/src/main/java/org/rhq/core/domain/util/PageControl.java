682d9f1ba389dae57b7ab1b2c03eef70bf95d8a3
Joseph Marques <jmarques@redhat.com>
2008-10-15 17:10:09 +0000

    RHQ-982 - fix the sorting oddites for the 'name' column on the platform/server/service tabs of the resource browser;

--- modules/core/domain/src/main/java/org/rhq/core/domain/util/PageControl.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/util/PageControl.java
@@ -1,25 +1,25 @@
- /*
-  * RHQ Management Platform
-  * Copyright (C) 2005-2008 Red Hat, Inc.
-  * All rights reserved.
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License, version 2, as
-  * published by the Free Software Foundation, and/or the GNU Lesser
-  * General Public License, version 2.1, also as published by the Free
-  * Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License and the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * and the GNU Lesser General Public License along with this program;
-  * if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  */
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.domain.util;
 
 import java.io.Serializable;
@@ -90,7 +90,12 @@ public class PageControl implements Serializable, Cloneable {
     public void addDefaultOrderingField(String defaultField, PageOrdering defaultPageOrdering) {
         for (OrderingField ordering : orderingFields) {
             if (ordering.getField().equals(defaultField)) {
-                ordering.setOrdering(defaultPageOrdering); // might be changing sort of default column
+                /* 
+                 * return immediately, since we've apparently already added this sort field to the 
+                 * list of OrderingFields; UI actions, in particular the integration of sort columns
+                 * on tabular displays with this PageControl object, will dictate whether or not the
+                 * PageOrdering will be reversed, but that logic occurs elsewhere  
+                 */
                 return;
             }
         }

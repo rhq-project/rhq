166d7dfa1f66ad2fd7aa853784ddeb6af6007b23
John Mazzitelli <mazz@redhat.com>
2008-11-20 19:19:39 +0000

    [RHQ-1161] do not allow summary strings to be longer than the maximum allowed in the db column

--- modules/core/domain/src/main/java/org/rhq/core/domain/resource/ResourceError.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/resource/ResourceError.java
@@ -1,25 +1,25 @@
- /*
-  * RHQ Management Platform
-  * Copyright (C) 2005-2008 Red Hat, Inc.
-  * All rights reserved.
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License, version 2, as
-  * published by the Free Software Foundation, and/or the GNU Lesser
-  * General Public License, version 2.1, also as published by the Free
-  * Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License and the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * and the GNU Lesser General Public License along with this program;
-  * if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  */
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.domain.resource;
 
 import java.io.PrintWriter;
@@ -64,6 +64,8 @@ public class ResourceError implements Serializable {
     public static final String QUERY_FIND_BY_RESOURCE_ID = "ResourceError.findByResource";
     public static final String QUERY_FIND_BY_RESOURCE_ID_AND_ERROR_TYPE = "ResourceError.findByResourceAndErrorType";
 
+    private static final int MAX_SUMMARY_LENGTH = 1000;
+
     @Column(name = "ID", nullable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "RHQ_RESOURCE_ERROR_SEQ")
     @Id
@@ -80,7 +82,7 @@ public class ResourceError implements Serializable {
     @Enumerated(EnumType.STRING)
     private ResourceErrorType errorType;
 
-    @Column(name = "SUMMARY", nullable = false, length = 1000)
+    @Column(name = "SUMMARY", nullable = false, length = ResourceError.MAX_SUMMARY_LENGTH)
     private String summary;
 
     @Column(name = "DETAIL", nullable = true)
@@ -98,11 +100,8 @@ public class ResourceError implements Serializable {
      * @param detail       a detailed description of the error - typically a stack trace; may be null
      * @param timeOccurred the epoch time when the error occurred
      */
-    public ResourceError(@NotNull
-    Resource resource, @NotNull
-    ResourceErrorType errorType, @NotNull
-    String summary, @Nullable
-    String detail, long timeOccurred) {
+    public ResourceError(@NotNull Resource resource, @NotNull ResourceErrorType errorType, @NotNull String summary,
+        @Nullable String detail, long timeOccurred) {
         setResource(resource);
         setErrorType(errorType);
         setSummary(summary);
@@ -119,10 +118,8 @@ public class ResourceError implements Serializable {
      * @param exception    exception whose stack will be used as this object's error message
      * @param timeOccurred the epoch time when the error occurred
      */
-    public ResourceError(@NotNull
-    Resource resource, @NotNull
-    ResourceErrorType errorType, @NotNull
-    Throwable exception, long timeOccurred) {
+    public ResourceError(@NotNull Resource resource, @NotNull ResourceErrorType errorType,
+        @NotNull Throwable exception, long timeOccurred) {
         setResource(resource);
         setErrorType(errorType);
         setSummary(exception.getLocalizedMessage());
@@ -159,6 +156,9 @@ public class ResourceError implements Serializable {
     }
 
     public void setSummary(String summary) {
+        if (summary != null && summary.length() > MAX_SUMMARY_LENGTH) {
+            summary = summary.substring(0, MAX_SUMMARY_LENGTH - 3) + "...";
+        }
         this.summary = summary;
     }
 
@@ -178,8 +178,7 @@ public class ResourceError implements Serializable {
      *
      * @throws NullPointerException if <code>t</code> is <code>null</code>
      */
-    public void setDetailFromThrowable(@NotNull
-    Throwable t) {
+    public void setDetailFromThrowable(@NotNull Throwable t) {
         //noinspection ConstantConditions
         if (t == null) {
             throw new IllegalArgumentException("t == null");

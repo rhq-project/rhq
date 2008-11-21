4bb427a618621d8f643e58ba3d9a6510ee7e58ec
John Mazzitelli <mazz@redhat.com>
2008-11-21 16:09:47 +0000

    append the output, don't overwrite the preceding output

--- etc/agentspawn/src/scripts/agentspawn.bat
+++ etc/agentspawn/src/scripts/agentspawn.bat
@@ -3,5 +3,5 @@
 rem because Windows has no replacement for the UNIX $!, we tell our Java code to generate the pidfile
 
 echo cmd.exe /S /C java %_JAVAOPTS% -Dperftest.pidfile=%_PIDFILE% -jar %_JAR% %1 > %_OUT% 2>&1
-cmd.exe /S /C java %_JAVAOPTS% -Dperftest.pidfile=%_PIDFILE% -jar %_JAR% %1 > %_OUT% 2>&1
+cmd.exe /S /C java %_JAVAOPTS% -Dperftest.pidfile=%_PIDFILE% -jar %_JAR% %1 >> %_OUT% 2>&1
 

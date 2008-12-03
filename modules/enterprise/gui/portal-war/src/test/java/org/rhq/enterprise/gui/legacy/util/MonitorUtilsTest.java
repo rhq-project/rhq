49ddad36977e13178efc57ba262cb71d0e606dd6
Heiko Rupp <hwr@redhat.com>
2008-12-03 12:59:07 +0000

    Due to different international spelling of numeric units and the bug in RHQ-1129, we need to compare the output with a localized version of the reference data.

--- modules/enterprise/gui/portal-war/src/test/java/org/rhq/enterprise/gui/legacy/util/MonitorUtilsTest.java
+++ modules/enterprise/gui/portal-war/src/test/java/org/rhq/enterprise/gui/legacy/util/MonitorUtilsTest.java
@@ -18,16 +18,18 @@
  */
 package org.rhq.enterprise.gui.legacy.util;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
 import org.apache.struts.config.MessageResourcesConfig;
 import org.apache.struts.util.MessageResources;
 import org.apache.struts.util.MessageResourcesFactory;
 import static org.testng.AssertJUnit.assertEquals;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import org.rhq.core.domain.measurement.MeasurementUnits;
 import org.rhq.enterprise.server.measurement.uibean.MetricDisplayConstants;
 import org.rhq.enterprise.server.measurement.uibean.MetricDisplaySummary;
@@ -35,13 +37,24 @@ import org.rhq.enterprise.server.measurement.uibean.MetricDisplayValue;
 
 /**
  * Test to make sure that refactoring to the MeasurementUnits, and ConvertUnits class does not break the current
- * MonitorUntils.formatMetrics() functionality
+ * MonitorUntils.formatMetrics() functionality.
+ *
+ * Due to different international spelling of numeric units and a bug in MonitorUntils.formatMetrics
+ * (http://jira.rhq-project.org/browse/RHQ-1129) we need to compare the output with a localized
+ * version of the reference data.
  *
  * @author Jessica Sant
+ * @author Heiko W. Rupp
  */
 @Test
 public class MonitorUtilsTest {
-    private static Log log = LogFactory.getLog(MonitorUtils.class.getName());
+
+    NumberFormat nf = NumberFormat.getInstance(); // Use Default locale
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("===> Locale is " + Locale.getDefault() + " <========");
+    }
 
     /**
      * test various byte-based metrics
@@ -49,7 +62,7 @@ public class MonitorUtilsTest {
     @Test
     public void testFormatByteMetrics() {
         List<MetricDisplaySummary> metricDisplaySummaries = new ArrayList<MetricDisplaySummary>();
-        Locale locale = Locale.US;
+        Locale locale = Locale.US; // This does not work as intended. See RHQ-1129
         MessageResources messageResources = createMessageResources();
 
         // test bytes
@@ -65,10 +78,12 @@ public class MonitorUtilsTest {
 
         Integer resourceCount = MonitorUtils.formatMetrics(metricDisplaySummaries, locale, messageResources);
 
-        assertEquals("1.1GB", t.getAvgMetric().getValueFmt().toString());
-        assertEquals("1,023.0B", u.getAvgMetric().getValueFmt().toString());
-        assertEquals("64.0KB", v.getAvgMetric().getValueFmt().toString());
-        assertEquals("188.9KB", w.getAvgMetric().getValueFmt().toString());
+
+        assertEquals(nf.format(1.1)+"GB", t.getAvgMetric().getValueFmt());
+        nf.setMinimumFractionDigits(1); // Otherwise NumberFormat swallows the fraction digit
+        assertEquals(nf.format(1023.0)+"B", u.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(64.0)+"KB", v.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(188.9)+"KB", w.getAvgMetric().getValueFmt());
     }
 
     /**
@@ -93,10 +108,10 @@ public class MonitorUtilsTest {
 
         Integer resourceCount = MonitorUtils.formatMetrics(metricDisplaySummaries, locale, messageResources);
 
-        assertEquals("5.1h", i.getAvgMetric().getValueFmt().toString());
-        assertEquals("14.3d", j.getAvgMetric().getValueFmt().toString());
-        assertEquals("22.3h", k.getAvgMetric().getValueFmt().toString());
-        assertEquals("270.1d", l.getAvgMetric().getValueFmt().toString());
+        assertEquals(nf.format(5.1)+"h", i.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(14.3)+"d", j.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(22.3)+"h", k.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(270.1)+"d", l.getAvgMetric().getValueFmt());
     }
 
     /**
@@ -121,16 +136,17 @@ public class MonitorUtilsTest {
 
         Integer resourceCount = MonitorUtils.formatMetrics(metricDisplaySummaries, locale, messageResources);
 
-        assertEquals("1.1Gb", a.getAvgMetric().getValueFmt().toString());
-        assertEquals("1,023.0b", b.getAvgMetric().getValueFmt().toString());
-        assertEquals("64.0Kb", c.getAvgMetric().getValueFmt().toString());
-        assertEquals("188.9Kb", d.getAvgMetric().getValueFmt().toString());
+        assertEquals(nf.format(1.1)+"Gb", a.getAvgMetric().getValueFmt());
+        nf.setMinimumFractionDigits(1); // Otherwise NumberFormat swallows the fraction digit
+        assertEquals(nf.format(1023)+"b", b.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(64)+"Kb", c.getAvgMetric().getValueFmt());
+        assertEquals(nf.format(188.9)+"Kb", d.getAvgMetric().getValueFmt());
     }
 
     /**
      * creates the MessageResources object needed for the metric formatting
      *
-     * @return
+     * @return a new MessageResources object for testing
      */
     private MessageResources createMessageResources() {
         MessageResources messageResources;

d2aa916c99a00fa89a430434470e6495a4a3af80
Heiko Rupp <hwr@redhat.com>
2008-12-03 09:25:57 +0000

    RHQ-1154 More at supporting mod_jk.

---
+++ modules/plugins/apache/src/main/java/org/rhq/plugins/apache/ModJKComponent.java
@@ -0,0 +1,139 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.plugins.apache;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.measurement.AvailabilityType;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.inventory.ResourceComponent;
+import org.rhq.core.pluginapi.inventory.ResourceContext;
+import org.rhq.core.pluginapi.operation.OperationResult;
+import org.rhq.plugins.apache.util.HttpdConfParser;
+
+/**
+ * Management of a mod_jk plugin in the parent apache server
+ *
+ * @author Heiko W. Rupp
+ */
+public class ModJKComponent implements ResourceComponent<ApacheServerComponent> {
+
+    private static final Log log = LogFactory.getLog(ModJKComponent.class);
+
+    public void start(
+            ResourceContext<ApacheServerComponent> parentResourceContext) throws InvalidPluginConfigurationException, Exception {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public AvailabilityType getAvailability() {
+        return AvailabilityType.UP; // For now we are always up when our parent is.
+    }
+
+    /**
+     * Delegate method to install a simple mod_jk in an apache httpd
+     * @param serverComponent The parents server component with the configuration
+     * @param params Params we got passed from the GUI
+     * @return The outcome of the operation
+     */
+    public static OperationResult installModJk(ApacheServerComponent serverComponent, Configuration params) throws Exception {
+
+        StringBuilder builder = new StringBuilder();
+        boolean errorSeen = false;
+        boolean needWorkersProps = false;
+        boolean needUriWorkers = false;
+
+        // First see (what) if stuff is present
+        File httpdConf = serverComponent.getHttpdConfFile();
+        String confPath = httpdConf.getAbsolutePath();
+
+        // If we can't update the file, then there is nothing left to do.
+        if (!httpdConf.canWrite()) {
+            throw new Exception("Httpd.conf is not writable at " + confPath);
+        }
+
+        HttpdConfParser cparser = new HttpdConfParser();
+        cparser.parse(confPath);
+        // TODO back up original file
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(httpdConf,true));
+
+            if (cparser.isModJkInstalled()) {
+                builder.append("Mod_jk is already installed\n");
+                if (cparser.getWorkerPropertiesFile() != null) {
+                    builder.append("Found a worker.properties file at ").append(cparser.getWorkerPropertiesFile());
+                    builder.append("\n");
+                }
+                else
+                    needWorkersProps = true;
+
+                if (cparser.getUriWorkerLocation() != null) {
+                    builder.append("Found a urimap file at ").append(cparser.getUriWorkerLocation());
+                }
+                else
+                    needUriWorkers = true;
+            } else {
+                builder.append("No mod_jk installed yet at ").append(confPath).append("\n");
+
+                writer.append("LoadModule jk_module modules/mod_jk.so"); // TODO obtain modules location
+                writer.newLine();
+
+
+                builder.append(".. written a LoadModule line \n");
+                needWorkersProps=true;
+                needUriWorkers=true;
+            }
+
+            if (needWorkersProps) {
+                writer.append("JkWorkersFile ").append("conf/workers.properties");
+                writer.newLine();
+                builder.append(".. installed worker.properties");
+            }
+            if (needUriWorkers) {
+                writer.append("JkMountFile ").append("conf/uriworkermap");
+                writer.newLine();
+                builder.append(".. installed uriworkermap");
+            }
+            writer.flush();
+            writer.close();
+
+        }
+        catch (IOException e) {
+            builder.append("Error when installing mod_jk: \n");
+            builder.append(e.fillInStackTrace());
+            throw new Exception(builder.toString());
+
+        }
+
+        OperationResult result = new OperationResult();
+        result.setOutput(builder.toString());
+
+        return result;
+    }
+}
---
+++ modules/plugins/apache/src/main/java/org/rhq/plugins/apache/ModJKDiscoveryComponent.java
@@ -0,0 +1,119 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.plugins.apache;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
+import org.rhq.plugins.apache.util.HttpdConfParser;
+import org.rhq.plugins.www.util.WWWUtils;
+
+/**
+ * Discovery of an installed mod_jk module in the parents component apache server
+ *
+ * @author Heiko W. Rupp
+ */
+public class ModJKDiscoveryComponent implements ResourceDiscoveryComponent<ModJKComponent> {
+
+    private final Log log = LogFactory.getLog(this.getClass());
+
+    /**
+     * Try to detect a mod_jk. We do this by looking in httpd.conf if the module should be loaded
+     * @param context The context that provides metadata
+     * @return A potentially empty set of details
+     * @throws InvalidPluginConfigurationException
+     * @throws Exception
+     */
+    public Set<DiscoveredResourceDetails> discoverResources(
+            ResourceDiscoveryContext<ModJKComponent> context) throws InvalidPluginConfigurationException, Exception {
+
+        Set<DiscoveredResourceDetails> details = new HashSet<DiscoveredResourceDetails>(1);
+
+        Configuration parentConfig = context.getParentResourceContext().getPluginConfiguration();
+
+        // Try to determine the presence of mod_jk and its version via a http request
+        PropertySimple urlString = parentConfig.getSimple(ApacheServerComponent.PLUGIN_CONFIG_PROP_URL);
+        String modJkVersion = null;
+        if (urlString!=null) {
+            try {
+                URL url = new URL(urlString.getStringValue());
+                String header = WWWUtils.getServerHeader(url);
+                if (header!=null) {
+                    if (header.contains("mod_jk")) {
+                        header = header.substring(header.indexOf("mod_jk")+7);
+                        int blankPos = header.indexOf(" ");
+                        if (blankPos>0)
+                            modJkVersion = header.substring(0,header.indexOf(" "));
+                        else
+                            modJkVersion = header;
+                    }
+                }
+            }
+            catch (MalformedURLException e) {
+                // nothing to do
+            }
+        }
+
+        PropertySimple confPathProp = parentConfig.getSimple(ApacheServerComponent.PLUGIN_CONFIG_PROP_HTTPD_CONF);
+        if (confPathProp == null || confPathProp.getStringValue() == null) {
+            log.error("Path to httpd.conf is not given - can't discover mod_jk");
+            return null;
+        }
+
+        String confPath = confPathProp.getStringValue();
+        if (!confPath.startsWith("/")) { // TODO implement for Windows too
+            String basePath = parentConfig.getSimpleValue(ApacheServerComponent.PLUGIN_CONFIG_PROP_SERVER_ROOT, "");
+            confPath = basePath + "/" + confPath;
+        }
+
+        HttpdConfParser parser = new HttpdConfParser();
+        if (parser.parse(confPath) && parser.isModJkInstalled()) {
+
+            Configuration config = context.getDefaultPluginConfiguration();
+            if (parser.getWorkerPropertiesFile()!=null) {
+                PropertySimple workers = new PropertySimple("workerFile", parser.getWorkerPropertiesFile());
+                config.put(workers);
+            }
+            if (parser.getUriWorkerLocation()!=null) {
+                PropertySimple workers = new PropertySimple("uriWorkerFile", parser.getUriWorkerLocation());
+                config.put(workers);
+            }
+
+
+            DiscoveredResourceDetails detail = new DiscoveredResourceDetails(context.getResourceType(),
+                    confPath,"mod_jk",modJkVersion,"Mod_JK", config, null);
+            details.add(detail);
+            return details;
+        }
+
+        return null;
+    }
+
+}

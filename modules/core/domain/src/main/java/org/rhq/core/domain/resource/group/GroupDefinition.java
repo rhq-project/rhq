0528c717112185946e6a3fc8c4285f695cc00290
Joseph Marques <jmarques@redhat.com>
2008-12-09 20:21:28 +0000

    RHQ-1238 - support sorting on category column of resource groups managed by a group definition;

--- modules/core/domain/src/main/java/org/rhq/core/domain/resource/group/GroupDefinition.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/resource/group/GroupDefinition.java
@@ -1,31 +1,32 @@
- /*
-  * RHQ Management Platform
-  * Copyright (C) 2005-2008 Red Hat, Inc.
-  * All rights reserved.
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License, version 2, as
-  * published by the Free Software Foundation, and/or the GNU Lesser
-  * General Public License, version 2.1, also as published by the Free
-  * Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License and the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * and the GNU Lesser General Public License along with this program;
-  * if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  */
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.domain.resource.group;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.io.Serializable;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -39,6 +40,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
 import org.rhq.core.domain.util.StringUtils;
 
 @Entity
@@ -49,7 +51,7 @@ import org.rhq.core.domain.util.StringUtils;
         + "    FROM ResourceGroup rg LEFT JOIN rg.implicitResources res LEFT JOIN res.availability a "
         + "   WHERE rg.groupDefinition.id = :groupDefinitionId "
         + "     AND (a is null OR a.startTime = (SELECT MAX(aa.startTime) FROM Availability aa where res.id = aa.resource.id)) "
-        + "GROUP BY rg, rg.name, rg.groupByClause "),
+        + "GROUP BY rg, rg.groupCategory, rg.name, rg.groupByClause "),
     @NamedQuery(name = GroupDefinition.QUERY_FIND_MEMBERS_count, query = "  SELECT COUNT(rg) "
         + "    FROM ResourceGroup rg " + "   WHERE rg.groupDefinition.id = :groupDefinitionId "),
     @NamedQuery(name = GroupDefinition.QUERY_FIND_MANAGED_RESOURCE_GROUP_IDS_ADMIN, query = "  SELECT rg.id "

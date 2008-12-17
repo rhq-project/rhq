022b98c229c1bfbd402705d166631762600de034
John Mazzitelli <mazz@redhat.com>
2008-12-17 21:08:17 +0000

    make sure tests have the thread pools started

--- modules/core/plugin-container/src/test/java/org/rhq/core/pc/inventory/ResourceContainerTest.java
+++ modules/core/plugin-container/src/test/java/org/rhq/core/pc/inventory/ResourceContainerTest.java
@@ -1,34 +1,36 @@
- /*
-  * RHQ Management Platform
-  * Copyright (C) 2005-2008 Red Hat, Inc.
-  * All rights reserved.
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License, version 2, as
-  * published by the Free Software Foundation, and/or the GNU Lesser
-  * General Public License, version 2.1, also as published by the Free
-  * Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License and the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * and the GNU Lesser General Public License along with this program;
-  * if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  */
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.pc.inventory;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import org.rhq.core.domain.resource.Resource;
 import org.rhq.core.domain.measurement.AvailabilityType;
+import org.rhq.core.domain.resource.Resource;
+import org.rhq.core.pc.util.FacetLockType;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
-import org.rhq.core.pc.util.FacetLockType;
 
 /**
  * Unit test for {@link ResourceContainer}.
@@ -37,48 +39,60 @@ import org.rhq.core.pc.util.FacetLockType;
  */
 @Test
 public class ResourceContainerTest {
+
+    @BeforeClass
+    public void beforeClass() {
+        ResourceContainer.initialize();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        ResourceContainer.shutdown();
+    }
+
     public void testCreateResourceComponentProxy() throws Exception {
         Resource resource = new Resource();
         ResourceContainer resourceContainer = new ResourceContainer(resource);
         ResourceComponent resourceComponent = new MockResourceComponent(false);
         resourceContainer.setResourceComponent(resourceComponent);
         System.out.println("Testing proxy call that should timeout...");
-        ResourceComponent resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class, FacetLockType.NONE, 50, true, false);
+        ResourceComponent resourceComponentProxy = resourceContainer.createResourceComponentProxy(
+            ResourceComponent.class, FacetLockType.NONE, 50, true, false);
         try {
             resourceComponentProxy.getAvailability();
             assert (false);
-        }
-        catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             assert (e instanceof TimeoutException);
         }
         System.out.println("SUCCESS!");
         System.out.println("Testing proxy call that should complete successfully...");
-        resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class, FacetLockType.NONE, 150, true, false);
+        resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class,
+            FacetLockType.NONE, 150, true, false);
         AvailabilityType avail = resourceComponentProxy.getAvailability();
         assert (avail == AvailabilityType.UP);
         System.out.println("SUCCESS!");
         System.out.println("Testing proxy call that should fail...");
         ResourceComponent naughtyResourceComponent = new MockResourceComponent(true);
-        resourceContainer.setResourceComponent(naughtyResourceComponent);        
-        resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class, FacetLockType.NONE, Long.MAX_VALUE, true, false);
+        resourceContainer.setResourceComponent(naughtyResourceComponent);
+        resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class,
+            FacetLockType.NONE, Long.MAX_VALUE, true, false);
         try {
             resourceComponentProxy.getAvailability();
             assert (false);
-        }
-        catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             assert (e instanceof MockRuntimeException);
         }
         System.out.println("SUCCESS!");
-        System.out.println("Testing proxy call that should not timeout because it is not to a method declared in the proxied interface...");
-        resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class, FacetLockType.NONE, 50, true, false);
+        System.out.println("Testing proxy call should not timeout; its not to a declared method in proxied interface");
+        resourceComponentProxy = resourceContainer.createResourceComponentProxy(ResourceComponent.class,
+            FacetLockType.NONE, 50, true, false);
         String string = resourceComponentProxy.toString();
         assert (string.equals(MockResourceComponent.class.toString()));
         System.out.println("SUCCESS!");
         // TODO: Test proxy locking.
     }
 
-    class MockResourceComponent implements ResourceComponent
-    {
+    class MockResourceComponent implements ResourceComponent {
         private boolean naughty;
 
         MockResourceComponent(boolean naughty) {
@@ -97,18 +111,17 @@ public class ResourceContainerTest {
             }
             try {
                 Thread.sleep(100);
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 return AvailabilityType.DOWN;
             }
             return AvailabilityType.UP;
         }
 
+        @Override
         public String toString() {
             try {
                 Thread.sleep(100);
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             return this.getClass().toString();

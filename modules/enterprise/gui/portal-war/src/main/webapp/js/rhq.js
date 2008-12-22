26e0684bcc8631afb74877d9a955dfa267b2c132
Joseph Marques <jmarques@redhat.com>
2008-12-22 18:58:07 +0000

    RHQ-1305 - as a result of work done as part of RHQ-1043, the window DOM object has two onload handlers; this revision corrects the implementation for using multiple onload handlers;

--- modules/enterprise/gui/portal-war/src/main/webapp/js/rhq.js
+++ modules/enterprise/gui/portal-war/src/main/webapp/js/rhq.js
@@ -475,3 +475,17 @@ function clickAlreadySelectedElements() {
       }
    }
 }
+
+function addWindowsOnLoadEvent(func) { 
+   var oldonload = window.onload; 
+   if (typeof window.onload != 'function') { 
+      window.onload = func; 
+   } else { 
+      window.onload = function() { 
+         if (oldonload) { 
+            oldonload(); 
+         } 
+         func(); 
+      } 
+   } 
+} 

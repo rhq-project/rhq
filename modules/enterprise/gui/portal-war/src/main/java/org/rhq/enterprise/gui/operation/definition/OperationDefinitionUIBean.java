37aa21c297eb1446315c7508ba4b30a136379610
Joseph Marques <jmarques@redhat.com>
2009-01-09 21:49:13 +0000

    perform all database interaction in one place;

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/operation/definition/OperationDefinitionUIBean.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/operation/definition/OperationDefinitionUIBean.java
@@ -40,20 +40,16 @@ public abstract class OperationDefinitionUIBean extends PagedDataTableUIBean {
     protected String description = null;
 
     public OperationDefinitionUIBean() {
-    }
+        Subject subject = EnterpriseFacesContextUtility.getSubject();
+        Integer operationId = FacesContextUtility.getRequiredRequestParameter("opId", Integer.class);
 
-    private void initOperationDefinition() {
-        if (this.operationDefinition == null) {
-            Subject subject = EnterpriseFacesContextUtility.getSubject();
-            Integer operationId = FacesContextUtility.getRequiredRequestParameter("opId", Integer.class);
+        this.operationDefinition = operationManager.getOperationDefinition(subject, operationId);
 
-            this.operationDefinition = operationManager.getOperationDefinition(subject, operationId);
-        }
+        Integer defaultTimeout = this.operationDefinition.getTimeout();
+        this.timeout = (defaultTimeout == null) ? "" : String.valueOf(defaultTimeout);
     }
 
     public String getName() {
-        initOperationDefinition();
-
         return this.operationDefinition.getName();
     }
 
@@ -62,14 +58,6 @@ public abstract class OperationDefinitionUIBean extends PagedDataTableUIBean {
     }
 
     public String getTimeout() {
-        // we want to cache the definition, but have a separately manageable timeout
-        // this will allow the user to override the timeout and pass this along in the request submission
-        if (this.timeout == null) {
-            initOperationDefinition();
-            Integer defaultTimeout = this.operationDefinition.getTimeout();
-            this.timeout = (defaultTimeout == null) ? "" : String.valueOf(defaultTimeout);
-        }
-
         return this.timeout;
     }
 

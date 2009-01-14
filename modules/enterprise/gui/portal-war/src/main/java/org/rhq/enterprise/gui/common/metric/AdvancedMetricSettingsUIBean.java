0328dc4156e72720c145f62bdb4de40422a79ee5
Fady Matar <fmatar@redhat.com>
2009-01-14 18:20:16 +0000

    Updated the advanced metric display

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/common/metric/AdvancedMetricSettingsUIBean.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/common/metric/AdvancedMetricSettingsUIBean.java
@@ -18,18 +18,35 @@
  */
 package org.rhq.enterprise.gui.common.metric;
 
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.model.DataModel;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.rhq.core.gui.util.FacesContextUtility;
 import org.rhq.enterprise.gui.common.framework.PagedDataTableUIBean;
+import org.rhq.enterprise.gui.legacy.WebUser;
+import org.rhq.enterprise.gui.util.EnterpriseFacesContextUtility;
+import org.rhq.enterprise.server.measurement.MeasurementPreferences;
+import org.rhq.enterprise.server.measurement.MeasurementPreferences.MetricRangePreferences;
 
 public class AdvancedMetricSettingsUIBean extends PagedDataTableUIBean {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
     public static final String MANAGED_BEAN_NAME = "AdvancedMetricSettingsUIBean";
+    private static final String DURATION_TYPE = "duration";
+    private static final String INTERVAL_TYPE = "interval";
+
+    private Integer duration;
+    private Integer unit;
+    private String metricType = AdvancedMetricSettingsUIBean.DURATION_TYPE;
+    private Date fromTime;
+    private Date toTime;
 
     public AdvancedMetricSettingsUIBean() {
     }
@@ -37,5 +54,65 @@ public class AdvancedMetricSettingsUIBean extends PagedDataTableUIBean {
     @Override
     public DataModel getDataModel() {
         return null;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Integer unit) {
+        this.unit = unit;
+    }
+
+    public void setMetricType(String metricType) {
+        this.metricType = metricType;
+    }
+
+    public String getMetricType() {
+        return this.metricType;
+    }
+
+    public Date getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(Date fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    public Date getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(Date toTime) {
+        this.toTime = toTime;
+    }
+
+    public void execute() {
+        FacesContext facesContext = FacesContextUtility.getFacesContext();
+        WebUser user = EnterpriseFacesContextUtility.getWebUser();
+        MeasurementPreferences preferences = user.getMeasurementPreferences();
+        MetricRangePreferences rangePreferences = preferences.getMetricRangePreferences();
+
+        if (this.getMetricType().equalsIgnoreCase(AdvancedMetricSettingsUIBean.DURATION_TYPE)) {
+            rangePreferences.end -= this.getDuration() * this.getUnit();
+        }
+
+        if (this.getMetricType().equalsIgnoreCase(AdvancedMetricSettingsUIBean.INTERVAL_TYPE)) {
+            Long fromTime = this.getFromTime().getTime();
+            Long toTime = this.getToTime().getTime();
+            if (toTime < fromTime) {
+                facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Timing error",
+                    "To time cannot be earlier than before time"));
+            }
+        }
     }
 }

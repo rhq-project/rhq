6680d126bdab5f880825e8b54b0c36b7be802541
Fady Matar <fmatar@redhat.com>
2009-01-15 18:01:33 +0000

    Updated UI controls validation

---
+++ modules/enterprise/gui/portal-war/src/main/webapp/js/advanced-metrics.js
@@ -0,0 +1,35 @@
+function toggleControls(elementId) {
+	var element = document.getElementById(elementId);
+	if (element.disabled = true) {
+		enableElement(element);
+	} else {
+		disableElement(element);
+	}
+}
+
+function disableElement(elementId) {
+	document.getElementById(elementId).disabled = true;
+}
+
+function enableElement(elementId) {
+	document.getElementById(elementId).disabled = false;
+}
+
+function uncheckRadio(elementName) {
+	var elementsList = document.getElementsByName(elementName);
+	for (i = 0; i < elementsList.length; i++) {
+		elementsList[i].checked = false;
+	}
+}
+
+function checkRadio(elementName) {
+	var elementsList = document.getElementsByName(elementName);
+	for (i = 0; i < elementsList.length; i++) {
+		elementsList[i].checked = true;
+	}
+}
+
+function checkUncheckRadio(elementName1, elementName2) {
+	checkRadio(elementName1);
+	uncheckRadio(elementName2);
+}

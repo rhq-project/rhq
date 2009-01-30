f9dc155244fe20147a754ec29da2a290f4cb6c6d
Fady Matar <fmatar@redhat.com>
2009-01-30 14:31:04 +0000

    Updated the test case / test configurations for the Tomcat plugin

--- modules/plugins/tomcat/src/test/java/org/jboss/on/plugins/tomcat/test/TomcatPluginTest.java
+++ modules/plugins/tomcat/src/test/java/org/jboss/on/plugins/tomcat/test/TomcatPluginTest.java
@@ -19,9 +19,13 @@
 package org.jboss.on.plugins.tomcat.test;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.rhq.core.domain.discovery.InventoryReport;
+import org.rhq.core.domain.resource.Resource;
 import org.rhq.core.pc.PluginContainer;
 import org.rhq.core.pc.PluginContainerConfiguration;
 import org.rhq.core.pc.plugin.FileSystemPluginFinder;
@@ -42,7 +46,7 @@ public class TomcatPluginTest {
     @BeforeSuite
     public void start() {
         try {
-            File pluginDir = new File("target/test/plugins");
+            File pluginDir = new File("target/itest/plugins");
             PluginContainerConfiguration pcConfig = new PluginContainerConfiguration();
             pcConfig.setPluginFinder(new FileSystemPluginFinder(pluginDir));
             pcConfig.setPluginDirectory(pluginDir);
@@ -70,5 +74,32 @@ public class TomcatPluginTest {
         PluginEnvironment pluginEnvironment = pluginManager.getPlugin(PLUGIN_NAME);
         assert (pluginEnvironment != null) : "Null environment, plugin not loaded";
         assert (pluginEnvironment.getPluginName().equals(PLUGIN_NAME));
+    }
+
+    @Test(dependsOnMethods = "testPluginLoad")
+    public void testDiscovery() throws Exception {
+        InventoryReport report = PluginContainer.getInstance().getInventoryManager().executeServerScanImmediately();
+        assert report != null;
+        log.info("Discovery took: " + (report.getEndTime() - report.getStartTime()) + "ms");
+        Set<Resource> resources = findResource(PluginContainer.getInstance().getInventoryManager().getPlatform(),
+            "Tomcat");
+        log.info("Found " + resources.size() + " ews / apache tomcat instance(s).");
+    }
+
+    private Set<Resource> findResource(Resource parent, String typeName) {
+        Set<Resource> found = new HashSet<Resource>();
+        Resource platform = parent;
+        for (Resource resource : platform.getChildResources()) {
+            log.info("Discovered resource of type: " + resource.getResourceType().getName());
+            if (resource.getResourceType().getName().equals(typeName)) {
+                found.add(resource);
+            }
+            if (resource.getChildResources() != null) {
+                for (Resource child : found) {
+                    found.addAll(findResource(child, typeName));
+                }
+            }
+        }
+        return found;
     }
 }

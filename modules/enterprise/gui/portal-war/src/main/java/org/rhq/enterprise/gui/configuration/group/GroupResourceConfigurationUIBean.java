64ae94bde70eeb86a58c1e793872028de80666b9
Joseph Marques <jmarques@redhat.com>
2009-02-22 04:25:11 +0000

    [RHQ-1633] - ensure that the user stays on the edit group config page if there are errors submitting the job to the backend;

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/configuration/group/GroupResourceConfigurationUIBean.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/configuration/group/GroupResourceConfigurationUIBean.java
@@ -18,35 +18,35 @@
  */
 package org.rhq.enterprise.gui.configuration.group;
 
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.application.FacesMessage;
 
-import org.rhq.core.domain.resource.group.ResourceGroup;
-import org.rhq.core.domain.resource.group.GroupCategory;
-import org.rhq.core.domain.resource.Resource;
-import org.rhq.core.domain.configuration.Configuration;
-import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
-import org.rhq.core.gui.configuration.propset.ConfigurationSet;
-import org.rhq.core.gui.configuration.propset.ConfigurationSetMember;
-import org.rhq.enterprise.gui.util.EnterpriseFacesContextUtility;
-import org.rhq.enterprise.gui.legacy.ParamConstants;
-import org.rhq.enterprise.server.configuration.ConfigurationManagerLocal;
-import org.rhq.enterprise.server.util.LookupUtil;
-import org.rhq.enterprise.server.resource.ResourceManagerLocal;
-
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.Begin;
-import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Create;
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.international.StatusMessage;
+import org.jboss.seam.core.Conversation;
 import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.faces.Redirect;
-import org.jboss.seam.core.Conversation;
+import org.jboss.seam.international.StatusMessage;
+
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
+import org.rhq.core.domain.resource.Resource;
+import org.rhq.core.domain.resource.group.GroupCategory;
+import org.rhq.core.domain.resource.group.ResourceGroup;
+import org.rhq.core.gui.configuration.propset.ConfigurationSet;
+import org.rhq.core.gui.configuration.propset.ConfigurationSetMember;
+import org.rhq.enterprise.gui.legacy.ParamConstants;
+import org.rhq.enterprise.gui.util.EnterpriseFacesContextUtility;
+import org.rhq.enterprise.server.configuration.ConfigurationManagerLocal;
+import org.rhq.enterprise.server.resource.ResourceManagerLocal;
+import org.rhq.enterprise.server.util.LookupUtil;
 
 /**
  * A POJO Seam component that handles loading and updating of Resource configurations across a compatible Group.
@@ -55,8 +55,7 @@ import org.jboss.seam.core.Conversation;
  */
 @Name("GroupResourceConfigurationUIBean")
 @Scope(ScopeType.CONVERSATION)
-public class GroupResourceConfigurationUIBean
-{
+public class GroupResourceConfigurationUIBean {
     private ConfigurationManagerLocal configurationManager = LookupUtil.getConfigurationManager();
     private ResourceManagerLocal resourceManager = LookupUtil.getResourceManager();
 
@@ -67,7 +66,7 @@ public class GroupResourceConfigurationUIBean
     private Redirect redirect;
 
     private ResourceGroup group;
-    private Map<Integer,Configuration> resourceConfigurations;
+    private Map<Integer, Configuration> resourceConfigurations;
     private ConfigurationSet configurationSet;
 
     @Create
@@ -78,19 +77,15 @@ public class GroupResourceConfigurationUIBean
      * @return viewId to be redirected to
      */
     public String loadConfigurations() {
-        try
-        {
+        try {
             this.group = loadGroup();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             FacesMessages.instance().add(FacesMessage.SEVERITY_FATAL, e.getMessage());
             return null;
         }
-        ConfigurationDefinition configurationDefinition =
-                this.configurationManager.getResourceConfigurationDefinitionForResourceType(
-                        EnterpriseFacesContextUtility.getSubject(),
-                        this.group.getResourceType().getId());
+        ConfigurationDefinition configurationDefinition = this.configurationManager
+            .getResourceConfigurationDefinitionForResourceType(EnterpriseFacesContextUtility.getSubject(), this.group
+                .getResourceType().getId());
         this.resourceConfigurations = this.configurationManager.getResourceConfigurationsForCompatibleGroup(this.group);
         List<ConfigurationSetMember> configurationSetMembers = new ArrayList(resourceConfigurations.size());
         for (Integer resourceId : this.resourceConfigurations.keySet()) {
@@ -111,21 +106,18 @@ public class GroupResourceConfigurationUIBean
      * @return viewId to be redirected to
      */
     public String updateConfigurations() {
-        try
-        {
+        try {
             // TODO: See if there's some way for the config renderer to handle calling applyAggregateConfiguration(),
             //       so the managed bean doesn't have to worry about doing it.
             this.configurationSet.applyAggregateConfiguration();
-            this.configurationManager.scheduleAggregateResourceConfigurationUpdate(EnterpriseFacesContextUtility.getSubject(),
-                    this.group.getId(), this.resourceConfigurations);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
+            this.configurationManager.scheduleAggregateResourceConfigurationUpdate(EnterpriseFacesContextUtility
+                .getSubject(), this.group.getId(), this.resourceConfigurations);
+        } catch (Exception e) {
             this.facesMessages.add(StatusMessage.Severity.FATAL,
-                    "Failed to schedule group Resource Configuration update - cause: " + e);
+                "Failed to schedule group Resource Configuration update - cause: " + e);
             this.redirect.setViewId("/rhq/group/configuration/current.xhtml");
             this.redirect.execute();
+            return null;
         }
         this.facesMessages.add(StatusMessage.Severity.INFO, "Group Resource Configuration update scheduled.");
         Conversation.instance().endBeforeRedirect();
@@ -134,20 +126,15 @@ public class GroupResourceConfigurationUIBean
         return null;
     }
 
-    public ConfigurationSet getConfigurationSet()
-    {
+    public ConfigurationSet getConfigurationSet() {
         return this.configurationSet;
     }
 
-    private ResourceGroup loadGroup() throws Exception
-    {
+    private ResourceGroup loadGroup() throws Exception {
         ResourceGroup group;
-        try
-        {
+        try {
             group = EnterpriseFacesContextUtility.getResourceGroup();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("No group is associated with this request ('groupId' request parameter is not set).");
         }
         if (group.getGroupCategory() != GroupCategory.COMPATIBLE) {
@@ -156,13 +143,11 @@ public class GroupResourceConfigurationUIBean
         return group;
     }
 
-    private String createLabel(Integer resourceId)
-    {
+    private String createLabel(Integer resourceId) {
         List<Resource> resourceLineage = this.resourceManager.getResourceLineage(resourceId);
         String previousName = resourceLineage.get(0).getName();
         StringBuilder label = new StringBuilder(previousName);
-        for (int i = 1; i < resourceLineage.size(); i++)
-        {
+        for (int i = 1; i < resourceLineage.size(); i++) {
             Resource resource = resourceLineage.get(i);
             String name = resource.getName();
             name = (name.startsWith(previousName)) ? name.substring(previousName.length()) : name;

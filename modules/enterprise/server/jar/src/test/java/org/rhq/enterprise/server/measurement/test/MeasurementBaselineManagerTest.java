664c49b222d48bd90e88d334365dd0ba5a22e948
Joseph Marques <jmarques@redhat.com>
2009-02-26 04:19:18 +0000

    oops, i guess i'd want to update tests if i change the baseline algorithm, huh?  ; )

--- modules/enterprise/server/jar/src/test/java/org/rhq/enterprise/server/measurement/test/MeasurementBaselineManagerTest.java
+++ modules/enterprise/server/jar/src/test/java/org/rhq/enterprise/server/measurement/test/MeasurementBaselineManagerTest.java
@@ -94,12 +94,18 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
         try {
             setupManyResources(entityManager, 20, 10);
 
+            long now = System.currentTimeMillis();
+            long eldest = now - 180000;
+            long elder = now - 120000;
+            long young = now - 60000;
+            long youngest = now;
+
             int dataCount = allScheds.size();
             for (MeasurementSchedule sched : allScheds) {
-                insertMeasurementDataNumeric1H(100, sched, 30.0, 20.0, 40.0);
-                insertMeasurementDataNumeric1H(500, sched, 5.0, 2.0, 8.0);
-                insertMeasurementDataNumeric1H(1000, sched, 6.0, 3.0, 9.0);
-                insertMeasurementDataNumeric1H(5000, sched, 40.0, 30.0, 50.0);
+                insertMeasurementDataNumeric1H(eldest, sched, 30.0, 20.0, 40.0);
+                insertMeasurementDataNumeric1H(elder, sched, 5.0, 2.0, 8.0);
+                insertMeasurementDataNumeric1H(young, sched, 6.0, 3.0, 9.0);
+                insertMeasurementDataNumeric1H(youngest, sched, 40.0, 30.0, 50.0);
                 if ((--dataCount % 500) == 0) {
                     System.out.println(String.valueOf(dataCount) + " more test measurement data left to insert");
                     commitAndBegin();
@@ -109,7 +115,8 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             commit();
 
             startingTime = System.currentTimeMillis();
-            long computeTime = baselineManager.calculateAutoBaselines(500, 1000);
+
+            long computeTime = baselineManager.calculateAutoBaselines(90000, System.currentTimeMillis());
             assert computeTime > 0;
 
             System.out.println(">>>>>>> a) [" + allScheds.size() + "] baselines calculated in ["
@@ -117,7 +124,7 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
 
             // calculate them again, the delete query will be triggered this time
             startingTime = System.currentTimeMillis();
-            computeTime = baselineManager.calculateAutoBaselines(500, 1000);
+            computeTime = baselineManager.calculateAutoBaselines(90000, System.currentTimeMillis());
             assert computeTime > 0;
 
             System.out.println(">>>>>>> b) [" + allScheds.size() + "] baselines calculated in ["
@@ -145,7 +152,6 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
         }
     }
 
-
     /**
      * This is a very important test - it tests native queries that are hardcoded. We need this test because just
      * deploying the EJB3 entities does not ensure these native queries didn't break; running this test will exercise
@@ -160,19 +166,29 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             setupResources(entityManager);
             assert entityManager.find(Resource.class, platform.getId()) != null : "Did not setup platform - cannot test";
 
-            insertMeasurementDataNumeric1H(100, measSched, 30.0, 20.0, 40.0);
-            insertMeasurementDataNumeric1H(500, measSched, 5.0, 2.0, 8.0);
-            insertMeasurementDataNumeric1H(1000, measSched, 6.0, 3.0, 9.0);
-            insertMeasurementDataNumeric1H(5000, measSched, 40.0, 30.0, 50.0);
+            long now = System.currentTimeMillis();
+            long eldest = now - 180000;
+            long elder = now - 120000;
+            long young = now - 60000;
+            long youngest = now;
 
-            insertMeasurementDataNumeric1H(100, measSched2, 5000.0, 3500.0, 6500.0);
-            insertMeasurementDataNumeric1H(500, measSched2, 5000.0, 3000.0, 7000.0);
-            insertMeasurementDataNumeric1H(1000, measSched2, 2000.0, 1000.0, 3000.0);
-            insertMeasurementDataNumeric1H(5000, measSched2, 1500.0, 500.0, 2500.0);
+            insertMeasurementDataNumeric1H(0, measSched, 0.0, 0.0, 0.0);
+            insertMeasurementDataNumeric1H(eldest, measSched, 30.0, 20.0, 40.0);
+            insertMeasurementDataNumeric1H(elder, measSched, 5.0, 2.0, 8.0);
+            insertMeasurementDataNumeric1H(young, measSched, 6.0, 3.0, 9.0);
+            insertMeasurementDataNumeric1H(youngest, measSched, 40.0, 30.0, 50.0);
+
+            insertMeasurementDataNumeric1H(0, measSched2, 0.0, 0.0, 0.0);
+            insertMeasurementDataNumeric1H(eldest, measSched2, 5000.0, 3500.0, 6500.0);
+            insertMeasurementDataNumeric1H(elder, measSched2, 5000.0, 3000.0, 7000.0);
+            insertMeasurementDataNumeric1H(young, measSched2, 2000.0, 1000.0, 3000.0);
+            insertMeasurementDataNumeric1H(youngest, measSched2, 1500.0, 500.0, 2500.0);
 
             commit();
 
-            long computeTime = baselineManager.calculateAutoBaselines(500, 1000);
+            // pass now for olderThanTime to ensure all existing baselines are deleted
+            // pass 30000 for amountOfData to only include the youngest in the baseline calculation
+            long computeTime = baselineManager.calculateAutoBaselines(30000, System.currentTimeMillis());
             assert computeTime > 0;
 
             MeasurementBaseline bl1;
@@ -188,17 +204,17 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             assert bl1 != null : "Baseline for measSched should have been inserted";
             assert bl1.getSchedule().getId() == measSched.getId();
             assert !bl1.isUserEntered();
-            assert bl1.getMin().toString().equals("2.0"); // comparing floating points, ugh
-            assert bl1.getMax().toString().equals("9.0");
-            assert bl1.getMean().toString().equals("5.5");
+            assertEquals(30.0, bl1.getMin());
+            assertEquals(50.0, bl1.getMax());
+            assertEquals(40.0, bl1.getMean());
 
             bl2 = entityManager.find(MeasurementSchedule.class, measSched2.getId()).getBaseline();
             assert bl2 != null : "Baseline for measSched2 should have been inserted";
             assert bl2.getSchedule().getId() == measSched2.getId();
             assert !bl2.isUserEntered();
-            assert bl2.getMin().toString().equals("1000.0"); // comparing floating points, ugh
-            assert bl2.getMax().toString().equals("7000.0");
-            assert bl2.getMean().toString().equals("3500.0");
+            assertEquals(500.0, bl2.getMin());
+            assertEquals(2500.0, bl2.getMax());
+            assertEquals(1500.0, bl2.getMean());
 
             // remember these, the next time we calculate, they will be deleted and new ones created
             bl1Id = bl1.getId();
@@ -211,7 +227,7 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             // calculate them again, the values will be the same, but the delete query will be triggered
             // wait a bit so our compute time will be assured to be different
             Thread.sleep(1000L);
-            computeTime = baselineManager.calculateAutoBaselines(500, 1000);
+            computeTime = baselineManager.calculateAutoBaselines(30000, System.currentTimeMillis());
             assert computeTime > 0;
 
             begin();
@@ -220,17 +236,48 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             assert bl1 != null : "Baseline for measSched should have been inserted";
             assert bl1.getSchedule().getId() == measSched.getId();
             assert !bl1.isUserEntered();
-            assert bl1.getMin().toString().equals("2.0"); // comparing floating points, ugh
-            assert bl1.getMax().toString().equals("9.0");
-            assert bl1.getMean().toString().equals("5.5");
+            assertEquals(30.0, bl1.getMin());
+            assertEquals(50.0, bl1.getMax());
+            assertEquals(40.0, bl1.getMean());
 
             bl2 = entityManager.find(MeasurementSchedule.class, measSched2.getId()).getBaseline();
             assert bl2 != null : "Baseline for measSched2 should have been inserted";
             assert bl2.getSchedule().getId() == measSched2.getId();
             assert !bl2.isUserEntered();
-            assert bl2.getMin().toString().equals("1000.0"); // comparing floating points, ugh
-            assert bl2.getMax().toString().equals("7000.0");
-            assert bl2.getMean().toString().equals("3500.0");
+            assertEquals(500.0, bl2.getMin());
+            assertEquals(2500.0, bl2.getMax());
+            assertEquals(1500.0, bl2.getMean());
+
+            // check the new IDs with the old ones - they should be different due to the delete query
+            assert bl1.getId() != bl1Id : "bl1.getId() was " + bl1.getId() + ", bl1Id was " + bl1Id;
+            assert bl2.getId() != bl2Id : "bl2.getId() was " + bl2.getId() + ", bl2Id was " + bl2Id;
+            assert bl1.getComputeTime().after(bl1ComputeTime);
+            assert bl2.getComputeTime().after(bl2ComputeTime);
+
+            commit();
+
+            // calculate them again using wider ranges to include more (all available) 1H data in the calculation
+            Thread.sleep(1000L);
+            computeTime = baselineManager.calculateAutoBaselines(240000, System.currentTimeMillis());
+            assert computeTime > 0;
+
+            begin();
+
+            bl1 = entityManager.find(MeasurementSchedule.class, measSched.getId()).getBaseline();
+            assert bl1 != null : "Baseline for measSched should have been inserted";
+            assert bl1.getSchedule().getId() == measSched.getId();
+            assert !bl1.isUserEntered();
+            assertEquals(2.0, bl1.getMin());
+            assertEquals(50.00, bl1.getMax());
+            assertEquals(20.25, bl1.getMean());
+
+            bl2 = entityManager.find(MeasurementSchedule.class, measSched2.getId()).getBaseline();
+            assert bl2 != null : "Baseline for measSched2 should have been inserted";
+            assert bl2.getSchedule().getId() == measSched2.getId();
+            assert !bl2.isUserEntered();
+            assertEquals(500.0, bl2.getMin());
+            assertEquals(7000.0, bl2.getMax());
+            assertEquals(3375.0, bl2.getMean());
 
             // check the new IDs with the old ones - they should be different due to the delete query
             assert bl1.getId() != bl1Id;
@@ -259,7 +306,7 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
      * Calculate Out of Bound values. Those need to use baselines, so they are tested here
      * @throws Throwable If anything goes wrong.
      */
-
+    @SuppressWarnings("unchecked")
     public void testCalculateOOB() throws Throwable {
 
         begin();
@@ -268,24 +315,32 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             setupResources(entityManager);
             assert entityManager.find(Resource.class, platform.getId()) != null : "Did not setup platform - cannot test";
 
-            insertMeasurementDataNumeric1H(100, measSched, 30.0, 20.0, 40.0);
-            insertMeasurementDataNumeric1H(500, measSched, 5.0, 2.0, 8.0);
-            insertMeasurementDataNumeric1H(1000, measSched, 6.0, 3.0, 9.0);
-            insertMeasurementDataNumeric1H(5000, measSched, 40.0, 30.0, 50.0);
+            long now = System.currentTimeMillis();
+            long eldest = now - 180000;
+            long elder = now - 120000;
+            long young = now - 60000;
+            long youngest = now;
 
-            insertMeasurementDataNumeric1H(100, measSched2, 5000.0, 3500.0, 6500.0);
-            insertMeasurementDataNumeric1H(500, measSched2, 5000.0, 3000.0, 7000.0);
-            insertMeasurementDataNumeric1H(1000, measSched2, 2000.0, 1000.0, 3000.0);
-            insertMeasurementDataNumeric1H(5000, measSched2, 1500.0, 500.0, 2500.0);
+            insertMeasurementDataNumeric1H(0, measSched, 0.0, 0.0, 0.0);
+            insertMeasurementDataNumeric1H(eldest, measSched, 30.0, 20.0, 40.0);
+            insertMeasurementDataNumeric1H(elder, measSched, 5.0, 2.0, 8.0);
+            insertMeasurementDataNumeric1H(young, measSched, 6.0, 3.0, 9.0);
+            insertMeasurementDataNumeric1H(youngest, measSched, 40.0, 30.0, 50.0);
+
+            insertMeasurementDataNumeric1H(0, measSched2, 0.0, 0.0, 0.0);
+            insertMeasurementDataNumeric1H(eldest, measSched2, 5000.0, 3500.0, 6500.0);
+            insertMeasurementDataNumeric1H(elder, measSched2, 5000.0, 3000.0, 7000.0);
+            insertMeasurementDataNumeric1H(young, measSched2, 2000.0, 1000.0, 3000.0);
+            insertMeasurementDataNumeric1H(youngest, measSched2, 1500.0, 500.0, 2500.0);
 
             commit();
 
-            long computeTime = baselineManager.calculateAutoBaselines(500, 1000);
+            long computeTime = baselineManager.calculateAutoBaselines(30000, System.currentTimeMillis());
             assert computeTime > 0;
 
             begin();
             // check the 2 values at 5000 against their bands computed between [500 and 1000]
-            oobManager.computeOOBsFromHourBeginingAt(overlord,5000);
+            oobManager.computeOOBsFromHourBeginingAt(overlord, eldest);
 
             // check results
             Query q = entityManager.createQuery("SELECT oo FROM MeasurementOOB oo");
@@ -293,33 +348,31 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             System.out.println("OOBs calculated: \n" + oobs);
             for (MeasurementOOB oob : oobs) {
                 if (oob.getScheduleId() == measSched.getId()) {
-                    assert oob.getOobFactor() == 586 : "Expected: 586, was " + oob.getOobFactor();
-                }
-                else {
-                    assert oob.getOobFactor() == 8 : "Expected: 8, was " + oob.getOobFactor();
+                    assert oob.getOobFactor() == 50 : "Expected: 586, was " + oob.getOobFactor();
+                } else {
+                    assert oob.getOobFactor() == 200 : "Expected: 8, was " + oob.getOobFactor();
                 }
             }
 
-            PageControl pc = new PageControl();
-            pc.setPageSize(15);
+            PageControl pc = PageControl.getUnlimitedInstance();
 
-            List<MeasurementOOBComposite> comps = oobManager.getSchedulesWithOOBs(overlord, 5000, pc);
-   //         System.out.println("Composites: " + comps);
+            List<MeasurementOOBComposite> comps = oobManager.getSchedulesWithOOBs(overlord, System.currentTimeMillis(),
+                pc);
+            //         System.out.println("Composites: " + comps);
             assert comps.size() == 2 : "Expected 2 composites, but got " + comps.size();
 
-            comps = oobManager.getHighestNOOBsForResource(overlord,5000,platform.getId(),2);
-            assert comps.size() == 1:  "Expected 1 composite, but got " + comps.size();
+            comps = oobManager.getHighestNOOBsForResource(overlord, System.currentTimeMillis(), platform.getId(), 2);
+            assert comps.size() == 1 : "Expected 1 composite, but got " + comps.size();
 
             // Compute some more OOBs
-            oobManager.computeOOBsFromHourBeginingAt(overlord,1000);
+            oobManager.computeOOBsFromHourBeginingAt(overlord, elder);
             q = entityManager.createQuery("SELECT oo FROM MeasurementOOB oo");
             oobs = q.getResultList();
-        //    System.out.println("OOBs calculated: \n" + oobs);
+            //    System.out.println("OOBs calculated: \n" + oobs);
 
-            comps = oobManager.getSchedulesWithOOBs(overlord, 5000, pc);
-       //     System.out.println("Composites: " + comps);
+            comps = oobManager.getSchedulesWithOOBs(overlord, elder, pc);
+            //     System.out.println("Composites: " + comps);
             assert comps.size() == 2 : "Expected 2, but was " + comps.size();
-
 
             commit();
 
@@ -327,9 +380,10 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
 
             begin();
 
-            q = entityManager.createQuery("DELETE FROM MeasurementOOB oo WHERE  oo.schedule = :sched1 OR oo.schedule = :sched2");
-            q.setParameter("sched1",measSched);
-            q.setParameter("sched2",measSched2);
+            q = entityManager
+                .createQuery("DELETE FROM MeasurementOOB oo WHERE  oo.schedule = :sched1 OR oo.schedule = :sched2");
+            q.setParameter("sched1", measSched);
+            q.setParameter("sched2", measSched2);
             q.executeUpdate();
             commit();
 
@@ -345,9 +399,7 @@ public class MeasurementBaselineManagerTest extends AbstractEJB3Test {
             }
         }
 
-
     }
-
 
     private void setupResources(EntityManager em) {
         agent = new Agent("test-agent", "localhost", 1234, "", "randomToken");

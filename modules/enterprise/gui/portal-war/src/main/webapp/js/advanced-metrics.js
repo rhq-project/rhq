ed91ffc277e079c1348c5d00d35abe8b9001b278
Fady Matar <fmatar@redhat.com>
2009-03-20 18:18:26 +0000

    RHQ-1711: Resolved validation through JS

--- modules/enterprise/gui/portal-war/src/main/webapp/js/advanced-metrics.js
+++ modules/enterprise/gui/portal-war/src/main/webapp/js/advanced-metrics.js
@@ -43,3 +43,41 @@ function closePopupAndReloadParent() {
     window.opener.focus();
     window.close();
 }
+
+function validateFields() {
+	var duration = document.getElementById('advancedMetricsValuesForm:durationMetricType:0').checked;
+	var interval = document.getElementById('advancedMetricsValuesForm:intervalMetricType:0').checked;
+
+	if(duration) {
+		var durationValue = document.getElementById('advancedMetricsValuesForm:duration').value;
+		if (isNaN(durationValue)){
+			alert("Please enter a valid number");
+			return false;
+		} else {
+			document.advancedMetricsValuesForm.submit();
+			closePopupAndReloadParent();
+		}
+	} else if(interval) {
+		var from= document.getElementById('advancedMetricsValuesForm:fromDateInputDate').value;
+		var to= document.getElementById('advancedMetricsValuesForm:toDateInputDate').value;
+
+		if(from== "" || to == "") {
+			alert ("Please fill in the required dates");
+			return false;
+		} else {
+			var fromDate = new Date(from);
+			var toDate = new Date(to);
+			var now = new Date();
+			if(fromDate > now || toDate > now) {
+				alert ("Cannot select a date in the future");
+				return false;
+			} else if (fromDate > toDate ) {
+				alert ("From date cannot be greater than to date");
+				return false;
+			} else {
+				document.advancedMetricsValuesForm.submit();
+				closePopupAndReloadParent();
+			}
+		}
+	}
+}

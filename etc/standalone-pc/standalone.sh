e4421da8b3eb98c30d76a6fb4d39a52e75e19886
John Mazzitelli <mazz@redhat.com>
2009-03-31 20:29:44 +0000

    forgot to set the mainclass

--- etc/standalone-pc/standalone.sh
+++ etc/standalone-pc/standalone.sh
@@ -2,5 +2,6 @@
 
 _DOLLARZERO=`readlink "$0" || echo "$0"`
 RHQ_AGENT_BIN_DIR_PATH=`dirname "$_DOLLARZERO"`
+RHQ_AGENT_MAINCLASS=org.rhq.core.pc.StandaloneContainer
 export RHQ_AGENT_MAINCLASS
 $RHQ_AGENT_BIN_DIR_PATH/rhq-agent.sh $*

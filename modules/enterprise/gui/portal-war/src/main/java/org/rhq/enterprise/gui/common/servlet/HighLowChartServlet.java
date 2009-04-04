51d52b42761a9df497c171256cb01a602f5dbaa9
Joseph Marques <jmarques@redhat.com>
2009-04-04 17:13:12 +0000

    [RHQ-743] - override parse parameters to handle cases when caller doesn't explicitly pass the requisite MeasurementUnits parameter; serialize access to HighLowChartServlet which solves incorrect Y-axis units in all other cases (caused b servlets clobbering each other's state);

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/common/servlet/HighLowChartServlet.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/common/servlet/HighLowChartServlet.java
@@ -22,13 +22,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.SingleThreadModel;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.rhq.core.domain.auth.Subject;
+import org.rhq.core.domain.measurement.MeasurementDefinition;
 import org.rhq.core.domain.measurement.MeasurementSchedule;
+import org.rhq.core.domain.measurement.MeasurementUnits;
 import org.rhq.core.domain.measurement.composite.MeasurementDataNumericHighLowComposite;
 import org.rhq.enterprise.gui.image.chart.Chart;
 import org.rhq.enterprise.gui.image.chart.DataPointCollection;
@@ -38,6 +41,7 @@ import org.rhq.enterprise.gui.legacy.WebUser;
 import org.rhq.enterprise.gui.legacy.util.SessionUtils;
 import org.rhq.enterprise.gui.util.WebUtility;
 import org.rhq.enterprise.server.measurement.MeasurementDataManagerLocal;
+import org.rhq.enterprise.server.measurement.MeasurementDefinitionManagerLocal;
 import org.rhq.enterprise.server.measurement.MeasurementPreferences;
 import org.rhq.enterprise.server.measurement.MeasurementScheduleManagerLocal;
 import org.rhq.enterprise.server.measurement.MeasurementPreferences.MetricRangePreferences;
@@ -55,10 +59,15 @@ import org.rhq.enterprise.server.util.LookupUtil;
  * @author Ian Springer
  * @author Heiko W. Rupp
  */
-public class HighLowChartServlet extends ChartServlet {
+public class HighLowChartServlet extends ChartServlet implements SingleThreadModel {
     private static final int NUMBER_OF_DATA_POINTS = DefaultConstants.DEFAULT_CHART_POINTS;
 
     private final Log log = LogFactory.getLog(HighLowChartServlet.class);
+    private int scheduleId;
+    private int definitionId;
+    private int groupId;
+    private int parentId;
+    private int childTypeId;
 
     /* (non-Javadoc)
      * @see org.rhq.enterprise.gui.common.servlet.ChartServlet#createChart()
@@ -84,6 +93,39 @@ public class HighLowChartServlet extends ChartServlet {
         highLowChart.columnWidth = 7;
     }
 
+    @Override
+    protected void parseParameters(HttpServletRequest request) {
+        // TODO if we have a schedule, we should also check the id= and see if that matches
+        scheduleId = WebUtility.getOptionalIntRequestParameter(request, "schedId", -1);
+        groupId = WebUtility.getOptionalIntRequestParameter(request, "groupId", -1);
+        parentId = WebUtility.getOptionalIntRequestParameter(request, "parent", -1);
+        childTypeId = WebUtility.getOptionalIntRequestParameter(request, "type", -1);
+        definitionId = WebUtility.getOptionalIntRequestParameter(request, "definitionId", -1);
+
+        /* 
+         * RHQ-743 - if we don't parse the request parameters here, on rare occasion the chart will be initialized 
+         *           without any units, which then defaults to percentage; however, if the data represents bytes, and
+         *           if that value is large (in the GB range) then the Y-axis formatter for the Chart will attempt
+         *           to render GB labels are percentages, causing results like "227,040,000,000%"; by parsing all
+         *           HighLowChartServlet parameters here, we can guarantee that even if the caller did not pass the
+         *           units, they can be deduced from the various other parameters passed
+         */
+        String parameter = request.getParameter(MEASUREMENT_UNITS_PARAM);
+        if (parameter == null || parameter.equals("")) {
+            Subject overlord = LookupUtil.getSubjectManager().getOverlord();
+            if (scheduleId > 0) {
+                units = getUnitsFromScheduleId(overlord, scheduleId);
+            } else {
+                units = getUnitsFromDefinitionId(overlord, definitionId);
+            }
+            log.info("Caller did not pass MeasuremntUnits, calculated them as " + units.getName());
+        } else {
+            log.info("Caller passed MeasurementUnits of " + parameter);
+        }
+
+        super.parseParameters(request);
+    }
+
     /* (non-Javadoc)
      * @see org.rhq.enterprise.gui.common.servlet.ChartServlet#plotData(javax.servlet.http.HttpServletRequest)
      */
@@ -91,27 +133,9 @@ public class HighLowChartServlet extends ChartServlet {
     protected void plotData(HttpServletRequest request, Chart chart) throws ServletException {
         // Make sure the schedule id was passed in.
         // TODO: Pass in a resource/group id and a measurement definition id instead. (ips, 04/16/07)
-        int groupId = -1;
-        int definitionId = -1;
-        int parentId = -1;
-        int childTypeId = -1;
-
-        // TODO if we have a schedule, we should also check the id= and see if that matches
-        int scheduleId = WebUtility.getOptionalIntRequestParameter(request, "schedId", -1);
-
-        // if no schedule is found, check for compatible group
-        if (scheduleId == -1) {
-            groupId = WebUtility.getOptionalIntRequestParameter(request, "groupId", -1);
+        if (log.isDebugEnabled()) {
+            log.debug("Requesting: " + request.getQueryString());
         }
-
-        // if no schedule and no compatible group are found check for autogroup
-        if ((scheduleId == -1) && (groupId == -1)) {
-            parentId = WebUtility.getOptionalIntRequestParameter(request, "parent", -1);
-            childTypeId = WebUtility.getOptionalIntRequestParameter(request, "type", -1);
-        }
-
-        // for group and autogroup
-        definitionId = WebUtility.getOptionalIntRequestParameter(request, "definitionId", -1);
 
         WebUser user = SessionUtils.getWebUser(request.getSession());
         MeasurementPreferences preferences = user.getMeasurementPreferences();
@@ -132,9 +156,10 @@ public class HighLowChartServlet extends ChartServlet {
 
             if (schedule != null) {
                 if (log.isDebugEnabled())
-                    log.debug("Plotting a high-low chart data for metric " + schedule.getDefinition().getName() + " on resource " + schedule.getResource().getName() + "...");
-                dataPoints = dataManager.getMeasurementDataForResource(subject, schedule.getResource().getId(), new int[] { schedule.getDefinition().getId() }, beginTime, endTime,
-                    NUMBER_OF_DATA_POINTS).get(0);
+                    log.debug("Plotting a high-low chart data for metric " + schedule.getDefinition().getName()
+                        + " on resource " + schedule.getResource().getName() + "...");
+                dataPoints = dataManager.getMeasurementDataForResource(subject, schedule.getResource().getId(),
+                    new int[] { schedule.getDefinition().getId() }, beginTime, endTime, NUMBER_OF_DATA_POINTS).get(0);
             } else {
                 log.debug("Passed scheduleId " + scheduleId + " has no schedule attached, ignoring");
                 return;
@@ -147,14 +172,16 @@ public class HighLowChartServlet extends ChartServlet {
          */
         else if ((groupId > 0) && (definitionId > 0)) // compatible group
         {
-            dataPoints = dataManager.getMeasurementDataForCompatibleGroup(subject, groupId, definitionId, beginTime, endTime, NUMBER_OF_DATA_POINTS, true).get(0);
+            dataPoints = dataManager.getMeasurementDataForCompatibleGroup(subject, groupId, definitionId, beginTime,
+                endTime, NUMBER_OF_DATA_POINTS, true).get(0);
         } else if ((parentId > 0) && (childTypeId > 0) && (definitionId > 0)) //  autogroup
         {
-            dataPoints = dataManager.getMeasurementDataForAutoGroup(subject, parentId, childTypeId, definitionId, beginTime, endTime, NUMBER_OF_DATA_POINTS, true).get(0);
+            dataPoints = dataManager.getMeasurementDataForAutoGroup(subject, parentId, childTypeId, definitionId,
+                beginTime, endTime, NUMBER_OF_DATA_POINTS, true).get(0);
         } else {
             if (log.isDebugEnabled()) {
-                log.debug("No valid input found for HighLowChart: [schedId=" + scheduleId + ", defId=" + definitionId + ", groupId=" + groupId + ", parentId=" + parentId + ", childTypeId="
-                    + childTypeId + "]");
+                log.debug("No valid input found for HighLowChart: [schedId=" + scheduleId + ", defId=" + definitionId
+                    + ", groupId=" + groupId + ", parentId=" + parentId + ", childTypeId=" + childTypeId + "]");
             }
 
             return;
@@ -168,5 +195,18 @@ public class HighLowChartServlet extends ChartServlet {
         HighLowChart highLowChart = (HighLowChart) chart;
         DataPointCollection bars = highLowChart.getDataPoints(0);
         bars.addAll(chartDataPoints);
+    }
+
+    private MeasurementUnits getUnitsFromScheduleId(Subject subject, int measurementScheduleId) {
+        MeasurementScheduleManagerLocal scheduleManager = LookupUtil.getMeasurementScheduleManager();
+        MeasurementSchedule schedule = scheduleManager.getMeasurementScheduleById(subject, measurementScheduleId);
+        return schedule.getDefinition().getUnits();
+    }
+
+    private MeasurementUnits getUnitsFromDefinitionId(Subject subject, int measurementDefinitionId) {
+        MeasurementDefinitionManagerLocal definitionManager = LookupUtil.getMeasurementDefinitionManager();
+        MeasurementDefinition definition = definitionManager.getMeasurementDefinitionById(subject,
+            measurementDefinitionId);
+        return definition.getUnits();
     }
 }

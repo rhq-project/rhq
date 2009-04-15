e06b76f9f38b37c2458ac561491ae93f0893cfed
Joseph Marques <jmarques@redhat.com>
2009-04-15 02:42:29 +0000

    protect against NPE if turning on logging while server is running; add copyright and author tags;

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/util/HibernatePerformanceMonitor.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/util/HibernatePerformanceMonitor.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.enterprise.server.util;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,7 +27,9 @@ import javax.persistence.EntityManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-
+/**
+ * @author Joseph Marques
+ */
 public class HibernatePerformanceMonitor {
     private static final Log log = LogFactory.getLog(HibernatePerformanceMonitor.class);
 
@@ -40,10 +60,11 @@ public class HibernatePerformanceMonitor {
     public void stop(long id, String logPrefix) {
         if (log.isDebugEnabled()) {
             HibernateStatisticsStopWatch watch = watches.remove(id);
-            watch.stop();
             if (watch == null) {
+                // could happen if debugging was turned on and the start() call was already skipped
                 return;
             }
+            watch.stop();
             log.debug(watch.toString() + (logPrefix == null ? "(unknown)" : " for " + logPrefix + " "));
         }
     }

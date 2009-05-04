a8cc2cf301aeec1cccf541e8273fd02a6467c624
Heiko Rupp <hwr@redhat.com>
2009-05-04 07:58:16 +0000

    Update for the event side - not yet done though.

--- modules/plugins/JBossOSGi/src/main/java/org/jboss/on/plugins/jbossOsgi/JBossOSGi/OsgiEventPoller.java
+++ modules/plugins/JBossOSGi/src/main/java/org/jboss/on/plugins/jbossOsgi/JBossOSGi/OsgiEventPoller.java
@@ -18,26 +18,37 @@
  */
 package org.jboss.on.plugins.jbossOsgi.JBossOSGi;
 
+import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jetbrains.annotations.NotNull;
+import org.osgi.service.log.LogEntry;
+import org.osgi.service.log.LogService;
+
+import org.jboss.osgi.spi.logging.LogEntryCache;
+import org.jboss.osgi.spi.logging.LogEntryFilter;
 
 import org.rhq.core.pluginapi.event.EventPoller;
 import org.rhq.core.domain.event.Event;
+import org.rhq.core.domain.event.EventSeverity;
 
 /**
- * TODO document me
+ * Remotely poll the logs from the JBossOSGi server and feed into the
+ * Events subsystem.
  *
  * @author Heiko W. Rupp
  */
 public class OsgiEventPoller implements EventPoller{
 
     private final Log log = LogFactory.getLog(OsgiEventPoller.class);
+    private LogEntryCache entryCache;
 
     public OsgiEventPoller() {
-
+        entryCache = new LogEntryCache();// new LogEntryFilter(".*"), LogService.LOG_INFO, ".*");
+        // TODO register with the remote
     }
 
 
@@ -51,6 +62,45 @@ public class OsgiEventPoller implements EventPoller{
     }
 
     public Set<Event> poll() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        Set<Event> eventSet = new HashSet<Event>();
+        List<LogEntry> entries = entryCache.getLog();
+        for (LogEntry entry: entries ) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(entry.getMessage());
+            if (entry.getBundle()!=null)
+                builder.append("\nBundle: ").append(entry.getBundle().getSymbolicName());
+            if (entry.getException()!=null)
+                builder.append("\nTrace: ").append(entry.getException());
+
+            Event event = new Event(getEventType(),"RemoteServer",entry.getTime(),
+                    levelToSeverity(entry.getLevel()),builder.toString());
+            eventSet.add(event);
+        }
+        return eventSet;
+    }
+
+
+    /**
+     * Translate the OSGi log levels into Jopr EventSeverity
+     * @param level OSGi Log level
+     * @return Corresonding EventSeverity
+     * @see org.osgi.service.log.LogService
+     */
+    private EventSeverity levelToSeverity(int level) {
+        EventSeverity ret;
+        switch (level) {
+            case 1: ret = EventSeverity.ERROR;
+                break;
+            case 2: ret = EventSeverity.WARN;
+                break;
+            case 3: ret = EventSeverity.INFO;
+                break;
+            case 4: ret = EventSeverity.DEBUG;
+                break;
+            default:
+                ret = EventSeverity.INFO;
+                log.warn("Got an unknown log level: " + level);
+        }
+        return ret;
     }
 }

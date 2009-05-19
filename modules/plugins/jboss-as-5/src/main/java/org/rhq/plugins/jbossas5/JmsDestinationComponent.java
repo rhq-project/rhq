b396dd53bd659d07121bac36ccf085f23906a446
Ian Springer <ispringe@redhat.com>
2009-05-19 16:14:54 +0000

    a 2nd pass at a workaround for https://jira.jboss.org/jira/browse/JOPR-198 that works for topics, as well as queues

--- modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/JmsDestinationComponent.java
+++ modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/JmsDestinationComponent.java
@@ -34,6 +34,8 @@ import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.pluginapi.configuration.ConfigurationUpdateReport;
 
 import org.jboss.managed.api.ManagedComponent;
+import org.jboss.managed.api.ComponentType;
+import org.jboss.deployers.spi.management.KnownComponentTypes;
 
 /**
  * A Resource component for JBoss AS 5 JBoss Messaging JMS topic and queues.
@@ -44,8 +46,9 @@ public class JmsDestinationComponent extends ManagedComponentComponent
 {
     private static final String[] OBJECT_NAME_PROPERTY_NAMES = new String[]{"DLQ", "expiryQueue", "serverPeer"};
 
-    // TODO (ips, 05/18/09): This is a workaround for https://jira.jboss.org/jira/browse/JBAS-6939. It should be removed
-    //      once that issue is fixed.
+    // TODO (ips, 05/18/09): The below override of getAvailability() is a workaround for
+    //                       https://jira.jboss.org/jira/browse/JBAS-6939.
+    //                       It should be removed once that issue is fixed.
     @Override
     public AvailabilityType getAvailability()
     {
@@ -54,7 +57,9 @@ public class JmsDestinationComponent extends ManagedComponentComponent
             ManagedComponent managedComponent = getManagedComponent();
             // If the topic or queue is stopped, the call to getValue() will throw an
             // "IllegalArgumentException: No such context: jboss.messaging.destination:name=FooQueue,service=Queue".
-            managedComponent.getProperty("consumerCount").getValue();
+            ComponentType queueComponentType = KnownComponentTypes.JMSDestination.Queue.getType();
+            String propName = (managedComponent.getType().equals(queueComponentType)) ? "consumerCount" : "allMessageCount";
+            managedComponent.getProperty(propName).getValue();
             return super.getAvailability();
         }
         catch (Exception e)

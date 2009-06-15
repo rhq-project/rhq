2f7976d709bc0dffe918ba183785563d6cdcfca5
Ian Springer <ispringe@redhat.com>
2009-06-15 23:34:12 +0000

    add support for "methodInvocationTime" call-time metric to EJB2 session bean ResourceTypes

--- modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/Ejb2BeanComponent.java
+++ modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/Ejb2BeanComponent.java
@@ -23,8 +23,16 @@
 package org.rhq.plugins.jbossas5;
 
 import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.jboss.managed.api.ManagedProperty;
+import org.jboss.managed.api.ManagedOperation;
 import org.jboss.metatype.api.values.CompositeValue;
 import org.jboss.metatype.api.values.SimpleValue;
 import org.rhq.core.domain.configuration.Configuration;
@@ -33,49 +41,130 @@ import org.rhq.core.domain.configuration.PropertyMap;
 import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.domain.measurement.MeasurementReport;
 import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
+import org.rhq.core.domain.measurement.calltime.CallTimeData;
 import org.rhq.core.pluginapi.operation.OperationFacet;
 import org.rhq.core.pluginapi.operation.OperationResult;
 
 /**
  * 
  * @author Lukas Krejci
+ * @author Ian Springer
  */
 public class Ejb2BeanComponent extends ManagedComponentComponent implements OperationFacet {
+    private final Log log = LogFactory.getLog(this.getClass());
 
     @Override
-    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) throws Exception {
-        super.getValues(report, metrics);
+    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> requests) throws Exception {
+        Set<MeasurementScheduleRequest> remainingRequests = new LinkedHashSet();
+        for (MeasurementScheduleRequest request : requests) {
+            String metricName = request.getName();
+            try {
+                if (metricName.equals("methodInvocationTime")) {
+                    // Convert the method stats CompositeValues into nice strongly typed objects.
+                    InvocationStats invocationStats = getInvocationStats();
+                    if (!invocationStats.methodStats.isEmpty()) {
+                        CallTimeData callTimeData = createCallTimeData(request, invocationStats);
+                        report.addData(callTimeData);
+                        resetInvocationStats();                        
+                    }                    
+                } else {
+                    remainingRequests.add(request);
+                }
+            }
+            catch (Exception e) {
+                // Don't let one bad apple spoil the barrel.
+                log.error("Failed to collect metric '" + metricName + "' for " + getResourceDescription() + ".", e);
+            }
+        }
+        // Let our superclass handle any metrics we didn't collect.
+        super.getValues(report, remainingRequests);
+    }
+
+    private void resetInvocationStats() {
+        Set<ManagedOperation> operations = getManagedComponent().getOperations();
+        for (ManagedOperation operation : operations) {
+            if (operation.getName().equals("resetInvocationStats")) {
+                operation.invoke();
+                break;
+            }
+        }
     }
 
     @Override
     public OperationResult invokeOperation(String name, Configuration parameters) throws Exception {
+        OperationResult result;
         if ("methodStats".equals(name)) {
-            OperationResult result = new OperationResult();
+            result = new OperationResult();
             PropertyList methodList = new PropertyList("methods");
             result.getComplexResults().put(methodList);
 
-            ManagedProperty stats = this.getManagedComponent().getProperty("DetypedInvocationStatistics");
-            CompositeValue methodsStats = (CompositeValue) ((CompositeValue) stats.getValue()).get("methodStats");
+            // Convert the invocation stats CompositeValues into a nice strongly typed object.
+            List<MethodStats> allMethodStats = getInvocationStats().methodStats;
 
-            Set<String> methodNames = methodsStats.getMetaType().keySet();
-
-            for (String methodName : methodNames) {
-                CompositeValue methodStats = (CompositeValue) methodsStats.get(methodName);
-
-                long count = Long.parseLong(((SimpleValue) methodStats.get("count")).getValue().toString());
-                long totalTime = Long.parseLong(((SimpleValue) methodStats.get("totalTime")).getValue().toString());
-                long minTime = Long.parseLong(((SimpleValue) methodStats.get("minTime")).getValue().toString());
-                long maxTime = Long.parseLong(((SimpleValue) methodStats.get("maxTime")).getValue().toString());
-
-                PropertyMap method = new PropertyMap("method", new PropertySimple("methodName", methodName),
-                    new PropertySimple("count", count), new PropertySimple("minTime", minTime), new PropertySimple(
-                        "maxTime", maxTime), new PropertySimple("totalTime", totalTime));
+            for (MethodStats methodStats : allMethodStats) {
+                PropertyMap method = new PropertyMap("method",
+                        new PropertySimple("methodName", methodStats.name),
+                        new PropertySimple("count", methodStats.count),
+                        new PropertySimple("minTime", methodStats.minTime),
+                        new PropertySimple("maxTime", methodStats.maxTime),
+                        new PropertySimple("totalTime", methodStats.totalTime));
                 methodList.add(method);
             }
-
-            return result;
+        } else {
+            result = super.invokeOperation(name, parameters);
         }
+        return result;
+    }
 
-        return super.invokeOperation(name, parameters);
+    private InvocationStats getInvocationStats() {
+        InvocationStats invocationStats = new InvocationStats();
+        List<MethodStats> allMethodStats = new ArrayList<MethodStats>();
+        ManagedProperty detypedInvokedStatsProp = this.getManagedComponent().getProperty("DetypedInvocationStatistics");
+        invocationStats.endTime = System.currentTimeMillis();
+        CompositeValue detypedInvokeStatsMetaValue = (CompositeValue)detypedInvokedStatsProp.getValue();
+        CompositeValue allMethodStatsMetaValue = (CompositeValue) detypedInvokeStatsMetaValue.get("methodStats");
+        Set<String> methodNames = allMethodStatsMetaValue.getMetaType().keySet();
+        for (String methodName : methodNames) {
+            CompositeValue methodStatsMetaValue = (CompositeValue) allMethodStatsMetaValue.get(methodName);
+            MethodStats methodStats = new MethodStats();
+            methodStats.name = methodName;
+            methodStats.count = Long.parseLong(((SimpleValue)methodStatsMetaValue.get("count")).getValue().toString());
+            methodStats.totalTime = Long.parseLong(((SimpleValue)methodStatsMetaValue.get("totalTime")).getValue().toString());
+            methodStats.minTime = Long.parseLong(((SimpleValue)methodStatsMetaValue.get("minTime")).getValue().toString());
+            methodStats.maxTime = Long.parseLong(((SimpleValue)methodStatsMetaValue.get("maxTime")).getValue().toString());
+            allMethodStats.add(methodStats);
+        }
+        invocationStats.methodStats = allMethodStats;
+
+        SimpleValue lastResetTimeMetaValue = (SimpleValue) ((CompositeValue) detypedInvokedStatsProp.getValue()).get("lastResetTime");
+        invocationStats.beginTime = Long.valueOf(lastResetTimeMetaValue.getValue().toString()); // TODO: handle null value?
+
+        return invocationStats;
+    }
+
+    private CallTimeData createCallTimeData(MeasurementScheduleRequest schedule, InvocationStats invocationStats)
+            throws Exception {
+        CallTimeData callTimeData = new CallTimeData(schedule);
+        Date beginDate = new Date(invocationStats.beginTime);
+        Date endDate = new Date(invocationStats.endTime);
+        for (MethodStats methodStats : invocationStats.methodStats) {
+            callTimeData.addAggregatedCallData(methodStats.name, beginDate, endDate, methodStats.minTime,
+                    methodStats.maxTime, methodStats.totalTime, methodStats.count);
+        }
+        return callTimeData;
+    }
+
+    class InvocationStats {
+        List<MethodStats> methodStats;
+        long beginTime;
+        long endTime;
+    }
+
+    class MethodStats {
+        String name;
+        long count;
+        long minTime;
+        long maxTime;
+        long totalTime;
     }
 }

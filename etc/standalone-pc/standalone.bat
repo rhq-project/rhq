204716f01302ce3e3b3f1cf68a996b24e23e6ab1
John Mazzitelli <mazz@redhat.com>
2009-06-18 21:26:46 +0000

    provide some commented out code to allow plugin developers to enable JPDA in the standalone PC

--- etc/standalone-pc/standalone.bat
+++ etc/standalone-pc/standalone.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem uncomment below if you want to enable JPDA debugging
+rem set RHQ_AGENT_ADDITIONAL_JAVA_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=9797,server=y,suspend=n
+
 set RHQ_AGENT_MAINCLASS=org.rhq.core.pc.StandaloneContainer
 call rhq-agent.bat %*
-
--- etc/standalone-pc/standalone.sh
+++ etc/standalone-pc/standalone.sh
@@ -4,4 +4,9 @@ _DOLLARZERO=`readlink "$0" || echo "$0"`
 RHQ_AGENT_BIN_DIR_PATH=`dirname "$_DOLLARZERO"`
 RHQ_AGENT_MAINCLASS=org.rhq.core.pc.StandaloneContainer
 export RHQ_AGENT_MAINCLASS
+
+# uncomment below if you want to enable JPDA debugging
+#RHQ_AGENT_ADDITIONAL_JAVA_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=9797,server=y,suspend=n"
+#export RHQ_AGENT_ADDITIONAL_JAVA_OPTS
+
 $RHQ_AGENT_BIN_DIR_PATH/rhq-agent.sh $*

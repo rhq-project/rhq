b39a770d821ced049cbe8bf75b1a920d459704c3
John Mazzitelli <mazz@redhat.com>
2009-06-20 02:56:20 +0000

    copy the test plugins to the agent as a convienence

--- etc/classloaders-test/build.bat
+++ etc/classloaders-test/build.bat
@@ -10,3 +10,6 @@ call mvn clean package
 cd %_ROOT_DIR%\plugin2
 call mvn clean package
 cd %_ROOT_DIR%
+cd ..\..\modules\enterprise\agent\target\rhq-agent\plugins
+copy %_ROOT_DIR%\plugin1\target\plugin-1.0.jar .
+copy %_ROOT_DIR%\plugin2\target\plugin-2.0.jar .

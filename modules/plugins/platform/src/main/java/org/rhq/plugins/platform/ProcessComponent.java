6733e27cc6cad0abc21526a56041af1d1a5c1993
John Mazzitelli <mazz@redhat.com>
2009-07-10 07:08:29 +0000

    some fixes - validate plugin config, closes the pidfile stream, some minor refactoring

--- modules/plugins/platform/src/main/java/org/rhq/plugins/platform/ProcessComponent.java
+++ modules/plugins/platform/src/main/java/org/rhq/plugins/platform/ProcessComponent.java
@@ -18,38 +18,49 @@
  */
 package org.rhq.plugins.platform;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.measurement.AvailabilityType;
+import org.rhq.core.domain.measurement.MeasurementDataNumeric;
 import org.rhq.core.domain.measurement.MeasurementReport;
 import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
-import org.rhq.core.domain.measurement.MeasurementDataNumeric;
-import org.rhq.core.domain.measurement.AvailabilityType;
-import org.rhq.core.domain.configuration.Configuration;
-import org.rhq.core.pluginapi.measurement.MeasurementFacet;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
-import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
-import org.rhq.core.system.ProcessInfo;
+import org.rhq.core.pluginapi.measurement.MeasurementFacet;
 import org.rhq.core.system.AggregateProcessInfo;
+import org.rhq.core.system.ProcessInfo;
 import org.rhq.core.system.SystemInfo;
-
-import java.util.Set;
-import java.util.List;
-import java.beans.PropertyDescriptor;
-import java.beans.Introspector;
-import java.beans.BeanInfo;
-import java.io.*;
+import org.rhq.core.util.exception.ThrowableUtil;
 
 /**
+ * Monitors a generic process.
+ *
  * @author Greg Hinkle
  */
 public class ProcessComponent implements ResourceComponent, MeasurementFacet {
+
+    private static final Log log = LogFactory.getLog(ProcessComponent.class);
 
     private enum Type {
         pidFile, piql
     }
 
-    private ProcessInfo process;
-
     private ResourceContext resourceContext;
+    private ProcessInfo process;
 
     private Type type;
     private String pidFile;
@@ -59,73 +70,103 @@ public class ProcessComponent implements ResourceComponent, MeasurementFacet {
     public void start(ResourceContext resourceContext) throws InvalidPluginConfigurationException, Exception {
         this.resourceContext = resourceContext;
 
-        Configuration config = this.resourceContext.getPluginConfiguration();
+        try {
+            Configuration config = this.resourceContext.getPluginConfiguration();
+            this.type = Type.valueOf(config.getSimpleValue("type", "pidFile"));
+            this.pidFile = config.getSimpleValue("pidFile", null);
+            this.piql = config.getSimpleValue("piql", null);
+            this.fullProcessTree = config.getSimple("fullProcessTree").getBooleanValue();
+        } catch (Exception e) {
+            throw new InvalidPluginConfigurationException(e);
+        }
 
-        this.type = Type.valueOf(config.getSimpleValue("type", "pidFile"));
-
-        this.pidFile = config.getSimpleValue("pidFile", null);
-        this.piql = config.getSimpleValue("piql", null);
-        this.fullProcessTree = config.getSimple("fullProcessTree").getBooleanValue();
-
-
+        // validate the plugin config some more
+        if (this.type == Type.pidFile && (this.pidFile == null || this.pidFile.length() == 0)) {
+            throw new InvalidPluginConfigurationException("Missing pidfile");
+        }
+        if (this.type == Type.piql && (this.piql == null || this.piql.length() == 0)) {
+            throw new InvalidPluginConfigurationException("Missing process query");
+        }
     }
 
     public void stop() {
     }
 
     public AvailabilityType getAvailability() {
-        return getProcess().isRunning() ? AvailabilityType.UP : AvailabilityType.DOWN;
+        try {
+            return getProcess().isRunning() ? AvailabilityType.UP : AvailabilityType.DOWN;
+        } catch (Exception e) {
+            if (log.isDebugEnabled()) {
+                log.debug("failed to get process info: " + ThrowableUtil.getAllMessages(e));
+            }
+            return AvailabilityType.DOWN;
+        }
     }
 
-    private ProcessInfo getProcess() {
+    private ProcessInfo getProcess() throws Exception {
         if (this.process == null || !this.process.isRunning()) {
-            this.process = getProcessForConfiguration(resourceContext.getPluginConfiguration(), resourceContext.getSystemInformation());
+            this.process = getProcessForConfiguration();
         }
         return this.process;
     }
 
+    private ProcessInfo getProcessForConfiguration() throws Exception {
+        SystemInfo sysInfo = this.resourceContext.getSystemInformation();
+        return getProcessForConfiguration(this.type, this.pidFile, this.piql, this.fullProcessTree, sysInfo);
+    }
 
-    protected static ProcessInfo getProcessForConfiguration(Configuration config, SystemInfo systemInfo) {
-        long pid = 0;
+    protected static ProcessInfo getProcessForConfiguration(Configuration config, SystemInfo systemInfo)
+        throws Exception {
+
         Type type = Type.valueOf(config.getSimpleValue("type", "pidFile"));
-
         String pidFile = config.getSimpleValue("pidFile", null);
         String piql = config.getSimpleValue("piql", null);
         boolean fullProcessTree = config.getSimple("fullProcessTree").getBooleanValue();
 
-        if (type == Type.pidFile) {
-            File file = new File(pidFile);
-            if (!file.exists()) {
-                try {
-                    FileInputStream fis = new FileInputStream(file);
-                    BufferedReader r = new BufferedReader(new InputStreamReader(fis));
-
-                    pid = Long.parseLong(r.readLine());
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (IOException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                }
-            }
-
-
-        } else {
-            List<ProcessInfo> processes = systemInfo.getProcesses(piql);
-
-            if (processes.size() == 1) {
-                pid = processes.get(0).getPid();
-            }
-        }
-
-        if (fullProcessTree) {
-            return new AggregateProcessInfo(pid);
-        } else {
-            return  new ProcessInfo(pid);
-        }
-
-
+        return getProcessForConfiguration(type, pidFile, piql, fullProcessTree, systemInfo);
     }
 
+    private static ProcessInfo getProcessForConfiguration(Type type, String pidFile, String piql,
+        boolean fullProcessTree, SystemInfo systemInfo) throws Exception {
+
+        long pid;
+
+        if (type == Type.pidFile) {
+            File file = new File(pidFile);
+            if (file.canRead()) {
+                FileInputStream fis = new FileInputStream(file);
+                try {
+                    BufferedReader r = new BufferedReader(new InputStreamReader(fis));
+                    pid = Long.parseLong(r.readLine());
+                } finally {
+                    try {
+                        fis.close();
+                    } catch (Exception ignore) {
+                    }
+                }
+            } else {
+                throw new FileNotFoundException("pidfile [" + pidFile
+                    + "] does not exist or is not allowed to be read. full path=" + file.getAbsolutePath());
+            }
+        } else if (type == Type.piql) {
+            List<ProcessInfo> processes = systemInfo.getProcesses(piql);
+            if (processes != null && processes.size() == 1) {
+                pid = processes.get(0).getPid();
+            } else {
+                throw new Exception("process query [" + piql + "] did not return a single process: " + processes);
+            }
+        } else {
+            throw new InvalidPluginConfigurationException("Invalid type [" + type + "]");
+        }
+
+        ProcessInfo processInfo;
+        if (fullProcessTree) {
+            processInfo = new AggregateProcessInfo(pid);
+        } else {
+            processInfo = new ProcessInfo(pid);
+        }
+        return processInfo;
+    }
 
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) {
         if (process != null) {
@@ -142,8 +183,7 @@ public class ProcessComponent implements ResourceComponent, MeasurementFacet {
         }
     }
 
-
-    protected Object lookupAttributeProperty(Object value, String property) {
+    private Object lookupAttributeProperty(Object value, String property) {
         String[] ps = property.split("\\.", 2);
 
         String searchProperty = ps[0];
@@ -157,8 +197,8 @@ public class ProcessComponent implements ResourceComponent, MeasurementFacet {
                 }
             }
         } catch (Exception e) {
-//            log.debug("Unable to read property from measurement attribute [" + searchProperty + "] not found on ["
-//                    + this.resourceContext.getResourceKey() + "]");
+            //            log.debug("Unable to read property from measurement attribute [" + searchProperty + "] not found on ["
+            //                    + this.resourceContext.getResourceKey() + "]");
         }
 
         if (ps.length > 1) {
@@ -168,7 +208,7 @@ public class ProcessComponent implements ResourceComponent, MeasurementFacet {
         return value;
     }
 
-    public double getObjectProperty(Object object, String name) {
+    private double getObjectProperty(Object object, String name) {
         try {
             BeanInfo info = Introspector.getBeanInfo(object.getClass());
             for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
@@ -177,11 +217,10 @@ public class ProcessComponent implements ResourceComponent, MeasurementFacet {
                 }
             }
         } catch (Exception e) {
-//            log.error("Error occurred while retrieving property '" + name + "' from object [" + object + "]", e);
+            //            log.error("Error occurred while retrieving property '" + name + "' from object [" + object + "]", e);
         }
 
         return Double.NaN;
     }
-
 
 }

288f4b1e0457e161124856012064fd4064d7dd6b
Joseph Marques <jmarques@redhat.com>
2009-07-17 08:25:40 +0000

    add copyright, author tag, and fix comments

--- modules/core/domain/src/main/java/org/rhq/core/domain/util/EntitySerializer.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/util/EntitySerializer.java
@@ -1,3 +1,25 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.domain.util;
 
 import java.io.FileInputStream;
@@ -28,6 +50,12 @@ import org.rhq.core.domain.resource.ResourceType;
 import org.rhq.core.domain.util.serial.ExternalizableStrategy;
 import org.rhq.core.domain.util.serial.ExternalizableStrategy.Subsystem;
 
+/**
+ * A utility specifically tailored to entities which will iterate over its persistence fields with a consistent
+ * ordering for serialization and deserialization. 
+ * 
+ * @author Joseph Marques
+ */
 public class EntitySerializer {
     private static Set<Class<? extends Annotation>> PERSISTENCE_ANNOTATIONS = new HashSet<Class<? extends Annotation>>();
     static {
@@ -143,7 +171,7 @@ public class EntitySerializer {
                         field.setBoolean(entity, in.readBoolean());
                     } else {
                         throw new IllegalStateException(
-                            "BASIC_TYPES contains an entry that doesn't have serialization support: " + type);
+                            "BASIC_TYPES contains an entry that doesn't have deserialization support: " + type);
                     }
                     continue;
                 }
@@ -151,7 +179,7 @@ public class EntitySerializer {
                 // either a string, an enum, numeric wrapper, collection, or some other object
                 field.set(entity, in.readObject());
             } catch (IllegalAccessException iae) {
-                throw new IllegalStateException("Could not access field '" + field.getName() + "' for serialization");
+                throw new IllegalStateException("Could not access field '" + field.getName() + "' for deserialization");
             }
         }
     }

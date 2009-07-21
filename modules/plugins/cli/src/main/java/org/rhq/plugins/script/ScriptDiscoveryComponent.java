ecb011c671da770c38498178e61172f72ef66df4
John Mazzitelli <mazz@redhat.com>
2009-07-21 17:04:23 +0000

    new package name is script, not cli

--- modules/plugins/cli/src/main/java/org/rhq/plugins/script/ScriptDiscoveryComponent.java
+++ modules/plugins/cli/src/main/java/org/rhq/plugins/script/ScriptDiscoveryComponent.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-package org.rhq.plugins.cli;
+package org.rhq.plugins.script;
 
 import java.io.File;
 import java.util.HashSet;

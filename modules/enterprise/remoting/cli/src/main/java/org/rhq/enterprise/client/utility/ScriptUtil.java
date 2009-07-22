eaf76192c763bbe8646b5dbb99f453fc8d9f0bd4
Joseph Marques <jmarques@redhat.com>
2009-07-22 01:59:11 +0000

    add "getFileBytes(stringFileName)" function to the scripting environment (with corresponding unit test);

--- modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/utility/ScriptUtil.java
+++ modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/utility/ScriptUtil.java
@@ -1,5 +1,10 @@
 package org.rhq.enterprise.client.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.script.ScriptEngine;
 
 public class ScriptUtil {
@@ -36,6 +41,44 @@ public class ScriptUtil {
                 throw new AssertionError(errorMessage);
             }
         }
+    }
+
+    public byte[] getFileBytes(String fileName) {
+        File file = new File(fileName);
+        long length = file.length();
+
+        if (length > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Can not read file larger than " + Integer.MAX_VALUE + " byte: "
+                + fileName);
+        }
+
+        byte[] bytes;
+        InputStream is = null;
+        try {
+            bytes = new byte[(int) length];
+            is = new FileInputStream(file);
+
+            int offset = 0, bytesRead = 0;
+            for (offset = 0, bytesRead = 0; offset < bytes.length && bytesRead >= 0; offset += bytesRead) {
+                bytesRead = is.read(bytes, offset, bytes.length - offset);
+            }
+
+            if (offset < bytes.length) {
+                throw new RuntimeException("Could not read entire file " + file.getName() + ", only " + offset + " of "
+                    + bytes.length + " bytes read");
+            }
+        } catch (IOException ioe) {
+            throw new RuntimeException("Error reading file: " + ioe.getMessage());
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException ioe) {
+                throw new RuntimeException("Error closing file: " + ioe.getMessage());
+            }
+        }
+        return bytes;
     }
 
     public void assertExists(String identifier) {

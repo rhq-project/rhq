4a875722b13928c8736afe65aa229d7e7f092b70
John Sanda <jsanda@redhat.com>
2009-07-23 01:35:52 +0000

    Adding comments

--- modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/SummaryFilter.java
+++ modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/SummaryFilter.java
@@ -1,8 +1,59 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.enterprise.client;
 
-/**
- * Created by IntelliJ IDEA. User: jsanda Date: Jul 22, 2009 Time: 7:38:10 PM To change this template use File |
- * Settings | File Templates.
- */
+import org.rhq.core.domain.util.Summary;
+
+import javax.persistence.Id;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class SummaryFilter {
+
+    public boolean filter(PropertyDescriptor property) {
+        try {
+            Field field = getField(property);
+            return field.isAnnotationPresent(Summary.class) || field.isAnnotationPresent(Id.class);
+        } catch (NoSuchFieldException e) {
+            return false;
+        }
+    }
+
+    private Field getField(PropertyDescriptor property) throws NoSuchFieldException {
+        String propertyName = property.getName();
+        Class<?> declaringClass = getDeclaringClass(property);
+        return declaringClass.getDeclaredField(propertyName);
+    }
+
+    private Class<?> getDeclaringClass(PropertyDescriptor property) {
+        Method method = property.getReadMethod();
+
+        if (method == null) {
+            method = property.getWriteMethod();
+        }
+
+        return method.getDeclaringClass();
+    }
+
 }
--- modules/enterprise/remoting/cli/src/test/java/org/rhq/enterprise/client/SummaryFilterTest.java
+++ modules/enterprise/remoting/cli/src/test/java/org/rhq/enterprise/client/SummaryFilterTest.java
@@ -1,8 +1,96 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 package org.rhq.enterprise.client;
 
-/**
- * Created by IntelliJ IDEA. User: jsanda Date: Jul 22, 2009 Time: 7:42:05 PM To change this template use File |
- * Settings | File Templates.
- */
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+import org.rhq.core.domain.util.Summary;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.beans.PropertyDescriptor;
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+
 public class SummaryFilterTest {
+
+    @Test
+    public void filterShouldReturnTrueForIdProperty() throws Exception {
+        SummaryFilter filter = new SummaryFilter();
+        PropertyDescriptor idProperty = getPropertyDescriptor("id");
+
+        assertTrue(filter.filter(idProperty), "Filter should return true for id property.");
+    }
+
+    @Test
+    public void filterShouldReturnTrueForSummaryProperty() throws Exception {                
+    }
+
+    PropertyDescriptor getPropertyDescriptor(String name) throws Exception {
+        BeanInfo beanInfo = Introspector.getBeanInfo(TestEntity.class);
+        for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+            if (descriptor.getName().equals(name)) {
+                return descriptor;
+            }
+        }
+        return null;
+    }
+
+    @Entity
+    static class TestEntity {
+        @Id
+        private int id;
+
+        @Summary
+        private String summaryField;
+
+        private String nonSummaryField;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getSummaryField() {
+            return summaryField;
+        }
+
+        public void setSummaryField(String summaryField) {
+            this.summaryField = summaryField;
+        }
+
+        public String getNonSummaryField() {
+            return nonSummaryField;
+        }
+
+        public void setNonSummaryField(String nonSummaryField) {
+            this.nonSummaryField = nonSummaryField;
+        }
+    }
+
 }

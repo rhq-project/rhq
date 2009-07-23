920686f90cd726c1f5fde0917e8153d0eba53127
Lukas Krejci <lkrejci@redhat.com>
2009-07-23 17:19:03 +0000

    [JOPR-282] Fixed a bug with binding set changes not persisted and made the "Resulting Bindings" an operation so that we don't have duplicate configuration history entries due to "Resulting Bindings" being dynamically recomputed.

--- modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/serviceBinding/SetComponent.java
+++ modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/serviceBinding/SetComponent.java
@@ -26,14 +26,18 @@ import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.managed.api.ManagedComponent;
 import org.jboss.managed.api.ManagedProperty;
 import org.jboss.metatype.api.types.MetaType;
+import org.jboss.metatype.api.values.ArrayValue;
 import org.jboss.metatype.api.values.CollectionValue;
 import org.jboss.metatype.api.values.CollectionValueSupport;
 import org.jboss.metatype.api.values.CompositeValue;
@@ -51,14 +55,18 @@ import org.rhq.core.pluginapi.configuration.ConfigurationUpdateReport;
 import org.rhq.core.pluginapi.inventory.DeleteResourceFacet;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
+import org.rhq.core.pluginapi.operation.OperationFacet;
+import org.rhq.core.pluginapi.operation.OperationResult;
 import org.rhq.plugins.jbossas5.serviceBinding.Util.PropertyDefinition;
 
-public class SetComponent implements ResourceComponent<ManagerComponent>, ConfigurationFacet, DeleteResourceFacet {
+public class SetComponent implements ResourceComponent<ManagerComponent>, ConfigurationFacet, DeleteResourceFacet, OperationFacet {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
     private static final String BINDING_PROPERTY = "binding";
     private static final String RESULTING_BINDINGS_PROPERTY = "resultingBindings";
+    
+    private static final String DISPLAY_BINDINGS_OPERATION_NAME = "displayBindings";
     
     private ResourceContext<ManagerComponent> context;
 
@@ -91,7 +99,11 @@ public class SetComponent implements ResourceComponent<ManagerComponent>, Config
                 bindingMap.put(prop);
             }
 
-            byte[] bindAddress = Util.getValue((SimpleValue) binding.get(Util.BIND_ADDRESS_PROPERTY), byte[].class);
+            ArrayValue bindingAddressValue = (ArrayValue) binding.get(Util.BIND_ADDRESS_PROPERTY);
+            byte[] bindAddress = null;
+            if (bindingAddressValue != null) {
+                bindAddress = (byte[]) bindingAddressValue.getValue();
+            }
 
             if (bindAddress == null) {
                 bindingMap.put(new PropertySimple(Util.BIND_ADDRESS_PROPERTY, null));
@@ -105,42 +117,6 @@ public class SetComponent implements ResourceComponent<ManagerComponent>, Config
                 }
             }
         }
-        
-        //now populate the resulting binding map so that the users have overview what the 
-        //the bindings would look like if this binding set was active.
-        PropertyList resultingBindings = new PropertyList(RESULTING_BINDINGS_PROPERTY);
-        configuration.put(resultingBindings);
-        
-        int portOffset = configuration.getSimple(Util.PORT_OFFSET_PROPERTY).getIntegerValue();
-        String defaultHostName = configuration.getSimple(Util.DEFAULT_HOST_NAME_PROPERTY).getStringValue();
-        
-        Configuration bindingManagerConfiguration = context.getParentResourceComponent().loadResourceConfiguration();
-        
-        for(Property p : bindingManagerConfiguration.getList(Util.STANDARD_BINDINGS_PROPERTY).getList()) {
-            PropertyMap standardBinding = (PropertyMap) p;
-            
-            PropertyMap bindingMap = new PropertyMap(BINDING_PROPERTY);
-            resultingBindings.add(bindingMap);
-            
-            for (PropertyDefinition def : Util.BINDING_SET_OVERRIDE_PROPERTIES) {
-                Property equivalent = standardBinding.get(def.propertyName);
-                if (equivalent != null) {
-                    bindingMap.put(equivalent);
-                }
-            }
-            
-            //now update the port and host name in the result
-            boolean fixedPort = standardBinding.getSimple(Util.FIXED_PORT_PROPERTY).getBooleanValue();
-            boolean fixedHostName = standardBinding.getSimple(Util.FIXED_HOST_NAME_PROPERTY).getBooleanValue();
-            int standardPort = standardBinding.getSimple(Util.PORT_PROPERTY).getIntegerValue();
-            String standardHostName = standardBinding.getSimple(Util.HOST_NAME_PROPERTY).getStringValue();
-            
-            PropertySimple resultingPort = bindingMap.getSimple(Util.PORT_PROPERTY);
-            PropertySimple resultingHostName = bindingMap.getSimple(Util.HOST_NAME_PROPERTY);
-            
-            resultingPort.setIntegerValue(fixedPort ? standardPort : (standardPort + portOffset));   
-            resultingHostName.setStringValue(fixedHostName ? standardHostName : defaultHostName);
-        }
         return configuration;
     }
 
@@ -152,10 +128,12 @@ public class SetComponent implements ResourceComponent<ManagerComponent>, Config
             CompositeValue currentBindingSet = Util.getBindingSetFromConfiguration(bindingSetValueMetaType,
                 updatedConfiguration);
 
+            ManagerComponent managerResourceComponent = context.getParentResourceComponent();
+            
             //ok, now we can update the bindingSets property with the update set of binding sets
-            ManagedComponent bindingManagerComponent = context.getParentResourceComponent().getBindingManager();
+            ManagedComponent bindingManagerComponent = managerResourceComponent.getBindingManager();
             ManagedProperty bindingSetsProperty = bindingManagerComponent.getProperty(Util.BINDING_SETS_PROPERTY);
-            String thisBindingSetName = context.getResourceKey();
+            String thisBindingSetName = managerResourceComponent.getBindingSetNameFromResourceKey(context.getResourceKey());
 
             //create new set of binding sets
             CollectionValue bindingSets = (CollectionValue) bindingSetsProperty.getValue();
@@ -216,6 +194,82 @@ public class SetComponent implements ResourceComponent<ManagerComponent>, Config
         context.getParentResourceComponent().updateBindingManager();
     }
 
+    
+    public OperationResult invokeOperation(String name, Configuration parameters) throws InterruptedException,
+        Exception {
+
+        if (!DISPLAY_BINDINGS_OPERATION_NAME.equals(name)) return null;
+        
+        OperationResult result = new OperationResult();
+        
+        Configuration currentConfiguration = loadResourceConfiguration();
+        Configuration bindingManagerConfiguration = context.getParentResourceComponent().loadResourceConfiguration();
+                
+        Configuration bindings = result.getComplexResults();
+        
+        //populate the resulting binding map so that the users have overview what the 
+        //the bindings would look like if this binding set was active.
+        PropertyList resultingBindings = new PropertyList(RESULTING_BINDINGS_PROPERTY);
+        bindings.put(resultingBindings);
+        
+        int portOffset = currentConfiguration.getSimple(Util.PORT_OFFSET_PROPERTY).getIntegerValue();
+        String defaultHostName = currentConfiguration.getSimple(Util.DEFAULT_HOST_NAME_PROPERTY).getStringValue();
+        
+        Map<String, PropertyMap> overridesMap = buildOverridesMap(currentConfiguration.getList(Util.OVERRIDE_BINDINGS_PROPERTY));
+        
+        for(Property p : bindingManagerConfiguration.getList(Util.STANDARD_BINDINGS_PROPERTY).getList()) {
+            PropertyMap standardBinding = (PropertyMap) p;
+            
+            PropertyMap bindingMap = new PropertyMap(BINDING_PROPERTY);
+            resultingBindings.add(bindingMap);
+            
+            for (PropertyDefinition def : Util.BINDING_SET_OVERRIDE_PROPERTIES) {
+                Property equivalent = standardBinding.get(def.propertyName);
+                if (equivalent != null) {
+                    bindingMap.put(equivalent);
+                }
+            }
+            
+            //now update the port and host name in the result
+            boolean fixedPort = standardBinding.getSimple(Util.FIXED_PORT_PROPERTY).getBooleanValue();
+            boolean fixedHostName = standardBinding.getSimple(Util.FIXED_HOST_NAME_PROPERTY).getBooleanValue();
+            int standardPort = standardBinding.getSimple(Util.PORT_PROPERTY).getIntegerValue();
+            String standardHostName = standardBinding.getSimple(Util.HOST_NAME_PROPERTY).getStringValue();
+            
+            PropertySimple resultingPort = bindingMap.getSimple(Util.PORT_PROPERTY);
+            PropertySimple resultingHostName = bindingMap.getSimple(Util.HOST_NAME_PROPERTY);
+            
+            int portToSet = fixedPort ? standardPort : (standardPort + portOffset);
+            String hostNameToSet = fixedHostName ? standardHostName : defaultHostName;
+            
+            //try to find an override for this binding
+            String fullyQualifiedName = standardBinding.getSimpleValue(Util.FULLY_QUALIFIED_NAME_PROPERTY, null);
+            PropertyMap override = overridesMap.get(fullyQualifiedName);
+            if (override != null) {
+                //remove this binding from overrides since we want to end up only
+                //with the new additions in it after we process the standard bindings
+                overridesMap.remove(fullyQualifiedName);
+                
+                //and get the resulting port and host name from the override instead
+                portToSet = fixedPort ? portToSet : override.getSimple(Util.PORT_PROPERTY).getIntegerValue();
+                
+                String overrideHostName = override.getSimpleValue(Util.HOST_NAME_PROPERTY, null);
+                overrideHostName = overrideHostName == null ? defaultHostName : overrideHostName;
+                
+                hostNameToSet = fixedHostName ? hostNameToSet : overrideHostName;
+            }
+            
+            resultingPort.setIntegerValue(portToSet);   
+            resultingHostName.setStringValue(hostNameToSet);
+        }
+        
+        for(PropertyMap override : overridesMap.values()) {
+            resultingBindings.add(override);
+        }
+        
+        return result;
+    }
+
     private CompositeValue getBindingSet() {
         String bindingSetName = context.getParentResourceComponent().getBindingSetNameFromResourceKey(
             context.getResourceKey());
@@ -238,4 +292,14 @@ public class SetComponent implements ResourceComponent<ManagerComponent>, Config
 
         return null;
     }
+    
+    private Map<String, PropertyMap> buildOverridesMap(PropertyList overrides) {
+        TreeMap<String, PropertyMap> ret = new TreeMap<String, PropertyMap>();
+        
+        for(Property p : overrides.getList()) {
+            PropertyMap overrideMap = (PropertyMap) p;
+            ret.put(overrideMap.getSimpleValue(Util.FULLY_QUALIFIED_NAME_PROPERTY, null), overrideMap);
+        }
+        return ret;
+    }    
 }

4f54bb8110ff000e564e775cd8756f73c4d5d32b
John Sanda <jsanda@redhat.com>
2009-07-30 11:23:25 +0000

    Adding a test for filtering by plugin name and reorganizing test code into functions.

--- modules/enterprise/remoting/scripts/src/test/script/org/rhq/enterprise/remoting/cli/test_ResourceTypeManager.js
+++ modules/enterprise/remoting/scripts/src/test/script/org/rhq/enterprise/remoting/cli/test_ResourceTypeManager.js
@@ -1,20 +1,50 @@
 rhq.login('rhqadmin', 'rhqadmin');
 
 criteria = new ResourceTypeCriteria();
-resourceTypes = ResourceTypeManager.findResourceTypesByCriteria(criteria);
+resourceTypes = null;
 
-assertNotNull(resourceTypes, 'Expected findResourceTypesByCriteria() to return a non-null result');
-assertTrue(resourceTypes.size() > 0, 'Expected findResourceTypesByCriteria() to return a non-empty result set');
-
-for (i = 0; i < resourceTypes.size(); ++i) {
-    resourceType = resourceTypes.get(i);
-    
-    assertNotNull(ResourceTypeManager.getResourceTypeById(resourceType.id),
-        'Expected getResourceTypeById to a return a ResourceType for id ' + resourceType.id);
-
-    assertNotNull(ResourceTypeManager.getResourceTypeByNameAndPlugin(resourceType.name, resourceType.plugin),
-        'Expected getResourceTypeByNameAndPLugin to return a ResourceType for ' + resourceType.name + ', ' +
-        resourceType.plugin);
-}
+testFindUnfiltered();
+testFilterByPluginName();
 
 logout();
+
+function testFindUnfiltered() {
+    resourceTypes = ResourceTypeManager.findResourceTypesByCriteria(criteria);
+
+    assertResourceTypesFound();
+
+    for (i = 0; i < resourceTypes.size(); ++i) {
+        resourceType = resourceTypes.get(i);
+    
+        assertNotNull(ResourceTypeManager.getResourceTypeById(resourceType.id),
+            'Expected getResourceTypeById to a return a ResourceType for id ' + resourceType.id);
+
+        assertNotNull(ResourceTypeManager.getResourceTypeByNameAndPlugin(resourceType.name, resourceType.plugin),
+            'Expected getResourceTypeByNameAndPLugin to return a ResourceType for ' + resourceType.name + ', ' +
+            resourceType.plugin);
+    }
+}
+
+function testFilterByPluginName() {
+    criteria.addFilterPluginName('PerfTest');
+    resourceTypes = ResourceTypeManager.findResourceTypesByCriteria(criteria);
+
+    assertResourceTypesFound('Failed to find ResourceTypes when filtering by plugin name');
+
+    for (i = 0; i < resourceTypes.size(); ++i) {
+        resourceType = resourceTypes.get(i);
+    
+        assertEquals(resourceType.plugin, 'PerfTest', 'Expected only ResourceTypes from the PerfTest plugin');
+    }
+}
+
+function assertResourceTypesFound(msg) {
+    if (msg == undefined) {
+        msg = '';
+    }
+    else {
+        msg = msg + ' - ';
+    }
+    assertNotNull(resourceTypes, msg + 'Expected findByResourceTypesByCriteria() to a reutrn a non-null result');
+    assertTrue(resourceTypes.size() > 0, msg + 'Expected findResourceTypesByCriteria() to return a non-empty results set');
+}

7cf3e866cdbb762089608dd5af2dc453fc32658a
John Sanda <jsanda@redhat.com>
2009-07-30 21:45:26 +0000

    Initial commit for new RecordCommand class.

---
+++ modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/commands/RecordCommand.java
@@ -0,0 +1,8 @@
+package org.rhq.enterprise.client.commands;
+
+/**
+ * Created by IntelliJ IDEA. User: jsanda Date: Jul 30, 2009 Time: 1:05:12 PM To change this template use File |
+ * Settings | File Templates.
+ */
+public class RecordCommand {
+}

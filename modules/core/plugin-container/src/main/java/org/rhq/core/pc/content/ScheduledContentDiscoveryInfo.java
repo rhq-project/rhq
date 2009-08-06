ce0abbceaf2402f6e4c03bc6c3f90fabd2754b8e
John Mazzitelli <mazz@redhat.com>
2009-08-06 17:30:10 +0000

    fix toString - I want to see the date, not the epoch mills

--- modules/core/plugin-container/src/main/java/org/rhq/core/pc/content/ScheduledContentDiscoveryInfo.java
+++ modules/core/plugin-container/src/main/java/org/rhq/core/pc/content/ScheduledContentDiscoveryInfo.java
@@ -1,26 +1,28 @@
- /*
-  * RHQ Management Platform
-  * Copyright (C) 2005-2008 Red Hat, Inc.
-  * All rights reserved.
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License, version 2, as
-  * published by the Free Software Foundation, and/or the GNU Lesser
-  * General Public License, version 2.1, also as published by the Free
-  * Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License and the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * and the GNU Lesser General Public License along with this program;
-  * if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  */
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.pc.content;
+
+import java.util.Date;
 
 import org.rhq.core.domain.content.PackageType;
 
@@ -101,8 +103,11 @@ public class ScheduledContentDiscoveryInfo implements Comparable<ScheduledConten
 
     @Override
     public String toString() {
-        return "ScheduledContentDiscoveryInfo[ResourceId=" + this.resourceId + ", PackageType="
-            + this.packageType.getName() + ", Interval=" + interval + ", NextDiscovery=" + nextDiscovery + ", Now="
-            + System.currentTimeMillis() + "]";
+        StringBuilder str = new StringBuilder("ScheduledContentDiscoveryInfo:");
+        str.append("resource-id=[").append(this.resourceId);
+        str.append("], package-type=[").append(this.packageType.getName());
+        str.append("], interval=[").append(this.interval);
+        str.append("], next-discovery=[").append(new Date(this.nextDiscovery));
+        return str.toString();
     }
 }

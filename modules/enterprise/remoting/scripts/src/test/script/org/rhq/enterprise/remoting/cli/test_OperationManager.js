1d01156704fea03a64bc181f0cb264adc84f0e18
John Sanda <jsanda@redhat.com>
2009-08-10 01:35:04 +0000

    Initial commit for test_OperationManager.js

---
+++ modules/enterprise/remoting/scripts/src/test/script/org/rhq/enterprise/remoting/cli/test_OperationManager.js
@@ -0,0 +1,44 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+rhq.login('rhqadmin', 'rhqadmin');
+
+executeAllTests();
+
+function testFindOperationDefinitionsUnfiltered() {
+    var operationDefinitions = OperationManager.findOperationDefinitionsByCriteria(OperationDefinitionCriteria());
+
+    Assert.assertNotNull(operationDefinitions, 'Expected non-null results for criteria search of operation definitions');    
+}
+
+function testFindResourceOperationHistoriesUnfiltered() {
+    var histories = OperationManager.findResourceOperationHistoriesByCriteria(ResourceOperationHistoryCriteria());
+
+    Assert.assertNotNull(histories, 'Expected non-null results for criteria search of resource operation histories');
+}
+
+function testFindGroupOperationHistoriesUnfiltered() {
+    var histories = OperationManager.findGroupOperationHistoriesByCriteria(GroupOperationHistoryCriteria());
+
+    Assert.assertNotNull(histories, 'Expected non-null results for criteria search of group operation histories');
+}

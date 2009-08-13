b4a5674c0dd30b4b3564899f84d570cff5e74f25
John Sanda <jsanda@redhat.com>
2009-08-13 21:18:24 +0000

    Adding an initial test for finding alerts.

--- modules/enterprise/remoting/scripts/src/test/script/org/rhq/enterprise/remoting/cli/test_alerts.js
+++ modules/enterprise/remoting/scripts/src/test/script/org/rhq/enterprise/remoting/cli/test_alerts.js
@@ -77,11 +77,119 @@ function testFindAlertDefinitionWithFilteringAndFetchingAssociations() {
     Assert.assertNumberEqualsJS(alertDefs.size(), 1, 'Expected to get back one alert when filtering and fetching associations');
 }
 
+function testFindAlertDefinitionsWithFilteringAndSortingAndFetchingAssociations() {
+    var serviceAlpha = findService('service-alpha-0', 'server-omega-0');
+    var serviceBeta = findService('service-alpha-1', 'server-omega-0');
+
+    var criteria = AlertDefinitionCriteria();
+    criteria.addFilterPriority(AlertPriority.MEDIUM);
+    criteria.addFilterResourceIds([serviceAlpha.id, serviceBeta.id]);
+
+    criteria.addSortName(PageOrdering.ASC);
+    criteria.addSortPriority(PageOrdering.DESC);
+
+    var alertDefs = AlertDefinitionManager.findAlertDefinitionsByCriteria(criteria);
+
+    Assert.assertNumberEqualsJS(alertDefs.size(), 2, 'Expected to get back two alert definitions when filtering, ' +
+            'sorting, and fetching associations');
+
+    // TODO verify sort order
+}
+
+function testFindAlertsWithoutFiltering() {
+    var criteria = AlertCriteria();
+
+    var alerts = AlertManager.findAlertsByCriteria(criteria);
+
+    Assert.assertNotNull(alerts, 'Expected to get back non-null results when fetching alerts without filtering');
+}
+
+function testFindAlertsWithFiltering() {
+    var service = findService('service-alpha-0', 'server-omega-0');
+    var eventDetails = java.util.Date() + " >> events created for " + service.name;
+    var severity = 'WARN';
+    var numberOfEvents = 1;
+
+    fireEvents(service, severity, numberOfEvents, eventDetails);
+
+    var pauseLenth = 1000; // in milliseconds
+    var numberOfIntervals = 10;
+
+    var eventCriteria = EventCriteria();
+    eventCriteria.addFilterDetail(eventDetails);
+
+    var events = waitForEventsToBeCommitted(pauseLenth, numberOfIntervals, eventCriteria, numberOfEvents);
+
+    Assert.assertNumberEqualsJS(events.size(), numberOfEvents, 'Failed to find all fired events when finding alerts ' +
+        'with filtering. This could just be a timeout. You may want to check your database and server logs to be sure though');
+
+    var alertDef1Name = 'service-alpha-0-alert-def-1';
+
+    var alertCriteria = AlertCriteria();
+    alertCriteria.addFilterName(alertDef1Name);
+    alertCriteria.addFilterDescription('Test alert definition 1 for service-alpha-0');
+    alertCriteria.addFilterPriority(AlertPriority.MEDIUM);
+    alertCriteria.addFilterResourceTypeName('service-alpha-0');
+
+    var alerts = AlertManager.findAlertsByCriteria(alertCriteria);
+
+    Assert.assertNumberEqualsJS(alerts.size(), 1, "Expected to get back one alert for alert definition '" + alertDef1Name + "'");
+}
+
 function findService(name, parentName) {
     var criteria = ResourceCriteria();
     criteria.addFilterName(name);
     criteria.addFilterParentResourceName(parentName);
 
     return ResourceManager.findResourcesByCriteria(criteria).get(0);
+}
+
+function fireEvents(resource, severity, numberOfEvents, details) {
+    var operationName = "createEvents";
+    var delay = 0;
+    var repeatInterval = 0;
+    var repeatCount = 0;
+    var timeout = 0;
+    var parameters = createParameters(resource, severity, numberOfEvents, details);
+    var description = "Test script event for " + resource.name;
+
+    return OperationManager.scheduleResourceOperation(
+        resource.id,
+        operationName,
+        delay,
+        repeatInterval,
+        repeatCount,
+        timeout,
+        parameters,
+        description
+    );
+}
+
+function createParameters(resource, severity, numberOfEvents, details) {
+    var params = new Configuration();
+    params.put(new PropertySimple("source", resource.name));
+    params.put(new PropertySimple("details", details));
+    params.put(new PropertySimple("severity", severity));
+    params.put(new PropertySimple("count", java.lang.Integer(numberOfEvents)));
+
+    return params;
+}
+
+function findEventsByResource(resource) {
+    var criteria = new EventCriteria();
+    criteria.addFilterResourceId(resource.id);
+
+    return EventManager.findEventsByCriteria(criteria);
+}
+
+function waitForEventsToBeCommitted(intervalLength, numberOfIntervals, eventCriteria, numberOfEvents) {
+    for (i = 0; i < numberOfIntervals; ++i) {
+        events = EventManager.findEventsByCriteria(eventCriteria);
+        if (events.size() == numberOfEvents) {
+            return events;
+        }
+        java.lang.Thread.sleep(intervalLength);
+    }
+    return null;
 }
 

25378321568f664b16c0722934ae6c7b53d5bd97
John Sanda <jsanda@redhat.com>
2009-09-06 19:59:54 +0000

    Initial commit for test-utils module. Right now just trying to get unitils set up and working for testing JPA mappings and queries.

---
+++ modules/enterprise/server/jar/src/test/java/org/rhq/enterprise/server/test/DataAccessTest.java
@@ -0,0 +1,8 @@
+package org.rhq.enterprise.server.test;
+
+/**
+ * Created by IntelliJ IDEA. User: jsanda Date: Aug 12, 2009 Time: 8:44:46 PM To change this template use File |
+ * Settings | File Templates.
+ */
+public class DataAccessTest {
+}

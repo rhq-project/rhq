2302fbe20af8c953552de994ed2ed964c5e81953
Ian Springer <ispringe@redhat.com>
2009-10-05 17:45:28 +0000

    implement updateResourceConfiguration() in HostsComponent

--- modules/plugins/hosts-augeas/src/main/java/org/rhq/plugins/hosts/HostsComponent.java
+++ modules/plugins/hosts-augeas/src/main/java/org/rhq/plugins/hosts/HostsComponent.java
@@ -19,9 +19,16 @@
 package org.rhq.plugins.hosts;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Date;
+import java.util.Set;
+
 import net.augeas.Augeas;
+
+import org.rhq.core.domain.configuration.ConfigurationUpdateStatus;
+import org.rhq.core.domain.configuration.Property;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
 import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
@@ -32,9 +39,10 @@ import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.domain.configuration.PropertyList;
 import org.rhq.core.domain.configuration.PropertyMap;
+import org.rhq.plugins.hosts.util.AugeasUtility;
 
 /**
- * @author Jason Dobies
+ * @author Jason Dobies, Ian Springer 
  */
 public class HostsComponent implements ResourceComponent, ConfigurationFacet {
 
@@ -73,13 +81,148 @@ public class HostsComponent implements ResourceComponent, ConfigurationFacet {
     }
 
     public Configuration loadResourceConfiguration() throws Exception {
-        Configuration pluginConfiguration = resourceContext.getPluginConfiguration();
-
-        return loadResourceConfiguration(pluginConfiguration);
+        Configuration pluginConfig = this.resourceContext.getPluginConfiguration();
+        return loadResourceConfiguration(pluginConfig);
     }
 
-    public Configuration loadResourceConfiguration(Configuration pluginConfiguration) throws Exception {
-        // Gather data necessary to create the Augeas hook
+    Configuration loadResourceConfiguration(Configuration pluginConfig) throws Exception {
+        Augeas augeas = createAugeas(pluginConfig);
+        List<String> entryPaths = getEntryPaths(pluginConfig, augeas);
+
+        Configuration resourceConfig = new Configuration();
+        resourceConfig.setNotes("Loaded from Augeas at " + new Date());
+
+        PropertyList entriesList = new PropertyList("entries");
+        resourceConfig.put(entriesList);
+
+        for (String entryPath : entryPaths) {
+            String ip = augeas.get(entryPath + "/ipaddr");
+            String canonical = augeas.get(entryPath + "/canonical");
+            List<String> aliasPaths = augeas.match(entryPath + "/alias[*]");
+
+            PropertyMap entry = new PropertyMap("entry");
+            entry.put(new PropertySimple("ipAddress", ip));
+            entry.put(new PropertySimple("canonicalName", canonical));
+            if (!aliasPaths.isEmpty()) {
+                StringBuilder aliasesPropValue = new StringBuilder();
+                for (String aliasPath : aliasPaths) {
+                    String alias = augeas.get(aliasPath);
+                    aliasesPropValue.append(alias).append("\n");
+                }
+                // Chop the final newline char.
+                aliasesPropValue.deleteCharAt(aliasesPropValue.length() - 1);
+                entry.put(new PropertySimple("aliases", aliasesPropValue));
+            }
+            entriesList.add(entry);
+        }
+
+        return resourceConfig;
+    }
+
+    public void updateResourceConfiguration(ConfigurationUpdateReport report) {
+        Configuration pluginConfig = this.resourceContext.getPluginConfiguration();
+        Augeas augeas;
+        try {
+            augeas = createAugeas(pluginConfig);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Failed to create Augeas object.", e);
+        }
+
+        PropertySimple augeasTreeNodeProperty = pluginConfig.getSimple("augeas-hosts-path");
+        if (augeasTreeNodeProperty == null) {
+            throw new IllegalStateException("Augeas tree node not specified for hosts, cannot retrieve configuration");
+        }
+        String hostsPath = augeasTreeNodeProperty.getStringValue();
+
+        Set<String> originalEntryPaths = new HashSet<String>(augeas.match("/files/etc/hosts/*"));
+        Set<String> updatedEntryPaths = new HashSet<String>();
+
+        Configuration resourceConfig = report.getConfiguration();
+        PropertyList entriesList = resourceConfig.getList("entries");
+        for (Property entryProp : entriesList.getList()) {
+            PropertyMap entry = (PropertyMap)entryProp;
+            String ipAddress = entry.getSimple("ipAddress").getStringValue();
+            String canonicalName = entry.getSimple("canonicalName").getStringValue();
+            String aliases = entry.getSimpleValue("aliases", null);
+            Set<String> aliasSet;
+            if (aliases == null) {
+                aliasSet = null;
+            } else {
+                String[] aliasArray = aliases.trim().split("\\s+");
+                aliasSet = new LinkedHashSet<String>(aliasArray.length);
+                for (String alias : aliasArray) {
+                    aliasSet.add(alias);
+                }
+            }
+
+            int newEntryIndex = 0;
+            String newEntryPathPattern = hostsPath + "/0%d";
+
+            String nameFilter = "/files/etc/hosts/*/canonical";
+            List<String> namePaths = AugeasUtility.matchFilter(augeas, nameFilter, canonicalName);
+            if (!namePaths.isEmpty()) {
+                // overwrite existing entry
+
+                // name
+                String namePath = namePaths.get(0);
+                augeas.set(namePath, canonicalName);
+
+                String entryPath = AugeasUtility.getParentPath(namePath);
+                updatedEntryPaths.add(entryPath);
+
+                // IP
+                String ipPath = entryPath + "/ipaddr";
+                augeas.set(ipPath, canonicalName);
+
+                // aliases
+                // remove existing alias nodes first
+                String aliasExpression = entryPath + "/alias[*]";
+                List<String> aliasPaths = augeas.match(aliasExpression);
+                for (String aliasPath : aliasPaths) {
+                    augeas.remove(aliasPath);
+                }
+                // then add the new ones                
+                String aliasPathPattern = entryPath + "/alias[%d]";
+                int i = 1;
+                for (String alias : aliasSet) {
+                    String aliasPath = String.format(aliasPathPattern, i++);
+                    augeas.set(aliasPath, alias);
+                }
+            } else {
+                // no existing entry w/ this canonical name - add a new entry
+                String newEntryPath = String.format(newEntryPathPattern, newEntryIndex++);
+                augeas.set(newEntryPath + "/ipaddr", ipAddress);
+                augeas.set(newEntryPath + "/canonical", canonicalName);
+                String aliasPathPattern = newEntryPath + "/alias[%d]";
+                int i = 1;
+                for (String alias : aliasSet) {
+                    String aliasPath = String.format(aliasPathPattern, i++);
+                    augeas.set(aliasPath, alias);
+                }
+            }
+
+        }
+
+        // Remove any entries from the Augeas tree that are not present in the updated Configuration.
+        for (String originalEntryPath : originalEntryPaths) {
+            if (!updatedEntryPaths.contains(originalEntryPath)) {
+                augeas.remove(originalEntryPath);
+            }
+        }
+
+        // TODO: Backup the original file.
+
+        // Write the updated Augeas tree out to the /etc/hosts file.
+        augeas.save();
+
+        // If we got this far, we've succeeded in out mission.
+        report.setStatus(ConfigurationUpdateStatus.SUCCESS);
+
+        return;
+    }
+
+    private Augeas createAugeas(Configuration pluginConfiguration) throws Exception {
         PropertySimple lensesPathProperty = pluginConfiguration.getSimple("lenses-path");
 
         if (lensesPathProperty == null) {
@@ -96,14 +239,15 @@ public class HostsComponent implements ResourceComponent, ConfigurationFacet {
         String rootPath = rootPathProperty.getStringValue();
 
         Augeas augeas = new Augeas(rootPath, lensesPath, Augeas.NONE);
+        return augeas;
+    }
 
+    private List<String> getEntryPaths(Configuration pluginConfiguration, Augeas augeas) throws Exception {
         // Find out where to look for the hosts tree
         PropertySimple augeasTreeNodeProperty = pluginConfiguration.getSimple("augeas-hosts-path");
-
         if (augeasTreeNodeProperty == null) {
-            throw new Exception("Augeas tree node not specified for hosts, cannot retrive configuration");
+            throw new Exception("Augeas tree node not specified for hosts, cannot retrieve configuration");
         }
-
         String hostsTreeNode = augeasTreeNodeProperty.getStringValue();
 
         // Request data from augeas
@@ -111,29 +255,6 @@ public class HostsComponent implements ResourceComponent, ConfigurationFacet {
         if (matches.size() == 0) {
             throw new Exception("Unable to load hosts data from augeas");
         }
-
-        // Parse out the properties
-        Configuration configuration = new Configuration();
-        configuration.setNotes("Loaded from Augeas at " + new Date());
-
-        PropertyList entriesList = new PropertyList("hostEntries");
-        configuration.put(entriesList);
-
-        for (String entryNode : matches) {
-            String ip = augeas.get(entryNode + "/ipaddr");
-            String canonical = augeas.get(entryNode + "/canonical");
-
-            PropertyMap entry = new PropertyMap("hostEntry");
-            entry.put(new PropertySimple("ipAddress", ip));
-            entry.put(new PropertySimple("canonicalName", canonical));
-
-            entriesList.add(entry);
-        }
-
-        return configuration;
-    }
-
-    public void updateResourceConfiguration(ConfigurationUpdateReport report) {
-        // TODO
+        return matches;
     }
 }

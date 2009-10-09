cda18682a9e74b01ad59af9662db0217579d4512
John Matthews <jmatthew@redhat.com>
2009-10-09 15:17:34 -0400

    Refine test so it's simpler and just calls getVersion() to RHN Hosted.

--- modules/enterprise/server/plugins/rhnhosted/src/test/java/org/rhq/enterprise/server/plugins/rhnhosted/BasicRHNTest.java
+++ modules/enterprise/server/plugins/rhnhosted/src/test/java/org/rhq/enterprise/server/plugins/rhnhosted/BasicRHNTest.java
@@ -28,53 +28,17 @@ public class BasicRHNTest extends TestCase
         return new TestSuite( BasicRHNTest.class );
     }
 
-    /*
-     * Returns a String representation of a 
-     * known systemId file registered to RHN Hosted.
-     * */
-    protected String getSystemId() throws Exception {
-        String systemIdPath = "/etc/sysconfig/rhn/systemid";
-        File systemId = new File(systemIdPath);
-        if (!systemId.exists()) {
-            System.out.println("Unable to read systemid file at: " + systemIdPath);
-            assertTrue(false);
-        }
-        return FileUtils.readFileToString(systemId);
-    }
-
     /**
-     * Tests authentication.check
+     * Tests get version
      * */
-    public void testAuthenticationCheck() throws Exception
+    public void testGetVersion() throws Exception
     {
         boolean success = true;
-        String systemId = getSystemId();
 
         try {
-            XmlRpcClient client = new XmlRpcClient( "http://xmlrpc.rhn.redhat.com/SAT", true );
-            Integer authYN = (Integer) client.invoke( "authentication.check", new Object[] {systemId} );
-            assertTrue(authYN.intValue() == 0);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            success = false;
-        }
-        assertTrue(success);
-    }
-
-
-    /**
-     * Tests up2date.login
-     */
-    public void testUp2dateLogin() throws Exception
-    {
-        boolean success = true;
-        String systemId = getSystemId();
-
-        try {
-            XmlRpcClient client = new XmlRpcClient( "http://xmlrpc.rhn.redhat.com/XMLRPC", true );
-            Object token = client.invoke( "up2date.login", new Object[] { systemId } );
-            assertTrue(token != null);
+            XmlRpcClient client = new XmlRpcClient( "http://xmlrpc.rhn.redhat.com/rpc/api", true );
+            String version = (String) client.invoke( "api.getVersion", new Object[] {});
+            assertTrue(version.length() > 0);
         }
         catch (Exception e) {
             e.printStackTrace();

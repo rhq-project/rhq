f05216f650f7393316947af3b76ff0c2ff35e5cc
Lukas Krejci <lkrejci@redhat.com>
2009-10-13 20:00:06 +0200

    Removing unused imports.

--- modules/plugins/antlr-config/src/main/java/org/rhq/plugins/antlrconfig/ConfigMapper.java
+++ modules/plugins/antlr-config/src/main/java/org/rhq/plugins/antlrconfig/ConfigMapper.java
@@ -23,22 +23,14 @@
 
 package org.rhq.plugins.antlrconfig;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.antlr.runtime.RecognitionException;
-import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenRewriteStream;
 import org.antlr.runtime.tree.CommonTree;
-import org.antlr.runtime.tree.Tree;
 
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.Property;

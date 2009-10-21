455c073b6bb2745c833db83e1213ef1e9dd36f6d
Heiko W. Rupp <hrupp@snert.(none)>
2009-10-21 09:39:46 +0200

    Annotations to allow for (semi-)automatic descriptor generation. Thanks Galder Zamareño.

---
+++ modules/helpers/pluginAnnotations/src/main/java/org/rhq/helpers/pluginAnnotations/agent/Metric.java
@@ -0,0 +1,43 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2009 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.helpers.pluginAnnotations.agent;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to denote a metric that should be measured
+ *
+ * @author Heiko W. Rupp
+ * @author Galder Zamarre–o
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.FIELD, ElementType.METHOD })
+public @interface Metric {
+   String property() default "";
+   String displayName() default "";
+   String description() default "";
+   long defaultInterval() default 120000000L; // 20 min
+   DisplayType displayType() default DisplayType.DETAIL;
+   DataType dataType() default DataType.MEASUREMENT;
+   Units units() default Units.NONE;
+   MeasurementType measurementType() default MeasurementType.DYNAMIC;
+ }

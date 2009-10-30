a9e92b899c174c4fa7ffef154bb033469a3643e6
John Sanda <john@localhost.localdomain>
2009-10-30 11:33:42 -0400

    [US 403] Initial commit for PluginTransformer, PluginTransformerTest, and PluginTransformerException
    PluginTransformer takes a PluginDescriptor and a URL for the plugin JAR file and converts them
    into a Plugin object. This logic is needed by PluginManager when the plugin container is used in
    embedded mode in order to build the list of loaded plugins. That list was previously a list of
    just plugin names, but now it is a list of the Plugin domain objects. In enterprise mode, we get
    those plugins from the server, but in embedded mode, we have to create the Plugin objects
    ourselves since there is no server to talk to.

---
+++ modules/core/client-api/src/main/java/org/rhq/core/clientapi/descriptor/PluginTransformer.java
@@ -0,0 +1,124 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.rhq.core.clientapi.descriptor;
+
+import org.rhq.core.domain.plugin.Plugin;
+import org.rhq.core.clientapi.descriptor.plugin.PluginDescriptor;
+import org.rhq.core.clientapi.descriptor.plugin.Help;
+import org.rhq.core.util.MessageDigestGenerator;
+
+import java.net.URL;
+import java.io.IOException;
+import java.io.File;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.jar.Attributes;
+
+/**
+ * Transforms some input into a {@link org.rhq.core.domain.plugin.Plugin} object.
+ */
+public class PluginTransformer {
+
+    /**
+     * Takes the given plugin descriptor and plugin JAR file URL and converts them into a
+     * {@link org.rhq.core.domain.plugin.Plugin} object. This method does not set the <code>content</code> property
+     * of the plugin.
+     *
+     * @param pluginDescriptor The plugin descriptor from which to create the plugin
+     * @param pluginURL The URL of the plugin JAR file from which to create the plugin
+     * @return A new plugin object create from the descriptor and plugin URL
+     * @throws PluginTransformException if any IO errors occur trying to read the JAR file or if a version not in the
+     * plugin descriptor or in the plugin JAR manifest
+     */
+    public Plugin toPlugin(PluginDescriptor pluginDescriptor, URL pluginURL) {
+        try {
+            Plugin plugin = new Plugin();
+            plugin.setName(pluginDescriptor.getName());
+
+            if (pluginDescriptor.getDisplayName() == null) {
+                plugin.setDisplayName(pluginDescriptor.getName());
+            }
+            else {
+                plugin.setDisplayName(pluginDescriptor.getDisplayName());
+            }
+
+            plugin.setAmpsVersion(pluginDescriptor.getAmpsVersion());
+            plugin.setDescription(pluginDescriptor.getDescription());
+            plugin.setPath(pluginURL.getPath());
+            plugin.setMtime(pluginURL.openConnection().getLastModified());
+            plugin.setHelp(getHelp(pluginDescriptor));
+            plugin.setMd5(getMd5(pluginURL));
+            plugin.setVersion(getVersion(pluginDescriptor, pluginURL));
+
+            return plugin;
+        }
+        catch (IOException e) {
+            throw new PluginTransformException("Failed to create plugin.", e);
+        }
+    }
+
+    private String getHelp(PluginDescriptor pluginDescriptor) {
+        Help help = pluginDescriptor.getHelp();
+        if (help == null || help.getContent().isEmpty()) {
+            return null;
+        }
+        return help.getContent().get(0).toString();
+    }
+
+    private String getMd5(URL pluginURL) throws IOException {
+        File jarFile = toFile(pluginURL);
+        return MessageDigestGenerator.getDigestString(jarFile);
+    }
+
+    String getVersion(PluginDescriptor pluginDescriptor, URL pluginURL) throws IOException {
+        if (pluginDescriptor.getVersion() != null) {
+            return pluginDescriptor.getVersion();
+        }
+
+        File pluginJarFile = toFile(pluginURL);
+
+        String version = getVersionFromPluginJarManifest(pluginJarFile);
+
+        if (version == null) {
+            throw new PluginTransformException("No version is defined for plugin jar [" + pluginJarFile
+                + "]. A version must be defined either via the MANIFEST.MF [" + Attributes.Name.IMPLEMENTATION_VERSION
+                + "] attribute or via the plugin descriptor 'version' attribute.");
+        }
+
+        return version;
+    }
+
+    private String getVersionFromPluginJarManifest(File pluginJarFile) throws IOException {
+        JarFile jarFile = new JarFile(pluginJarFile);
+        Manifest manifest = jarFile.getManifest();
+        Attributes attributes = manifest.getMainAttributes();
+
+        return attributes.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+    }
+
+    private File toFile(URL url) {
+        return new File(url.getPath());
+    }
+
+}

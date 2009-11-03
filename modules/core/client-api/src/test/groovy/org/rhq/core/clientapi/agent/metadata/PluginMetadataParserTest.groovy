25178ad9771a941a40c8e7af5ea0de090767b6c5
John Sanda <john@localhost.localdomain>
2009-11-03 14:35:00 -0500

    [US 2] Adding more unit tests

--- modules/core/client-api/src/test/groovy/org/rhq/core/clientapi/agent/metadata/PluginMetadataParserTest.groovy
+++ modules/core/client-api/src/test/groovy/org/rhq/core/clientapi/agent/metadata/PluginMetadataParserTest.groovy
@@ -29,6 +29,7 @@ import javax.xml.validation.SchemaFactory
 import org.rhq.core.clientapi.descriptor.DescriptorPackages
 import org.rhq.core.clientapi.descriptor.plugin.PluginDescriptor
 import org.testng.annotations.Test
+import org.rhq.core.clientapi.descriptor.configuration.ConfigurationFormat
 
 class PluginMetadataParserTest {
 
@@ -51,8 +52,36 @@ class PluginMetadataParserTest {
 
     def parser = new PluginMetadataParser(pluginDescriptor, parsersByPlugin)
 
-    assertEquals(parser.allTypes.size(), 1, "allTypes property should have 1 element for descriptor with a single resource type.")
+    assertEquals(parser.allTypes.size(), 1, "The allTypes property should have 1 element for descriptor with a single resource type.")
     assertEquals(parser.allTypes[0].name, "testServer", "Expected to find resource type named 'testServer' in allTypes.")
+  }
+
+  @Test
+  void allTypesShouldHaveTwoElementsForDescriptorWithOneServerAndOneService() {
+    def pluginDescriptor = toPluginDescriptor(
+    """
+    <plugin name="TestServer" displayName="Test Server" package="org.rhq.plugins.test"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns="urn:xmlns:rhq-plugin">
+      <server name="testServer"
+              class="TestServer"
+              discovery="TestServerDiscoveryComponent">
+        <service name="testService"
+                 class="TestService"
+                 discovery="TestServiceDiscoveryComponent"/>
+      </server>
+    </plugin>
+    """
+    )
+
+    def parsersByPlugin = [:]
+
+    def parser = new PluginMetadataParser(pluginDescriptor, parsersByPlugin)
+
+    assertEquals(parser.allTypes.size(), 2, "The allTypes property should have 2 elements for a descriptor with a server and a child service.")
+
+    assertNotNull(parser.allTypes.find { it.name == "testServer" }, "Expected to find 'testServer' resource type.")
+    assertNotNull(parser.allTypes.find { it.name == "testService" }, "Expected to find 'testService' resource type.")
   }
 
   @Test
@@ -164,7 +193,8 @@ class PluginMetadataParserTest {
     )
   }
 
-  @Test getDiscoveryComponentClassShouldIncludePluginPackageWhenDiscoveryAttributeDoesNotSpecifyPackageName() {
+  @Test
+  void getDiscoveryComponentClassShouldIncludePluginPackageWhenDiscoveryAttributeDoesNotSpecifyPackageName() {
     def pluginDescriptor = toPluginDescriptor(
     """
     <plugin name="TestServer" displayName="Test Server" package="org.rhq.plugins.test"
@@ -190,6 +220,101 @@ class PluginMetadataParserTest {
         "org.rhq.plugins.test.TestServerDiscoveryComponent",
         "Expected the package name from the 'package' attribute of the <plugin> element to be included when the " +
             "pacage is not included in the 'discovery' attribute."
+    )
+  }
+
+  @Test
+  void childServiceResourceTypeShouldBeAddedToParentServerResourceType() {
+    def pluginDescriptor = toPluginDescriptor(
+    """
+    <plugin name="TestServer" displayName="Test Server" package="org.rhq.plugins.test"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns="urn:xmlns:rhq-plugin">
+      <server name="testServer"
+              class="TestServer"
+              discovery="TestServerDiscoveryComponent">
+        <service name="testService"
+                 class="TestService"
+                 discovery="TestServiceDiscoveryComponent"/>
+      </server>
+    </plugin>
+    """
+    )
+
+    def parsersByPlugin = [:]
+
+    def parser = new PluginMetadataParser(pluginDescriptor, parsersByPlugin)
+
+    def serverResourceType = parser.allTypes.find { it.name == "testServer" }
+
+    assertNotNull(
+        serverResourceType.childResourceTypes.find { it.name == "testService" },
+        "Expected resource type 'testService' to be added to its parent resource type 'testServer'"
+    )
+  }
+
+  @Test
+  void resourceTypeHavingResourceConfigurationDefinitionShouldBeBuiltForServerWithResourceConfiguration() {
+    def pluginDescriptor = toPluginDescriptor(
+    """
+    <plugin name="TestServer" displayName="Test Server" package="org.rhq.plugins.test"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns="urn:xmlns:rhq-plugin"
+        xmlns:c="urn:xmlns:rhq-configuration">
+      <server name="testServer"
+              class="TestServer"
+              discovery="TestServerDiscoveryComponent">
+        <resource-configuration>
+          <c:simple-property name="foo" defaultValue="bar"/>
+        </resource-configuration>
+      </server>
+    </plugin>
+    """
+    )
+
+    def parsersByPlugin = [:]
+
+    def parser = new PluginMetadataParser(pluginDescriptor, parsersByPlugin)
+
+    def serverResourceType = parser.allTypes.find { it.name == "testServer" }
+
+    assertNotNull(serverResourceType.resourceConfigurationDefinition, "Expected resource type to have a resource configuration definition.")
+    assertNotNull(
+        serverResourceType.resourceConfigurationDefinition.getPropertyDefinitionSimple("foo"),
+        "Expected resource configuration to contain a simple property named 'foo'."
+    )
+  }
+
+  @Test
+  void configurationDefinitionShouldHaveConfigurationFormatSetWhenSpecifiedInDescriptor() {
+    def pluginDescriptor = toPluginDescriptor(
+    """
+    <plugin name="TestServer" displayName="Test Server" package="org.rhq.plugins.test"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns="urn:xmlns:rhq-plugin"
+        xmlns:c="urn:xmlns:rhq-configuration">
+      <server name="testServer"
+              class="TestServer"
+              discovery="TestServerDiscoveryComponent">
+        <resource-configuration configurationFormat="structured">
+          <c:simple-property name="foo" defaultValue="bar"/>
+        </resource-configuration>
+      </server>
+    </plugin>
+    """
+    )
+
+    def parsersByPlugin = [:]
+
+    def parser = new PluginMetadataParser(pluginDescriptor, parsersByPlugin)
+
+    def serverResourceType = parser.allTypes.find { it.name == "testServer" }
+    def resourceConfigurationDefinition = serverResourceType.resourceConfigurationDefinition
+
+    assertEquals(
+        resourceConfigurationDefinition.configurationFormat,
+        ConfigurationFormat.STRUCTURED,
+        "Expected configurationFormat property to be set on resource configuration definition."
     )
   }
 

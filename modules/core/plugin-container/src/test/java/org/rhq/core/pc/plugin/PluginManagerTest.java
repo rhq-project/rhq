5ab854284fa164a62efe4056232ef5cefab3ba1a
John Sanda <john@localhost.localdomain>
2009-11-04 12:22:50 -0500

    [US 403] Initial commit for PluginManagerTest.
    This is an integration test class that right now has a single test method. The test verifies that
    PluginManager loads plugins.

---
+++ modules/core/plugin-container/src/test/java/org/rhq/core/pc/plugin/PluginManagerTest.java
@@ -0,0 +1,8 @@
+package org.rhq.core.pc.plugin;
+
+/**
+ * Created by IntelliJ IDEA. User: john Date: Nov 4, 2009 Time: 9:01:17 AM To change this template use File | Settings |
+ * File Templates.
+ */
+public class PluginManagerTest {
+}

c206d5cab0f4f91bcfc986475e5b989af5db5dd3
Sayli Karmarkar <skarmark@redhat.com>
2009-11-05 21:19:28 -0500

    552 - Adding generic tag object, taggable interface and implementation

---
+++ modules/core/domain/src/main/java/org/rhq/core/domain/common/Tag.java
@@ -0,0 +1,105 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.rhq.core.domain.common;
+
+import java.io.Serializable;
+
+/**
+ * A generic tag that can be associated with domain objects such as 
+ * Repository, Content Provider, Resource, Package etc. 
+ * @author Sayli Karmarkar
+ */
+
+//@Entity
+//@SequenceGenerator(name = "SEQ", sequenceName = "RHQ_TAG_ID_SEQ")
+//@Table(name = "RHQ_TAG")
+public class Tag implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    //@Column(name = "ID", nullable = false)
+    //@GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ")
+    //@Id
+    //private int id;
+
+    //@Column(name = "NAME", nullable = false)
+    private String name;
+
+    //@Column(name = "DESCRIPTION", nullable = true)
+    private String description;
+
+    public Tag() {
+    }
+
+    /**
+     * String representation of tag
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Description of tag
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag: [name=" + this.name + " , description=" + this.description + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if ((o == null) || (!(o instanceof Tag))) {
+            return false;
+        }
+
+        Tag other = (Tag) o;
+
+        if (!name.equals(other.name)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+}

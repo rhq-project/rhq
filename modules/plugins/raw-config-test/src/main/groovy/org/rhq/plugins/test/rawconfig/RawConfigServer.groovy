8c6e010511ab7c3bb8dc37527df2e637f67dd6bd
John Sanda <jsanda@redhat.com>
2009-11-10 13:44:37 -0500

    Increasing length of config_format column to 32.

---
+++ modules/plugins/raw-config-test/src/main/groovy/org/rhq/plugins/test/rawconfig/RawConfigServer.groovy
@@ -0,0 +1,8 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: jsanda
+ * Date: Nov 10, 2009
+ * Time: 12:11:14 PM
+ */
+package org.rhq.plugins.test.rawconfig;
+public groovyclass.groovy RawConfigServer { }

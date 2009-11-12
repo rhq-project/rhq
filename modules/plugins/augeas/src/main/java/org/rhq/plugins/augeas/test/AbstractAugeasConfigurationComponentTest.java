3b6e81de84ba5b3ec4af37fd7c1ba7afafe65d8a
Partha Aji <paji@redhat.com>
2009-11-11 20:38:51 -0500

    Added Load and Update tests for the  alias component

--- modules/plugins/augeas/src/main/java/org/rhq/plugins/augeas/test/AbstractAugeasConfigurationComponentTest.java
+++ modules/plugins/augeas/src/main/java/org/rhq/plugins/augeas/test/AbstractAugeasConfigurationComponentTest.java
@@ -28,21 +28,22 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
+import net.augeas.jna.Aug;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.rhq.core.clientapi.agent.PluginContainerException;
-import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
-import org.rhq.core.domain.configuration.definition.ConfigurationTemplate;
-import org.rhq.core.pluginapi.util.FileUtils;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import org.rhq.core.clientapi.agent.PluginContainerException;
 import org.rhq.core.clientapi.agent.configuration.ConfigurationUpdateRequest;
 import org.rhq.core.clientapi.agent.metadata.PluginMetadataManager;
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
+import org.rhq.core.domain.configuration.definition.ConfigurationTemplate;
 import org.rhq.core.domain.resource.Resource;
 import org.rhq.core.domain.resource.ResourceType;
 import org.rhq.core.pc.PluginContainer;
@@ -51,10 +52,9 @@ import org.rhq.core.pc.configuration.ConfigurationManager;
 import org.rhq.core.pc.inventory.InventoryManager;
 import org.rhq.core.pc.plugin.FileSystemPluginFinder;
 import org.rhq.core.pc.plugin.PluginManager;
+import org.rhq.core.pluginapi.util.FileUtils;
 import org.rhq.core.util.file.FileUtil;
 import org.rhq.plugins.augeas.AugeasConfigurationComponent;
-
-import net.augeas.jna.Aug;
 
 /**
  * A base class for integration tests for instances of {@link AugeasConfigurationComponent}.
@@ -96,7 +96,7 @@ public abstract class AbstractAugeasConfigurationComponentTest {
             pluginContainer.initialize();
             Set<String> pluginNames = pluginContainer.getPluginManager().getMetadataManager().getPluginNames();
             System.out.println("Plugin container started with the following plugins: " + pluginNames);
-           
+
             System.out.println("Updating Augeas root in default plugin config...");
             ResourceType resourceType = getResourceType();
             ConfigurationDefinition pluginConfigDef = resourceType.getPluginConfigurationDefinition();
@@ -104,7 +104,7 @@ public abstract class AbstractAugeasConfigurationComponentTest {
             Configuration defaultPluginConfig = defaultPluginConfigTemplate.getConfiguration();
             resetConfigFiles(defaultPluginConfig);
             PropertySimple rootPathProp = new PropertySimple(AugeasConfigurationComponent.AUGEAS_ROOT_PATH_PROP,
-                    AUGEAS_ROOT);
+                AUGEAS_ROOT);
             defaultPluginConfig.put(rootPathProp);
 
             InventoryManager inventoryManager = PluginContainer.getInstance().getInventoryManager();
@@ -128,15 +128,12 @@ public abstract class AbstractAugeasConfigurationComponentTest {
         ConfigurationManager configurationManager = PluginContainer.getInstance().getConfigurationManager();
         Resource resource = getResource();
         Configuration resourceConfig = null;
-        try
-        {
+        try {
             resourceConfig = configurationManager.loadResourceConfiguration(resource.getId());
             Configuration expectedResourceConfig = getExpectedResourceConfig();
             assert resourceConfig.equals(expectedResourceConfig) : "Unexpected Resource configuration - \nExpected:\n\t"
                 + expectedResourceConfig.toString(true) + "\nActual:\n\t" + resourceConfig.toString(true);
-        }
-        catch (PluginContainerException e)
-        {
+        } catch (PluginContainerException e) {
             if (isResourceConfigSupported()) {
                 throw e;
             }
@@ -148,8 +145,8 @@ public abstract class AbstractAugeasConfigurationComponentTest {
         ConfigurationManager configurationManager = PluginContainer.getInstance().getConfigurationManager();
         Resource resource = getResource();
         Configuration updatedResourceConfig = getUpdatedResourceConfig();
-        ConfigurationUpdateRequest updateRequest = new ConfigurationUpdateRequest(0, updatedResourceConfig,
-                resource.getId());
+        ConfigurationUpdateRequest updateRequest = new ConfigurationUpdateRequest(0, updatedResourceConfig, resource
+            .getId());
         configurationManager.updateResourceConfiguration(updateRequest);
 
         if (isResourceConfigSupported()) {
@@ -178,9 +175,7 @@ public abstract class AbstractAugeasConfigurationComponentTest {
 
     protected abstract Configuration getUpdatedResourceConfig();
 
-    protected PluginContainerConfiguration createPluginContainerConfiguration()
-            throws IOException
-    {
+    protected PluginContainerConfiguration createPluginContainerConfiguration() throws IOException {
         PluginContainerConfiguration pcConfig = new PluginContainerConfiguration();
         File pluginsDir = new File(ITEST_DIR, "plugins");
         pcConfig.setPluginFinder(new FileSystemPluginFinder(pluginsDir));
@@ -260,12 +255,9 @@ public abstract class AbstractAugeasConfigurationComponentTest {
     }
 
     private void deleteAugeasRootDir() {
-        try
-        {
+        try {
             FileUtils.purge(AUGEAS_ROOT, true);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             log.warn("Failed to delete Augeas root dir (" + AUGEAS_ROOT + ").");
         }
     }

3a07b8ddc64cd60be4eb52ab7f2db1750a207591
John Sanda <john@localhost.localdomain>
2009-11-13 08:51:10 -0500

    [US 2] Implementing merge operations in raw-config-test plugin

---
+++ modules/core/plugin-container/src/main/java/org/rhq/core/pc/configuration/ConfigurationUpdateService.java
@@ -0,0 +1,8 @@
+package org.rhq.core.pc.configuration;
+
+/**
+ * Created by IntelliJ IDEA. User: john Date: Nov 12, 2009 Time: 4:37:07 PM To change this template use File | Settings
+ * | File Templates.
+ */
+public interface ConfigurationUpdateService {
+}

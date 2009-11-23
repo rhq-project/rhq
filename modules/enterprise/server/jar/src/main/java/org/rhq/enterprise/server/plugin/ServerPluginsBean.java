263227e335728de980194c0632ae04938396536e
John Mazzitelli <mazz@redhat.com>
2009-11-23 11:36:47 -0500

    put the proper values in the query params

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/plugin/ServerPluginsBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/plugin/ServerPluginsBean.java
@@ -405,8 +405,8 @@ public class ServerPluginsBean implements ServerPluginsLocal {
             q.setParameter("version", plugin.getVersion());
             q.setParameter("ampsVersion", plugin.getAmpsVersion());
             q.setParameter("deployment", plugin.getDeployment());
-            q.setParameter("pluginConfiguration", config);
-            q.setParameter("scheduledJobsConfiguration", config);
+            q.setParameter("pluginConfiguration", plugin.getPluginConfiguration());
+            q.setParameter("scheduledJobsConfiguration", plugin.getScheduledJobsConfiguration());
             q.setParameter("description", plugin.getDescription());
             q.setParameter("help", plugin.getHelp());
             q.setParameter("mtime", plugin.getMtime());

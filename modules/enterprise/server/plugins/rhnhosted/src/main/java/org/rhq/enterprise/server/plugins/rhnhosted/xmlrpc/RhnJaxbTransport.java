21d7c4623c2f2c37f315ef0d6736a6f5f7cad594
John Matthews <jmatthew@redhat.com>
2009-12-03 13:45:09 -0500

    Cleanup and remove unused methods.

--- modules/enterprise/server/plugins/rhnhosted/src/main/java/org/rhq/enterprise/server/plugins/rhnhosted/xmlrpc/RhnJaxbTransport.java
+++ modules/enterprise/server/plugins/rhnhosted/src/main/java/org/rhq/enterprise/server/plugins/rhnhosted/xmlrpc/RhnJaxbTransport.java
@@ -1,45 +1,33 @@
 package org.rhq.enterprise.server.plugins.rhnhosted.xmlrpc;
 
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.FileWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.Reader;
-import java.io.FileInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.BufferedOutputStream;
-
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.common.XmlRpcStreamRequestConfig;
-import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.Log;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
-
-import org.jdom.input.SAXBuilder;
 import org.jdom.Document;
 import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 
 public class RhnJaxbTransport extends CustomReqPropTransport {
 
     protected String PROP_NAME_TO_SAVE_TEMP_FILES = "RhnJaxbSaveTempFiles";
     protected String jaxbDomain;
-    protected boolean dumpMessageToFile;  // used during debug to save a copy of the data received
-    protected String dumpPath;   // location to save
+    protected boolean dumpMessageToFile; // used during debug to save a copy of the data received
+    protected String dumpPath; // location to save
     private final Log log = LogFactory.getLog(RhnJaxbTransport.class);
 
     public RhnJaxbTransport(XmlRpcClient pClient) {
@@ -79,73 +67,13 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
             if (name.equalsIgnoreCase("rhn-satellite")) {
                 return true;
             }
-        }
-        catch (JDOMException e) {
+        } catch (JDOMException e) {
             log.error("isJaxbMessage()", e);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             log.error("isJaxbMessage()", e);
         }
 
         return false;
-    }
-
-    protected File cacheResponseToFile_v2(InputStream inStream) throws XmlRpcException {
-
-        //
-        // We will read the response and write it to a file.  Prior approach read all data in memory and parsed it.
-        // This created a problem when retrieving package details for large package lists of 6000+ packages.
-        // We were seeing out of memory exceptions,
-        // estimate on "rhel-i386-server-5" are 6200+ packages, metadata size written to disk in xml is 1.1GB
-        //
-        File tempFile = null;
-        Reader in = null;
-        BufferedWriter fileOut = null;
-        try {
-            if (dumpMessageToFile) {
-                tempFile = new File(getDumpFilePath());
-            }
-            else {
-                tempFile = File.createTempFile("rhn-jaxb-xmlrpc-output", ".tmp");
-            }
-            final char[] buffer = new char[0x100000];
-
-            in = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
-            fileOut = new BufferedWriter(new FileWriter(tempFile, false));
-
-            int read;
-            do {
-                //long startRead = System.currentTimeMillis();
-                read = in.read(buffer, 0, buffer.length);
-                if (read>0) {
-                    //long endRead = System.currentTimeMillis();
-                    //log.debug("Read " + read + " bytes took " + (endRead - startRead) + "ms");
-                    //long startWrite = System.currentTimeMillis();
-                    fileOut.write(buffer, 0, read);
-                    //long endWrite = System.currentTimeMillis();
-                    //log.debug("Wrote " + read + " bytes in " + (endWrite - startWrite) + "ms");
-                }
-            } while (read>=0);
-        }
-        catch (Exception e) {
-            log.warn("RhnJaxbTransport readResponse exception", e);
-            throw new XmlRpcException(e.getMessage());
-        } finally {
-            try {
-                if (fileOut != null) {
-                    fileOut.close();
-                    fileOut = null;
-                }
-                if (in != null) {
-                    in.close();
-                    in = null;
-                }
-            }
-            catch (Exception e) {
-                ; //ignore exceptions from close
-            }
-        }
-        return tempFile;
     }
 
     protected File cacheResponseToFile(InputStream inStream) throws XmlRpcException {
@@ -161,8 +89,7 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
         try {
             if (dumpMessageToFile) {
                 tempFile = new File(getDumpFilePath());
-            }
-            else {
+            } else {
                 tempFile = File.createTempFile("rhn-jaxb-xmlrpc-output", ".tmp");
             }
             outStream = new BufferedOutputStream(new FileOutputStream(tempFile, false));
@@ -175,7 +102,7 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
                 //long startRead = System.currentTimeMillis();
                 read = buffInStream.read(buffer, 0, buffer.length);
                 //read = inStream.read(buffer, 0, buffer.length);
-                if (read>0) {
+                if (read > 0) {
                     //long endRead = System.currentTimeMillis();
                     //log.debug("Read " + read + " bytes took " + (endRead - startRead) + "ms");
                     //long startWrite = System.currentTimeMillis();
@@ -183,9 +110,8 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
                     //long endWrite = System.currentTimeMillis();
                     //log.debug("Wrote " + read + " bytes in " + (endWrite - startWrite) + "ms");
                 }
-            } while (read>=0);
-        }
-        catch (Exception e) {
+            } while (read >= 0);
+        } catch (Exception e) {
             log.warn("RhnJaxbTransport readResponse exception", e);
             throw new XmlRpcException(e.getMessage());
         } finally {
@@ -194,13 +120,13 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
                     outStream.close();
                     outStream = null;
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 ; //ignore exceptions from close
             }
         }
         return tempFile;
     }
+
     protected Object readResponse(XmlRpcStreamRequestConfig pConfig, InputStream pStream) throws XmlRpcException {
 
         log.debug("readResponse invoked");
@@ -208,10 +134,10 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
         long start = System.currentTimeMillis();
         File tempFile = cacheResponseToFile(pStream);
         long end = System.currentTimeMillis();
-        float bandwidth = (float)tempFile.length() / (1024);
-        bandwidth = (bandwidth / (end-start))*1000;
-        log.info("response cached " + ((float)tempFile.length()/1024) + " KB in " + (end - start) +
-                "ms.  Estimated Bandwidth: " + bandwidth + " KB/sec");
+        float bandwidth = (float) tempFile.length() / (1024);
+        bandwidth = (bandwidth / (end - start)) * 1000;
+        log.info("response cached " + ((float) tempFile.length() / 1024) + " KB in " + (end - start)
+            + "ms.  Estimated Bandwidth: " + bandwidth + " KB/sec");
 
         /**
          * Point of this method is to not require the traditional "methodResponse" xml wrapping
@@ -229,35 +155,30 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
                 log.info("Message is not a JAXB element");
                 return super.readResponse(pConfig, dataStream);
             }
-   
+
             JAXBContext jc = JAXBContext.newInstance(jaxbDomain);
             Unmarshaller u = jc.createUnmarshaller();
             return u.unmarshal(dataStream);
-        }
-        catch (JAXBException e) {
+        } catch (JAXBException e) {
             log.error(e);
             e.printStackTrace();
             throw new XmlRpcException(e.getMessage());
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             log.error(e);
             e.printStackTrace();
             throw new XmlRpcException(e.getMessage());
-        }
-        finally {
+        } finally {
             if (doWeDeleteTempFile(tempFile)) {
                 log.info("Deleting temp file: " + tempFile.getAbsolutePath());
                 tempFile.delete();
-            }
-            else {
+            } else {
                 log.info("Temporary file of xmlrpc data is available at: " + tempFile.getAbsolutePath());
             }
             try {
                 if (dataStream != null) {
                     dataStream.close();
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 ; //ignore exception from close
             }
         }
@@ -265,7 +186,7 @@ public class RhnJaxbTransport extends CustomReqPropTransport {
 
     protected boolean doWeDeleteTempFile(File f) {
         // Check basic file access to ensure we could delete the file
-        if ( !f.exists() || f.isDirectory() || !f.canWrite()) {
+        if (!f.exists() || f.isDirectory() || !f.canWrite()) {
             return false;
         }
         // If we set dumpMessageToFile, then we obviously don't want to delete it

0cfd934d8e1a09ba72042a833922fef1cdecb932
Filip Drabek <fdrabek@dhcp-lab-136.englab.brq.redhat.com>
2009-12-07 13:43:30 +0100

    mapping returns Configuration

--- modules/plugins/apache/src/main/java/org/rhq/plugins/apache/augeas/ApacheAugeasMapping.java
+++ modules/plugins/apache/src/main/java/org/rhq/plugins/apache/augeas/ApacheAugeasMapping.java
@@ -1,12 +1,38 @@
 package org.rhq.plugins.apache.augeas;
 
+import java.util.Collection;
+
 import org.rhq.augeas.AugeasProxy;
+import org.rhq.augeas.node.AugeasNode;
+import org.rhq.augeas.tree.AugeasTree;
 import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.configuration.Property;
 import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
+import org.rhq.core.domain.configuration.definition.PropertyDefinition;
 import org.rhq.rhqtransform.RhqAugeasMapping;
 
 public class ApacheAugeasMapping implements RhqAugeasMapping {
 
+	private AugeasTree tree;
+	
+	public void updateAugeas(AugeasNode node,Configuration config,ConfigurationDefinition configDef){
+
+        Configuration resourceConfig = new Configuration();
+
+        Collection<PropertyDefinition> propDefs = configDef.getPropertyDefinitions().values();
+
+        for (PropertyDefinition propDef : propDefs) {
+        	DirectiveMappingEnum mapping = ApacheDirectiveRegExpression.getMappingType(propDef.getName());
+        	ConfigurationDefinition def = new ConfigurationDefinition("temp","");
+        	def.put(propDef);
+        	Configuration conf = mapping.execute(tree, node, def);
+        	for (Property prop : conf.getProperties())
+        	{
+        		resourceConfig.put(prop);
+        	}
+        }
+	}
+	
     public void updateAugeas(AugeasProxy augeasComponent, Configuration config, ConfigurationDefinition configDef) {
      
     }
--- modules/plugins/apache/src/main/java/org/rhq/plugins/apache/augeas/DirectiveMappingEnum.java
+++ modules/plugins/apache/src/main/java/org/rhq/plugins/apache/augeas/DirectiveMappingEnum.java
@@ -2,6 +2,7 @@ package org.rhq.plugins.apache.augeas;
 
 import org.rhq.augeas.node.AugeasNode;
 import org.rhq.augeas.tree.AugeasTree;
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
 import org.rhq.plugins.apache.augeas.mappingImpl.MappingDirectivePerMap;
 import org.rhq.plugins.apache.augeas.mappingImpl.MappingParamPerMap;
@@ -11,28 +12,28 @@ import org.rhq.rhqtransform.AugeasToConfiguration;
 public enum DirectiveMappingEnum {
 	
 	DirectivePerMap{ 
-		public void execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef){
+		public Configuration execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef){
 			AugeasToConfiguration config = new MappingDirectivePerMap();
 			config.setTree(tree);
-			config.loadResourceConfiguration(startNode, resourceConfigDef);
+			return config.loadResourceConfiguration(startNode, resourceConfigDef);
 			
 		};
 	},
 	ParamPerMap{
-		public void execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef){
+		public Configuration execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef){
 			AugeasToConfiguration config = new MappingParamPerMap();
 			config.setTree(tree);
-			config.loadResourceConfiguration(startNode, resourceConfigDef);
+			return config.loadResourceConfiguration(startNode, resourceConfigDef);
 		};
 	},
 	ParamPerMapIndex{
-		public void execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef){
+		public Configuration execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef){
 			AugeasToConfiguration config = new MappingParamPerMapIndex();
 			config.setTree(tree);
-			config.loadResourceConfiguration(startNode, resourceConfigDef);
+			return config.loadResourceConfiguration(startNode, resourceConfigDef);
 		};
 	};
 
-	public abstract void execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef);
+	public abstract Configuration execute(AugeasTree tree,AugeasNode startNode, ConfigurationDefinition resourceConfigDef);
 
 }

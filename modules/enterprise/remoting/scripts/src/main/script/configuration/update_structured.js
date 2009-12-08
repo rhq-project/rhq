f9515bc6cd7ee88de95321dcd5b457eead77e147
John Sanda <john@localhost.localdomain>
2009-12-08 10:44:05 -0500

    Rewrite of script to make it more modular and to support for scenarios.
    The update function now supports modifying multiple properties for a structured update. It
    supports updating multiple raws for a raw update, and it now also supports updating a config
    that suppoprts both structured and raw.

--- modules/enterprise/remoting/scripts/src/main/script/configuration/update_structured.js
+++ modules/enterprise/remoting/scripts/src/main/script/configuration/update_structured.js
@@ -21,179 +21,228 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-var serverName = '';
 var waitInterval = 100;
-var STRUCTURED_SERVER = 'Structured Server';
-var STRUCTURED_ANR_RAW_SERVER = 'Structured and Raw Server'
-var RAW_SERVER = 'Raw Server';
+var structured = true;
+var raw = false;
 
-if (cmdLineArgsValid()) {
-    updateConfiguration();
+loadResourceTypes();
+
+function loadResourceTypes() {
+    var resourceTypes = context.getAttribute('resourceTypes');
+    if (resourceTypes != null && !resourceTypes.isEmpty()) {
+        return;
+    }
+
+    var criteria = ResourceTypeCriteria();
+    criteria.fetchResourceConfigurationDefinition(true);
+
+    resourceTypes = ResourceTypeManager.findResourceTypesByCriteria(criteria);
+    context.setAttribute('resourceTypes', resourceTypes, javax.script.ScriptContext.ENGINE_SCOPE);
 }
 
-function cmdLineArgsValid() {
-    var isValid = true;
+function ConfigUtil(resourceName) {
+    var util = this;
 
-//    if (!scriptUtil.isDefined('server')) {
-//        println("'server' is a required argument");
-//        isValid = false;
-//    }
-//
-//    serverName = server;
-//    if (!(serverName == 'structured' || serverName == 'structuredandraw' || 'raw')) {
-//        println("Invalid value for server argument. Must be one of <structured | raw | structuredandraw>");
-//        isValid = false;
-//    }
-//
-//    if (server)
-//
-//    if (!scriptUtil.isDefined('propertyName')) {
-//        println("'propertyName' is a required argument");
-//        isValid = false;
-//    }
-//
-//    if (!scriptUtil.isDefined('propertyValue')) {
-//        println("'propertyValue' is a required argument\n");
-//        isValid = false;
-//    }
+    this.resource = findServer(resourceName);
 
-    return isValid;
+    this.getLatestConfiguration = function () {
+        var latestUpdate = ConfigurationManager.getLatestResourceConfigurationUpdate(this.resource.id);
+
+        Assert.assertNotNull(
+            latestUpdate,
+            "Failed to load the latest resource configuration update for " + this.resource.name
+        );
+        Assert.assertNotNull(
+            latestUpdate.configuration,
+            "No configuration is attached to the latest config update object."
+        );
+
+        return latestUpdate.configuration;
+    }
+
+    this.printLatestConfiguration = function() {
+        var configuration = this.getLatestConfiguration();
+        printConfiguration(configuration);
+    }
+
+    function printConfiguration(configuration)  {
+        pretty.print(configuration);
+
+        if (util.isRawSupported() || util.isStructuredAndRawSupported()) {
+            println("\nRaw configuration files:");
+            var rawConfigs = java.util.ArrayList(configuration.rawConfigurations);
+            for (i = 0; i < rawConfigs.size(); i++) {
+                var rawConfig = rawConfigs.get(i);
+                println("\n[" + rawConfig.path + "]");
+                println(java.lang.String(rawConfig.contents) + "\n\n---------------------");
+            }
+        }
+    }
+
+    this.updateConfiguration = function(update, doStructured) {
+        println("Loading latest configuration for " + this.resource.name);
+        var configuration = this.getLatestConfiguration();
+
+        printConfiguration(configuration);
+        println('');
+
+        if (this.isStructuredSupported()) {
+            applyStructuredUpdate(this.resource, configuration, update);
+        }
+        else if (this.isRawSupported()) {
+            applyRawUpdate(this.resource, configuration, update);
+        }
+        else if (this.isStructuredAndRawSupported()) {
+            if (doStructured == null) {
+                throw "\nMust specify structured or raw when updating a configuration that supports both " +
+                    "structured and raw. Update will abort.";
+            }
+
+            if (doStructured) {
+                applyStructuredUpdate(this.resource, configuration, update);
+            }
+            else {
+                applyRawUpdate(this.resource, configuration, update);
+            }
+        }
+
+        this.waitForResourceConfigurationUpdateToComplete();
+        println("Configuration update has completed.");
+
+        println("Loading latest, updated configuration");
+        this.printLatestConfiguration();
+    }
+
+    this.waitForResourceConfigurationUpdateToComplete = function() {
+        print("Waiting for configuration update to complete...")
+        while (ConfigurationManager.isResourceConfigurationUpdateInProgress(this.resource.id)) {
+            print(".");
+            java.lang.Thread.sleep(waitInterval);
+        }
+        println('');
+    }
+
+    this.isRawSupported = function() {
+        var configFormat = this.resource.resourceType.resourceConfigurationDefinition.configurationFormat;
+        return ConfigurationFormat.RAW == configFormat;
+    }
+
+    this.isStructuredSupported = function() {
+        var configFormat = this.resource.resourceType.resourceConfigurationDefinition.configurationFormat;
+        return ConfigurationFormat.STRUCTURED == configFormat;    
+    }
+
+    this.isStructuredAndRawSupported = function() {
+        var configFormat = this.resource.resourceType.resourceConfigurationDefinition.configurationFormat;
+        return ConfigurationFormat.STRUCTURED_AND_RAW == configFormat;
+    }
+
+    function verifyPropertyToUpdateExists(configuration) {
+        if (!isDefined('propertyName')) {
+            throw "\nThe 'propertyName' variable must be defined. Update will abort.";
+        }
+
+        if (!isDefined('propertyValue')) {
+            throw "\nThe 'propertyValue' variable must be defined. Update will abort.";
+        }
+
+        if(configuration.getSimple(propertyName) == null) {
+            throw "\nThe property '" + propertyName + "' is undefined. Update will abort.";
+        }
+    }
+
+    function verifyRawContentExists() {
+        if (!isDefined('contents')) {
+            throw "\n'contents' argument is required when updating " + serverName;
+        }
+    }
+
+    function applyStructuredUpdate(resource, configuration, update) {
+        println("Applying resource configuration update...");
+
+        for (key in update) {
+            var property = configuration.getSimple(key.toString());
+            if (property == null) {
+                throw "\nThe property '" + key + "' does not exist in the configuration. Update will abort.";
+            }
+            property.stringValue = update[key];
+        }
+
+        ConfigurationManager.updateStructuredOrRawConfiguration(resource.id, configuration, structured);
+    }
+
+    function applyRawUpdate(resource, configuration, update) {
+        var updates = 0;
+        var rawConfigs = java.util.ArrayList(configuration.rawConfigurations);
+        for (path in update) {
+            var rawConfig = findRawConfig(path, rawConfigs);
+            if (rawConfig == null) {
+                println("Failed to find raw config with path ending with " + path);
+            }
+            else {
+                configuration.rawConfigurations.remove(rawConfig);
+                var contents = java.lang.String(update[path]);
+                rawConfig.contents = contents.bytes;
+                configuration.rawConfigurations.add(rawConfig);
+                updates++;
+            }
+        }
+
+        if (updates == 0) {
+            throw "\nNo matching paths were found. Update will abort.";
+        }
+
+        println("Applying resource configuration update...");
+        ConfigurationManager.updateStructuredOrRawConfiguration(resource.id, configuration, raw);
+    }
+
+    function findRawConfig(path, rawConfigs) {
+        for (i = 0; i < rawConfigs.size(); i++) {
+            var rawConfig = rawConfigs.get(i);
+            if (rawConfig.path.endsWith(path.toString())) {
+                return rawConfig;
+            }
+        }
+        return null;
+    }
+
 }
 
-function updateConfiguration() {
-    setServerName();
-
-    println("\nPreapring to update configuration for " + serverName);
-    
-    var resource = findServer();
-    var configuration = getLatestConfiguration(resource);
-
-    if (isStructuredUpdate()) {
-        printConfiguration(resource, configuration);
-        verifyPropertyToUpdateExists(configuration)
-        applyStructuredUpdate(resource, configuration);
-    }
-    else {
-        printRawConfiguration(resource, configuration);
-        verifyRawContentExists();
-        applyRawUpdate(resource, configuration);
-    }
-
-    waitForResourceConfigurationUpdateToComplete(resource.id);
-
-    println("Configuration update has completed.");
-    configuration = getLatestConfiguration(resource);
-
-    if (isStructuredUpdate()) {
-        printConfiguration(resource, configuration);
-    }
-    else {
-        printRawConfiguration(resource, configuration);
-    }
-}
-
-function setServerName() {
-    if (server == 'structured') {
-        serverName = STRUCTURED_SERVER;
-    }
-    else if (server == 'raw') {
-        serverName = RAW_SERVER;
-    }
-    else if (server == 'structuredandraw') {
-        serverName = STRUCTURED_ANR_RAW_SERVER;
-    }
-    else {
-        throw "\nInvalid value for server argument. Must be one of <structured | raw | structuredandraw>";
-    }
-}
-
-function findServer() {
+function findServer(name) {
     var criteria = ResourceCriteria();
     criteria.strict = true;
-    criteria.addFilterName(serverName);
+    criteria.addFilterName(name);
+    criteria.fetchResourceType(true);
 
     var resources = ResourceManager.findResourcesByCriteria(criteria);
 
     Assert.assertTrue(resources.size() == 1, "Expected to get back one resource but got back " + resources.size());
 
-    return resources.get(0);
-}
+    var resource = resources.get(0);
+    var resourceType = findResourceType(resource);
 
-function getLatestConfiguration(resource) {
-    var latestUpdate = ConfigurationManager.getLatestResourceConfigurationUpdate(resource.id);
+    if (resourceType == null) {
+        resourceType = loadResourceType(resource);
 
-    Assert.assertNotNull(latestUpdate, "Failed to load the latest resource configuration update for " + resource.name);
-    Assert.assertNotNull(latestUpdate.configuration, "No configuration is attached to the latest config update object.");
-    
-    return latestUpdate.configuration;
-}
-
-function printConfiguration(resource, configuration) {
-    println("Fetching the latest resource configuration for " + resource.name);
-    pretty.print(configuration);
-    println('');
-}
-
-function printRawConfiguration(resource, configuration) {
-    var rawConfig = getRawConfig(configuration, 0);
-
-    println("Fetching the latest resource configuration for " + resource.name);
-    pretty.print(configuration);
-    println("The contents of " + rawConfig.path + ":\n");
-    println(java.lang.String(rawConfig.contents) + "\n--------------------------------------\n");
-}
-
-function isStructuredUpdate() {
-    return serverName == STRUCTURED_ANR_RAW_SERVER || serverName == STRUCTURED_SERVER;
-}
-
-function verifyPropertyToUpdateExists(configuration) {
-    if(configuration.getSimple(propertyName) == null) {
-        throw "\nThe property '" + propertyName + "' is undefined. Update will abort."
+        if (resourceType == null) {
+            throw "Failed to initialize resource type for " + resource.name;
+        }
     }
+    resource.resourceType = resourceType;
+
+    return resource;
 }
 
-function verifyRawContentExists() {
-    if (!isDefined('contents')) {
-        throw "\n'contents' argument is required when updating " + serverName;
+function loadResourceType(resource) {
+    return ResourceTypeManager.getResourceTypeById(resource.resourceType.id);
+}
+
+function findResourceType(resource) {
+    for (i = 0; i < resourceTypes.size(); i++) {
+        var resourceType = resourceTypes.get(i);
+        if (resourceType.id == resource.resourceType.id) {
+            return resourceType;
+        }
     }
-}
-
-function applyStructuredUpdate(resource, configuration) {
-    var property = configuration.getSimple(propertyName);
-    property.stringValue = propertyValue;
-
-    println("Apply resource configuration update...");
-
-    ConfigurationManager.updateStructuredOrRawConfiguration(resource.id, configuration, true);
-}
-
-function applyRawUpdate(resource, configuration) {
-    var rawConfig = getRawConfig(configuration, 0);
-
-    configuration.rawConfigurations.remove(rawConfig);
-
-    var contentsString = java.lang.String(eval(contents));
-    rawConfig.contents = contentsString.bytes;
-    configuration.rawConfigurations.add(rawConfig);
-
-    println("Apply resource configuration update...");
-
-    ConfigurationManager.updateStructuredOrRawConfiguration(resource.id, configuration, false);
-}
-
-function getRawConfig(configuration, index) {
-    var rawConfigs = java.util.LinkedList(configuration.rawConfigurations);
-    var rawConfig = rawConfigs.get(index);
-
-    return rawConfig;
-}
-
-function waitForResourceConfigurationUpdateToComplete(resourceId) {
-    print("Waiting for configuration update to complete...")
-    while (ConfigurationManager.isResourceConfigurationUpdateInProgress(resourceId)) {
-        print(".");
-        java.lang.Thread.sleep(waitInterval);
-    }
-    println('');
+    return null;
 }

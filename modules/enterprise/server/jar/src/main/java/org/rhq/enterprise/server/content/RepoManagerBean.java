b41888613a79519dce74f078f3b248e8cb68691c
Jason Dobies <jason.dobies@redhat.com>
2009-12-10 09:06:23 -0500

    1176 - Change call from UI beans to use the job wrapper instead of executing them synchronously.

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/content/RepoManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/content/RepoManagerBean.java
@@ -33,6 +33,7 @@ import javax.persistence.Query;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.quartz.SchedulerException;
 
 import org.jboss.annotation.IgnoreDependency;
 
@@ -950,13 +951,15 @@ public class RepoManagerBean implements RepoManagerLocal, RepoManagerRemote {
         int syncCount = 0;
 
         ContentServerPluginContainer pc = ContentManagerHelper.getPluginContainer();
-        ContentProviderManager providerManager = pc.getAdapterManager();
 
         for (Integer id : repoIds) {
-            boolean syncExecuted = providerManager.synchronizeRepo(id);
-
-            if (syncExecuted) {
+            try {
+                Repo repo = getRepo(subject, id);
+                pc.syncRepoNow(repo);
                 syncCount++;
+            }
+            catch (SchedulerException e) {
+                log.error("Error synchronizing repo with id [" + id + "]", e);
             }
         }
 

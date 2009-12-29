43e5e21d776fa203cb8be0942a0f6db0a676beb1
John Mazzitelli <mazz@redhat.com>
2009-12-29 02:14:55 -0500

    POC to get lsof network connection info as an rhq operation

--- modules/plugins/lsof/src/main/java/org/rhq/plugins/lsof/LsofComponent.java
+++ modules/plugins/lsof/src/main/java/org/rhq/plugins/lsof/LsofComponent.java
@@ -18,112 +18,72 @@
  */
 package org.rhq.plugins.lsof;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.rhq.core.domain.configuration.Configuration;
-import org.rhq.core.domain.configuration.ConfigurationUpdateStatus;
-import org.rhq.core.domain.measurement.AvailabilityType;
-import org.rhq.core.pluginapi.configuration.ConfigurationFacet;
-import org.rhq.core.pluginapi.configuration.ConfigurationUpdateReport;
-import org.rhq.core.pluginapi.inventory.ResourceComponent;
-import org.rhq.core.pluginapi.inventory.ResourceContext;
-import org.rhq.core.pluginapi.operation.OperationFacet;
+import org.rhq.core.domain.configuration.PropertyList;
+import org.rhq.core.domain.configuration.PropertyMap;
+import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.pluginapi.operation.OperationResult;
+import org.rhq.plugins.script.ScriptServerComponent;
 
 /**
  * Component that represents the lsof tool.
  * 
  * @author John Mazzitelli
  */
-public class LsofComponent implements ResourceComponent, OperationFacet, ConfigurationFacet {
+public class LsofComponent extends ScriptServerComponent {
     private final Log log = LogFactory.getLog(LsofComponent.class);
 
-    /**
-     * Represents the resource configuration of the custom product being managed.
-     */
-    private Configuration resourceConfiguration;
+    @Override
+    public OperationResult invokeOperation(String name, Configuration params) throws Exception {
+        OperationResult result;
 
-    /**
-     * All AMPS plugins are stateful - this context contains information that your resource component can use when
-     * performing its processing.
-     */
-    private ResourceContext resourceContext;
+        if ("getNetworkConnections".equals(name)) {
+            // compile the regex that will be used to parse the output
+            String regex = params.getSimpleValue("regex", "");
+            if (regex.length() == 0) {
+                throw new Exception("missing regex parameter");
+            }
+            Pattern pattern = Pattern.compile(regex);
 
-    /**
-     * This is called when your component has been started with the given context. You normally initialize some internal
-     * state of your component as well as attempt to make a stateful connection to your managed resource.
-     *
-     * @see ResourceComponent#start(ResourceContext)
-     */
-    public void start(ResourceContext context) {
-        resourceContext = context;
-    }
+            // first run the executable
+            OperationResult intermediaryResult = super.invokeOperation(name, params);
+            Configuration intermediaryConfig = intermediaryResult.getComplexResults();
 
-    /**
-     * This is called when the component is being stopped, usually due to the plugin container shutting down. You can
-     * perform some cleanup here; though normally not much needs to be done here.
-     *
-     * @see ResourceComponent#stop()
-     */
-    public void stop() {
-    }
+            // now build our results object
+            result = new OperationResult();
+            Configuration config = result.getComplexResults();
+            config.put(intermediaryConfig.getSimple(OPERATION_RESULT_EXITCODE));
+            if (intermediaryResult.getErrorMessage() != null) {
+                result.setErrorMessage(intermediaryResult.getErrorMessage());
+            } else {
+                String output = intermediaryConfig.getSimpleValue(OPERATION_RESULT_OUTPUT, "");
+                if (output.length() > 0) {
+                    PropertyList list = new PropertyList("networkConnections");
+                    config.put(list);
 
-    /**
-     * All resource components must be able to tell the plugin container if the managed resource is available or not.
-     * This method is called by the plugin container when it needs to know if the managed resource is actually up and
-     * available.
-     *
-     * @see ResourceComponent#getAvailability()
-     */
-    public AvailabilityType getAvailability() {
-        // TODO: here you normally make some type of connection attempt to the managed resource
-        //       to determine if it is really up and running.
-        return AvailabilityType.UP;
-    }
-
-    /**
-     * The plugin container will call this method when it wants to invoke an operation on your managed resource. Your
-     * plugin will connect to the managed resource and invoke the analogous operation in your own custom way.
-     *
-     * @see OperationFacet#invokeOperation(String, Configuration)
-     */
-    public OperationResult invokeOperation(String name, Configuration configuration) {
-        return null;
-    }
-
-    /**
-     * The plugin container will call this method and it needs to obtain the current configuration of the managed
-     * resource. Your plugin will obtain the managed resource's configuration in your own custom way and populate the
-     * returned Configuration object with the managed resource's configuration property values.
-     *
-     * @see ConfigurationFacet#loadResourceConfiguration()
-     */
-    public Configuration loadResourceConfiguration() {
-        // here we simulate the loading of the managed resource's configuration
-
-        if (resourceConfiguration == null) {
-            // for this example, we will create a simple dummy configuration to start with.
-            // note that it is empty, so we're assuming there are no required configs in the plugin descriptor.
-            resourceConfiguration = new Configuration();
+                    String[] lines = output.split("\n");
+                    for (String line : lines) {
+                        Matcher matcher = pattern.matcher(line);
+                        if (matcher.matches()) {
+                            PropertyMap map = new PropertyMap("networkConnection");
+                            list.add(map);
+                            map.put(new PropertySimple("host", matcher.group(1)));
+                            map.put(new PropertySimple("port", matcher.group(2)));
+                            map.put(new PropertySimple("remoteHost", matcher.group(3)));
+                            map.put(new PropertySimple("remotePort", matcher.group(4)));
+                        }
+                    }
+                }
+            }
+        } else {
+            result = super.invokeOperation(name, params);
         }
-
-        Configuration config = resourceConfiguration;
-
-        return config;
-    }
-
-    /**
-     * The plugin container will call this method when it has a new configuration for your managed resource. Your plugin
-     * will re-configure the managed resource in your own custom way, setting its configuration based on the new values
-     * of the given configuration.
-     *
-     * @see ConfigurationFacet#updateResourceConfiguration(ConfigurationUpdateReport)
-     */
-    public void updateResourceConfiguration(ConfigurationUpdateReport report) {
-        // this simulates the plugin taking the new configuration and reconfiguring the managed resource
-        resourceConfiguration = report.getConfiguration().deepCopy();
-
-        report.setStatus(ConfigurationUpdateStatus.SUCCESS);
+        return result;
     }
 }

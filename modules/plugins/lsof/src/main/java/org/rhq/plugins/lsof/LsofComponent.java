1dfd6c55d8f929bc57a7cfb6f57c95e6c103b6ee
John Mazzitelli <mazz@redhat.com>
2009-12-29 15:51:19 -0500

    build out the lsof plugin some more - add the ability to use either lsof external app or internal sigar API

--- modules/plugins/lsof/src/main/java/org/rhq/plugins/lsof/LsofComponent.java
+++ modules/plugins/lsof/src/main/java/org/rhq/plugins/lsof/LsofComponent.java
@@ -18,17 +18,20 @@
  */
 package org.rhq.plugins.lsof;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hyperic.sigar.NetConnection;
 
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertyList;
 import org.rhq.core.domain.configuration.PropertyMap;
 import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.pluginapi.operation.OperationResult;
+import org.rhq.core.util.exception.ThrowableUtil;
 import org.rhq.plugins.script.ScriptServerComponent;
 
 /**
@@ -39,46 +42,84 @@ import org.rhq.plugins.script.ScriptServerComponent;
 public class LsofComponent extends ScriptServerComponent {
     private final Log log = LogFactory.getLog(LsofComponent.class);
 
+    protected static final String VERSION = "1.0"; // our plugin version, used when this resource is told to use its internal mechanism
+    protected static final String PLUGINCONFIG_DETECTION_MECHANISM = "detectionMechanism"; // also used for operation results
+    protected static final String PLUGINCONFIG_EXECUTABLE = ScriptServerComponent.PLUGINCONFIG_EXECUTABLE;
+
     @Override
     public OperationResult invokeOperation(String name, Configuration params) throws Exception {
         OperationResult result;
 
         if ("getNetworkConnections".equals(name)) {
-            // compile the regex that will be used to parse the output
-            String regex = params.getSimpleValue("regex", "");
-            if (regex.length() == 0) {
-                throw new Exception("missing regex parameter");
-            }
-            Pattern pattern = Pattern.compile(regex);
+            Configuration pluginConfiguration = getResourcContext().getPluginConfiguration();
+            DetectionMechanism mechanism = LsofDiscoveryComponent.getDetectionMechanism(pluginConfiguration);
 
-            // first run the executable
-            OperationResult intermediaryResult = super.invokeOperation(name, params);
-            Configuration intermediaryConfig = intermediaryResult.getComplexResults();
+            if (mechanism == DetectionMechanism.EXTERNAL) {
+                log.info("Getting network connections using the external mechanism");
 
-            // now build our results object
-            result = new OperationResult();
-            Configuration config = result.getComplexResults();
-            config.put(intermediaryConfig.getSimple(OPERATION_RESULT_EXITCODE));
-            if (intermediaryResult.getErrorMessage() != null) {
-                result.setErrorMessage(intermediaryResult.getErrorMessage());
-            } else {
-                String output = intermediaryConfig.getSimpleValue(OPERATION_RESULT_OUTPUT, "");
-                if (output.length() > 0) {
-                    PropertyList list = new PropertyList("networkConnections");
-                    config.put(list);
+                // compile the regex that will be used to parse the output
+                String regex = params.getSimpleValue("regex", "");
+                if (regex.length() == 0) {
+                    throw new Exception("missing regex parameter");
+                }
+                Pattern pattern = Pattern.compile(regex);
 
-                    String[] lines = output.split("\n");
-                    for (String line : lines) {
-                        Matcher matcher = pattern.matcher(line);
-                        if (matcher.matches()) {
-                            PropertyMap map = new PropertyMap("networkConnection");
-                            list.add(map);
-                            map.put(new PropertySimple("host", matcher.group(1)));
-                            map.put(new PropertySimple("port", matcher.group(2)));
-                            map.put(new PropertySimple("remoteHost", matcher.group(3)));
-                            map.put(new PropertySimple("remotePort", matcher.group(4)));
+                // first run the executable
+                OperationResult intermediaryResult = super.invokeOperation(name, params);
+                Configuration intermediaryConfig = intermediaryResult.getComplexResults();
+
+                // now build our results object
+                result = new OperationResult();
+                Configuration config = result.getComplexResults();
+                config.put(new PropertySimple("detectionMechanism", mechanism.toString()));
+                config.put(intermediaryConfig.getSimple(OPERATION_RESULT_EXITCODE));
+                if (intermediaryResult.getErrorMessage() != null) {
+                    result.setErrorMessage(intermediaryResult.getErrorMessage());
+                } else {
+                    String output = intermediaryConfig.getSimpleValue(OPERATION_RESULT_OUTPUT, "");
+                    if (output.length() > 0) {
+                        PropertyList list = new PropertyList("networkConnections");
+                        config.put(list);
+
+                        String[] lines = output.split("\n");
+                        for (String line : lines) {
+                            Matcher matcher = pattern.matcher(line);
+                            if (matcher.matches()) {
+                                PropertyMap map = new PropertyMap("networkConnection");
+                                list.add(map);
+                                map.put(new PropertySimple("localHost", matcher.group(1)));
+                                map.put(new PropertySimple("localPort", matcher.group(2)));
+                                map.put(new PropertySimple("remoteHost", matcher.group(3)));
+                                map.put(new PropertySimple("remotePort", matcher.group(4)));
+                            }
                         }
                     }
+                }
+            } else {
+                log.info("Getting network connections using the internal mechanism");
+                result = new OperationResult();
+                Configuration config = result.getComplexResults();
+                config.put(new PropertySimple("detectionMechanism", mechanism.toString()));
+
+                try {
+                    List<NetConnection> conns;
+                    conns = getResourcContext().getSystemInformation().getNetworkConnections(null, 0);
+                    config.put(new PropertySimple(OPERATION_RESULT_EXITCODE, "0"));
+                    if (conns.size() > 0) {
+                        PropertyList list = new PropertyList("networkConnections");
+                        config.put(list);
+                        for (NetConnection conn : conns) {
+                            PropertyMap map = new PropertyMap("networkConnection");
+                            list.add(map);
+                            map.put(new PropertySimple("localHost", conn.getLocalAddress()));
+                            map.put(new PropertySimple("localPort", conn.getLocalPort()));
+                            map.put(new PropertySimple("remoteHost", conn.getRemoteAddress()));
+                            map.put(new PropertySimple("remotePort", conn.getRemotePort()));
+                        }
+                    }
+                } catch (Exception e) {
+                    config.put(new PropertySimple(OPERATION_RESULT_EXITCODE, "1"));
+                    result.setErrorMessage(ThrowableUtil.getAllMessages(e));
                 }
             }
         } else {
--- modules/plugins/lsof/src/main/java/org/rhq/plugins/lsof/LsofDiscoveryComponent.java
+++ modules/plugins/lsof/src/main/java/org/rhq/plugins/lsof/LsofDiscoveryComponent.java
@@ -18,6 +18,17 @@
  */
 package org.rhq.plugins.lsof;
 
+import java.io.File;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
+import org.rhq.core.system.SystemInfoFactory;
 import org.rhq.plugins.script.ScriptDiscoveryComponent;
 
 /**
@@ -26,4 +37,72 @@ import org.rhq.plugins.script.ScriptDiscoveryComponent;
  * @author John Mazzitelli
  */
 public class LsofDiscoveryComponent extends ScriptDiscoveryComponent {
+    private final Log log = LogFactory.getLog(LsofDiscoveryComponent.class);
+
+    private static final String DEFAULT_NAME = "Network Resource Detector";
+    private static final String DEFAULT_DESCRIPTION = "A resource that is used to detect network resources.";
+
+    @Override
+    public DiscoveredResourceDetails discoverResource(Configuration pluginConfig,
+        ResourceDiscoveryContext discoveryContext) throws InvalidPluginConfigurationException {
+
+        DiscoveredResourceDetails details;
+
+        DetectionMechanism detectionMechanism = getDetectionMechanism(pluginConfig);
+        log.debug("resource will have detection mechanism of [" + detectionMechanism + "]");
+
+        switch (detectionMechanism) {
+        case INTERNAL:
+            if (!SystemInfoFactory.isNativeSystemInfoAvailable()) {
+                throw new InvalidPluginConfigurationException(
+                    "The native system is not available - cannot use the internal detection mechanism");
+            }
+            if (SystemInfoFactory.isNativeSystemInfoDisabled()) {
+                throw new InvalidPluginConfigurationException(
+                    "The native system is disabled - cannot use the internal detection mechanism");
+            }
+            String version = LsofComponent.VERSION;
+            details = new DiscoveredResourceDetails(discoveryContext.getResourceType(), "*lsof-internal*",
+                DEFAULT_NAME, version, DEFAULT_DESCRIPTION, pluginConfig, null);
+            break;
+        case EXTERNAL:
+            // do all that we can to make sure we have a valid full path to the executable
+            PropertySimple executable = pluginConfig.getSimple(LsofComponent.PLUGINCONFIG_EXECUTABLE);
+            executable.setStringValue(findExecutable(executable.getStringValue()));
+
+            details = super.discoverResource(pluginConfig, discoveryContext);
+            details.setResourceName(DEFAULT_NAME);
+            break;
+        default:
+            throw new InvalidPluginConfigurationException("Unknown detection mechanism: " + detectionMechanism);
+        }
+
+        return details;
+    }
+
+    private String findExecutable(String executable) {
+        File findIt = new File(executable);
+        if (!findIt.isAbsolute()) {
+            // the typical locations where lsof can usually be found
+            String[] possibleLocations = { "/usr/bin", "/usr/sbin", "/bin", "/sbin", "/usr/local/bin" };
+            for (String possibleLocation : possibleLocations) {
+                findIt = new File(possibleLocation, executable);
+                if (findIt.exists()) {
+                    executable = findIt.getAbsolutePath();
+                    break;
+                }
+            }
+        }
+        return executable;
+    }
+
+    @Override
+    protected String determineDescription(ResourceDiscoveryContext context, Configuration pluginConfig) {
+        return DEFAULT_DESCRIPTION; // we don't use the discovery tool to get us a description - we hard code one
+    }
+
+    public static DetectionMechanism getDetectionMechanism(Configuration pluginConfig) {
+        String mechanism = pluginConfig.getSimpleValue(LsofComponent.PLUGINCONFIG_DETECTION_MECHANISM, "external");
+        return DetectionMechanism.valueOf(mechanism);
+    }
 }

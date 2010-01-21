a2b83b54b47d745f18dd8c749dfe3698c2f3fe7a
Jason Dobies <jason.dobies@redhat.com>
2010-01-21 15:46:04 -0500

    Initial work on the distribution manager tests. Not all of the test cases are flushed out, but this at least proves it's configured correctly to allow remote calls to be made into it.

---
+++ modules/enterprise/remoting/webservices/src/test/java/org/rhq/enterprise/server/ws/test/WsDistributionManagerTest.java
@@ -0,0 +1,84 @@
+/*
+* RHQ Management Platform
+* Copyright (C) 2009 Red Hat, Inc.
+* All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License, version 2, as
+* published by the Free Software Foundation, and/or the GNU Lesser
+* General Public License, version 2.1, also as published by the Free
+* Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License and the GNU Lesser General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License
+* and the GNU Lesser General Public License along with this program;
+* if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+package org.rhq.enterprise.server.ws.test;
+
+import org.testng.annotations.Test;
+
+import org.rhq.enterprise.server.ws.Distribution;
+import org.rhq.enterprise.server.ws.DistributionType;
+import org.rhq.enterprise.server.ws.Subject;
+
+/**
+ * Web service tests for the distribution manager service.
+ *
+ * @author Jason Dobies
+ */
+@Test(groups = "ws")
+public class WsDistributionManagerTest extends WsUnitTestBase {
+
+    private static final boolean TESTS_ENABLED = true;
+
+    @Test(enabled = TESTS_ENABLED)
+    public void createGetDeleteDistribution() throws Exception {
+
+        // Setup
+        Subject admin = subjectUtil.admin();
+        String label = "WsDistributionManagerTest.createGetDeleteDistribution.label";
+        String path = "WsDistributionManagerTest.createGetDeleteDistribution.path";
+
+        DistributionType type = objectFactory.createDistributionType();
+        type.setName("WsDistributionManagerTest.createGetDeleteDistribution.type");
+        type.setDescription("WsDistributionManagerTest.createGetDeleteDistribution.desc");
+
+        // Test
+        Distribution created = service.createDistribution(admin, label, path, type);
+
+        assert created != null;
+
+        Distribution byLabel = service.getDistributionByLabel(label);
+        Distribution byPath = service.getDistributionByPath(path);
+
+        DistributionType typeByName = service.getDistributionTypeByName(type.getName());
+
+        service.deleteDistributionByDistId(admin, created.getId());
+
+        // Verify
+        assert byLabel != null;
+        assert label.equals(byLabel.getLabel());
+        assert path.equals(byLabel.getBasePath());
+        assert type.getName().equals(byLabel.getDistributionType().getName());
+
+        assert byPath != null;
+        assert label.equals(byPath.getLabel());
+        assert path.equals(byPath.getBasePath());
+        assert type.getName().equals(byPath.getDistributionType().getName());
+
+        assert typeByName != null;
+        assert type.getName().equals(typeByName.getName());
+        assert type.getDescription().equals(typeByName.getDescription());
+
+        Distribution afterDelete = service.getDistributionByLabel(label);
+        assert afterDelete == null;
+    }
+
+}

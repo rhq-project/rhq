5fead75ead855d6ad42c740761c1a588b7e21668
Greg Hinkle <ghinkle@redhat.com>
2010-01-26 15:47:16 -0500

    first cut at gwt module for core domain

---
+++ modules/core/domain/src/main/java/org/rhq/core/client/RHQDomain.java
@@ -0,0 +1,9 @@
+package org.rhq.core.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+public class RHQDomain implements EntryPoint {
+    public void onModuleLoad() {
+    }
+}

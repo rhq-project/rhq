caa20c37e0c8bc9a71b82be73e59582988b081cc
Heiko W. Rupp <pilhuhn@fedorapeople.org>
2010-01-29 17:07:06 +0100

    Get the subject sender to basically work

--- modules/enterprise/server/plugins/alert-subject/src/main/java/org/rhq/enterprise/server/plugins/alertSubject/SubjectsBackingBean.java
+++ modules/enterprise/server/plugins/alert-subject/src/main/java/org/rhq/enterprise/server/plugins/alertSubject/SubjectsBackingBean.java
@@ -18,6 +18,7 @@
  */
 package org.rhq.enterprise.server.plugins.alertSubject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,22 +27,42 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.jboss.seam.annotations.Create;
+
 import org.rhq.core.domain.auth.Subject;
+import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.domain.util.PageControl;
 import org.rhq.core.domain.util.PageList;
 import org.rhq.enterprise.server.auth.SubjectManagerLocal;
+import org.rhq.enterprise.server.plugin.pc.alert.CustomAlertSenderBackingBean;
 import org.rhq.enterprise.server.util.LookupUtil;
 
 /**
  * Backing Bean for the subjects sender alert sender plugin custom UI
  * @author Heiko W. Rupp
  */
-public class SubjectsBackingBean  {
+public class SubjectsBackingBean extends CustomAlertSenderBackingBean {
 
     private final Log log = LogFactory.getLog(SubjectsBackingBean.class);
 
     private Map<String, String> subjectsMap;
     private List<String> currentSubjects;
+    private static final String SUBJECT_ID = "subjectId";
+
+    @Create
+    public void init() {
+        getSubjectsMap();
+        fillSubjectsFromAlertParameters();
+    }
+
+    private void fillSubjectsFromAlertParameters() {
+        String rolesString = alertParameters.getSimpleValue(SUBJECT_ID,"");
+        String[] subjects = rolesString.split(",");
+        if (currentSubjects==null)
+            currentSubjects = new ArrayList<String>();
+        for (String r : subjects)
+            currentSubjects.add(r);
+    }
 
     public List<String> getCurrentSubjects() {
         return currentSubjects;
@@ -61,7 +82,7 @@ public class SubjectsBackingBean  {
             PageList<Subject> subjectList = mgr.findAllSubjects(new PageControl());
             Map<String,String> ret = new HashMap<String, String>();
             for (Subject subject : subjectList) {
-                subjectsMap.put(subject.getName(),subject.getName());
+                subjectsMap.put(subject.getName(),String.valueOf(subject.getId()));
             }
         }
         return subjectsMap;
@@ -69,7 +90,28 @@ public class SubjectsBackingBean  {
     }
 
     public String submit() {
-        // TODO: Customise this generated block
+
+        System.out.println("Selected subjects:  ");
+        String subjects="";
+        for (String subject : currentSubjects) {
+            System.out.println(subject);
+            subjects += subject;
+            subjects += ",";
+        }
+        if (subjects.endsWith(","))
+                subjects = subjects.substring(0,subjects.length()-1);
+
+        PropertySimple p = alertParameters.getSimple(SUBJECT_ID);
+        if (p==null) {
+                p = new PropertySimple(SUBJECT_ID,subjects);
+                alertParameters.put(p);
+        }
+        else
+            p.setStringValue(subjects);
+
+        alertParameters = persistConfiguration(alertParameters);
+
+        fillSubjectsFromAlertParameters();
 
         return "ALERT_NOTIFICATIONS";
     }

ce22709b260f1c6dd6135691b46b0fcc699da07c
Bryan Kearney <bkearney@redhat.com>
2010-01-29 14:52:42 -0500

    Modify the AgentMain to use jline Consoles instead of reading from standard in. The big benefit is to be able to use command history in the agent

--- modules/enterprise/agent/src/main/java/org/rhq/enterprise/agent/AgentMain.java
+++ modules/enterprise/agent/src/main/java/org/rhq/enterprise/agent/AgentMain.java
@@ -58,6 +58,8 @@ import java.util.prefs.Preferences;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
+import jline.ConsoleReader;
+
 import mazz.i18n.Logger;
 import mazz.i18n.Msg;
 
@@ -206,7 +208,7 @@ public class AgentMain {
     /**
      * The stream where the commands are input.
      */
-    private BufferedReader m_input;
+    private ConsoleReader m_input;
 
     /**
      * Will be <code>true</code> if the input is coming directly from stdin; <code>false</code> if an input script file
@@ -461,7 +463,7 @@ public class AgentMain {
 
         m_agentHomeDirectory = null;
         m_daemonMode = false;
-        m_input = new BufferedReader(new InputStreamReader(System.in));
+        m_input = new ConsoleReader() ;
         m_output = new AgentPrintWriter(System.out, true);
         m_stdinInput = true;
         m_configuration = null;
@@ -802,7 +804,7 @@ public class AgentMain {
      *
      * @return the input stream or <code>null</code> if the agent is not currently accepting input
      */
-    public BufferedReader getIn() {
+    public ConsoleReader getIn() {
         return m_input;
     }
 
@@ -1091,14 +1093,14 @@ public class AgentMain {
             }
         } else if (!m_stdinInput) {
             // if we are processing a script, we hit the EOF, so close the input stream
-            try {
-                m_input.close();
-            } catch (IOException e1) {
-            }
+
 
             // if we are not in daemon mode, let's now start processing prompt commands coming in via stdin
             if (!m_daemonMode) {
-                m_input = new BufferedReader(new InputStreamReader(System.in));
+                try {
+                    m_input = new ConsoleReader() ;
+                } catch (IOException e1) {
+                }            
                 m_stdinInput = true;
                 input_string = "";
             } else {
@@ -2770,7 +2772,7 @@ public class AgentMain {
                 File script = new File(getopt.getOptarg());
 
                 try {
-                    m_input = new BufferedReader(new FileReader(script));
+                    m_input = new ConsoleReader(new FileInputStream(script), null);
                     m_stdinInput = false;
                 } catch (Exception e) {
                     throw new IllegalArgumentException(MSG.getMsg(AgentI18NResourceKeys.BAD_INPUT_FILE, script, e));

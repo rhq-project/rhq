7bdec1c039fbda0b202c078fe0c7be1507d95612
Heiko W. Rupp <hwr@redhat.com>
2010-02-23 10:57:34 +0100

    Display, persist and use operation parameters.

--- modules/enterprise/server/plugins/alert-operations/src/main/java/org/rhq/enterprise/server/plugins/alertOperations/OperationsBackingBean.java
+++ modules/enterprise/server/plugins/alert-operations/src/main/java/org/rhq/enterprise/server/plugins/alertOperations/OperationsBackingBean.java
@@ -25,38 +25,64 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.web.RequestParameter;
+
 import org.rhq.core.domain.auth.Subject;
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
 import org.rhq.core.domain.operation.OperationDefinition;
+import org.rhq.core.domain.resource.Resource;
+import org.rhq.enterprise.server.configuration.ConfigurationManagerLocal;
 import org.rhq.enterprise.server.operation.OperationManagerLocal;
 import org.rhq.enterprise.server.plugin.pc.alert.CustomAlertSenderBackingBean;
+import org.rhq.enterprise.server.resource.ResourceManagerLocal;
 import org.rhq.enterprise.server.util.LookupUtil;
 
 /**
  * Backing bean for the operations alert sender
  * @author Heiko W. Rupp
  */
-
+@Scope(ScopeType.PAGE)
 public class OperationsBackingBean extends CustomAlertSenderBackingBean {
 
     private final Log log = LogFactory.getLog(OperationsBackingBean.class);
 
     private String resMode;
+    private String tokenMode;
     Integer resId;
-    private String operationName;
-    private Map<String,String> operationNames = new HashMap<String,String>();
+    private Integer operationName;
+    private Map<String, Integer> operationNames = new HashMap<String, Integer>();
+    private String resourceName;
+
+
     private ConfigurationDefinition configurationDefinition;
     private Configuration configuration;
     private static final String ALERT_NOTIFICATIONS = "ALERT_NOTIFICATIONS";
+
+    public OperationsBackingBean() {
+        log.info("new " + hashCode());
+    }
+
+    @Create
+    public void init() {
+        log.info("init");
+    }
+
 
     public String selectResource() {
 
         log.info("In select Resource, resId is " + resId + " resMode is " + resMode);
 
-        if (resId != null)
+        if (resId != null) {
             persistProperty(alertParameters, OperationsSender.RESOURCE_ID,resId);
+            cleanProperty(alertParameters,OperationsSender.OPERATION_NAME);
+            cleanProperty(alertParameters,OperationsSender.USABLE);
+
+        }
 
         obtainOperationNames();
 
@@ -68,15 +94,47 @@ public class OperationsBackingBean extends CustomAlertSenderBackingBean {
     public String selectOperation() {
         log.info("In selectOperation, resId is " + resId + " opName is " + operationName);
 
-        if (operationName != null )
+        if (operationName != null ) {
             persistProperty(alertParameters, OperationsSender.OPERATION_NAME,operationName);
+            lookupConfiguration();
+        }
 
         return ALERT_NOTIFICATIONS;
     }
 
-    public String useConfiguration() {
-        log.info("In useConfiguration");
+    private void lookupConfiguration() {
 
+
+//        log.info("getCD: " + configurationDefinition);
+        try {
+//            Subject subject = EnterpriseFacesContextUtility.getSubject();
+            Subject subject = LookupUtil.getSubjectManager().getOverlord(); // TODO replace with real subject
+
+//            int operationId = Integer.valueOf(FacesContextUtility.getRequiredRequestParameter("opId"));
+            OperationManagerLocal opMan = LookupUtil.getOperationManager();
+            obtainOperationNames();
+
+
+            OperationDefinition operationDefinition = opMan.getOperationDefinition(subject, operationName);
+            configurationDefinition = operationDefinition.getParametersConfigurationDefinition();
+
+
+            // call a SLSB method to get around lazy initialization of configDefs and configTemplates
+            ConfigurationManagerLocal configurationManager = LookupUtil.getConfigurationManager();
+            configuration = configurationManager.getConfigurationFromDefaultTemplate(configurationDefinition);
+//            Configuration newConfiguration = configuration.deepCopy(false);
+log.info("gConfig: " + configuration + ", " + configuration.hashCode() + ", " + configuration.getSimpleValue("detailedDiscovery","-unset-"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String useConfiguration() {
+        log.info("In useConfiguration, Configuration is " + configuration );
+
+        // COnfiguration should be valid here ...
+        super.persistConfiguration(configuration);
+        persistProperty(alertParameters,OperationsSender.PARAMETERS_CONFIG,configuration.getId());
         persistProperty(alertParameters, OperationsSender.USABLE,true);
 
         return ALERT_NOTIFICATIONS;
@@ -94,7 +152,7 @@ public class OperationsBackingBean extends CustomAlertSenderBackingBean {
             Subject subject = LookupUtil.getSubjectManager().getOverlord(); // TODO replace with real subject
             List<OperationDefinition> opDefs = opMan.findSupportedResourceOperations(subject, resId, false);
             for (OperationDefinition def : opDefs) {
-                operationNames.put(def.getDisplayName(),def.getName()); // TODO add more distinctive stuff in display
+                operationNames.put(def.getDisplayName(),def.getId()); // TODO add more distinctive stuff in display
             }
         }
     }
@@ -107,6 +165,7 @@ public class OperationsBackingBean extends CustomAlertSenderBackingBean {
 
     public void setResMode(String resMode) {
         this.resMode = resMode;
+        log.info("setResMode: " + resMode);
     }
 
     public Integer getResId() {
@@ -122,47 +181,104 @@ public class OperationsBackingBean extends CustomAlertSenderBackingBean {
 
     public void setResId(Integer resId) {
         this.resId = resId;
+        log.info("Set resid " + resId);
+        if (resId!=null) {
+            persistProperty(alertParameters,OperationsSender.RESOURCE_ID,resId);
+        }
     }
 
-    public String getOperationName() {
+    public String getResourceName() {
+        if (resId==null)
+            getResId();
+
+        if (resId!=null) {
+            ResourceManagerLocal resMgr = LookupUtil.getResourceManager();
+            Subject subject = LookupUtil.getSubjectManager().getOverlord(); // TODO replace with real subject
+            Resource res = resMgr.getResource(subject,resId);
+
+            resourceName = res.getName() + " (" + res.getResourceType().getName() + ")";
+        }
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public Integer getOperationName() {
 
         if (operationName==null) {
             PropertySimple prop = alertParameters.getSimple(OperationsSender.OPERATION_NAME);
             if (prop!=null)
-                operationName = prop.getStringValue();
+                operationName = prop.getIntegerValue();
         }
 
         return operationName;
     }
 
-    public void setOperationName(String operationName) {
+    public void setOperationName(Integer operationName) {
         this.operationName = operationName;
     }
 
-    public Map<String,String> getOperationNames() {
+    public Map<String, Integer> getOperationNames() {
 
         obtainOperationNames();
 
         return operationNames;
     }
 
-    public void setOperationNames(Map<String,String> operationNames) {
+    public void setOperationNames(Map<String, Integer> operationNames) {
         this.operationNames = operationNames;
     }
 
     public ConfigurationDefinition getConfigurationDefinition() {
         return configurationDefinition;
+
+
+
     }
 
     public void setConfigurationDefinition(ConfigurationDefinition configurationDefinition) {
+        log.info("set CD: " + configurationDefinition);
         this.configurationDefinition = configurationDefinition;
     }
 
     public Configuration getConfiguration() {
+
         return configuration;
+
+
     }
 
     public void setConfiguration(Configuration configuration) {
         this.configuration = configuration;
+        log.info("setC: " + configuration);
     }
+
+        public String getNullConfigurationDefinitionMessage() {
+        return "This operation does not take any parameters.";
+    }
+
+    public String getNullConfigurationMessage() {
+        return "This operation parameters definition has not been initialized.";
+    }
+
+    public String getTokenMode() {
+        if (tokenMode==null) {
+            PropertySimple prop = alertParameters.getSimple(OperationsSender.TOKEN_MODE);
+            if (prop!=null)
+                tokenMode = prop.getStringValue();
+        }
+
+        return tokenMode;
+    }
+
+    public void setTokenMode(String tokenMode) {
+        this.tokenMode = tokenMode;
+        log.info("token mode" + tokenMode);
+
+        persistProperty(alertParameters, OperationsSender.TOKEN_MODE,tokenMode);
+    }
+
+
 }
--- modules/enterprise/server/plugins/alert-operations/src/main/java/org/rhq/enterprise/server/plugins/alertOperations/OperationsSender.java
+++ modules/enterprise/server/plugins/alert-operations/src/main/java/org/rhq/enterprise/server/plugins/alertOperations/OperationsSender.java
@@ -18,14 +18,24 @@
  */
 package org.rhq.enterprise.server.plugins.alertOperations;
 
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Scope;
 
 import org.rhq.core.domain.alert.Alert;
 import org.rhq.core.domain.alert.notification.ResultState;
 import org.rhq.core.domain.alert.notification.SenderResult;
 import org.rhq.core.domain.auth.Subject;
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.domain.resource.Resource;
+import org.rhq.enterprise.server.configuration.ConfigurationManagerLocal;
 import org.rhq.enterprise.server.exception.ScheduleException;
 import org.rhq.enterprise.server.operation.OperationManagerLocal;
 import org.rhq.enterprise.server.operation.ResourceOperationSchedule;
@@ -36,12 +46,17 @@ import org.rhq.enterprise.server.util.LookupUtil;
  * Alert sender that triggers an operation on the resource
  * @author Heiko W. Rupp
  */
+@Scope(value = ScopeType.PAGE)
 public class OperationsSender extends AlertSender {
 
     private final Log log = LogFactory.getLog(OperationsSender.class);
     static final String RESOURCE_ID = "resourceId";
     static final String OPERATION_NAME = "operationName";
     static final String USABLE = "usable";
+    protected static final String TOKEN_MODE = "tokenMode";
+    private static final String LITERAL = "literal";
+    private static final String INTERPRETED = "interpreted";
+    public static final String PARAMETERS_CONFIG = "parametersConfig";
 
     @Override
     public SenderResult send(Alert alert) {
@@ -52,7 +67,7 @@ public class OperationsSender extends AlertSender {
             return new SenderResult(ResultState.FAILURE, "Not enough parameters given");
 
         PropertySimple usableProp = alertParameters.getSimple(USABLE);
-        if (usableProp==null || usableProp.getBooleanValue()== null || usableProp.getBooleanValue() == false)
+        if (usableProp==null || usableProp.getBooleanValue()== null || !usableProp.getBooleanValue())
             return new SenderResult(ResultState.FAILURE,"Not yet configured");
 
         Integer resourceId = resProp.getIntegerValue();
@@ -61,17 +76,53 @@ public class OperationsSender extends AlertSender {
         OperationManagerLocal opMgr = LookupUtil.getOperationManager();
         Subject subject = LookupUtil.getSubjectManager().getOverlord(); // TODO get real subject
 
+        PropertySimple parameterConfigProp = alertParameters.getSimple(PARAMETERS_CONFIG);
+        Configuration parameters = null ;
+        if (parameterConfigProp!=null) {
+            Integer paramId = parameterConfigProp.getIntegerValue();
+            if (paramId!=null) {
+                ConfigurationManagerLocal cmgr = LookupUtil.getConfigurationManager();
+                parameters = cmgr.getConfiguration(subject,paramId);
+            }
+        }
+
+
+        String tokenMode = alertParameters.getSimpleValue(TOKEN_MODE, LITERAL);
+
+        /*
+         * If we have parameters and the user wants tokens to be interpreted, then loop
+         * over the parameters and do token replacement.
+         */
+        if (parameters!=null && tokenMode.equals(INTERPRETED)) {
+            Map<String,PropertySimple> propsMap = parameters.getSimpleProperties();
+            if (!propsMap.isEmpty()) {
+                TokenReplacer tr = new TokenReplacer(alert);
+                for (PropertySimple prop  : propsMap.values()) {
+                    String tmp = prop.getStringValue();
+                    tmp = tr.replaceTokens(tmp);
+                    prop.setStringValue(tmp);
+                }
+            }
+        }
+
+
+
+        /*
+         * Now fire off the operation with no delay and no repetition.
+         */
         ResourceOperationSchedule sched;
         try {
-            sched = opMgr.scheduleResourceOperation(subject, resourceId, opName, 0, 0, 0, 0, null,
+            sched = opMgr.scheduleResourceOperation(subject, resourceId, opName, 0, 0, 0, 0, parameters,
                     "Alert operation for " + alert.getAlertDefinition().getName());
         } catch (ScheduleException e) {
             return new SenderResult(ResultState.FAILURE, "Scheduling of operation " + opName + " on resource " + resourceId + " failed: " + e.getMessage());
         }
 
-        // TODO evaluate retuen of schedule() call and defer, so we can leter check
-        // If op sending was successfull
+        // If op sending was successful
         return new SenderResult(ResultState.SUCCESS, "Scheduled operation " + opName + " on resource " + resourceId + " with jobId " + sched.getJobId() );
 
     }
+
+
+
 }

9a012b3232e0610bcd6ce7f283f6700235fcc5d6
John Mazzitelli <mazz@redhat.com>
2010-03-22 14:04:14 -0400

    joe says this is broke - rg. should be resourcegroup. now

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/resource/group/ResourceGroupManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/resource/group/ResourceGroupManagerBean.java
@@ -967,10 +967,10 @@ public class ResourceGroupManagerBean implements ResourceGroupManagerLocal, Reso
         CriteriaQueryGenerator generator = new CriteriaQueryGenerator(criteria);
         String replacementSelectList = ""
             + " new org.rhq.core.domain.resource.group.composite.ResourceGroupComposite( "
-            + "   ( SELECT COUNT(avail) FROM rg.explicitResources res JOIN res.currentAvailability avail ) AS explicitCount,"
-            + "   ( SELECT AVG(avail.availabilityType) FROM rg.explicitResources res JOIN res.currentAvailability avail ) AS explicitAvail,"
-            + "   ( SELECT COUNT(avail) FROM rg.implicitResources res JOIN res.currentAvailability avail ) AS implicitCount,"
-            + "   ( SELECT AVG(avail.availabilityType) FROM rg.implicitResources res JOIN res.currentAvailability avail ) AS implicitAvail,"
+            + "   ( SELECT COUNT(avail) FROM resourcegroup.explicitResources res JOIN res.currentAvailability avail ) AS explicitCount,"
+            + "   ( SELECT AVG(avail.availabilityType) FROM resourcegroup.explicitResources res JOIN res.currentAvailability avail ) AS explicitAvail,"
+            + "   ( SELECT COUNT(avail) FROM resourcegroup.implicitResources res JOIN res.currentAvailability avail ) AS implicitCount,"
+            + "   ( SELECT AVG(avail.availabilityType) FROM resourcegroup.implicitResources res JOIN res.currentAvailability avail ) AS implicitAvail,"
             + "   rg ) ";
         generator.alterProjection(replacementSelectList);
 

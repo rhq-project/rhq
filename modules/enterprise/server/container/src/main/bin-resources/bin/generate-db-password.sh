73626e4146d1acf382dca898748227475755afec
Heiko W. Rupp <hwr@redhat.com>
2010-03-26 13:42:37 +0100

    Fix comments

--- modules/enterprise/server/container/src/main/bin-resources/bin/generate-db-password.sh
+++ modules/enterprise/server/container/src/main/bin-resources/bin/generate-db-password.sh
@@ -6,20 +6,12 @@
 # processname: java
 
 # =============================================================================
-# RHQ Server UNIX Startup Script
+# RHQ Server UNIX Generate db password script
 #
-# This file is used to execute the RHQ Server on a UNIX platform as part of
-# the platform's bootup sequence or as a foreground console process.
-# Run this script without any command line options for the syntax help.
+# This file is used to execute the generate a new encrypted db password.
 #
 # This script is customizable by setting the following environment variables:
 #
-# Note that if this script is to be used as an init.d script, you must set
-# RHQ_SERVER_HOME so this script knows where to find the Server installation.
-#
-#    RHQ_SERVER_DEBUG - If this is defined, the script will emit debug
-#                       messages. If this is not defined or set to "false"
-#                       debug messages are not emitted.
 #
 #    RHQ_SERVER_HOME - Defines where the server's home install directory is.
 #                      If not defined, it will be assumed to be the parent
@@ -40,44 +32,9 @@
 #                                    RHQ_SERVER_JAVA_HOME are not set, the
 #                                    Server's embedded JRE will be used.
 #
-#    RHQ_SERVER_JAVA_OPTS - Java VM command line options to be
-#                           passed into the Server's VM. If this is not defined
-#                           this script will pass in a default set of options.
-#                           If this is set, it completely overrides the
-#                           Server's defaults. If you only want to add options
-#                           to the Server's defaults, then you will want to
-#                           use RHQ_SERVER_ADDITIONAL_JAVA_OPTS instead.
-#
-#    RHQ_SERVER_ADDITIONAL_JAVA_OPTS - additional Java VM command line options
-#                                      to be passed into the Server's VM. This
-#                                      is added to RHQ_SERVER_JAVA_OPTS; it
-#                                      is mainly used to augment the Server's
-#                                      default set of options. This can be
-#                                      left unset if it is not needed.
-#
-#    RHQ_SERVER_CMDLINE_OPTS - If this is defined, these are the command line
-#                              arguments that will be passed to the RHQ Server
-#                              JBossAS run.sh. If you only want to add options
-#                              to the Server's defaults, then you will want to
-#                              use RHQ_SERVER_ADDITIONAL_CMDLINE_OPTS instead.
-#
-#    RHQ_SERVER_ADDITIONAL_CMDLINE_OPTS - additional command line arguments to
-#                                         be passed to the RHQ Server JBossAS
-#                                         run.sh. This is added to
-#                                         RHQ_SERVER_CMDLINE_OPTS; it is mainly
-#                                         used to augment the Server's default
-#                                         set of options. This can be left unset
-#                                         if it is not needed.
-#
-#    RHQ_SERVER_PIDFILE_DIR - a full path to a writable directory where this
-#                             script can write its pidfile to.
-#                             If not defined, this defaults to the Server's
-#                             bin directory.
-#
 # If the embedded JRE is to be used but is not available, the fallback
 # JRE to be used will be determined by the JAVA_HOME environment variable.
 #
-# This script calls run.sh when starting the underlying JBossAS server.
 # =============================================================================
 
 # ----------------------------------------------------------------------
@@ -88,11 +45,6 @@
 # RHQ_SERVER_DEBUG=true
 # JAVA_HOME=/path/to/java/installation
 # RHQ_SERVER_JAVA_EXE_FILE_PATH=/path/directly/to/java/executable
-# RHQ_SERVER_JAVA_OPTS=VM options
-# RHQ_SERVER_ADDITIONAL_JAVA_OPTS=additional VM options
-# RHQ_SERVER_CMDLINE_OPTS=run.sh options
-# RHQ_SERVER_ADDITIONAL_CMDLINE_OPTS=additional run.sh options
-
 # ----------------------------------------------------------------------
 # Make sure we unset any lingering JBossAS environment variables that
 # were set in the user's environment.  This might happen if the user

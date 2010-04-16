07b28294ef1811d7877153de9516dfa6252fd2e3
Simeon Pinder <spinder@redhat.com>
2010-04-16 03:47:22 -0400

    BZ-580127: modified ldap group auth i)to throw more specific exceptions ii)only do group auth check when group auth components configured iii)gui to gracefully fail when exceptions thrown.

---
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/exception/LdapFilterException.java
@@ -0,0 +1,45 @@
+package org.rhq.enterprise.server.exception;
+
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2008 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+* This exception should be used whenever there was an error with the ldap filters
+* defined.  
+* @author Simeon Pinder */
+public class LdapFilterException extends LdapCommunicationException {
+    private static final long serialVersionUID = 1L;
+
+    public LdapFilterException() {
+        super();
+    }
+
+    public LdapFilterException(String message) {
+        super(message);
+    }
+
+    public LdapFilterException(Throwable cause) {
+        super(cause);
+    }
+
+    public LdapFilterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}

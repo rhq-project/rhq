90cf7c05970831d6c4e5170e3363f7ca8a5f6ceb
Heiko W. Rupp <hrupp@mackie.(none)>
2010-04-22 09:22:21 +0200

    Plugin now uses manual add instead of autodiscovery; mk_livestatus host+port can be configured.

--- modules/plugins/NagiosMonitor/src/main/java/org/rhq/NagiosMonitor/NagiosMonitorComponent.java
+++ modules/plugins/NagiosMonitor/src/main/java/org/rhq/NagiosMonitor/NagiosMonitorComponent.java
@@ -20,35 +20,39 @@ import org.rhq.core.pluginapi.measurement.MeasurementFacet;
 public class NagiosMonitorComponent implements ResourceComponent, MeasurementFacet
 {
 	private final Log log = LogFactory.getLog(this.getClass());
-    
-    private final String NAGIOSIP = "127.0.0.1";
-    private final int NAGIOSPORT = 6557;
-        
+
+    public static final String NAGIOSIP = "127.0.0.1";
+    public static final String NAGIOSPORT = "6557";
+
     private NetworkConnection myConnection;
-    
-     /**
+   private String nagiosHost;
+   private int nagiosPort;
+
+   /**
      * Return availability of this resource
      *  @see org.rhq.core.pluginapi.inventory.ResourceComponent#getAvailability()
      */
-    public AvailabilityType getAvailability() 
+    public AvailabilityType getAvailability()
     {
-        // TODO supply real implementation
-        return AvailabilityType.UP;
+       // TODO check if the Nagios server can be reached
+       return AvailabilityType.UP;
     }
 
     /**
      * Start the resource connection
      * @see org.rhq.core.pluginapi.inventory.ResourceComponent#start(org.rhq.core.pluginapi.inventory.ResourceContext)
      */
-    public void start(ResourceContext context) throws InvalidPluginConfigurationException, Exception 
+    public void start(ResourceContext context) throws InvalidPluginConfigurationException, Exception
     {
-        @SuppressWarnings("unused")
-		Configuration conf = context.getPluginConfiguration();	
-	
-        LqlServiceRequest lqlServiceRequest = new LqlServiceRequest();
-    	myConnection = new NetworkConnection(NAGIOSIP, NAGIOSPORT, lqlServiceRequest);
-			
-		log.info("Plugin started");
+       Configuration conf = context.getPluginConfiguration();
+       nagiosHost = conf.getSimpleValue("nagiosHost",NAGIOSIP);
+       String tmp = conf.getSimpleValue("nagiosPort",NAGIOSPORT);
+       nagiosPort = Integer.parseInt(tmp);
+
+       LqlServiceRequest lqlServiceRequest = new LqlServiceRequest();
+       myConnection = new NetworkConnection(nagiosHost, nagiosPort, lqlServiceRequest);
+
+       log.info("Plugin started");
     }
 
 
@@ -56,9 +60,9 @@ public class NagiosMonitorComponent implements ResourceComponent, MeasurementFac
      * Tear down the rescource connection
      * @see org.rhq.core.pluginapi.inventory.ResourceComponent#stop()
      */
-    public void stop() 
+    public void stop()
     {
-    			
+
     }
 
     /**
@@ -68,54 +72,54 @@ public class NagiosMonitorComponent implements ResourceComponent, MeasurementFac
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics)
     {
     	log.info("getValues() called at " + System.currentTimeMillis());
-    		
-		NetworkConnection networkConnection = new NetworkConnection(NAGIOSIP, NAGIOSPORT, new LqlServiceRequest());
+
+		NetworkConnection networkConnection = new NetworkConnection(nagiosHost, nagiosPort, new LqlServiceRequest());
 		networkConnection.sendAndReceive();
-		
+
 		Controller controller = new Controller(networkConnection.getLqlReply());
-		
+
 		NagiosData data = controller.createDataModel();
-	
+
 		try
 		{
 			log.info("execution_time: " + data.getSingleMetricForRessource("execution_time", "Current Load").getValue());
 			log.info("host_check_period: " + data.getSingleMetricForRessource("host_check_period", "Current Load").getValue());
 			log.info("host_execution_time: " + data.getSingleMetricForRessource("host_execution_time", "Current Load").getValue());
-			
-			for (MeasurementScheduleRequest req : metrics) 
-	         {    
+
+			for (MeasurementScheduleRequest req : metrics)
+	         {
 				//TODO switch instead of if-else?
-				if("execution_time".equals(req.getName()) ) 
-				{      	        		
+				if("execution_time".equals(req.getName()) )
+				{
 		    		String value = data.getSingleMetricForRessource("execution_time", "Current Load").getValue();
-					
+
 					MeasurementDataNumeric res = new MeasurementDataNumeric(req, Double.valueOf(value));
 	    			report.addData(res);
 				}
-				else if("host_check_period".equals(req.getName()) ) 
-				{   
+				else if("host_check_period".equals(req.getName()) )
+				{
 					String value = data.getSingleMetricForRessource("host_check_period", "Current Load").getValue();
 					MeasurementDataTrait res = new MeasurementDataTrait(req, value);
 					report.addData(res);
 				}
-				else if("host_execution_time".equals(req.getName()) ) 
-				{   
+				else if("host_execution_time".equals(req.getName()) )
+				{
 					String value = data.getSingleMetricForRessource("host_execution_time", "Current Load").getValue();
 					MeasurementDataNumeric res = new MeasurementDataNumeric(req, Double.valueOf(value));
 					report.addData(res);
-					
+
 				}
 	     	}
 		}
 		catch(InvalidMetricRequestException e)
 		{
 			log.error(e);
-		} 
+		}
 		catch (InvalidServiceRequestException e)
 		{
 			log.error(e);
 		}
-		
-		
+
+
 	}
 }
--- modules/plugins/NagiosMonitor/src/main/java/org/rhq/NagiosMonitor/NagiosMonitorDiscovery.java
+++ modules/plugins/NagiosMonitor/src/main/java/org/rhq/NagiosMonitor/NagiosMonitorDiscovery.java
@@ -1,10 +1,15 @@
 package org.rhq.NagiosMonitor;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.inventory.ManualAddFacet;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
 
@@ -12,31 +17,49 @@ import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
 /**
  * Discovery class
  */
-public class NagiosMonitorDiscovery implements ResourceDiscoveryComponent
+public class NagiosMonitorDiscovery implements ResourceDiscoveryComponent, ManualAddFacet
 {
     private final Log log = LogFactory.getLog(this.getClass());
 
+   /**
+    * Support manually adding this resource type via Platform's inventory tab
+    * @param configuration
+    * @param resourceDiscoveryContext
+    * @return
+    * @throws org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException
+    */
+   public DiscoveredResourceDetails discoverResource(Configuration configuration,
+                                                     ResourceDiscoveryContext resourceDiscoveryContext) throws InvalidPluginConfigurationException {
+
+      String nagiosHost = configuration.getSimpleValue("nagiosHost",NagiosMonitorComponent.NAGIOSIP);
+      String nagiosPort = configuration.getSimpleValue("nagiosPort",NagiosMonitorComponent.NAGIOSPORT);
+
+
+      DiscoveredResourceDetails detail = new DiscoveredResourceDetails(
+
+         resourceDiscoveryContext.getResourceType(),
+          "nagios@"+nagiosHost+":"+nagiosPort,
+          "Nagios@"+nagiosHost+":"+nagiosPort,
+          null,
+          "Nagios server @ " + nagiosHost + ":" + nagiosPort,
+          configuration,
+          null
+      );
+
+      return detail;
+   }
+
+
+
     /**
-     * Run the auto-discovery
+     * Don't run the auto-discovery of this "NagiosMonitor" server type,
+     * as we probably won't have one on each platform. Rather have the admin
+     * explicitly add it to one platform.
      */
     public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext discoveryContext) throws Exception {
     	Set<DiscoveredResourceDetails> discoveredResources = new HashSet<DiscoveredResourceDetails>();
 
-        DiscoveredResourceDetails detail = new DiscoveredResourceDetails(
-            
-        	discoveryContext.getResourceType(),
-            "nagiosMonitorPluginKey",
-            "nagiosMonitorPluginName",
-            null,
-            "This plugin communicates with Nagios and fills some defined metrics",
-            null,
-            null
-        );
-
-        // Add to return values
-        discoveredResources.add(detail);
-        log.info("Discovered new NagiosMonitor Ressource");
-
-        return discoveredResources;
+       return discoveredResources;
     }
+
 }

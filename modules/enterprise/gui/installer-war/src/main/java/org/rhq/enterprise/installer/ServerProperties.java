8df8d763d2f2a2f22a92659b75ab1d2c105eba45
John Sanda <jsanda@redhat.com>
2010-05-20 11:00:22 -0400

    [BZ 581818] Adding option to select oracle 11g as database type

--- modules/enterprise/gui/installer-war/src/main/java/org/rhq/enterprise/installer/ServerProperties.java
+++ modules/enterprise/gui/installer-war/src/main/java/org/rhq/enterprise/installer/ServerProperties.java
@@ -152,6 +152,7 @@ public class ServerProperties {
         DATABASE_TYPES = new ArrayList<SelectItem>();
         DATABASE_TYPES.add(new SelectItem("PostgreSQL", "PostgreSQL"));
         DATABASE_TYPES.add(new SelectItem("Oracle10g", "Oracle 10g"));
+        DATABASE_TYPES.add(new SelectItem("Oracle11g", "Oracle 11g"));
         DATABASE_TYPES.add(new SelectItem("sqlserver", "MS SQL Server"));
         DATABASE_TYPES.add(new SelectItem("H2", "H2 (embedded)"));
         //DATABASE_TYPES.add(new SelectItem("MySQL", "MySQL"));

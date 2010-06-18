3598a98701f39ff3d0772c1327375b039dba4216
Greg Hinkle <ghinkle@redhat.com>
2010-06-18 11:30:17 -0400

    New Dashboard persistence implementation

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/dashboard/DashboardsView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/dashboard/DashboardsView.java
@@ -1,51 +1,83 @@
 /*
  * RHQ Management Platform
- * Copyright (C) 2005-2010 Red Hat, Inc.
+ * Copyright (C) 2005-2009 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.rhq.enterprise.gui.coregui.client.dashboard;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.types.Overflow;
+import com.smartgwt.client.util.BooleanCallback;
+import com.smartgwt.client.util.SC;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 import com.smartgwt.client.widgets.tab.Tab;
 import com.smartgwt.client.widgets.tab.TabSet;
+import com.smartgwt.client.widgets.tab.events.CloseClickHandler;
+import com.smartgwt.client.widgets.tab.events.TabCloseClickEvent;
+import com.smartgwt.client.widgets.tab.events.TabSelectedEvent;
+import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
 
-import org.rhq.core.domain.resource.InventorySummary;
+import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.domain.dashboard.Dashboard;
+import org.rhq.core.domain.dashboard.DashboardPortlet;
+import org.rhq.enterprise.gui.coregui.client.BookmarkableView;
+import org.rhq.enterprise.gui.coregui.client.CoreGUI;
+import org.rhq.enterprise.gui.coregui.client.ViewPath;
 import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.inventory.queue.AutodiscoveryPortlet;
 import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.inventory.resource.graph.GraphPortlet;
-import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.platform.PlatformPortletView;
 import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.recent.alerts.RecentAlertsPortlet;
 import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.recent.imported.RecentlyAddedView;
 import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.summary.InventorySummaryView;
 import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.summary.TagCloudPortlet;
-import org.rhq.enterprise.gui.coregui.client.dashboard.store.DashboardStore;
-import org.rhq.enterprise.gui.coregui.client.dashboard.store.StoredDashboard;
-import org.rhq.enterprise.gui.coregui.client.dashboard.store.StoredPortlet;
+import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.util.MashupPortlet;
+import org.rhq.enterprise.gui.coregui.client.dashboard.portlets.util.MessagePortlet;
+import org.rhq.enterprise.gui.coregui.client.gwt.DashboardGWTServiceAsync;
+import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
 
 /**
  * @author Greg Hinkle
  */
-public class DashboardsView extends VLayout {
+public class DashboardsView extends VLayout implements BookmarkableView {
 
     private TabSet tabSet;
 
-    private DashboardStore dashboardStore;
+    private IButton editButton;
 
+    private boolean editMode = false;
+
+    private List<Dashboard> dashboards;
+
+    private DashboardView selectedDashboardView;
+    private Dashboard selectedDashboard;
+
+    private DashboardGWTServiceAsync dashboardService = GWTServiceLookup.getDashboardService();
+
+
+    private String selectedTab;
 
     public DashboardsView() {
         setOverflow(Overflow.AUTO);
@@ -59,80 +91,216 @@ public class DashboardsView extends VLayout {
     protected void onInit() {
         super.onInit();
 
+        dashboardService.findDashboardsForSubject(new AsyncCallback<List<Dashboard>>() {
+            public void onFailure(Throwable caught) {
+                CoreGUI.getErrorHandler().handleError("Failed to load user dashboards", caught);
+            }
+
+            public void onSuccess(List<Dashboard> result) {
+                if (result.isEmpty()) {
+                    result.add(getDefaultDashboard());
+                }
+                updateDashboards(result);
+            }
+        });
+    }
+
+    private void updateDashboards(List<Dashboard> dashboards) {
+        removeMembers(getMembers());
+        this.dashboards = dashboards;
+
+
         tabSet = new TabSet();
+
         tabSet.setWidth100();
         tabSet.setHeight100();
 
-        dashboardStore = new DashboardStore();
-
-        dashboardStore.getStoredDashboards().add(getDefaultDashboard());
+        tabSet.setCanCloseTabs(true);
 
 
-        for (StoredDashboard dashboard : dashboardStore.getStoredDashboards()) {
-            DashboardView dashboardView = new DashboardView(dashboard);
+        editButton = new IButton(editMode ? "View Mode" : "Edit Mode");
+        editButton.setAutoFit(true);
+        editButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent clickEvent) {
+                editMode = !editMode;
+                editButton.setTitle(editMode ? "View Mode" : "Edit Mode");
+                selectedDashboardView.setEditMode(editMode);
+            }
+        });
+
+
+        final IButton newDashboardButton = new IButton("New Dashboard");
+        newDashboardButton.setAutoFit(true);
+        newDashboardButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent clickEvent) {
+                addNewDashboard();
+            }
+        });
+
+        HLayout buttons = new HLayout(5);
+        buttons.addMember(editButton);
+                buttons.addMember(newDashboardButton);
+
+        tabSet.setTabBarControls(buttons);
+
+
+        tabSet.addTabSelectedHandler(new TabSelectedHandler() {
+            public void onTabSelected(TabSelectedEvent tabSelectedEvent) {
+                History.newItem("Dashboard/" + tabSelectedEvent.getTab().getTitle(), false);
+                selectedDashboardView = (DashboardView) tabSelectedEvent.getTab().getPane();
+                selectedDashboard = selectedDashboardView.getDashboard();
+                selectedDashboardView.setEditMode(editMode);                
+            }
+        });
+
+        for (Dashboard dashboard : dashboards) {
+            DashboardView dashboardView = new DashboardView(this, dashboard);
 
             Tab tab = new Tab(dashboard.getName());
             tab.setPane(dashboardView);
+            tab.setCanClose(true);
+            
 
             tabSet.addTab(tab);
+            if (dashboard.getName().equals(selectedTab)) {
+                tabSet.selectTab(tab);
+            }
         }
 
+        tabSet.addCloseClickHandler(new CloseClickHandler() {
+            public void onCloseClick(final TabCloseClickEvent tabCloseClickEvent) {
+                final DashboardView dashboardView = (DashboardView) tabCloseClickEvent.getTab().getPane();
+                SC.ask("Are you sure you want to delete [" + tabCloseClickEvent.getTab().getTitle() + "]?", new BooleanCallback() {
+                    public void execute(Boolean aBoolean) {
+                        if (aBoolean) {
+                            dashboardView.delete();
+                        } else {
+                            tabCloseClickEvent.cancel();
+                        }
+                    }
+                });
+            }
+        });
+
         addMember(tabSet);
+
     }
 
 
-    protected StoredDashboard getDefaultDashboard() {
+    protected Dashboard getDefaultDashboard() {
 
-        StoredDashboard dashboard = new StoredDashboard();
+        Dashboard dashboard = new Dashboard();
         dashboard.setName("Default Dashboard");
         dashboard.setColumns(2);
-        dashboard.setColumnWidths("30%","70%");
+        dashboard.setColumnWidths("30%", "70%");
+        dashboard.getConfiguration().put(new PropertySimple(Dashboard.CFG_BACKGROUND,"#F1F2F3"));
 
 
-        ArrayList<StoredPortlet> col1 = new ArrayList<StoredPortlet>();
-        ArrayList<StoredPortlet> col2 = new ArrayList<StoredPortlet>();
+        DashboardPortlet summary = new DashboardPortlet("Inventory Summary", InventorySummaryView.KEY, 230);
+        dashboard.addPortlet(summary, 0, 0);
 
-
-
-
-        StoredPortlet summary = new StoredPortlet("Inventory Summary", InventorySummaryView.KEY, 250);
-        col1.add(summary);
-
-        StoredPortlet tagCloud = new StoredPortlet("Tag Cloud", TagCloudPortlet.KEY, 250);
-        col1.add(tagCloud);
-
-        StoredPortlet graph = new StoredPortlet("ghinkle (MAC OS X) CPU", GraphPortlet.KEY, 250);
-        graph.setProperty("resourceId","10001");
-        graph.setProperty("definitionId","10100");
-        col1.add(graph);
-
-        StoredPortlet graphTwo = new StoredPortlet("JBoss AS Txn / Min", GraphPortlet.KEY, 250);
-        graphTwo.setProperty("resourceId","10003");
-        graphTwo.setProperty("definitionId","10916");
-        col1.add(graphTwo);
-
-
+        DashboardPortlet tagCloud = new DashboardPortlet("Tag Cloud", TagCloudPortlet.KEY, 200);
+        dashboard.addPortlet(tagCloud, 0, 1);
 
         // Experimental
 //        StoredPortlet platformSummary = new StoredPortlet("Platform Summary", PlatformPortletView.KEY, 300);
 //        col2.add(platformSummary);
+
+
+        DashboardPortlet welcome = new DashboardPortlet("Welcome To RHQ", MessagePortlet.KEY, 180);
+        welcome.getConfiguration().put(new PropertySimple("message","<h1>Welcome to RHQ</h1>\n" +
+                "<p>The RHQ project is an abstraction and plug-in based systems management suite that provides " +
+                "extensible and integrated systems management for multiple products and platforms across a set " +
+                "of core features. The project is designed with layered modules that provide a flexible " +
+                "architecture for deployment. It delivers a core user interface that delivers audited and " +
+                "historical management across an entire enterprise. A Server/Agent architecture provides " +
+                "remote management and plugins implement all specific support for managed products.</p>\n" +
+                "<p>This default dashboard can be edited by clicking the \"edit mode\" button above.</p>"));
+        dashboard.addPortlet(welcome, 1, 0);
+
+        DashboardPortlet news = new DashboardPortlet("RHQ News", MashupPortlet.KEY, 320);
+        news.getConfiguration().put(new PropertySimple("address", "http://rhq-project.org/display/RHQ/RHQ+News?decorator=popup"));
+        dashboard.addPortlet(news, 1, 1);
 //
-        StoredPortlet discoveryQueue = new StoredPortlet("Discovery Queue", AutodiscoveryPortlet.KEY, 250);
-        col2.add(discoveryQueue);
-
-        StoredPortlet recentAlerts = new StoredPortlet("Recent Alerts", RecentAlertsPortlet.KEY, 250);
-        col2.add(recentAlerts);
-
-        StoredPortlet recentlyAdded = new StoredPortlet("Recently Added Resources", RecentlyAddedView.KEY, 250);
-        col2.add(recentlyAdded);
+        DashboardPortlet discoveryQueue = new DashboardPortlet("Discovery Queue", AutodiscoveryPortlet.KEY, 250);
+        dashboard.addPortlet(discoveryQueue, 1, 2);
 
 
-        dashboard.getPortlets().add(col1);
-        dashboard.getPortlets().add(col2);
+        DashboardPortlet recentAlerts = new DashboardPortlet("Recent Alerts", RecentAlertsPortlet.KEY, 250);
+        dashboard.addPortlet(recentAlerts, 1, 3);
 
-
+        DashboardPortlet recentlyAdded = new DashboardPortlet("Recently Added Resources", RecentlyAddedView.KEY, 250);
+        dashboard.addPortlet(recentlyAdded, 1, 4);
 
         return dashboard;
 
+    }
+
+
+    public void addNewDashboard() {
+
+        Dashboard dashboard = new Dashboard();
+        dashboard.setName("Dashboard");
+
+        int i = 1;
+        while (true) {
+            boolean exists = false;
+            for (Dashboard db : dashboards) {
+                if (("Dashboard " + i).equals(db.getName()))
+                    exists = true;
+            }
+            if (!exists) {
+                break;
+            }
+        }
+        dashboard.setName("Dashboard " + i);
+
+        dashboard.setColumns(2);
+        dashboard.setColumnWidths("30%", "70%");
+
+        dashboardService.storeDashboard(dashboard, new AsyncCallback<Dashboard>() {
+            public void onFailure(Throwable caught) {
+                CoreGUI.getErrorHandler().handleError("Failed to add new dashboard", caught);
+            }
+
+            public void onSuccess(Dashboard result) {
+                DashboardView dashboardView = new DashboardView(DashboardsView.this, result);
+
+                Tab tab = new Tab(result.getName());
+                tab.setPane(dashboardView);
+                tab.setCanClose(true);
+
+                tabSet.addTab(tab);
+
+                tabSet.selectTab(tab);
+                editMode = true;
+                editButton.setTitle(editMode ? "View Mode" : "Edit Mode");
+                dashboardView.setEditMode(editMode);
+
+
+            }
+        });
+    }
+
+    public void updateNames() {
+        for (Tab t : tabSet.getTabs()) {
+            DashboardView view = (DashboardView) t.getPane();
+            t.setTitle(view.getDashboard().getName());
+        }
+    }
+
+
+
+    public void renderView(ViewPath viewPath) {
+        if (!viewPath.isEnd()) {
+            selectedTab = viewPath.getCurrent().getPath();
+            if (tabSet != null) {
+                tabSet.selectTab(selectedTab);
+            }
+        }
+    }
+
+    public Dashboard getDashboard() {
+        return selectedDashboard;
     }
 }

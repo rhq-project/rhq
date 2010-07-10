bebf8612034b07ef1983bcb01fab27ba01f1e792
Joseph Marques <joseph@redhat.com>
2010-07-10 01:23:20 -0400

    show result counts for saved searches in the suggestions pop-up

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/search/execution/SearchAssistManager.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/search/execution/SearchAssistManager.java
@@ -580,6 +580,9 @@ public class SearchAssistManager {
         List<SearchSuggestion> results = new ArrayList<SearchSuggestion>();
         for (SavedSearch next : savedSearchResults) {
             String label = next.getName();
+            if (next.getResultCount() != null) {
+                label += " (" + next.getResultCount() + ")";
+            }
             String value = next.getPattern();
             int index = next.getName().toLowerCase().indexOf(expression);
             SearchSuggestion suggestion = new SearchSuggestion(Kind.UserSavedSearch, label, value, index, expression

acffdbd990b59dd1a87d602a645d18481927f1b8
John Mazzitelli <mazz@redhat.com>
2010-07-15 16:20:57 -0400

    redirect error stream of readlink to dev/null when starting standalone pc

--- etc/standalone-pc/standalone.sh
+++ etc/standalone-pc/standalone.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-_DOLLARZERO=`readlink "$0" || echo "$0"`
+_DOLLARZERO=`readlink "$0" 2>/dev/null || echo "$0"`
 RHQ_AGENT_BIN_DIR_PATH=`dirname "$_DOLLARZERO"`
 RHQ_AGENT_MAINCLASS=org.rhq.core.pc.StandaloneContainer
 export RHQ_AGENT_MAINCLASS

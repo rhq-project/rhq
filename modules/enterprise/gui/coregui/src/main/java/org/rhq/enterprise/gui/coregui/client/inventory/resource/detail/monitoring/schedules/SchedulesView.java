98b4e155771fe69e5aa325d938f554c10aa8bd16
Jay Shaughnessy <jshaughn@redhat.com>
2010-08-18 10:08:35 -0400

    Selenium Work - Avoid having an Abstract class re-use the same name/title for all of its subclasses.  This causes ID conflicts. Let the subclasses define unique names/titles.

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/inventory/resource/detail/monitoring/schedules/SchedulesView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/inventory/resource/detail/monitoring/schedules/SchedulesView.java
@@ -19,6 +19,7 @@
 package org.rhq.enterprise.gui.coregui.client.inventory.resource.detail.monitoring.schedules;
 
 import com.smartgwt.client.data.Criteria;
+
 import org.rhq.core.domain.criteria.MeasurementScheduleCriteria;
 import org.rhq.enterprise.gui.coregui.client.inventory.common.AbstractMeasurementScheduleListView;
 
@@ -28,10 +29,13 @@ import org.rhq.enterprise.gui.coregui.client.inventory.common.AbstractMeasuremen
  * @author Ian Springer
  */
 public class SchedulesView extends AbstractMeasurementScheduleListView {
+
+    private static final String TITLE = "Metric Collection Schedules";
+
     private static final String[] EXCLUDED_FIELD_NAMES = new String[] { MeasurementScheduleCriteria.FILTER_FIELD_RESOURCE_ID };
 
     public SchedulesView(int resourceId) {
-        super(new SchedulesDataSource(resourceId), createCriteria(resourceId), EXCLUDED_FIELD_NAMES);
+        super(TITLE, new SchedulesDataSource(resourceId), createCriteria(resourceId), EXCLUDED_FIELD_NAMES);
     }
 
     private static Criteria createCriteria(int resourceId) {

cb6fd0c410135b62995de1a34692f2759ea63ddb
John Sanda <jsanda@redhat.com>
2010-08-23 07:17:21 -0400

    Adding support for specifying lib directories to be added to script classpath
    Every jar file found in a lib directory is added to the script's classpath.

--- modules/enterprise/server/plugins/groovy-script/src/main/groovy/org/rhq/enterprise/server/plugins/groovy/ClasspathInitializer.groovy
+++ modules/enterprise/server/plugins/groovy-script/src/main/groovy/org/rhq/enterprise/server/plugins/groovy/ClasspathInitializer.groovy
@@ -6,5 +6,12 @@ class ClasspathInitializer {
 
   def initClasspath(String paths, String libDirs, scriptClassLoader) {
     paths.eachLine { scriptClassLoader.addClasspath(it.trim()) }
+
+    libDirs.eachLine { path ->
+      def dir = new File(path.trim())
+      dir.eachFileMatch( ~/.*\.jar/) { jarFile ->
+        scriptClassLoader.addClasspath(jarFile.absolutePath)  
+      }
+    }
   }
 }

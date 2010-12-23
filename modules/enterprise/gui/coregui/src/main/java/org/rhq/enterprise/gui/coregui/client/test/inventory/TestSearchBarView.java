59567145c4c931d29961bf25de76b2dc4d960390
Joseph Marques <joseph@redhat.com>
2010-12-23 00:42:11 -0500

    remove all filters except the SearchBar for the TestSearchBarView

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/test/inventory/TestSearchBarView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/test/inventory/TestSearchBarView.java
@@ -1,13 +1,6 @@
 package org.rhq.enterprise.gui.coregui.client.test.inventory;
 
-import static org.rhq.enterprise.gui.coregui.client.inventory.resource.ResourceDataSourceField.CATEGORY;
-import static org.rhq.enterprise.gui.coregui.client.inventory.resource.ResourceDataSourceField.NAME;
-
-import com.smartgwt.client.widgets.form.fields.TextItem;
-
-import org.rhq.core.domain.resource.ResourceCategory;
 import org.rhq.core.domain.search.SearchSubsystem;
-import org.rhq.enterprise.gui.coregui.client.components.form.EnumSelectItem;
 import org.rhq.enterprise.gui.coregui.client.components.form.SearchBarItem;
 import org.rhq.enterprise.gui.coregui.client.inventory.resource.ResourceSearchView;
 
@@ -18,11 +11,7 @@ public class TestSearchBarView extends ResourceSearchView {
 
     @Override
     protected void configureTableFilters() {
-        final TextItem nameFilter = new TextItem(NAME.propertyName(), NAME.title());
-        final EnumSelectItem categoryFilter = new EnumSelectItem(CATEGORY.propertyName(), CATEGORY.title(),
-            ResourceCategory.class);
         final SearchBarItem searchFilter = new SearchBarItem("search", "Search", SearchSubsystem.RESOURCE);
-
-        setFilterFormItems(nameFilter, categoryFilter, searchFilter);
+        setFilterFormItems(searchFilter);
     }
 }

28827776f86388ba1068064d4f4c795f0f35c3af
John Mazzitelli <mazz@redhat.com>
2011-01-04 17:04:09 -0500

    javadoc

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/common/PerformanceMonitorInterceptor.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/common/PerformanceMonitorInterceptor.java
@@ -5,6 +5,16 @@ import javax.interceptor.InvocationContext;
 
 import org.rhq.enterprise.server.util.HibernatePerformanceMonitor;
 
+/**
+ * To enable this interceptor, add a binding to the ejb-jar.xml:
+ * 
+ * &lt;!-- enable this to get lots of performance data as you navigate around the UI
+ *     &lt;interceptor-binding>
+ *       &lt;ejb-name>*&lt;/ejb-name>
+ *        &lt;interceptor-class>org.rhq.enterprise.server.common.PerformanceMonitorInterceptor&lt;/interceptor-class>
+ *     &lt;/interceptor-binding>
+ * -->
+ */
 public class PerformanceMonitorInterceptor {
     @AroundInvoke
     public Object monitorHibernatePerformance(InvocationContext context) throws Exception {

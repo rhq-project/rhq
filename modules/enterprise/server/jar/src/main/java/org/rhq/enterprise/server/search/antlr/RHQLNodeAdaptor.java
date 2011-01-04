2e5932bc3d91e6720e78715631835b5a2f8762f8
Joseph Marques <joseph@redhat.com>
2011-01-04 10:50:32 -0500

    add copyright and author tags to search.antlr sub-package

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/search/antlr/RHQLNodeAdaptor.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/search/antlr/RHQLNodeAdaptor.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2010 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.enterprise.server.search.antlr;
 
 import java.util.ArrayList;
@@ -9,6 +27,9 @@ import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.CommonTreeAdaptor;
 
+/**
+ * @author Joseph Marques
+ */
 public class RHQLNodeAdaptor extends CommonTreeAdaptor {
     List<String> errorMessages = new ArrayList<String>();
 

819c1683ca85e90512b5583d6c7522f9f8fe6f9a
John Mazzitelli <mazz@redhat.com>
2011-02-11 15:39:23 -0500

    fix test to use the new api

--- modules/enterprise/remoting/webservices/src/test/java/org/rhq/enterprise/server/ws/WsSystemManagerTest.java
+++ modules/enterprise/remoting/webservices/src/test/java/org/rhq/enterprise/server/ws/WsSystemManagerTest.java
@@ -31,37 +31,32 @@ import org.rhq.enterprise.server.ws.utility.WsUtility;
  * @author Jay Shaughnessy, Simeon Pinder
  */
 @Test(groups = "ws")
-public class WsSystemManagerTest extends AssertJUnit implements
-		TestPropertiesInterface {
+public class WsSystemManagerTest extends AssertJUnit implements TestPropertiesInterface {
 
-	// Test variables
-	private static ObjectFactory WS_OBJECT_FACTORY;
-	private static WebservicesRemote WEBSERVICE_REMOTE;
-	private static Subject subject = null;
+    // Test variables
+    private static ObjectFactory WS_OBJECT_FACTORY;
+    private static WebservicesRemote WEBSERVICE_REMOTE;
+    private static Subject subject = null;
 
-	@BeforeClass
-	public void init() throws ClassNotFoundException, MalformedURLException,
-			SecurityException, NoSuchMethodException, IllegalArgumentException,
-			InstantiationException, IllegalAccessException,
-			InvocationTargetException, LoginException_Exception {
+    @BeforeClass
+    public void init() throws ClassNotFoundException, MalformedURLException, SecurityException, NoSuchMethodException,
+        IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException,
+        LoginException_Exception {
 
-		// build reference variable bits
-		URL gUrl = WsUtility.generateRemoteWebserviceURL(
-				WebservicesManagerBeanService.class, host, port, useSSL);
-		QName gQName = WsUtility
-				.generateRemoteWebserviceQName(WebservicesManagerBeanService.class);
-		WebservicesManagerBeanService jws = new WebservicesManagerBeanService(
-				gUrl, gQName);
+        // build reference variable bits
+        URL gUrl = WsUtility.generateRemoteWebserviceURL(WebservicesManagerBeanService.class, host, port, useSSL);
+        QName gQName = WsUtility.generateRemoteWebserviceQName(WebservicesManagerBeanService.class);
+        WebservicesManagerBeanService jws = new WebservicesManagerBeanService(gUrl, gQName);
 
-		WEBSERVICE_REMOTE = jws.getWebservicesManagerBeanPort();
-		WS_OBJECT_FACTORY = new ObjectFactory();
-		WsSubjectTest.checkForWsTestUserAndRole();
-	}
+        WEBSERVICE_REMOTE = jws.getWebservicesManagerBeanPort();
+        WS_OBJECT_FACTORY = new ObjectFactory();
+        WsSubjectTest.checkForWsTestUserAndRole();
+    }
 
-	@Test(enabled = TESTS_ENABLED)
-	public void testServerNameSpaceVersion() throws java.lang.Exception {
-		// Check the server version
-		ServerVersion version = WEBSERVICE_REMOTE.getServerVersion(subject);
-		assertNotNull("ServerVersion was not located.", version);
-	}
+    @Test(enabled = TESTS_ENABLED)
+    public void testServerNameSpaceVersion() throws java.lang.Exception {
+        // Check the server version
+        ServerDetails version = WEBSERVICE_REMOTE.getServerDetails(subject);
+        assertNotNull("ServerVersion was not located.", version);
+    }
 }

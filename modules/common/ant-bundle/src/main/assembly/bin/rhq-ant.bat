d3799e2f59f40845bdf2da3898d347ae4e795cdd
John Mazzitelli <mazz@redhat.com>
2011-02-11 14:52:59 -0500

    missed two places where the rhq ant main should be specified

--- modules/common/ant-bundle/src/main/assembly/bin/rhq-ant.bat
+++ modules/common/ant-bundle/src/main/assembly/bin/rhq-ant.bat
@@ -135,14 +135,14 @@ if not "%JIKESPATH%"=="" goto runAntWithJikes
 :runAnt
 if "%_USE_CLASSPATH%"=="no" goto runAntNoClasspath
 :runAntWithClasspath
-"%_JAVACMD%" %ANT_OPTS% -classpath "%RHQ_ANT_HOME%\lib\ant-launcher.jar" "-Dant.home=%RHQ_ANT_HOME%" org.apache.tools.ant.launch.Launcher %ANT_ARGS% -cp "%CLASSPATH%" %ANT_CMD_LINE_ARGS%
+"%_JAVACMD%" %ANT_OPTS% -classpath "%RHQ_ANT_HOME%\lib\ant-launcher.jar" "-Dant.home=%RHQ_ANT_HOME%" org.apache.tools.ant.launch.Launcher %ANT_ARGS% -cp "%CLASSPATH%" -main org.rhq.bundle.ant.AntMain %ANT_CMD_LINE_ARGS%
 rem Check the error code of the Ant build
 if not "%OS%"=="Windows_NT" goto onError
 set ANT_ERROR=%ERRORLEVEL%
 goto end
 
 :runAntNoClasspath
-"%_JAVACMD%" %ANT_OPTS% -classpath "%RHQ_ANT_HOME%\lib\ant-launcher.jar" "-Dant.home=%RHQ_ANT_HOME%" org.apache.tools.ant.launch.Launcher %ANT_ARGS% %ANT_CMD_LINE_ARGS%
+"%_JAVACMD%" %ANT_OPTS% -classpath "%RHQ_ANT_HOME%\lib\ant-launcher.jar" "-Dant.home=%RHQ_ANT_HOME%" org.apache.tools.ant.launch.Launcher %ANT_ARGS% -main org.rhq.bundle.ant.AntMain %ANT_CMD_LINE_ARGS%
 rem Check the error code of the Ant build
 if not "%OS%"=="Windows_NT" goto onError
 set ANT_ERROR=%ERRORLEVEL%

c4fa41af0487cd9b6efce6cfd3955ae81e0c0cb8
John Mazzitelli <mazz@redhat.com>
2011-02-18 15:19:18 -0500

    BZ 571873 gracefully create the alert def even if the description is too long. just truncate the desc

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/alert/AlertDefinitionManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/alert/AlertDefinitionManagerBean.java
@@ -594,6 +594,11 @@ public class AlertDefinitionManagerBean implements AlertDefinitionManagerLocal, 
 
     private void checkAlertDefinition(AlertDefinition alertDefinition, Integer resourceId)
         throws InvalidAlertDefinitionException {
+        // if someone enters a really long description, we need to truncate it - the column is only 250 chars
+        if (alertDefinition.getDescription().length() > 250) {
+            alertDefinition.setDescription(alertDefinition.getDescription().substring(0, 250));
+        }
+
         for (AlertCondition alertCondition : alertDefinition.getConditions()) {
             AlertConditionCategory alertConditionCategory = alertCondition.getCategory();
             if (alertConditionCategory == AlertConditionCategory.ALERT) {

4e5db13557b7252088110c795b7218c8c47fbd7f
Ian Springer <ian.springer@redhat.com>
2011-04-06 16:08:27 -0400

    only display HTML within the message portlet's content if the logged-in user is rhqadmin; otherwise escape HTML to prevent XSS attacks (https://bugzilla.redhat.com/show_bug.cgi?id=636734)

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/dashboard/DashboardsView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/dashboard/DashboardsView.java
@@ -268,7 +268,7 @@ public class DashboardsView extends LocatableVLayout implements DashboardContain
     }
 
     /**
-     * The stored name for a dashboard is initally set to a generated, localizable name. It can later be edited
+     * The stored name for a dashboard is initially set to a generated, localizable name. It can later be edited
      * by the user. Automation tests must be valid independent of localization so we must use repeatable locators.
      * This method checks for generated dash names and returns a repeatable locator for them.
      * 
@@ -363,8 +363,6 @@ public class DashboardsView extends LocatableVLayout implements DashboardContain
 
         // Left Column
         DashboardPortlet welcome = new DashboardPortlet(MessagePortlet.NAME, MessagePortlet.KEY, 250);
-        welcome.getConfiguration().put(
-            new PropertySimple("message", MSG.view_dashboardsManager_message_title_details()));
         dashboard.addPortlet(welcome, 0, 0);
 
         DashboardPortlet summary = new DashboardPortlet(InventorySummaryPortlet.NAME, InventorySummaryPortlet.KEY, 250);

3e284ad5dd6ceb1b1cb93eb89d2b3e688a7f4c56
John Sanda <jsanda@redhat.com>
2011-05-10 01:23:15 -0400

    First cut at opeation to send snapshot report to server

--- modules/core/plugin-container/src/main/java/org/rhq/core/pc/drift/DriftManager.java
+++ modules/core/plugin-container/src/main/java/org/rhq/core/pc/drift/DriftManager.java
@@ -1,16 +1,29 @@
 package org.rhq.core.pc.drift;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.rhq.core.clientapi.server.drift.SnapshotReport;
 import org.rhq.core.pc.ContainerService;
+import org.rhq.core.pc.PluginContainer;
 import org.rhq.core.pc.PluginContainerConfiguration;
 import org.rhq.core.pc.agent.AgentService;
 
 public class DriftManager extends AgentService implements ContainerService {
+    private PluginContainerConfiguration pluginContainerConfiguration;
+
+    private File snapshotsDir;
+
     public DriftManager() {
         super(DriftAgentService.class);
     }
 
     @Override
     public void setConfiguration(PluginContainerConfiguration configuration) {
+        pluginContainerConfiguration = configuration;
+        snapshotsDir = new File(pluginContainerConfiguration.getDataDirectory(), "snapshots");
+        snapshotsDir.mkdir();
     }
 
     @Override
@@ -19,5 +32,22 @@ public class DriftManager extends AgentService implements ContainerService {
 
     @Override
     public void shutdown() {
+    }
+
+    public SnapshotHandle generateSnapshot(int resourceId, File basedir) {
+        SnapshotGenerator generator = new SnapshotGenerator();
+        generator.setSnapshotDir(snapshotsDir);
+        return generateSnapshot(resourceId, basedir);
+    }
+
+    public void sendSnapshotReport(int resourceId, SnapshotHandle handle) throws Exception {
+        SnapshotReport report = new SnapshotReport();
+        report.setMetadataSize(handle.getMetadataFile().length());
+        report.setDataSize(handle.getDataFile().length());
+        report.setMetadataInputStream(remoteInputStream(new BufferedInputStream(new FileInputStream(
+            handle.getMetadataFile()))));
+        report.setDataInputStream(remoteInputStream(new BufferedInputStream(new FileInputStream(
+            handle.getDataFile()))));
+        report.setResourceId(resourceId);
     }
 }

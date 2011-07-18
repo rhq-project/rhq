fc8d638828caaf235ddeff2bbe3dfd19dcbbf0a5
John Sanda <jsanda@redhat.com>
2011-07-18 13:25:26 -0400

    Initial commit for drift mongodb server plugin

---
+++ modules/enterprise/server/plugins/drift-mongodb/src/main/java/org/rhq/enterprise/server/plugins/drift/mongodb/MongoDBDriftServer.java
@@ -0,0 +1,40 @@
+package org.rhq.enterprise.server.plugins.drift.mongodb;
+
+import java.io.File;
+
+import org.rhq.enterprise.server.plugin.pc.ServerPluginContext;
+import org.rhq.enterprise.server.plugin.pc.drift.DriftServerPluginFacet;
+
+public class MongoDBDriftServer implements DriftServerPluginFacet {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void saveChangeSet(int resourceId, File changeSetZip) throws Exception {
+
+    }
+
+    @Override
+    public void saveChangeSetFiles(File changeSetFilesZip) throws Exception {
+
+    }
+
+    @Override
+    public void initialize(ServerPluginContext context) throws Exception {
+
+    }
+
+}

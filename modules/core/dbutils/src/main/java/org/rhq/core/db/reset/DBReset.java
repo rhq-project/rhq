19437cae8ca6ceb053fa191e08dff6368b8574b9
Stefan Negrea <snegrea@redhat.com>
2011-08-04 07:44:43 -0500

    Throw an exception if trying to use dbreset with anything else other than Oracle and Postgres.

--- modules/core/dbutils/src/main/java/org/rhq/core/db/reset/DBReset.java
+++ modules/core/dbutils/src/main/java/org/rhq/core/db/reset/DBReset.java
@@ -132,5 +132,8 @@ public class DBReset {
                 }
             }
         }
+        else {
+            throw new Exception("dbreset not supported for "+ dbTypeMapping +"!");
+        }
     }
 }

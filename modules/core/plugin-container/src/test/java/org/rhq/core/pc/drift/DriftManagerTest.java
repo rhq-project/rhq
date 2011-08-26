66085d0afba8abd80f7288924ec1b34c2b7f7043
John Sanda <jsanda@redhat.com>
2011-08-26 13:53:47 -0400

    Updating clean up code in DriftManager to not delete files until streams are closed

--- modules/core/plugin-container/src/test/java/org/rhq/core/pc/drift/DriftManagerTest.java
+++ modules/core/plugin-container/src/test/java/org/rhq/core/pc/drift/DriftManagerTest.java
@@ -20,6 +20,7 @@
 package org.rhq.core.pc.drift;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -31,6 +32,8 @@ import org.rhq.core.domain.drift.DriftConfiguration;
 import org.rhq.core.pc.PluginContainerConfiguration;
 import org.rhq.core.pc.ServerServices;
 
+import static org.apache.commons.io.FileUtils.deleteDirectory;
+import static org.rhq.core.domain.drift.DriftChangeSetCategory.COVERAGE;
 import static org.rhq.core.util.ZipUtil.unzipFile;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -64,15 +67,72 @@ public class DriftManagerTest extends DriftTest {
         driftMgr.setConfiguration(pcConfig);
     }
 
-//    @AfterMethod
-//    public void shutDownDriftManager() {
-//        driftMgr.shutdown();
-//    }
+    @Test
+    public void writeChangeSetZipFileToChangeSetDirectory() throws Exception {
+        final DriftConfiguration config = driftConfiguration("write-changeset-file", resourceDir.getAbsolutePath());
+        final File changeSetDir = changeSetDir(config.getName());
+        createRandomFile(changeSetDir, "changeset.txt");
+
+        setDriftServiceCallback(new DriftServiceCallback() {
+            @Override
+            public void execute() {
+                assertThatZipFileExists(changeSetDir, "changeset_",
+                    "Expected to find change set zip file " +
+                        "in " + changeSetDir.getPath() + ". The file name should follow the pattern " +
+                        "changeset_<integer_timestamp>.zip");
+            }
+        });
+
+        driftMgr.setChangeSetMgr(changeSetMgr);
+        driftMgr.getSchedulesQueue().addSchedule(new DriftDetectionSchedule(resourceId(), config));
+        driftMgr.sendChangeSetToServer(resourceId(), config, COVERAGE);
+    }
+
+    @Test
+    public void sendChangeSetReportInZipFile() throws Exception {
+        final DriftConfiguration config = driftConfiguration("send-changeset-in-zip", resourceDir.getAbsolutePath());
+        final File changeSetDir = changeSetDir(config.getName());
+        final File changeSetFile = createRandomFile(changeSetDir, "changeset.txt");
+
+        setDriftServiceCallback(new DriftServiceCallback() {
+            @Override
+            public void execute() {
+                assertZipFileMatches(driftServerService.inputStream, changeSetFile);
+            }
+        });
+
+        driftMgr.setChangeSetMgr(changeSetMgr);
+        driftMgr.getSchedulesQueue().addSchedule(new DriftDetectionSchedule(resourceId(), config));
+        driftMgr.sendChangeSetToServer(resourceId(), config, COVERAGE);
+    }
+
+    @Test
+    public void cleanUpAfterSendingChangeSetToServer() throws Exception {
+        final DriftConfiguration config = driftConfiguration("send-changeset-in-zip", resourceDir.getAbsolutePath());
+        final File changeSetDir = changeSetDir(config.getName());
+        createRandomFile(changeSetDir, "changeset.txt");
+
+        driftMgr.setChangeSetMgr(changeSetMgr);
+        driftMgr.getSchedulesQueue().addSchedule(new DriftDetectionSchedule(resourceId(), config));
+        driftMgr.sendChangeSetToServer(resourceId(), config, COVERAGE);
+
+        // clean up should not happen until after the input stream is closed. The remote
+        // input stream is consumed asynchronously; so, we have to wait until the stream
+        // is closed. We could otherwise disrupt the transmission of bits.
+
+        File changeSetZip = assertThatZipFileExists(changeSetDir, "changeset_", "Expected to find change set zip " +
+            "zip file in " + changeSetDir.getPath() + ". The file name should follow the pattern " +
+            "changeset_<integer_timestamp>.zip");
+
+        driftServerService.inputStream.close();
+
+        assertFalse(changeSetZip.exists(), "The change set zip should be deleted when the remote input stream is closed.");
+    }
 
     @Test
     public void writeContentZipFileToChangeSetContentDirectory() throws Exception {
         String configName = "send-content-in-zip";
-        File changeSetDir = changeSetDir(configName);
+        final File changeSetDir = changeSetDir(configName);
         final File contentDir = mkdir(changeSetDir, "content");
 
         createRandomFile(contentDir, "content-1");
@@ -81,9 +141,9 @@ public class DriftManagerTest extends DriftTest {
         setDriftServiceCallback(new DriftServiceCallback() {
             @Override
             public void execute() {
-                File contentZip = new File(contentDir, "content.zip");
-                assertTrue(contentZip.exists(), "Expected content zip file to be written to the change set content " +
-                    "directory: " + contentDir.getPath());
+                assertThatZipFileExists(changeSetDir, "content_", "Expected to find content zip file in " +
+                    changeSetDir.getPath() + ". The file name should follow the pattern " +
+                    "content_<integer_timestamp>.zip");
             }
         });
 
@@ -102,7 +162,7 @@ public class DriftManagerTest extends DriftTest {
         setDriftServiceCallback(new DriftServiceCallback() {
             @Override
             public void execute() {
-                assertZipFileMatches(driftServerService.filesZipStream, content1, content2);
+                assertZipFileMatches(driftServerService.inputStream, content1, content2);
             }
         });
 
@@ -120,6 +180,16 @@ public class DriftManagerTest extends DriftTest {
 
         driftMgr.sendChangeSetContentToServer(resourceId(), configName, contentDir);
 
+        // clean up should not happen until after the input stream is closed. The remote
+        // input stream is consumed asynchronously; so, we have to wait until the stream
+        // is closed. We could otherwise disrupt the transmission of bits.
+        File contentZipFile = assertThatZipFileExists(changeSetDir, "content_",
+            "Expected to find content zip file in " +
+                changeSetDir.getPath() + ". The file name should have a pattern of content_integer_timestamp>.zip");
+
+        driftServerService.inputStream.close();
+
+        assertFalse(contentZipFile.exists(), "The content zip should be deleted when the remote input stream is closed");
         assertThatDirectoryIsEmpty(contentDir);
     }
 
@@ -183,9 +253,11 @@ public class DriftManagerTest extends DriftTest {
     private void assertZipFileMatches(InputStream zipStream, File... expectedFiles) {
         File zipDir = new File(tmpDir, "output");
         try {
+            deleteDirectory(zipDir);
+            zipDir.mkdirs();
             unzipFile(zipStream, zipDir);
         } catch (IOException e) {
-            fail("Failed to unzip zip file from intput stream into " + zipDir.getPath(), e);
+            fail("An error occurred while trying to unzip " + zipDir.getPath(), e);
         }
 
         assertEquals(zipDir.listFiles().length, expectedFiles.length, "The zip file has the wrong number of files");
@@ -217,6 +289,28 @@ public class DriftManagerTest extends DriftTest {
     }
 
     /**
+     * This method searches the specified directory for a zip file with the specified
+     * prefix. The file name must start with the prefix and end with an extension of .zip.
+     * This method assumes that there should be one and only one match. If there is not
+     * exactly one match, it will fail the test.
+     *
+     * @param dir The directory to search (sub directories are not searched)
+     * @param fileNamePrefix The zip file name prefix
+     * @param msg An error message
+     * @return The matching zip file
+     */
+    private File assertThatZipFileExists(final File dir, final String fileNamePrefix, String msg) {
+        File[] files = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.startsWith(fileNamePrefix) && name.endsWith(".zip");
+            }
+        });
+        assertEquals(files.length, 1, msg);
+        return files[0];
+    }
+
+    /**
      * Sets a callback that will be invoked immediately after DriftManager calls
      * {@link DriftServerService#sendChangesetZip(int, long, java.io.InputStream)} or
      * {@link DriftServerService#sendFilesZip(int, long, java.io.InputStream)}. The callback
@@ -231,21 +325,28 @@ public class DriftManagerTest extends DriftTest {
 
     private static class TestDriftServerService implements DriftServerService {
 
-        public int filesZipResourceId;
-        public long filesZipSize;
-        public InputStream filesZipStream;
+        public int resourceId;
+        public long fileSize;
+        public InputStream inputStream;
 
         public DriftServiceCallback callback;
 
         @Override
         public void sendChangesetZip(int resourceId, long zipSize, InputStream zipStream) {
+            this.resourceId = resourceId;
+            fileSize = zipSize;
+            inputStream = zipStream;
+
+            if (callback != null) {
+                callback.execute();
+            }
         }
 
         @Override
         public void sendFilesZip(int resourceId, long zipSize, InputStream zipStream) {
-            filesZipResourceId = resourceId;
-            filesZipSize = zipSize;
-            filesZipStream = zipStream;
+            this.resourceId = resourceId;
+            fileSize = zipSize;
+            inputStream = zipStream;
 
             if (callback != null) {
                 callback.execute();

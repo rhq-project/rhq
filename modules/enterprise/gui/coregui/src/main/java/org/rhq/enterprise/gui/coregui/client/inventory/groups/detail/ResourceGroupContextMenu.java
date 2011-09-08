34cb25f075a22714af01ea08cd0de453b638a139
Ian Springer <ian.springer@redhat.com>
2011-09-08 17:07:44 -0400

    [BZ 736848] add new method to LinkManager that can return the appropriate form of group URL given an EntityContext; update several spots in the code to start using this new method to ensure the correct form of URL is used for the different types of compat groups (https://bugzilla.redhat.com/show_bug.cgi?id=736848)

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/inventory/groups/detail/ResourceGroupContextMenu.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/inventory/groups/detail/ResourceGroupContextMenu.java
@@ -184,16 +184,7 @@ public class ResourceGroupContextMenu extends LocatableMenu {
         if (pluginConfigEnabled) {
             pluginConfiguration.addClickHandler(new ClickHandler() {
                 public void onClick(MenuItemClickEvent event) {
-                    if (isAutoGroup) {
-                        CoreGUI.goToView(LinkManager.getAutoGroupTabLink(group.getId(), "Inventory",
-                            "ConnectionSettings"));
-                    } else if (isAutoCluster) {
-                        CoreGUI.goToView(LinkManager.getAutoClusterTabLink(group.getId(), "Inventory",
-                            "ConnectionSettings"));
-                    } else {
-                        CoreGUI.goToView(LinkManager.getResourceGroupTabLink(group.getId(), "Inventory",
-                            "ConnectionSettings"));
-                    }
+                    CoreGUI.goToView(LinkManager.getResourceGroupTabLink(group, "Inventory", "ConnectionSettings"));
                 }
             });
         }
@@ -207,14 +198,7 @@ public class ResourceGroupContextMenu extends LocatableMenu {
         if (resourceConfigEnabled) {
             resourceConfiguration.addClickHandler(new ClickHandler() {
                 public void onClick(MenuItemClickEvent event) {
-                    if (isAutoGroup) {
-                        CoreGUI.goToView(LinkManager.getAutoGroupTabLink(group.getId(), "Configuration", "Current"));
-                    } else if (isAutoCluster) {
-                        CoreGUI.goToView(LinkManager.getAutoClusterTabLink(group.getId(), "Configuration", "Current"));
-                    } else {
-                        CoreGUI
-                            .goToView(LinkManager.getResourceGroupTabLink(group.getId(), "Configuration", "Current"));
-                    }
+                    CoreGUI.goToView(LinkManager.getResourceGroupTabLink(group, "Configuration", "Current"));
                 }
             });
         }

9e689005aef5ace6f4e66a6e94ae737e545849e5
Stefan Negrea <snegrea@redhat.com>
2011-09-30 11:25:35 -0500

    Remove WORKING_DIR concept, the current folder is working folder. Simplify the assumptions for maven to run either Hudson or default local methods.

--- release.sh
+++ release.sh
@@ -87,16 +87,13 @@ set_variables()
 
    # Set various local variables.
 
-   if [ -n "$HUDSON_URL" ] && [ -n "$WORKSPACE" ]; then
+   if [ -n "$WORKSPACE" ]; then
       echo "We appear to be running in a Hudson job." 
-      WORKING_DIR="$WORKSPACE"
-      MAVEN_LOCAL_REPO_DIR="$HOME/.m2/hudson-release-$RELEASE_TYPE-repository"
-      MAVEN_SETTINGS_FILE="$HOME/.m2/hudson-$JOB_NAME-settings.xml"
-   elif [ -z "$WORKING_DIR" ]; then
-      WORKING_DIR="$HOME/release/rhq"
-   #   MAVEN_LOCAL_REPO_DIR="$HOME/release/m2-repository"
+      MAVEN_LOCAL_REPO_DIR="$WORKSPACE/.m2/repository"
+      MAVEN_SETTINGS_FILE="$WORKSPACE/.m2/settings.xml"
+   else
       MAVEN_LOCAL_REPO_DIR="$HOME/.m2/repository"
-      MAVEN_SETTINGS_FILE="$HOME/release/m2-settings.xml"
+      MAVEN_SETTINGS_FILE="$HOME/.m2/settings.xml"
    fi
 
    PROJECT_GIT_URL="ssh://${GIT_USERNAME}@git.fedorahosted.org/git/rhq/rhq.git"
@@ -127,7 +124,6 @@ set_variables()
 
    TAG_VERSION=`echo $RELEASE_VERSION | sed 's/\./_/g'`
    RELEASE_TAG="${TAG_PREFIX}_${TAG_VERSION}"
-
 
    # Set the system character encoding to ISO-8859-1 to ensure i18log reads its 
    # messages and writes its resource bundle properties files in that encoding, 
@@ -221,7 +217,7 @@ print_variables "${environment_variables[@]}"
 
 
 print_centered "Local Variables"
-local_variables=("WORKING_DIR" "PROJECT_NAME" "PROJECT_GIT_URL" "RELEASE_TYPE" "DEVELOPMENT_VERSION" \
+local_variables=( "PROJECT_NAME" "PROJECT_GIT_URL" "RELEASE_TYPE" "DEVELOPMENT_VERSION" \
                   "RELEASE_BRANCH" "MODE" "MAVEN_LOCAL_REPO_DIR" \
                   "MAVEN_SETTINGS_FILE" "MAVEN_ARGS" "MAVEN_RELEASE_PERFORM_GOAL" "JBOSS_ORG_USERNAME" \
                   "RELEASE_VERSION" "RELEASE_TAG")
@@ -238,7 +234,7 @@ print_centered "="
 git status >/dev/null 2>&1
 GIT_STATUS_EXIT_CODE=$?
 # Note, git 1.6 and earlier returns an exit code of 1, rather than 0, if there are any uncommitted changes,
-# and git 1.7 returns 0, so we check if the exit code is less than or equal to 1 to determine if $WORKING_DIR
+# and git 1.7 returns 0, so we check if the exit code is less than or equal to 1 to determine if current folder
 # is truly a git working copy.
 if [ "$GIT_STATUS_EXIT_CODE" -le 1 ]; then       
     echo "Checking out a clean copy of the release branch ($RELEASE_BRANCH)..."
@@ -257,7 +253,7 @@ if [ "$GIT_STATUS_EXIT_CODE" -le 1 ]; then
     git pull origin $RELEASE_BRANCH
     [ "$?" -ne 0 ] && abort "Failed to update release branch ($RELEASE_BRANCH)."
 else
-    echo "$WORKING_DIR does not appear to be a git working directory ('git status' returned $GIT_STATUS_EXIT_CODE) - removing it so we can freshly clone the repo..."
+    echo "Current folder does not appear to be a git working directory ('git status' returned $GIT_STATUS_EXIT_CODE) - removing it so we can freshly clone the repo..."
 fi
 
 # if this is a test build then create a temporary build branch off of RELEASE_BRANCH.  This allows checkins to

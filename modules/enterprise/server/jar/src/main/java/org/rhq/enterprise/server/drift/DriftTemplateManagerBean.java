fd1c55e8558ad9c0b471a548da09e8b32ec736f1
John Sanda <jsanda@redhat.com>
2011-10-10 23:19:32 -0400

    fixing another dbupgrade error

---
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/drift/DriftTemplateManagerBean.java
@@ -0,0 +1,63 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.rhq.enterprise.server.drift;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.rhq.core.domain.auth.Subject;
+import org.rhq.core.domain.authz.Permission;
+import org.rhq.core.domain.drift.DriftDefinition;
+import org.rhq.core.domain.drift.DriftDefinitionTemplate;
+import org.rhq.core.domain.resource.ResourceType;
+import org.rhq.enterprise.server.RHQConstants;
+import org.rhq.enterprise.server.authz.RequiredPermission;
+import org.rhq.enterprise.server.resource.ResourceTypeManagerLocal;
+import org.rhq.enterprise.server.resource.ResourceTypeNotFoundException;
+
+@Stateless
+public class DriftTemplateManagerBean implements DriftTemplateManagerLocal {
+
+    @PersistenceContext(unitName = RHQConstants.PERSISTENCE_UNIT_NAME)
+    private EntityManager entityMgr;
+
+    @EJB
+    ResourceTypeManagerLocal resourceTypeMgr;
+
+    @RequiredPermission(Permission.MANAGE_SETTINGS)
+    @Override
+    public void createTemplate(Subject subject, int resourceTypeId, DriftDefinition definition) {
+        try {
+            ResourceType resourceType = resourceTypeMgr.getResourceTypeById(subject, resourceTypeId);
+            DriftDefinitionTemplate template = new DriftDefinitionTemplate();
+            template.setName(definition.getName());
+            //template.setDescription(definition.);
+            template.setConfiguration(definition.getConfiguration().deepCopyWithoutProxies());
+
+            resourceType.addDriftDefinitionTemplate(template);
+            entityMgr.merge(template);
+
+        } catch (ResourceTypeNotFoundException e) {
+        }
+    }
+
+}
---
+++ modules/enterprise/server/jar/src/test/java/org/rhq/enterprise/server/drift/DriftTemplateManagerBeanTest.java
@@ -0,0 +1,146 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.rhq.enterprise.server.drift;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import org.rhq.core.domain.auth.Subject;
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.drift.DriftDefinition;
+import org.rhq.core.domain.drift.DriftDefinitionTemplate;
+import org.rhq.core.domain.resource.ResourceType;
+import org.rhq.core.domain.shared.ResourceTypeBuilder;
+import org.rhq.enterprise.server.test.AbstractEJB3Test;
+import org.rhq.enterprise.server.util.LookupUtil;
+import org.rhq.test.TransactionCallback;
+
+import static org.rhq.core.domain.drift.DriftConfigurationDefinition.BaseDirValueContext.fileSystem;
+import static org.rhq.core.domain.drift.DriftConfigurationDefinition.DriftHandlingMode.normal;
+import static org.rhq.core.domain.resource.ResourceCategory.SERVER;
+import static org.rhq.enterprise.server.util.LookupUtil.getDriftTemplateManager;
+import static org.rhq.enterprise.server.util.LookupUtil.getSubjectManager;
+import static org.rhq.test.JPAUtils.executeInTransaction;
+
+public class DriftTemplateManagerBeanTest extends AbstractEJB3Test {
+
+    private final String RESOURCE_TYPE_NAME = DriftTemplateManagerBeanTest.class.getName();
+
+    private ResourceType resourceType;
+
+    private DriftTemplateManagerLocal templateMgr;
+
+    @BeforeClass
+    public void initClass() {
+        templateMgr = getDriftTemplateManager();
+    }
+
+    @BeforeMethod
+    public void initDB() {
+        executeInTransaction(new TransactionCallback() {
+            @Override
+            public void execute() throws Exception {
+                purgeDB();
+                createResourceType();
+                getEntityManager().persist(resourceType);
+            }
+        });
+    }
+
+    @AfterClass
+    public void resetDB() {
+        executeInTransaction(new TransactionCallback() {
+            @Override
+            public void execute() throws Exception {
+                purgeDB();
+            }
+        });
+    }
+
+    private void purgeDB() {
+        EntityManager em = getEntityManager();
+
+        List results =  em.createQuery("from ResourceType where name = :name")
+            .setParameter("name", RESOURCE_TYPE_NAME)
+            .getResultList();
+        if (results.isEmpty()) {
+            return;
+        }
+        ResourceType type = (ResourceType) results.get(0);
+        for (DriftDefinitionTemplate template : type.getDriftDefinitionTemplates()) {
+            em.remove(template);
+        }
+        em.remove(type);
+    }
+
+    private void createResourceType() {
+        resourceType = new ResourceTypeBuilder().createResourceType()
+            .withId(0)
+            .withName(DriftTemplateManagerBeanTest.class.getName())
+            .withCategory(SERVER)
+            .withPlugin(DriftTemplateManagerBeanTest.class.getName().toLowerCase())
+            .build();
+    }
+
+    @Test
+    public void createNewTemplate() {
+        final DriftDefinition definition = new DriftDefinition(new Configuration());
+        definition.setName("test::createNewTemplate");
+        definition.setEnabled(true);
+        definition.setDriftHandlingMode(normal);
+        definition.setInterval(2400L);
+        definition.setBasedir(new DriftDefinition.BaseDirectory(fileSystem, "/foo/bar/test"));
+
+        templateMgr.createTemplate(getOverlord(), resourceType.getId(), definition);
+
+        executeInTransaction(new TransactionCallback() {
+            @Override
+            public void execute() throws Exception {
+                EntityManager em = getEntityManager();
+                ResourceType updatedType = em.find(ResourceType.class, resourceType.getId());
+
+                assertEquals("Failed to add new drift definition to resource type", 1,
+                    updatedType.getDriftDefinitionTemplates().size());
+
+                DriftDefinitionTemplate newTemplate = updatedType.getDriftDefinitionTemplates().iterator().next();
+                assertEquals("Failed to save template - name is wrong", definition.getName(), newTemplate.getName());
+                assertEquals("Failed to save template - enabled flag is wrong", definition.isEnabled(),
+                    newTemplate.isEnabled());
+                assertEquals("Failed to save template - drift handling mode is wrong",
+                    definition.getDriftHandlingMode(), newTemplate.getDriftHandlingMode());
+                assertEquals("Failed to save template - interval is wrong", definition.getInterval(),
+                    (long) newTemplate.getInterval());
+                assertEquals("Failed to save template - base directory is wrong", definition.getBasedir(),
+                    newTemplate.getBaseDirectory());
+            }
+        });
+    }
+
+    private Subject getOverlord() {
+        return getSubjectManager().getOverlord();
+    }
+
+}

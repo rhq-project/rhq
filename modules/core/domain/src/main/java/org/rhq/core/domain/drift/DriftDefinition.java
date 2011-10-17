e7779d65cc5264c9b135ab9108274c73f2e0109b
Jay Shaughnessy <jshaughn@redhat.com>
2011-10-17 12:21:40 -0400

    Fix issue in sequence name in the upgrade and the entity.

--- modules/core/domain/src/main/java/org/rhq/core/domain/drift/DriftDefinition.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/drift/DriftDefinition.java
@@ -64,7 +64,7 @@ import org.rhq.core.domain.resource.Resource;
  */
 @Entity
 @Table(name = "RHQ_DRIFT_DEFINITION")
-@SequenceGenerator(name = "SEQ", sequenceName = "RHQ_DRIFT_DEF_ID_SEQ")
+@SequenceGenerator(name = "SEQ", sequenceName = "RHQ_DRIFT_DEFINITION_ID_SEQ")
 public class DriftDefinition implements Serializable {
     private static final long serialVersionUID = 1L;
 

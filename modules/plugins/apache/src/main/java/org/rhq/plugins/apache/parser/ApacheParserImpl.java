38daab95271e3bd733e8b7909a3cf9c6607408ed
Lukas Krejci <lkrejci@redhat.com>
2011-10-26 16:11:16 +0200

    Formatting

--- modules/plugins/apache/src/main/java/org/rhq/plugins/apache/parser/ApacheParserImpl.java
+++ modules/plugins/apache/src/main/java/org/rhq/plugins/apache/parser/ApacheParserImpl.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.plugins.apache.parser;
 
 import java.io.File;
@@ -5,24 +23,28 @@ import java.util.List;
 
 import org.rhq.augeas.util.Glob;
 
-public class ApacheParserImpl implements ApacheParser{
+/**
+ *
+ * @author Filip Drabek
+ */
+public class ApacheParserImpl implements ApacheParser {
 
     private final static String INCLUDE_DIRECTIVE = "Include";
     private final ApacheDirectiveTree tree;
     private ApacheDirectiveStack stack;
     private String serverRootPath;
 
-    public ApacheParserImpl(ApacheDirectiveTree tree,String serverRootPath){          
-      stack = new ApacheDirectiveStack();
-      this.serverRootPath = serverRootPath;
-      this.tree = tree;
-      stack.addDirective(this.tree.getRootNode());
+    public ApacheParserImpl(ApacheDirectiveTree tree, String serverRootPath) {
+        stack = new ApacheDirectiveStack();
+        this.serverRootPath = serverRootPath;
+        this.tree = tree;
+        stack.addDirective(this.tree.getRootNode());
     }
-    
-    public void addDirective(ApacheDirective directive) throws Exception{
-        if (directive.getName().equals(INCLUDE_DIRECTIVE)){
+
+    public void addDirective(ApacheDirective directive) throws Exception {
+        if (directive.getName().equals(INCLUDE_DIRECTIVE)) {
             List<File> files = getIncludeFiles(directive.getValuesAsString());
-            for (File fl : files){
+            for (File fl : files) {
                 if (fl.exists() && fl.isFile()) {
                     ApacheConfigReader.searchFile(fl.getAbsolutePath(), this);
                 }
@@ -32,20 +54,20 @@ public class ApacheParserImpl implements ApacheParser{
         stack.getLastDirective().addChildDirective(directive);
     }
 
-    public void endNestedDirective(ApacheDirective directive) {       
-        stack.removeLastDirective();        
+    public void endNestedDirective(ApacheDirective directive) {
+        stack.removeLastDirective();
     }
 
     public void startNestedDirective(ApacheDirective directive) {
-      directive.setParentNode(stack.getLastDirective());
-      stack.getLastDirective().addChildDirective(directive);
-      stack.addDirective(directive);      
+        directive.setParentNode(stack.getLastDirective());
+        stack.getLastDirective().addChildDirective(directive);
+        stack.addDirective(directive);
     }
 
-    private  List<File> getIncludeFiles(String foundInclude) {
-        File check = new File(foundInclude);        
+    private List<File> getIncludeFiles(String foundInclude) {
+        File check = new File(foundInclude);
         File root = new File(check.isAbsolute() ? Glob.rootPortion(foundInclude) : serverRootPath);
-                 
+
         return Glob.match(root, foundInclude, Glob.ALPHABETICAL_COMPARATOR);
     }
 }

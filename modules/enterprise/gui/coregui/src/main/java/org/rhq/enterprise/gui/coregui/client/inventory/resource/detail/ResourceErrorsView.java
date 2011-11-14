2af03f656585a994e8959faca9da0d3d83988c44
Ian Springer <ian.springer@redhat.com>
2011-11-14 11:56:16 -0500

    [BZ 752893] refresh the resource-errors (yellow triangle) icon after the user deletes one or more resource errors or closes the resource errors modal (https://bugzilla.redhat.com/show_bug.cgi?id=752893)

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/inventory/resource/detail/ResourceErrorsView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/inventory/resource/detail/ResourceErrorsView.java
@@ -54,10 +54,16 @@ import org.rhq.enterprise.gui.coregui.client.util.selenium.LocatableHTMLPane;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.LocatableWindow;
 
 public class ResourceErrorsView extends Table<ResourceErrorsDataSource> {
+
     private static final String ERROR_ICON = "[SKIN]/Dialog/warn.png";
 
-    public ResourceErrorsView(String locatorId, String string, Criteria criteria, Object object, String[] strings) {
+    private ResourceTitleBar titleBar;
+
+    public ResourceErrorsView(String locatorId, Criteria criteria, ResourceTitleBar titleBar) {
         super(locatorId, MSG.common_title_component_errors(), criteria);
+
+        this.titleBar = titleBar;
+
         setWidth100();
         setHeight100();
         setShowHeader(false);
@@ -144,6 +150,7 @@ public class ResourceErrorsView extends Table<ResourceErrorsDataSource> {
                     for (ListGridRecord record : selection) {
                         resourceErrorIds[i++] = record.getAttributeAsInt(Field.ID);
                     }
+
                     GWTServiceLookup.getResourceService().deleteResourceErrors(resourceErrorIds,
                         new AsyncCallback<Void>() {
                             public void onSuccess(Void result) {
@@ -162,6 +169,12 @@ public class ResourceErrorsView extends Table<ResourceErrorsDataSource> {
             });
     }
 
+    @Override
+    public void refresh() {
+        super.refresh();
+        this.titleBar.refresh();
+    }
+
     private void popupDetails(String details) {
         final Window winModal = new LocatableWindow(extendLocatorId("errorDetailsWin"));
         winModal.setTitle(MSG.common_title_component_errors());
@@ -176,9 +189,9 @@ public class ResourceErrorsView extends Table<ResourceErrorsDataSource> {
         winModal.setCanDragResize(true);
         winModal.centerInPage();
         winModal.addCloseClickHandler(new CloseClickHandler() {
-            @Override
             public void onCloseClick(CloseClientEvent event) {
                 winModal.markForDestroy();
+                titleBar.refresh();
             }
         });
 
@@ -190,4 +203,5 @@ public class ResourceErrorsView extends Table<ResourceErrorsDataSource> {
         winModal.addItem(htmlPane);
         winModal.show();
     }
+
 }

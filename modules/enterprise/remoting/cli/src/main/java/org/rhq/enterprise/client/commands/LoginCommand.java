1ca6ad91df7548052c46f32c7c6acae6f38b07af
Lukas Krejci <lkrejci@redhat.com>
2011-12-05 16:43:27 +0100

    [BZ 760148] - Use the unprivileged method introduced by the commit 0589b9ee8488bd1 for BZ 694892 for obtaining the version info upon the CLI login.

--- modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/commands/LoginCommand.java
+++ modules/enterprise/remoting/cli/src/main/java/org/rhq/enterprise/client/commands/LoginCommand.java
@@ -115,7 +115,7 @@ public class LoginCommand implements ClientCommand {
 
         Subject subject = remoteClient.login(username, password);
 
-        ProductInfo info = remoteClient.getSystemManager().getServerDetails(subject).getProductInfo();
+        ProductInfo info = remoteClient.getSystemManager().getProductInfo(subject);
         String version = info.getVersion()
             + " (" + info.getBuildNumber() + ")";
         client.getPrintWriter().println("Remote server version is: " + version);

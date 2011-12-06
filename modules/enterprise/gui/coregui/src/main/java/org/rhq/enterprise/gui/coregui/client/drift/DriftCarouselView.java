32e06f0466e3a6a440518c4bd72e21c012bd6afa
Jay Shaughnessy <jshaughn@redhat.com>
2011-12-06 12:08:35 -0500

    [Bug 760209 - Drift Carousel not filtering the drift entries] I think this broke when initial criteria handling changed in the underlying Table implementation.  The initial criteria was set correctly but that was no longer applied directly to the grid, instead we now manually perform the initial fetch. But that led to an overriden method getting called to return the current criteria, which had yet to be determined (it was thought to be needed on refresh only). So, the fix was to immediately set the current criteria to the initial criteria in the subclass.

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/drift/DriftCarouselView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/drift/DriftCarouselView.java
@@ -242,7 +242,7 @@ public class DriftCarouselView extends BookmarkableCarousel implements DetailsVi
         }
         changeSetCriteria.addFilterStartVersion(String.valueOf(startVersion));
 
-        // apply the drift-level carousel filters in order to filter out changesets that have no applicab;e drift 
+        // apply the drift-level carousel filters in order to filter out changesets that have no applicable drift 
         Criteria criteria = getCurrentCriteria();
         DriftCategory[] driftCategoriesFilter = RPCDataSource.getArrayFilter(criteria,
             DriftDataSource.FILTER_CATEGORIES, DriftCategory.class);

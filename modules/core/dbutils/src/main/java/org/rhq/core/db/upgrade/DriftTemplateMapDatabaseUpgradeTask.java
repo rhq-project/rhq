dedfe6ad8738d1be1c5a411943e4e48d4410ffd2
Jay Shaughnessy <jshaughn@redhat.com>
2011-12-08 15:28:14 -0500

    [Bug 753866 - DB Upgrade needs more work for existing drift records] More work to support 4.1 and 4.2 upgrades for drift. - In the java template map upgrade task   - add logic to generate the rhq_drift_def_template records   - add logic to generate rhq_drift_set records for each initial changeset - Move some logic from 2.115 to 2.116 to make 2.115 match the   schema produced by a 4.2 dbsetup (fresh db).  Note that prior to   an upgrade 4.2 databases must (for now at least) be manually set   to schema version 2.115.  They are errantly set to 2.114 in the 4.2   release.

--- modules/core/dbutils/src/main/java/org/rhq/core/db/upgrade/DriftTemplateMapDatabaseUpgradeTask.java
+++ modules/core/dbutils/src/main/java/org/rhq/core/db/upgrade/DriftTemplateMapDatabaseUpgradeTask.java
@@ -20,6 +20,7 @@ package org.rhq.core.db.upgrade;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import mazz.i18n.Logger;
 
@@ -38,6 +39,8 @@ public class DriftTemplateMapDatabaseUpgradeTask implements DatabaseUpgradeTask 
     private static final Logger LOG = DbUtilsI18NFactory.getLogger(DriftPathDirectoryDatabaseUpgradeTask.class);
 
     public void execute(DatabaseType databaseType, Connection connection) throws SQLException {
+
+        // First generate a drift def template for each of the existing mappings
 
         String sql = "" //
             + " insert into RHQ_DRIFT_DEF_TEMPLATE (" //
@@ -61,5 +64,53 @@ public class DriftTemplateMapDatabaseUpgradeTask implements DatabaseUpgradeTask 
         LOG.debug(DbUtilsI18NResourceKeys.EXECUTING_SQL, sql);
 
         databaseType.executeSql(connection, sql);
+
+        // Second, assign one of the new templates to each of the existing definitions. Note that in the
+        // upgrade scenario we are guaranteed to only have 1 template for the given resource type so the
+        // select below is safe to return a single value.
+        sql = "" //
+            + " update RHQ_DRIFT_DEFINITION dd set DRIFT_DEF_TEMPLATE_ID =" // 
+            + " (   select dt.ID" // 
+            + "       from RHQ_DRIFT_DEF_TEMPLATE dt" //
+            + "       join RHQ_RESOURCE_TYPE rt on dt.RESOURCE_TYPE_ID = rt.ID" //
+            + "       join RHQ_RESOURCE res     on rt.ID = res.RESOURCE_TYPE_ID" //
+            + "        and dd.RESOURCE_ID = res.ID" //
+            + " )" //
+            + " where dd.DRIFT_DEF_TEMPLATE_ID is null";
+
+        LOG.debug(DbUtilsI18NResourceKeys.EXECUTING_SQL, sql);
+
+        databaseType.executeSql(connection, sql);
+
+        // third, for each initial snapshot, create the newly required rhq_drift_set and map the drift to it
+        sql = "select cs.ID from RHQ_DRIFT_CHANGE_SET cs where cs.VERSION = 0";
+        LOG.debug(DbUtilsI18NResourceKeys.EXECUTING_SQL, sql);
+
+        List<Object[]> rows = databaseType.executeSelectSql(connection, sql);
+
+        sql = "insert into RHQ_DRIFT_SET ( ID ) values ( "
+            + databaseType.getSequenceInsertValue(connection, "RHQ_DRIFT_SET_ID_SEQ") + " )";
+
+        for (Object[] row : rows) {
+            int csId = ((Number) row[0]).intValue();
+
+            // insert the new drift set row
+            LOG.debug(DbUtilsI18NResourceKeys.EXECUTING_SQL, sql);
+
+            databaseType.executeSql(connection, sql);
+            int setId = databaseType.getSequenceValue(connection, "RHQ_DRIFT_SET", "ID");
+
+            // update the drift records for the initial change set to refer to the drift set
+            String updateSql = "update RHQ_DRIFT set DRIFT_SET_ID = " + setId + " where DRIFT_CHANGE_SET_ID = " + csId;
+            LOG.debug(DbUtilsI18NResourceKeys.EXECUTING_SQL, updateSql);
+
+            databaseType.executeSql(connection, updateSql);
+
+            // update the change set to refer to the drift set 
+            updateSql = "update RHQ_DRIFT_CHANGE_SET set DRIFT_SET_ID = " + setId + " where ID = " + csId;
+            LOG.debug(DbUtilsI18NResourceKeys.EXECUTING_SQL, updateSql);
+
+            databaseType.executeSql(connection, updateSql);
+        }
     }
 }

772e9d247e524071a86d6a25b8e1615ed77e9fcf
Lukas Krejci <lkrejci@redhat.com>
2012-01-09 14:05:09 +0100

    Fixing the application of various decorators to JNDI contexts to support contexts that implement more than 1 context interface (like LdapCtx which implements both LdapContext and EventDirContext).
    This should fix the RHQ's LDAP integration for good.

--- modules/enterprise/server/container-lib/src/main/java/org/rhq/jndi/DecoratingInitialContextFactory.java
+++ modules/enterprise/server/container-lib/src/main/java/org/rhq/jndi/DecoratingInitialContextFactory.java
@@ -19,45 +19,82 @@
 
 package org.rhq.jndi;
 
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Set;
 
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.naming.spi.InitialContextFactory;
 
-import org.rhq.jndi.context.ContextDecoratorPicker;
+import org.rhq.jndi.context.ContextDecorator;
+import org.rhq.jndi.util.DecoratorPicker;
+import org.rhq.jndi.util.DecoratingInvocationHandler;
 
 /**
+ * This class implements an initial context factory that decorates the contexts
+ * returned from a "wrapped" initial factory passed to this class in the constructor.
+ * <p>
+ * The contexts returned from the wrapped initial factory are hidden behind a proxy
+ * that implements the intersection of interfaces from the <code>supportedContextInterfaces</code>
+ * constructor parameter and the actual interfaces the wrapped context implements.
+ * <p>
+ * The proxy method calls are handled using the {@link DecoratingInvocationHandler} which is initialized
+ * with the list of {@link DecoratorPicker pickers} that are used to intercept the method
+ * calls on the wrapped context.
  * 
- *
+ * @see DecoratorPicker
+ * @see DecoratingInvocationHandler
+ * 
  * @author Lukas Krejci
  */
 public class DecoratingInitialContextFactory implements InitialContextFactory {
 
-    private List<ContextDecoratorPicker> pickers;
+    List<DecoratorPicker<Context, ContextDecorator>> pickers;
     private InitialContextFactory factory;
+    private Set<Class<? extends Context>> supportedContextInterfaces;
     
-    public DecoratingInitialContextFactory(InitialContextFactory factory, List<ContextDecoratorPicker> decoratorPickers) {
+    public DecoratingInitialContextFactory(InitialContextFactory factory, List<DecoratorPicker<Context, ContextDecorator>> decoratorPickers) {
         this.factory = factory;
         this.pickers = decoratorPickers;
+        this.supportedContextInterfaces = new HashSet<Class<? extends Context>>();
+        for(DecoratorPicker<Context, ContextDecorator> picker : pickers) {
+            supportedContextInterfaces.addAll(picker.getContext().getSupportedInterfaces());
+        }
     }
     
     public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
         Context ctx = factory.getInitialContext(environment);
         
-        try {
-            for(ContextDecoratorPicker picker : pickers) {
-                ctx = picker.wrapInAppropriateDecorator(ctx);
-            }
-        } catch (IllegalArgumentException e) {
-            NamingException ex = new NamingException();
-            ex.initCause(ex);
-            
-            throw e;
-        }
+        Set<Class<?>> implementedIfaces = getAllImplementedInterfaces(ctx.getClass());
+        Class<?>[] ii = new Class<?>[implementedIfaces.size()];
+        implementedIfaces.toArray(ii);
         
-        return ctx;
+        return (Context) Proxy.newProxyInstance(ctx.getClass().getClassLoader(), ii, new DecoratingInvocationHandler<Context, ContextDecorator>(pickers, ctx));
     }
 
+    private Set<Class<?>> getAllImplementedInterfaces(Class<?> cls) {
+        HashSet<Class<?>> ret = new HashSet<Class<?>>();
+        getAllImplementedInterfaces(cls, ret);
+
+        ret.retainAll(supportedContextInterfaces);
+
+        return ret;
+    }
+
+    private static void getAllImplementedInterfaces(Class<?> cls, Set<Class<?>> output) {
+        Class<?>[] ifaces = cls.getInterfaces();
+
+        for (Class<?> iface : Arrays.asList(ifaces)) {
+            output.add(iface);
+            getAllImplementedInterfaces(iface, output);
+        }
+
+        if (cls.getSuperclass() != null) {
+            getAllImplementedInterfaces(cls.getSuperclass(), output);
+        }
+    }    
 }

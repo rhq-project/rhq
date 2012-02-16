2e7a519b1dc50b321804849fed2991af8e8ce909
Torben Jaeger <torben@jit-central.com>
2012-02-16 14:37:34 +0100

    BZ 791126 - Enable transactions subsystem.

--- modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/BaseComponent.java
+++ modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/BaseComponent.java
@@ -182,7 +182,7 @@ public class BaseComponent<T extends ResourceComponent<?>> implements ResourceCo
                 if (req.getDataType()== DataType.MEASUREMENT) {
                     if (!val.equals("no metrics available")) { // AS 7 returns this
                         try {
-                            Double d = Double.parseDouble((String)val);
+                            Double d = Double.parseDouble(getStringValue(val));
                             MeasurementDataNumeric data = new MeasurementDataNumeric(req,d);
                             report.addData(data);
                         } catch (NumberFormatException e) {
@@ -191,17 +191,22 @@ public class BaseComponent<T extends ResourceComponent<?>> implements ResourceCo
                     }
                 } else if (req.getDataType()== DataType.TRAIT) {
 
-                    String realVal;
-                    if (val instanceof String)
-                        realVal = (String)val;
-                    else
-                        realVal = String.valueOf(val);
+                    String realVal = getStringValue(val);
 
                     MeasurementDataTrait data = new MeasurementDataTrait(req,realVal);
                     report.addData(data);
                 }
             }
         }
+    }
+
+    private String getStringValue(Object val) {
+        String realVal;
+        if (val instanceof String)
+            realVal = (String)val;
+        else
+            realVal = String.valueOf(val);
+        return realVal;
     }
 
     /**

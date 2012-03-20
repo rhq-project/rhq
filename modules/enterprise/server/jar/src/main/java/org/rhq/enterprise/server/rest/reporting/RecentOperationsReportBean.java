c4920b52d6122cc08d49e4eb73863db3ca651ffe
Mike Thompson <mithomps@redhat.com>
2012-03-20 15:29:19 -0700

    [BZ 800453] Export Csv Reports. Added Recent Operations report.

---
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/rest/reporting/RecentOperationsReportBean.java
@@ -0,0 +1,80 @@
+package org.rhq.enterprise.server.rest.reporting;
+
+import org.rhq.core.domain.criteria.ResourceOperationHistoryCriteria;
+import org.rhq.core.domain.operation.ResourceOperationHistory;
+import org.rhq.core.domain.util.PageList;
+import org.rhq.enterprise.server.auth.SubjectManagerLocal;
+import org.rhq.enterprise.server.operation.OperationManagerLocal;
+import org.rhq.enterprise.server.rest.AbstractRestBean;
+import org.rhq.enterprise.server.rest.SetCallerInterceptor;
+import org.rhq.enterprise.server.util.CriteriaQuery;
+import org.rhq.enterprise.server.util.CriteriaQueryExecutor;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.rhq.enterprise.server.rest.reporting.ReportHelper.cleanForCSV;
+import static org.rhq.enterprise.server.rest.reporting.ReportHelper.formatDateTime;
+
+@Interceptors(SetCallerInterceptor.class)
+@Stateless
+public class RecentOperationsReportBean extends AbstractRestBean implements RecentOperationsReportLocal {
+
+    @EJB
+    private OperationManagerLocal operationManager;
+
+    @EJB
+    private SubjectManagerLocal subjectMgr;
+
+    @Override
+    public StreamingOutput recentAlerts(UriInfo uriInfo, javax.ws.rs.core.Request request, HttpHeaders headers ) {
+
+        return new StreamingOutput() {
+            @Override
+            public void write(OutputStream stream) throws IOException, WebApplicationException {
+                final ResourceOperationHistoryCriteria criteria = new ResourceOperationHistoryCriteria();
+
+                CriteriaQueryExecutor<ResourceOperationHistory, ResourceOperationHistoryCriteria> queryExecutor =
+                        new CriteriaQueryExecutor<ResourceOperationHistory, ResourceOperationHistoryCriteria>() {
+                            @Override
+                            public PageList<ResourceOperationHistory> execute(ResourceOperationHistoryCriteria criteria) {
+
+                                return operationManager.findResourceOperationHistoriesByCriteria(caller, criteria);
+                            }
+                        };
+
+                CriteriaQuery<ResourceOperationHistory, ResourceOperationHistoryCriteria> query =
+                        new CriteriaQuery<ResourceOperationHistory, ResourceOperationHistoryCriteria>(criteria, queryExecutor);
+
+                stream.write((getHeader() + "\n").getBytes());
+                for (ResourceOperationHistory alert : query) {
+                    String record = toCSV(alert)  + "\n";
+                    stream.write(record.getBytes());
+                }
+
+            }
+            private String toCSV(ResourceOperationHistory operation) {
+                return formatDateTime(operation.getStartedTime()) + "," +
+                        cleanForCSV(operation.getJobName()) + "," +
+                        operation.getSubjectName() + "," +
+                        operation.getStatus() + "," +
+                        cleanForCSV(operation.getResource().getName()) +","+
+                        cleanForCSV(operation.getResource().getAncestry());
+            }
+
+            private String getHeader(){
+                return "Date Submitted,Operation,Requestor,Status,Resource,Ancestry";
+            }
+
+        };
+
+    }
+
+}
---
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/rest/reporting/RecentOperationsReportLocal.java
@@ -0,0 +1,21 @@
+package org.rhq.enterprise.server.rest.reporting;
+
+import javax.ejb.Local;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
+
+@Path("/reports/recentOperations")
+@Local
+public interface RecentOperationsReportLocal {
+
+    @GET
+    @Path("/")
+    @Produces({"text/csv", "application/xml"})
+    StreamingOutput recentAlerts(
+            @Context UriInfo uriInfo,
+            @Context Request request,
+            @Context HttpHeaders headers);
+
+}

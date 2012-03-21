bb5196978b4b1bfd5e6d0753a80453352538c69b
Mike Thompson <mithomps@redhat.com>
2012-03-21 16:05:37 -0700

    [BZ 800453] Export Csv Reports. Add Recent Drift Handler.

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/rest/reporting/RecentDriftHandler.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/rest/reporting/RecentDriftHandler.java
@@ -1,9 +1,15 @@
 package org.rhq.enterprise.server.rest.reporting;
 
+import org.rhq.core.domain.criteria.DriftCriteria;
+import org.rhq.core.domain.criteria.GenericDriftCriteria;
+import org.rhq.core.domain.drift.DriftComposite;
+import org.rhq.core.domain.util.PageList;
 import org.rhq.enterprise.server.auth.SubjectManagerLocal;
 import org.rhq.enterprise.server.drift.DriftManagerLocal;
 import org.rhq.enterprise.server.rest.AbstractRestBean;
 import org.rhq.enterprise.server.rest.SetCallerInterceptor;
+import org.rhq.enterprise.server.util.CriteriaQuery;
+import org.rhq.enterprise.server.util.CriteriaQueryExecutor;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -14,6 +20,9 @@ import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
 import java.io.IOException;
 import java.io.OutputStream;
+
+import static org.rhq.enterprise.server.rest.reporting.ReportHelper.cleanForCSV;
+import static org.rhq.enterprise.server.rest.reporting.ReportHelper.formatDateTime;
 
 @Interceptors(SetCallerInterceptor.class)
 @Stateless
@@ -32,40 +41,42 @@ public class RecentDriftHandler extends AbstractRestBean implements RecentDriftL
         return new StreamingOutput() {
             @Override
             public void write(OutputStream stream) throws IOException, WebApplicationException {
+                GenericDriftCriteria criteria  = new GenericDriftCriteria();
                 //@todo: DriftCriteria add filtering params
+                //criteria.add
 
-//                CriteriaQueryExecutor<DriftComposite, DriftCriteria> queryExecutor =
-//                        new CriteriaQueryExecutor<DriftComposite, DriftCriteria>() {
-//                            @Override
-//                            public PageList<DriftComposite> execute(DriftCriteria criteria) {
-//
-//                                return driftManager.findDriftCompositesByCriteria(caller, criteria);
-//                            }
-//                        };
-//
-//                CriteriaQuery<DriftComposite, DriftCriteria> query =
-//                        new CriteriaQuery<DriftComposite, DriftCriteria>(criteria, queryExecutor);
-//
-//                stream.write((getHeader() + "\n").getBytes());
-//                for (DriftComposite alert : query) {
-//                    String record = toCSV(alert)  + "\n";
-//                    stream.write(record.getBytes());
-//                }
-//
-//            }
-//            private String toCSV(DriftComposite drift) {
-//                return formatDateTime(drift.getDrift().getCtime()) + "," +
-//                        cleanForCSV(drift.getDriftDefinitionName()) + "," +
-//                        "snapshot-TBD" + "," +
-//                        drift.getDrift().getCategory() + "," +
-//                        drift.getDrift().getPath() + "," +
-//                        cleanForCSV(drift.getResource().getName()) +","+
-//                        cleanForCSV(drift.getResource().getAncestry());
-//                //@todo:Snapshot
-//            }
-//
-//            private String getHeader(){
-//                return "Creation Time,Definition,Snapshot,Category,Path,Resource,Ancestry";
+                CriteriaQueryExecutor<DriftComposite, DriftCriteria> queryExecutor =
+                        new CriteriaQueryExecutor<DriftComposite, DriftCriteria>() {
+                            @Override
+                            public PageList<DriftComposite> execute(DriftCriteria criteria) {
+
+                                return driftManager.findDriftCompositesByCriteria(caller, criteria);
+                            }
+                        };
+
+                CriteriaQuery<DriftComposite, DriftCriteria> query =
+                        new CriteriaQuery<DriftComposite, DriftCriteria>(criteria, queryExecutor);
+
+                stream.write((getHeader() + "\n").getBytes());
+                for (DriftComposite alert : query) {
+                    String record = toCSV(alert)  + "\n";
+                    stream.write(record.getBytes());
+                }
+
+            }
+            private String toCSV(DriftComposite drift) {
+                return formatDateTime(drift.getDrift().getCtime()) + "," +
+                        cleanForCSV(drift.getDriftDefinitionName()) + "," +
+                        "snapshot-TBD" + "," +
+                        drift.getDrift().getCategory() + "," +
+                        drift.getDrift().getPath() + "," +
+                        cleanForCSV(drift.getResource().getName()) +","+
+                        cleanForCSV(drift.getResource().getAncestry());
+                //@todo:Snapshot
+            }
+
+            private String getHeader(){
+                return "Creation Time,Definition,Snapshot,Category,Path,Resource,Ancestry";
             }
 
         };

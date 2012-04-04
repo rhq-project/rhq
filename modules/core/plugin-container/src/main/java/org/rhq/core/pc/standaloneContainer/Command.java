23829f976ee97970f6c4ec053635a03b4710daa3
Heiko W. Rupp <hwr@redhat.com>
2012-04-04 09:58:17 +0200

    Improve help text.

--- modules/core/plugin-container/src/main/java/org/rhq/core/pc/standaloneContainer/Command.java
+++ modules/core/plugin-container/src/main/java/org/rhq/core/pc/standaloneContainer/Command.java
@@ -47,8 +47,8 @@ public enum Command {
     WAIT("w", "milliseconds", 1, "Waits the given amount of time"),
     P_CONFIG("pc", "", 0, "Shows the plugin configuration of the current resource."),
     R_CONFIG("rc", "", 0, "Shows the resource configuration of the current resource."),
-    SR_CONFIG("rcs", "", 1, "[-m] [parameters] set resource config. '-m' merges with current config; default is overwrite."),
-    SP_CONFIG("pcs", "", 1, "[-m] [parameters] set plugin config.'-m' merges with current config; default is overwrite.")
+    SR_CONFIG("rcs", "", 1, "[-m] [parameters] set resource config. '-m' merges with current config; default is overwrite. Properties are separated by ||."),
+    SP_CONFIG("pcs", "", 1, "[-m] [parameters] set plugin config.'-m' merges with current config; default is overwrite. Properties are separated by ||.")
     ;
 
     private String abbrev;

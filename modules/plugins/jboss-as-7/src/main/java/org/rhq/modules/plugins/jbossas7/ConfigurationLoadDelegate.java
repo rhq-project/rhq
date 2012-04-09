7370f3c9fc2f46d6b26b209c28d40bb954ec2d80
Stefan Negrea <snegrea@redhat.com>
2012-04-09 15:40:08 -0500

    Fix unit test failure. The root logger does not have a map in the config. The child: moniker maps the entire subsystem on property by property basis.

--- modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/ConfigurationLoadDelegate.java
+++ modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/ConfigurationLoadDelegate.java
@@ -238,15 +238,6 @@ public class ConfigurationLoadDelegate implements ConfigurationFacet {
                     if (propertyList != null)
                         config.put(propertyList);
                 } else if (propDef instanceof PropertyDefinitionMap) {
-
-                    PropertyGroupDefinition propertyGroupDefinition = propDef.getPropertyGroupDefinition();
-                    if (propertyGroupDefinition != null ) {
-                       if (propertyGroupDefinition.getName().startsWith("child:")) {
-                          valueObject = results;
-                       }
-                    }
-
-
                     PropertyMap propertyMap = loadHandlePropertyMap((PropertyDefinitionMap) propDef, valueObject, null);
 
                     if (propertyMap != null)

adb9f7ea0c3cea791bccb5c27dbdd4badcad349b
Stefan Negrea <snegrea@redhat.com>
2012-06-11 11:21:50 -0500

    Add two generic integration tests for Standalone server: 1) Update resoure configuration with the configuration loaded from server (no changes) 2) Execute all operations with zero arguments.
    The tests are currently disabled (and pending further development) because of test framework issues.

---
+++ modules/plugins/jboss-as-7/src/test/java/org/rhq/modules/plugins/jbossas7/itest/standalone/ResourcesStandaloneServerTest.java
@@ -0,0 +1,174 @@
+/*
+ * RHQ Management Platform
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.rhq.modules.plugins.jbossas7.itest.standalone;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import org.rhq.core.clientapi.agent.configuration.ConfigurationUpdateRequest;
+import org.rhq.core.clientapi.server.configuration.ConfigurationUpdateResponse;
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.operation.OperationDefinition;
+import org.rhq.core.domain.resource.InventoryStatus;
+import org.rhq.core.domain.resource.Resource;
+import org.rhq.core.domain.resource.ResourceCategory;
+import org.rhq.core.domain.resource.ResourceType;
+import org.rhq.core.pc.configuration.ConfigurationManager;
+import org.rhq.core.pc.inventory.InventoryManager;
+import org.rhq.core.pc.inventory.ResourceContainer;
+import org.rhq.modules.plugins.jbossas7.itest.AbstractJBossAS7PluginTest;
+import org.rhq.test.arquillian.RunDiscovery;
+
+/**
+ * @author Stefan Negrea
+ *
+ */
+@Test(groups = { "integration", "pc", "standalone" }, singleThreaded = true)
+public class ResourcesStandaloneServerTest extends AbstractJBossAS7PluginTest  {
+    private Log log = LogFactory.getLog(this.getClass());
+
+    @Test(priority = 10, groups = "discovery")
+    @RunDiscovery(discoverServices = true, discoverServers = true)
+    public void discoverPlatform() throws Exception {
+        InventoryManager inventoryManager = this.pluginContainer.getInventoryManager();
+
+        Resource platform = inventoryManager.getPlatform();
+        assertNotNull(platform);
+        assertEquals(platform.getInventoryStatus(), InventoryStatus.COMMITTED);
+
+        Thread.sleep(20 * 1000L);
+
+        ResourceContainer platformContainer = inventoryManager.getResourceContainer(platform);
+        Resource server = getResourceByTypeAndKey(platform, StandaloneServerComponentTest.RESOURCE_TYPE,
+            StandaloneServerComponentTest.RESOURCE_KEY);
+        inventoryManager.activateResource(server, platformContainer, false);
+
+        Thread.sleep(100 * 1000L);
+    }
+
+
+    @Test(priority = 12, enabled = false)
+    public void loadUpdateTemplatedResourceConfiguration() throws Exception {
+        Resource platform = this.pluginContainer.getInventoryManager().getPlatform();
+        Resource server = getResourceByTypeAndKey(platform, StandaloneServerComponentTest.RESOURCE_TYPE,
+            StandaloneServerComponentTest.RESOURCE_KEY);
+
+        ConfigurationManager configurationManager = this.pluginContainer.getConfigurationManager();
+        configurationManager.initialize();
+        Thread.sleep(100 * 1000L);
+
+        Queue<Resource> unparsedResources = new LinkedList<Resource>();
+        for (Resource topLevelResource : server.getChildResources()) {
+            ResourceType resourceType = new ResourceType(topLevelResource.getResourceType().getName(), PLUGIN_NAME,
+                ResourceCategory.SERVICE, null);
+            topLevelResource = getResourceByTypeAndKey(server, resourceType, topLevelResource.getResourceKey());
+
+            if (topLevelResource.getInventoryStatus().equals(InventoryStatus.COMMITTED)) {
+                unparsedResources.add(topLevelResource);
+            } else {
+                log.info("Subsystem not COMMITTED " + topLevelResource + " - " + topLevelResource.getInventoryStatus());
+            }
+        }
+
+        int errorCount = 0;
+        while (!unparsedResources.isEmpty()) {
+            Resource resourceUnderTest = unparsedResources.poll();
+
+            for (Resource childResource : resourceUnderTest.getChildResources()) {
+                if (childResource.getInventoryStatus().equals(InventoryStatus.COMMITTED)) {
+                    unparsedResources.add(childResource);
+                } else {
+                    log.info("Subsystem not COMMITTED " + childResource + " - " + childResource.getInventoryStatus());
+                }
+            }
+
+            Configuration resourceUnderTestConfig = configurationManager.loadResourceConfiguration(resourceUnderTest
+                .getId());
+
+            Assert.assertNotNull(resourceUnderTestConfig);
+
+            ConfigurationUpdateRequest updateRequest = new ConfigurationUpdateRequest(1, resourceUnderTestConfig,
+                resourceUnderTest.getId());
+            ConfigurationUpdateResponse updateResponse = configurationManager
+                .executeUpdateResourceConfigurationImmediately(updateRequest);
+
+            Assert.assertNotNull(updateResponse);
+            if (updateResponse.getErrorMessage() != null) {
+                log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                log.info(updateResponse.getErrorMessage());
+                log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+                errorCount++;
+            }
+        }
+
+        log.info("?????????????????????????????");
+        log.info(errorCount);
+        log.info("?????????????????????????????");
+
+        Assert.assertEquals(errorCount, 0);
+    }
+
+    @Test(priority = 11, enabled = false)
+    public void executeNoArgOperations() throws Exception {
+        final List<String> ignoredSubsystems = new ArrayList<String>();
+        ignoredSubsystems.add("ModCluster Standalone Service");
+
+        Resource platform = this.pluginContainer.getInventoryManager().getPlatform();
+        Resource server = getResourceByTypeAndKey(platform, StandaloneServerComponentTest.RESOURCE_TYPE,
+            StandaloneServerComponentTest.RESOURCE_KEY);
+
+        Queue<Resource> unparsedResources = new LinkedList<Resource>();
+        for (Resource topLevelResource : server.getChildResources()) {
+            if (topLevelResource.getInventoryStatus().equals(InventoryStatus.COMMITTED)
+                && !ignoredSubsystems.contains(topLevelResource.getResourceType().getName())) {
+                unparsedResources.add(topLevelResource);
+            }
+        }
+
+        while (!unparsedResources.isEmpty()) {
+            Resource resourceUnderTest = unparsedResources.poll();
+
+            for (Resource childResource : resourceUnderTest.getChildResources()) {
+                if (childResource.getInventoryStatus().equals(InventoryStatus.COMMITTED)) {
+                    unparsedResources.add(childResource);
+                }
+            }
+
+            for (OperationDefinition operationUnderTest : resourceUnderTest.getResourceType().getOperationDefinitions()) {
+                if (operationUnderTest.getParametersConfigurationDefinition() == null
+                    || operationUnderTest.getParametersConfigurationDefinition().getPropertyDefinitions().isEmpty()) {
+                    this.invokeOperationAndAssertSuccess(resourceUnderTest, operationUnderTest.getName(), null);
+                }
+            }
+        }
+    }
+}

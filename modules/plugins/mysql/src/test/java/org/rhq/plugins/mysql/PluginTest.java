6a23948ca9b2c87c3414ac78b4d20ee1a55b7fe5
Heiko W. Rupp <hwr@redhat.com>
2012-07-06 10:06:15 +0200

    Don't shut down the PC between tests and don't assume we have a MySQL server running.

--- modules/plugins/mysql/src/test/java/org/rhq/plugins/mysql/PluginTest.java
+++ modules/plugins/mysql/src/test/java/org/rhq/plugins/mysql/PluginTest.java
@@ -1,3 +1,22 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2012 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 package org.rhq.plugins.mysql;
 
 import org.rhq.core.domain.configuration.Configuration;
@@ -34,8 +53,9 @@ public class PluginTest extends ComponentTest {
 
     public void test() throws Exception {
         manuallyAdd("MySql Server");
-        ResourceComponent resource = getComponent("MySql Server");
-        assertUp(resource);
+        ResourceComponent resourceComponent = getComponent("MySql Server");
+//        assertUp(resource); // TODO this requires a running mysql server
+        assert resourceComponent != null;
     }
 
 }

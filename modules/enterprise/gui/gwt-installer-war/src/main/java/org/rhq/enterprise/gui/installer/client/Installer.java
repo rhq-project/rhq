89b106e3f1d870da035aae22a926e69a0efa22c5
John Mazzitelli <mazz@redhat.com>
2012-07-12 18:14:16 -0400

    the start of a GWT installer. this builds and runs for AS7 only (it uses EE6)

---
+++ modules/enterprise/gui/gwt-installer-war/src/main/java/org/rhq/enterprise/gui/installer/client/Installer.java
@@ -0,0 +1,68 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2012 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.enterprise.gui.installer.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.smartgwt.client.util.SC;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+
+import org.rhq.enterprise.gui.installer.client.gwt.InstallerGWTServiceAsync;
+
+/**
+ * The GWT {@link EntryPoint entry point} to the RHQ Installer GUI.
+ *
+ * @author John Mazzitelli
+ */
+public class Installer implements EntryPoint {
+
+    // This must come first to ensure proper I18N class loading for dev mode
+    private static final Messages MSG = GWT.create(Messages.class);
+
+    public void onModuleLoad() {
+        IButton button = new IButton(MSG.hello_msg());
+        button.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                InstallerGWTServiceAsync rpc = InstallerGWTServiceAsync.Util.getInstance();
+                rpc.testMe(new AsyncCallback<String>() {
+
+                    @Override
+                    public void onSuccess(String result) {
+                        SC.say("SUCCESS: " + result);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        SC.say("FAILURE: " + caught);
+                    }
+                });
+            }
+        });
+        button.draw();
+
+        // Remove loading image in case we don't completely cover it
+        Element loadingPanel = DOM.getElementById("Loading-Panel");
+        loadingPanel.removeFromParent();
+    }
+}
---
+++ modules/enterprise/gui/gwt-installer-war/src/main/java/org/rhq/enterprise/gui/installer/server/InstallerGWTServiceImpl.java
@@ -0,0 +1,40 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2012 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.enterprise.gui.installer.server;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import org.rhq.enterprise.gui.installer.client.gwt.InstallerGWTService;
+
+/**
+ * @author John Mazzitelli
+ */
+@WebServlet(value = "/org.rhq.enterprise.gui.installer.Installer/InstallerGWTService")
+public class InstallerGWTServiceImpl extends RemoteServiceServlet implements InstallerGWTService {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String testMe() {
+        return "foo";
+    }
+
+}

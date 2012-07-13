rhq-project/rhq

commit 1c7723b1fab7b3e0c16d94804014f70f3976960d

[BZ 675150 - GWT: Alert definition : Conditions : metrics not sorted] Added new a class for serted version of the SelectItem

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/alert/definitions/NewNotificationEditor.java
@@ -45,6 +45,7 @@ import org.rhq.core.domain.alert.notification.AlertNotification;
 import org.rhq.core.domain.resource.Resource;
 import org.rhq.core.domain.resource.ResourceType;
 import org.rhq.enterprise.gui.coregui.client.CoreGUI;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.LocatableDynamicForm;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.SeleniumUtility;
@@ -84,7 +85,7 @@ public class NewNotificationEditor extends LocatableDynamicForm {
         senderCanvasItem.setShowTitle(false);
         senderCanvasItem.setColSpan(2);
 
-        notificationSenderSelectItem = new SelectItem("notificationSender", MSG
+        notificationSenderSelectItem = new SortedSelectItem("notificationSender", MSG
             .view_alert_definition_notification_editor_sender());
         notificationSenderSelectItem.setDefaultToFirstOption(true);
         notificationSenderSelectItem.setWrapTitle(false);

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/admin/ServerPluginControlView.java
@@ -47,6 +47,7 @@ import org.rhq.enterprise.gui.coregui.client.CoreGUI;
 import org.rhq.enterprise.gui.coregui.client.ErrorMessageWindow;
 import org.rhq.enterprise.gui.coregui.client.ImageManager;
 import org.rhq.enterprise.gui.coregui.client.components.configuration.ConfigurationEditor;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
 import org.rhq.enterprise.gui.coregui.client.gwt.PluginGWTServiceAsync;
 import org.rhq.enterprise.gui.coregui.client.util.message.Message;
@@ -100,7 +101,7 @@ public class ServerPluginControlView extends LocatableVStack {
         executeButton.setSrc(ImageManager.getOperationIcon());
         executeButton.setPrompt(MSG.common_button_execute());
 
-        final SelectItem controlNamesItem = new SelectItem("controlMenu", MSG.view_admin_plugins_serverControls_name());
+        final SelectItem controlNamesItem = new SortedSelectItem("controlMenu", MSG.view_admin_plugins_serverControls_name());
         LinkedHashMap<String, String> controlNames = new LinkedHashMap<String, String>();
         for (ServerPluginControlDefinition def : controlDefinitions) {
             controlNames.put(def.getName(), def.getDisplayName());

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/alert/definitions/CliNotificationSenderForm.java
@@ -59,6 +59,7 @@ import org.rhq.enterprise.gui.coregui.client.CoreGUI;
 import org.rhq.enterprise.gui.coregui.client.ImageManager;
 import org.rhq.enterprise.gui.coregui.client.UserSessionManager;
 import org.rhq.enterprise.gui.coregui.client.components.form.RadioGroupWithComponentsItem;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.components.upload.DynamicFormHandler;
 import org.rhq.enterprise.gui.coregui.client.components.upload.DynamicFormSubmitCompleteEvent;
 import org.rhq.enterprise.gui.coregui.client.components.upload.PackageVersionFileUploadForm;
@@ -226,7 +227,7 @@ public class CliNotificationSenderForm extends AbstractNotificationSenderForm {
                     SectionItem userSection = new SectionItem("userSection");
                     userSection.setDefaultValue(MSG.view_alert_definition_notification_cliScript_editor_whichUser());
 
-                    repoSelector = new SelectItem(extendLocatorId("repoSelector"), MSG
+                    repoSelector = new SortedSelectItem(extendLocatorId("repoSelector"), MSG
                         .view_alert_definition_notification_cliScript_editor_selectRepo());
                     repoSelector.setDefaultToFirstOption(true);
                     repoSelector.setWrapTitle(false);
@@ -557,7 +558,7 @@ public class CliNotificationSenderForm extends AbstractNotificationSenderForm {
     private DynamicForm createExistingPackageForm() {
         LocatableDynamicForm form = new LocatableDynamicForm(extendLocatorId("existingPackageForm"));
         form.setTitleOrientation(TitleOrientation.TOP);
-        existingPackageSelector = new SelectItem(extendLocatorId("existingPackageSelector"), "");
+        existingPackageSelector = new SortedSelectItem(extendLocatorId("existingPackageSelector"), "");
         existingPackageSelector.setDefaultToFirstOption(true);
         existingPackageSelector.setWrapTitle(false);
         existingPackageSelector.setRedrawOnChange(true);

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/alert/definitions/NewConditionEditor.java
@@ -24,14 +24,12 @@
 package org.rhq.enterprise.gui.coregui.client.alert.definitions;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedSet;
 import java.util.TreeSet;
 
 import com.smartgwt.client.types.Alignment;
@@ -62,6 +60,7 @@ import org.rhq.core.domain.resource.ResourceType;
 import org.rhq.enterprise.gui.coregui.client.CoreGUI;
 import org.rhq.enterprise.gui.coregui.client.components.form.DurationItem;
 import org.rhq.enterprise.gui.coregui.client.components.form.NumberWithUnitsValidator;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.components.form.TimeUnit;
 import org.rhq.enterprise.gui.coregui.client.util.measurement.MeasurementParser;
 import org.rhq.enterprise.gui.coregui.client.util.message.Message;
@@ -176,7 +175,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
 
         setMargin(20);
 
-        conditionTypeSelectItem = new SelectItem("conditionType",
+        conditionTypeSelectItem = new SortedSelectItem("conditionType",
             MSG.view_alert_definition_condition_editor_option_label());
         LinkedHashMap<String, String> condTypes = new LinkedHashMap<String, String>(7);
         condTypes.put(AlertConditionCategory.AVAILABILITY.name(),
@@ -761,7 +760,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
             }
         }
 
-        SelectItem traitSelection = new SelectItem(TRAIT_METRIC_ITEMNAME,
+        SelectItem traitSelection = new SortedSelectItem(TRAIT_METRIC_ITEMNAME,
             MSG.view_alert_definition_condition_editor_metric_trait_change_value());
         traitSelection.setValueMap(traitsMap);
         traitSelection.setDefaultValue(traitsMap.keySet().iterator().next()); // just use the first one
@@ -782,7 +781,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         StaticTextItem helpItem = buildHelpTextItem("availabilityHelp", helpStr, ifFunc);
         formItems.add(helpItem);
 
-        SelectItem selection = new SelectItem(AVAILABILITY_ITEMNAME,
+        SelectItem selection = new SortedSelectItem(AVAILABILITY_ITEMNAME,
             MSG.view_alert_definition_condition_editor_availability_value());
         LinkedHashMap<String, String> avails = new LinkedHashMap<String, String>(2);
         avails.put(AlertConditionOperator.AVAIL_GOES_DOWN.name(),
@@ -815,7 +814,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         StaticTextItem helpItem = buildHelpTextItem("availabilityDurationHelp", helpStr, ifFunc);
         formItems.add(helpItem);
 
-        SelectItem selection = new SelectItem(AVAILABILITY_DURATION_ITEMNAME,
+        SelectItem selection = new SortedSelectItem(AVAILABILITY_DURATION_ITEMNAME,
             MSG.view_alert_definition_condition_editor_availabilityDuration());
         LinkedHashMap<String, String> avails = new LinkedHashMap<String, String>(2);
         avails.put(AlertConditionOperator.AVAIL_DURATION_DOWN.name(),
@@ -856,7 +855,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
             ops.put(opDef.getName(), opDef.getDisplayName());
         }
 
-        SelectItem opSelection = new SelectItem(OPERATION_NAME_ITEMNAME, MSG.common_title_value());
+        SelectItem opSelection = new SortedSelectItem(OPERATION_NAME_ITEMNAME, MSG.common_title_value());
         opSelection.setValueMap(ops);
         opSelection.setDefaultValue(ops.keySet().iterator().next()); // just use the first one
         opSelection.setWidth("*");
@@ -864,7 +863,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         opSelection.setShowIfCondition(ifFunc);
         formItems.add(opSelection);
 
-        SelectItem opResultsSelection = new SelectItem(OPERATION_RESULTS_ITEMNAME, MSG.common_title_status());
+        SelectItem opResultsSelection = new SortedSelectItem(OPERATION_RESULTS_ITEMNAME, MSG.common_title_status());
         LinkedHashMap<String, String> operationStatuses = new LinkedHashMap<String, String>(4);
         operationStatuses.put(OperationRequestStatus.INPROGRESS.name(), MSG.common_status_inprogress());
         operationStatuses.put(OperationRequestStatus.SUCCESS.name(), MSG.common_status_success());
@@ -969,15 +968,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         }
 
         LinkedHashMap<String, String> metricsMap = new LinkedHashMap<String, String>();
-        SortedSet<MeasurementDefinition> sortedDefs = new TreeSet<MeasurementDefinition>(
-            new Comparator<MeasurementDefinition>() {
-
-                @Override
-                public int compare(MeasurementDefinition o1, MeasurementDefinition o2) {
-                    return o1.getDisplayName().compareTo(o2.getDisplayName());
-                }
-            });
-        sortedDefs.addAll(this.resourceType.getMetricDefinitions());
+        Set<MeasurementDefinition> sortedDefs = new HashSet<MeasurementDefinition>(this.resourceType.getMetricDefinitions());
 
         for (MeasurementDefinition def : sortedDefs) {
             if (def.getDataType() == DataType.MEASUREMENT) {
@@ -994,7 +985,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
             return null; // all metrics should be hidden
         }
 
-        SelectItem metricSelection = new SelectItem(itemName,
+        SelectItem metricSelection = new SortedSelectItem(itemName,
             MSG.view_alert_definition_condition_editor_metric_threshold_name());
         metricSelection.setValueMap(metricsMap);
         metricSelection.setDefaultValue(metricsMap.keySet().iterator().next()); // just use the first one
@@ -1013,7 +1004,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
             }
         }
 
-        SelectItem metricSelection = new SelectItem(itemName,
+        SelectItem metricSelection = new SortedSelectItem(itemName,
             MSG.view_alert_definition_condition_editor_metric_calltime_common_name());
         metricSelection.setValueMap(metricsMap);
         metricSelection.setDefaultValue(metricsMap.keySet().iterator().next()); // just use the first one
@@ -1033,7 +1024,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         comparators.put(">", "> (" + MSG.view_alert_definition_condition_editor_metric_threshold_comparator_greater()
             + ")");
 
-        SelectItem comparatorSelection = new SelectItem(itemName,
+        SelectItem comparatorSelection = new SortedSelectItem(itemName,
             MSG.view_alert_definition_condition_editor_metric_threshold_comparator());
         comparatorSelection.setValueMap(comparators);
         comparatorSelection.setDefaultValue("<");
@@ -1051,7 +1042,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         comparators.put("CH", MSG.view_alert_definition_condition_editor_metric_calltime_common_comparator_changes());
         comparators.put("HI", MSG.view_alert_definition_condition_editor_metric_calltime_common_comparator_grows());
 
-        SelectItem comparatorSelection = new SelectItem(itemName,
+        SelectItem comparatorSelection = new SortedSelectItem(itemName,
             MSG.view_alert_definition_condition_editor_metric_calltime_common_comparator());
         comparatorSelection.setValueMap(comparators);
         comparatorSelection.setDefaultValue("CH");
@@ -1070,7 +1061,7 @@ public class NewConditionEditor extends LocatableDynamicForm {
         comparators.put("<=", MSG.view_alert_definition_condition_editor_metric_range_comparator_inside_inclusive());
         comparators.put(">=", MSG.view_alert_definition_condition_editor_metric_range_comparator_outside_inclusive());
 
-        SelectItem comparatorSelection = new SelectItem(itemName,
+        SelectItem comparatorSelection = new SortedSelectItem(itemName,
             MSG.view_alert_definition_condition_editor_metric_range_comparator());
         comparatorSelection.setValueMap(comparators);
         comparatorSelection.setDefaultValue("<");

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/alert/definitions/RecoveryAlertDefinitionForm.java
@@ -37,6 +37,7 @@ import org.rhq.core.domain.alert.AlertDefinition;
 import org.rhq.core.domain.criteria.AlertDefinitionCriteria;
 import org.rhq.core.domain.util.PageList;
 import org.rhq.enterprise.gui.coregui.client.CoreGUI;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.LocatableDynamicForm;
 
@@ -171,7 +172,7 @@ public class RecoveryAlertDefinitionForm extends LocatableDynamicForm implements
                 .view_alert_definition_recovery_editor_disable_when_fired());
             disableWhenFiredStatic.setWrapTitle(false);
 
-            recoverAlertSelection = new SelectItem("recoveryAlert", MSG
+            recoverAlertSelection = new SortedSelectItem("recoveryAlert", MSG
                 .view_alert_definition_recovery_editor_recovery_alert());
             recoverAlertSelection.setDefaultValue("0");
             recoverAlertSelection.setWrapTitle(false);

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/alert/definitions/ResourceOperationNotificationSenderForm.java
@@ -59,9 +59,10 @@ import org.rhq.core.domain.util.PageList;
 import org.rhq.enterprise.gui.coregui.client.CoreGUI;
 import org.rhq.enterprise.gui.coregui.client.alert.definitions.ResourceOperationNotificationInfo.ResourceSelectionMode;
 import org.rhq.enterprise.gui.coregui.client.components.configuration.ConfigurationEditor;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
-import org.rhq.enterprise.gui.coregui.client.inventory.resource.selection.SingleResourcePicker;
 import org.rhq.enterprise.gui.coregui.client.inventory.resource.selection.ResourcePicker.OkHandler;
+import org.rhq.enterprise.gui.coregui.client.inventory.resource.selection.SingleResourcePicker;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.LocatableDynamicForm;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.LocatableHLayout;
 import org.rhq.enterprise.gui.coregui.client.util.selenium.SeleniumUtility;
@@ -109,7 +110,7 @@ public class ResourceOperationNotificationSenderForm extends AbstractNotificatio
         operationArgumentsCanvasItem.setHeight(400);
         operationArgumentsCanvasItem.setWidth(500);
 
-        operationSelectItem = new SelectItem("operationSelectItem", MSG
+        operationSelectItem = new SortedSelectItem("operationSelectItem", MSG
             .view_alert_definition_notification_operation_editor_common_operation());
         operationSelectItem.setStartRow(true);
         operationSelectItem.setEndRow(true);
@@ -160,7 +161,7 @@ public class ResourceOperationNotificationSenderForm extends AbstractNotificatio
 
         // for RELATIVE mode
 
-        ancestorTypeSelectItem = new SelectItem("ancestorTypeSelectItem", MSG
+        ancestorTypeSelectItem = new SortedSelectItem("ancestorTypeSelectItem", MSG
             .view_alert_definition_notification_operation_editor_relative_ancestor());
         ancestorTypeSelectItem.setStartRow(true);
         ancestorTypeSelectItem.setEndRow(true);
@@ -178,7 +179,7 @@ public class ResourceOperationNotificationSenderForm extends AbstractNotificatio
             }
         });
 
-        descendantTypeSelectItem = new SelectItem("descendantTypeSelectItem", MSG
+        descendantTypeSelectItem = new SortedSelectItem("descendantTypeSelectItem", MSG
             .view_alert_definition_notification_operation_editor_relative_descendant());
         descendantTypeSelectItem.setStartRow(true);
         descendantTypeSelectItem.setEndRow(false);
@@ -208,7 +209,7 @@ public class ResourceOperationNotificationSenderForm extends AbstractNotificatio
 
         // the mode selector menu
 
-        modeSelectItem = new SelectItem("modeSelectItem", MSG
+        modeSelectItem = new SortedSelectItem("modeSelectItem", MSG
             .view_alert_definition_notification_operation_editor_mode_title());
         modeSelectItem.setStartRow(true);
         modeSelectItem.setEndRow(true);

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/bundle/BundleSelector.java
@@ -32,6 +32,7 @@ import org.rhq.core.domain.bundle.BundleType;
 import org.rhq.core.domain.criteria.BundleCriteria;
 import org.rhq.enterprise.gui.coregui.client.CoreGUI;
 import org.rhq.enterprise.gui.coregui.client.bundle.list.BundlesDataSource;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.components.selector.AbstractSelector;
 import org.rhq.enterprise.gui.coregui.client.gwt.BundleGWTServiceAsync;
 import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
@@ -54,7 +55,7 @@ public class BundleSelector extends AbstractSelector<Bundle, BundleCriteria> {
         availableFilterForm.setNumCols(4);
         final TextItem search = new TextItem("search", MSG.common_title_search());
 
-        final SelectItem bundleTypeSelect = new SelectItem("bundleType", MSG.view_bundle_bundleType());
+        final SelectItem bundleTypeSelect = new SortedSelectItem("bundleType", MSG.view_bundle_bundleType());
         bundleService.getAllBundleTypes(new AsyncCallback<ArrayList<BundleType>>() {
             public void onFailure(Throwable caught) {
                 CoreGUI.getErrorHandler().handleError(MSG.dataSource_bundle_loadFailed(), caught);

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/bundle/create/BundleTypeDropDownSelectItem.java
@@ -23,12 +23,12 @@ import java.util.HashMap;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.types.Alignment;
-import com.smartgwt.client.widgets.form.fields.SelectItem;
 import com.smartgwt.client.widgets.form.fields.events.ChangedEvent;
 import com.smartgwt.client.widgets.form.fields.events.ChangedHandler;
 
 import org.rhq.core.domain.bundle.BundleType;
 import org.rhq.enterprise.gui.coregui.client.CoreGUI;
+import org.rhq.enterprise.gui.coregui.client.components.form.SortedSelectItem;
 import org.rhq.enterprise.gui.coregui.client.gwt.BundleGWTServiceAsync;
 import org.rhq.enterprise.gui.coregui.client.gwt.GWTServiceLookup;
 import org.rhq.enterprise.gui.coregui.client.util.message.Message;
@@ -37,7 +37,7 @@ import org.rhq.enterprise.gui.coregui.client.util.message.Message.Severity;
 /**
  * Provides a drop down menu that allows one to select a bundle type.
  */
-public class BundleTypeDropDownSelectItem extends SelectItem {
+public class BundleTypeDropDownSelectItem extends SortedSelectItem {
 
     private final HashMap<String, BundleType> knownBundleTypes = new HashMap<String, BundleType>();
     private BundleType selected = null;

commit 5f0388a7ba23c1f395892950c322e3d649840d2f

[BZ 839720 - The alert notifications dialog initially shows wrong UI] This was caused by commit 1c7723b

modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/alert/definitions/NewNotificationEditor.java
@@ -118,6 +118,7 @@ public class NewNotificationEditor extends LocatableDynamicForm {
                         notificationSenderSelectItem.setDisabled(false);
                         notificationSenderSelectItem.redraw();
 
+                        notificationSenderSelectItem.setValue(result[0]);
                         switchToAlertSender(result[0]);
                         senderCanvasItem.show();
                     } else {
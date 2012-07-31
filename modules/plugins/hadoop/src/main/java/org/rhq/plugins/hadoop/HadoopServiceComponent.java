0e51f3179a68b70cbaa73d25063d823ea01e1cbd
Jirka Kremser <jkremser@redhat.com>
2012-07-31 18:37:05 +0200

    Metrics subsystem is now collecting the data via JMX

--- modules/plugins/hadoop/src/main/java/org/rhq/plugins/hadoop/HadoopServiceComponent.java
+++ modules/plugins/hadoop/src/main/java/org/rhq/plugins/hadoop/HadoopServiceComponent.java
@@ -44,6 +44,7 @@ import org.rhq.core.domain.configuration.definition.PropertyDefinition;
 import org.rhq.core.domain.configuration.definition.PropertyDefinitionSimple;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.domain.measurement.MeasurementDataNumeric;
+import org.rhq.core.domain.measurement.MeasurementDataTrait;
 import org.rhq.core.domain.measurement.MeasurementReport;
 import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
 import org.rhq.core.pluginapi.configuration.ConfigurationFacet;
@@ -57,22 +58,22 @@ import org.rhq.plugins.jmx.JMXServerComponent;
 
 public class HadoopServiceComponent extends JMXServerComponent<ResourceComponent<?>> implements
     JMXComponent<ResourceComponent<?>>, MeasurementFacet, OperationFacet, ConfigurationFacet {
-    
+
     private static final Log LOG = LogFactory.getLog(HadoopServiceComponent.class);
 
     private static final XMLInputFactory XML_INPUT_FACTORY = XMLInputFactory.newInstance();
-    
+
     private static final String PROPERTY_TAG_NAME = "property";
     private static final String NAME_TAG_NAME = "name";
     private static final String VALUE_TAG_NAME = "value";
-    
+
     /**
      * Return availability of this resource
      *  @see org.rhq.core.pluginapi.inventory.ResourceComponent#getAvailability()
      */
     @Override
     public AvailabilityType getAvailability() {
-        return getResourceContext().getNativeProcess().isRunning() ? AvailabilityType.UP: AvailabilityType.DOWN;
+        return getResourceContext().getNativeProcess().isRunning() ? AvailabilityType.UP : AvailabilityType.DOWN;
     }
 
     @Override
@@ -90,55 +91,68 @@ public class HadoopServiceComponent extends JMXServerComponent<ResourceComponent
      *  @see org.rhq.core.pluginapi.measurement.MeasurementFacet#getValues(org.rhq.core.domain.measurement.MeasurementReport, java.util.Set)
      */
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) throws Exception {
-
-        for (MeasurementScheduleRequest req : metrics) {
-            String property = req.getName();
-            String props[] = property.split("\\|");
-
-            EmsConnection conn = getEmsConnection();
-            EmsBean bean = conn.getBean(props[0]);
-            if (bean != null) {
-                bean.refreshAttributes();
-                EmsAttribute att = bean.getAttribute(props[1]);
-                if (att != null) {
-                    Long val = (Long) att.getValue(); // TODO check for real type
-
-                    MeasurementDataNumeric res = new MeasurementDataNumeric(req, Double.valueOf(val));
-                    report.addData(res);
-                } else
-                    LOG.warn("Attribute " + props[1] + " not found");
-            } else
-                LOG.warn("MBean " + props[0] + " not found");
+        for (MeasurementScheduleRequest request : metrics) {
+            String name = request.getName();
+            int delimIndex = name.lastIndexOf(':');
+            String beanName = name.substring(0, delimIndex);
+            String attributeName = name.substring(delimIndex + 1);
+            try {
+                EmsConnection emsConnection = getEmsConnection();
+                EmsBean bean = emsConnection.getBean(beanName);
+                if (bean != null) {
+                    bean.refreshAttributes();
+                    EmsAttribute attribute = bean.getAttribute(attributeName);
+                    if (attribute != null) {
+                        Object valueObject = attribute.refresh();
+                        if (valueObject instanceof Number) {
+                            Number value = (Number) valueObject;
+                            report.addData(new MeasurementDataNumeric(request, value.doubleValue()));
+                        } else {
+                            report.addData(new MeasurementDataTrait(request, valueObject.toString()));
+                        }
+                    } else {
+                        LOG.warn("Attribute " + attributeName + " not found");
+                    }
+                } else {
+                    LOG.warn("MBean " + beanName + " not found");
+                }
+            } catch (Exception e) {
+                LOG.error("Failed to obtain measurement [" + name + "]", e);
+            }
         }
     }
 
     public Configuration loadResourceConfiguration() throws Exception {
-        ConfigurationDefinition definition = getResourceContext().getResourceType().getResourceConfigurationDefinition();
+        ConfigurationDefinition definition = getResourceContext().getResourceType()
+            .getResourceConfigurationDefinition();
         Configuration config = new Configuration();
-        
-        File homeDir = new File(getResourceContext().getPluginConfiguration().getSimpleValue(HadoopServiceDiscovery.HOME_DIR_PROPERTY));
-        
+
+        File homeDir = new File(getResourceContext().getPluginConfiguration().getSimpleValue(
+            HadoopServiceDiscovery.HOME_DIR_PROPERTY));
+
         if (!homeDir.exists()) {
-            throw new IllegalArgumentException("The configured home directory of this Hadoop instance (" + homeDir.getAbsolutePath() + ") no longer exists.");
+            throw new IllegalArgumentException("The configured home directory of this Hadoop instance ("
+                + homeDir.getAbsolutePath() + ") no longer exists.");
         }
-        
+
         if (!homeDir.isDirectory()) {
-            throw new IllegalArgumentException("The configured home directory of this Hadoop instance (" + homeDir.getAbsolutePath() + ") is not a directory.");
+            throw new IllegalArgumentException("The configured home directory of this Hadoop instance ("
+                + homeDir.getAbsolutePath() + ") is not a directory.");
         }
-        
+
         if (!homeDir.canRead()) {
-            throw new IllegalArgumentException("The configured home directory of this Hadoop instance (" + homeDir.getAbsolutePath() + ") is not readable.");
+            throw new IllegalArgumentException("The configured home directory of this Hadoop instance ("
+                + homeDir.getAbsolutePath() + ") is not readable.");
         }
-        
+
         fillResourceConfiguration(homeDir, config, definition);
-        
+
         return config;
     }
-    
 
     public void updateResourceConfiguration(ConfigurationUpdateReport report) {
         // TODO Auto-generated method stub
-        
+
     }
 
     /**
@@ -157,59 +171,62 @@ public class HadoopServiceComponent extends JMXServerComponent<ResourceComponent
         }
         return res;
     }
-    
-    public static void fillResourceConfiguration(File homeDir, Configuration config, ConfigurationDefinition definition) throws XMLStreamException, IOException {
+
+    public static void fillResourceConfiguration(File homeDir, Configuration config, ConfigurationDefinition definition)
+        throws XMLStreamException, IOException {
         //the config is just a bunch of simples, so this is rather easy.. no cumbersome traversal of property maps and lists
-        
+
         Map<String, PropertySimple> propertiesToFind = new HashMap<String, PropertySimple>();
         Set<File> configFilesToParse = new HashSet<File>();
-        
-        for(PropertyDefinition pd : definition.getPropertyDefinitions().values()) {
+
+        for (PropertyDefinition pd : definition.getPropertyDefinitions().values()) {
             if (!(pd instanceof PropertyDefinitionSimple)) {
                 //hmm... well, someone thought it's enough to change the config and the code would be clever.
                 //it's not ;)
                 continue;
             }
-            
+
             String propertyName = pd.getName();
             String[] parts = propertyName.split(":");
             String fileName = parts[0];
             String configName = parts[1];
-            
+
             File configFile = new File(homeDir, fileName);
-            
+
             if (!configFile.exists()) {
-                throw new IllegalArgumentException("The expected configuration file (" + configFile.getAbsolutePath() + ") doesn't exist.");
+                throw new IllegalArgumentException("The expected configuration file (" + configFile.getAbsolutePath()
+                    + ") doesn't exist.");
             }
-            
+
             configFilesToParse.add(configFile);
-            
+
             PropertySimple prop = new PropertySimple();
             prop.setName(propertyName);
             config.put(prop);
-            
+
             propertiesToFind.put(configName, prop);
         }
-        
-        for(File configFile : configFilesToParse) {
+
+        for (File configFile : configFilesToParse) {
             parseAndAssignProps(configFile, propertiesToFind);
         }
     }
-    
-    private static void parseAndAssignProps(File configFile, Map<String, PropertySimple> props) throws XMLStreamException, IOException {
+
+    private static void parseAndAssignProps(File configFile, Map<String, PropertySimple> props)
+        throws XMLStreamException, IOException {
         FileInputStream in = new FileInputStream(configFile);
         XMLStreamReader rdr = XML_INPUT_FACTORY.createXMLStreamReader(in);
         try {
             boolean inProperty = false;
             String propertyName = null;
             String propertyValue = null;
-            
-            while(rdr.hasNext()) {
+
+            while (rdr.hasNext()) {
                 int event = rdr.next();
-                
+
                 String tag = null;
-                
-                switch(event) {
+
+                switch (event) {
                 case XMLStreamReader.START_ELEMENT:
                     tag = rdr.getName().getLocalPart();
                     if (PROPERTY_TAG_NAME.equals(tag)) {
@@ -224,12 +241,12 @@ public class HadoopServiceComponent extends JMXServerComponent<ResourceComponent
                     tag = rdr.getName().getLocalPart();
                     if (PROPERTY_TAG_NAME.equals(tag)) {
                         inProperty = false;
-                        
+
                         PropertySimple prop = props.get(propertyName);
                         if (prop != null) {
                             prop.setValue(propertyValue);
                         }
-                        
+
                         propertyName = null;
                         propertyValue = null;
                     }
@@ -241,5 +258,5 @@ public class HadoopServiceComponent extends JMXServerComponent<ResourceComponent
             in.close();
         }
     }
-    
+
 }

52c9717c80df575fda23745e935b8974726c2053
John Mazzitelli <mazz@redhat.com>
2012-10-04 11:54:37 -0400

    trying to prototype the new rhq-plugin. has a hack in it - we don't want the hack

--- modules/plugins/jboss-as-7-jmx/src/main/java/org/rhq/plugins/jbossas7jmx/JBossAS7JMXComponent.java
+++ modules/plugins/jboss-as-7-jmx/src/main/java/org/rhq/plugins/jbossas7jmx/JBossAS7JMXComponent.java
@@ -1,54 +1,182 @@
-/*
- * RHQ Management Platform
- * Copyright 2011, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 package org.rhq.plugins.jbossas7jmx;
 
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mc4j.ems.connection.ConnectionFactory;
+import org.mc4j.ems.connection.EmsConnection;
+import org.mc4j.ems.connection.settings.ConnectionSettings;
+import org.mc4j.ems.connection.support.ConnectionProvider;
+import org.mc4j.ems.connection.support.metadata.JSR160ConnectionTypeDescriptor;
+
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
+import org.rhq.plugins.jmx.JMXComponent;
 
 /**
- * This resource does not really do anything, it serves as a parent to MBeans being exposed for
- * management via plugins injecting child resources.
- * 
+ * This resource serves as the parent to MBeans being exposed for management via plugins injecting child resources.
+ * This resource provides the connection to the parent AS7's MBeanServer.
+ *
+ * Note that this resource must be configured to have its own hostname - this is because this could be different
+ * than the public binding address. The hostname this resource needs is called "jboss.bind.address.management" in the AS7
+ * configuration.
+ *
+ * Additionally, the username and password are that of a valid management user defined for the AS7 instance.
+ *
  * @author Jay Shaughnessy
+ * @author John Mazzitelli
  */
-public class JBossAS7JMXComponent<T extends ResourceComponent<?>> implements ResourceComponent<T> {
+public class JBossAS7JMXComponent<T extends ResourceComponent<?>> implements ResourceComponent<T>, JMXComponent<T> {
+
+    public static final String PLUGIN_CONFIG_CLIENT_JAR_LOCATION = "clientJarLocation";
+    public static final String PLUGIN_CONFIG_PORT = "port";
+    public static final String PLUGIN_CONFIG_HOSTNAME = "hostname"; // jboss.bind.address.management
+    public static final String PLUGIN_CONFIG_USERNAME = "username";
+    public static final String PLUGIN_CONFIG_PASSWORD = "password";
+    public static final String DEFAULT_PLUGIN_CONFIG_PORT = "6999";
+
+    private Log log = LogFactory.getLog(JBossAS7JMXComponent.class);
+    private ResourceContext<T> resourceContext;
+    private EmsConnection connection;
+
+    /**
+     * Controls the dampening of connection error stack traces in an attempt to control spam to the log
+     * file. Each time a connection error is encountered, this will be incremented. When the connection
+     * is finally established, this will be reset to zero.
+     */
+    private int consecutiveConnectionErrors;
 
     @Override
     public AvailabilityType getAvailability() {
+        // TODO: this should return DOWN if the jmx remote port is inaccessible
         return AvailabilityType.UP;
     }
 
     @Override
     public void start(ResourceContext<T> context) throws InvalidPluginConfigurationException, Exception {
-
-        // has no startup activity
-        return;
+        this.resourceContext = context;
+        loadConnection();
     }
 
     @Override
     public void stop() {
-
-        // has no stop activity
         return;
+    }
+
+    @Override
+    public EmsConnection getEmsConnection() {
+        EmsConnection emsConnection = null;
+
+        try {
+            emsConnection = loadConnection();
+        } catch (Exception e) {
+            log.error("Component attempting to access a connection that could not be loaded");
+        }
+
+        return emsConnection;
+    }
+
+    private EmsConnection loadConnection() throws Exception {
+        if (connection != null) {
+            return connection;
+        }
+
+        try {
+
+            // TODO can I get the parent-parent context?????
+
+            Configuration pluginConfig = resourceContext.getPluginConfiguration();
+            String hostname = pluginConfig.getSimpleValue(PLUGIN_CONFIG_HOSTNAME, "127.0.0.1");
+            String port = pluginConfig.getSimpleValue(PLUGIN_CONFIG_PORT, DEFAULT_PLUGIN_CONFIG_PORT);
+            String username = pluginConfig.getSimpleValue(PLUGIN_CONFIG_USERNAME, "rhqadmin");
+            String password = pluginConfig.getSimpleValue(PLUGIN_CONFIG_PASSWORD, "rhqadmin");
+
+            ConnectionSettings connectionSettings = new ConnectionSettings();
+            connectionSettings.initializeConnectionType(new JSR160ConnectionTypeDescriptor());
+            connectionSettings.setServerUrl("service:jmx:remoting-jmx://" + hostname + ":" + port);
+            connectionSettings.setPrincipal(username);
+            connectionSettings.setCredentials(password);
+            //String connectionTypeDescriptorClass = pluginConfig.getSimple(JMXDiscoveryComponent.CONNECTION_TYPE).getStringValue();
+
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.discoverServerClasses(connectionSettings);
+
+            if (connectionSettings.getAdvancedProperties() == null) {
+                connectionSettings.setAdvancedProperties(new Properties());
+            }
+
+            // Tell EMS to make copies of jar files so that the ems classloader doesn't lock
+            // application files (making us unable to update them)  Bug: JBNADM-670
+            // TODO GH: turn this off in the embedded case
+            //connectionSettings.getControlProperties().setProperty(ConnectionFactory.COPY_JARS_TO_TEMP,  String.valueOf(Boolean.TRUE));
+
+            // But tell it to put them in a place that we clean up when shutting down the agent
+            //connectionSettings.getControlProperties().setProperty(ConnectionFactory.JAR_TEMP_DIR, context.getParentResourceContext().getTemporaryDirectory().getAbsolutePath());
+            //connectionSettings.getAdvancedProperties().setProperty(InternalVMTypeDescriptor.DEFAULT_DOMAIN_SEARCH, "jboss");
+
+            log.info("Loading AS7 connection [" + connectionSettings.getServerUrl() + "] with install path ["
+                + connectionSettings.getLibraryURI() + "]...");
+
+            ConnectionProvider connectionProvider = connectionFactory.getConnectionProvider(connectionSettings);
+            this.connection = connectionProvider.connect();
+
+            //connectionSettings.getAdvancedProperties().setProperty(JNP_DISABLE_DISCOVERY_JNP_INIT_PROP, "true");
+
+            // Make sure the timeout always happens, even if the JBoss server is hung.
+            //connectionSettings.getAdvancedProperties().setProperty("jnp.timeout", String.valueOf(JNP_TIMEOUT));
+            //connectionSettings.getAdvancedProperties().setProperty("jnp.sotimeout", String.valueOf(JNP_SO_TIMEOUT));
+
+            /*
+            try {
+                org.mc4j.ems.impl.JMXRemotingConnectionProvider foo;
+                JMXServiceURL serviceURL = new JMXServiceURL("service:jmx:remoting-jmx://" + hostname + ":" + port);
+                Map<String, String[]> env = new HashMap<String, String[]>();
+                env.put(JMXConnector.CREDENTIALS, new String[] { username, password });
+                RemotingConnectorProvider provider = new RemotingConnectorProvider();
+                JMXConnector connector = provider.newJMXConnector(serviceURL, env);
+                MBeanServerConnection conn = connector.getMBeanServerConnection();
+
+            } catch (Exception e) {
+            }
+            */
+            if (log.isDebugEnabled()) {
+                log.debug("Successfully made connection to the AS7 instance for resource ["
+                    + resourceContext.getResourceKey() + "]");
+            }
+
+            return connection;
+
+        } catch (Exception e) {
+            // The connection will be established even in the case that the principal cannot be authenticated,
+            // but the connection will not work. That failure seems to come from the call to loadSynchronous after
+            // the connection is established. If we get to this point that an exception was thrown, close any
+            // connection that was made and null it out so we can try to establish it again.
+            if (connection != null) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Connection created but an exception was thrown. Closing the connection.", e);
+                }
+                connection.close();
+                connection = null;
+            }
+
+            // Since the connection is attempted each time it's used, failure to connect could result in log
+            // file spamming. Log it once for every 10 consecutive times it's encountered.
+            if (consecutiveConnectionErrors % 10 == 0) {
+                log.warn("Could not establish connection to the JBoss AS instance ["
+                    + (consecutiveConnectionErrors + 1) + "] times for resource [" + resourceContext.getResourceKey()
+                    + "]", e);
+            }
+            consecutiveConnectionErrors++;
+
+            if (log.isDebugEnabled()) {
+                log.debug("Can't connect to JBoss AS resource  [" + resourceContext.getResourceKey() + "]", e);
+            }
+
+            throw e;
+        }
     }
 }
--- modules/plugins/jboss-as-7-jmx/src/main/java/org/rhq/plugins/jbossas7jmx/JBossAS7JMXDiscoveryComponent.java
+++ modules/plugins/jboss-as-7-jmx/src/main/java/org/rhq/plugins/jbossas7jmx/JBossAS7JMXDiscoveryComponent.java
@@ -1,30 +1,16 @@
-/*
- * RHQ Management Platform
- * Copyright 2011, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 package org.rhq.plugins.jbossas7jmx;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.pluginapi.inventory.ClassLoaderFacet;
 import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
@@ -32,34 +18,101 @@ import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
 
 /**
- * Just returns the singleton jmx mbeans parent resource.x
+ * Just returns the singleton jmx mbeans parent resource.
  * 
  * @author Jay Shaughnessy
+ * @author John Mazzitelli
  */
-public class JBossAS7JMXDiscoveryComponent<T extends ResourceComponent<?>> implements ResourceDiscoveryComponent<T>,
-    ClassLoaderFacet<ResourceComponent<?>> {
+public class JBossAS7JMXDiscoveryComponent<T extends ResourceComponent<JBossAS7JMXComponent<?>>> implements
+    ResourceDiscoveryComponent<T>, ClassLoaderFacet<ResourceComponent<JBossAS7JMXComponent<?>>> {
+
+    private Log log = LogFactory.getLog(JBossAS7JMXDiscoveryComponent.class);
 
     @Override
-    public List<URL> getAdditionalClasspathUrls(ResourceDiscoveryContext<ResourceComponent<?>> context,
+    public List<URL> getAdditionalClasspathUrls(
+        ResourceDiscoveryContext<ResourceComponent<JBossAS7JMXComponent<?>>> context,
         DiscoveredResourceDetails details) throws Exception {
-        // TODO we need to get the AS7's install directory so we can find the client jars
-        //      the parent component is the JBossAS7JMX server resource, see if it can somehow ask its parent
-        //      for this information which can then be passed down to us. Not sure how to do this - via plugin config perhaps?
-        return null;
+
+        Configuration pluginConfig = details.getPluginConfiguration();
+        String clientJarLocation = pluginConfig.getSimpleValue(JBossAS7JMXComponent.PLUGIN_CONFIG_CLIENT_JAR_LOCATION);
+        if (clientJarLocation == null) {
+            log.warn("Missing the client jar location - cannot connect to the JBossAS instance: "
+                + details.getResourceKey());
+            return null;
+        }
+
+        File clientJarDir = new File(clientJarLocation);
+        if (!clientJarDir.isDirectory()) {
+            log.warn("The client jar location [" + clientJarDir.getAbsolutePath()
+                + "] does not exist - cannot connect to the JBossAS instance: " + details.getResourceKey());
+            return null;
+        }
+
+        ArrayList<URL> clientJars = new ArrayList<URL>();
+        for (File clientJarFile : clientJarDir.listFiles()) {
+            if (clientJarFile.getName().endsWith(".jar")) {
+                clientJars.add(clientJarFile.toURI().toURL());
+            }
+        }
+
+        if (clientJars.size() == 0) {
+            log.warn("The client jar location [" + clientJarDir.getAbsolutePath()
+                + "] is missing client jars - cannot connect to the JBossAS instance: " + details.getResourceKey());
+            return null;
+        }
+
+        // XXX: hack to get the JMX plugin class in our classloader
+        File tmpDir = new File("/home/mazz/source/rhq/modules/enterprise/agent/target/rhq-agent/data/tmp");
+        File pluginsDir = new File(tmpDir, "../../plugins");
+        for (File jarFile : pluginsDir.listFiles()) {
+            if (jarFile.getName().startsWith("rhq-jmx-plugin")) {
+                clientJars.add(jarFile.toURI().toURL());
+                break;
+            }
+        }
+        clientJars
+            .add(new File("/home/mazz/.m2/repository/mc4j/org-mc4j-ems/1.3/org-mc4j-ems-1.3.jar").toURI().toURL());
+
+        return clientJars;
     }
 
     @Override
     public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext<T> context) {
+
+        // TODO: use additional methods to look around for other places where this can be find
+        // for example, we might be able to look in the /modules directory for some jars to use if the bin/client dir is gone
+        File clientJarDir = null;
+        String homeDirStr = context.getParentResourceContext().getPluginConfiguration().getSimpleValue("homeDir");
+        if (homeDirStr != null) {
+            File homeDirFile = new File(homeDirStr);
+            if (homeDirFile.exists()) {
+                clientJarDir = new File(homeDirFile, "bin/client");
+                if (!clientJarDir.exists()) {
+                    log.warn("The client jar location [" + clientJarDir.getAbsolutePath()
+                        + "] does not exist - will not be able to connect to the AS7 instance");
+                }
+            }
+        }
+
+        String clientJarLocation = (clientJarDir != null) ? clientJarDir.getAbsolutePath() : null;
 
         HashSet<DiscoveredResourceDetails> result = new HashSet<DiscoveredResourceDetails>();
 
         String key = "JBossAS7JMX";
         String name = "jmx mbeans";
         String version = "7"; // this should probably be the actual version of the remote AS7 server being monitored
-        String description = "JMX MBeans Deployed to AS 7.x";
+        String description = "Container for JMX MBeans deployed to AS7";
+
+        Configuration pluginConfig = context.getDefaultPluginConfiguration();
+        pluginConfig.setSimpleValue(JBossAS7JMXComponent.PLUGIN_CONFIG_HOSTNAME, "127.0.0.1"); // TODO how can we get this from the parent?
+        pluginConfig.setSimpleValue(JBossAS7JMXComponent.PLUGIN_CONFIG_PORT,
+            JBossAS7JMXComponent.DEFAULT_PLUGIN_CONFIG_PORT); // TODO how can we get this from the parent?
+        pluginConfig.setSimpleValue(JBossAS7JMXComponent.PLUGIN_CONFIG_USERNAME, "rhqadmin");
+        pluginConfig.setSimpleValue(JBossAS7JMXComponent.PLUGIN_CONFIG_PASSWORD, "rhqadmin");
+        pluginConfig.setSimpleValue(JBossAS7JMXComponent.PLUGIN_CONFIG_CLIENT_JAR_LOCATION, clientJarLocation);
 
         DiscoveredResourceDetails resource = new DiscoveredResourceDetails(context.getResourceType(), key, name,
-            version, description, null, null);
+            version, description, pluginConfig, null);
 
         result.add(resource);
 

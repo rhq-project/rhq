14c170585212141b3b4e6190788c96719526cd32
Heiko W. Rupp <hwr@pilhuhn.de>
2012-10-18 22:05:05 +0200

    removed plugins

--- modules/plugins/jboss-as-7/d2d.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-
-RHQ_DEV_HOME=${HOME}/im/rhq
-M2_REPO=${HOME}/.m2/repository
-RHQ_VERSION='4.4.0-SNAPSHOT'
-OPTS=""
-#OPTS="-agentlib:jdwp=transport=dt_socket,address=8790,server=y,suspend=y"
-
-java $OPTS -cp target/rhq-jboss-as-7-plugin-${RHQ_VERSION}.jar:${M2_REPO}/commons-logging/commons-logging-api/1.1/commons-logging-api-1.1.jar:${M2_REPO}/org/codehaus/jackson/jackson-core-asl/1.7.4/jackson-core-asl-1.7.4.jar:${M2_REPO}/org/codehaus/jackson/jackson-mapper-asl/1.7.4/jackson-mapper-asl-1.7.4.jar:${M2_REPO}/org/rhq/rhq-core-plugin-api/${RHQ_VERSION}/rhq-core-plugin-api-${RHQ_VERSION}.jar:${M2_REPO}/org/rhq/rhq-core-util/${RHQ_VERSION}/rhq-core-util-${RHQ_VERSION}.jar org.rhq.modules.plugins.jbossas7.Domain2Descriptor $*

6239ffc776249ab6ee4e08c855ffab95baefe44e
John Mazzitelli <mazz@redhat.com>
2012-12-06 17:21:46 -0500

    for developers, have the dev builds automatically run the installer for you so you don't have to do it in a separate step.
    this works so well, we might want to do this even for production builds. But for now, leave it only for -Pdev builds.
    
    If we want this in production builds, too, we just hardcode the line in rhq-server.sh that rhq-container.build.xml adds for dev builds.

---
+++ modules/enterprise/server/appserver/src/main/dev-resources/bin/rhq-autoinstall.sh
@@ -0,0 +1,29 @@
+if [ -z ${RHQ_SERVER_HOME} ]; then
+   echo RHQ_SERVER_HOME not defined
+   exit 1
+fi
+
+cd "$RHQ_SERVER_HOME"
+RHQ_SERVER_HOME=`pwd`
+
+_INSTALLER_SCRIPT="${RHQ_SERVER_HOME}/bin/rhq-installer.sh"
+
+# we are normally executed just before the server starts, so give it time to initialize
+sleep 5
+
+_TRIES="1 2 3 4 5"
+for _TRY in $_TRIES
+do
+   echo Running Installer Now...
+   eval ${_INSTALLER_SCRIPT}
+   if [ "$?" -eq "0" ]; then
+      echo Installer finished
+      break;
+   elif [ "$?" -eq "1" ]; then
+      echo The installer has been disabled - please fix rhq-server.properties
+      break;
+   else
+      echo Installer exited with failure code "$?" - will try again in a few seconds
+      sleep 5
+   fi
+done

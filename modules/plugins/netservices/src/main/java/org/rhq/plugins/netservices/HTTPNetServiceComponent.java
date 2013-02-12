e0ea2fd840197301df934d34088b419e97279d83
Thomas Segismont <tsegismo@redhat.com>
2013-02-12 10:23:51 +0100

    netservices plugin strengthening
    Added Unit tests for HTTPNetServiceDiscoveryComponent
    
    Added HTTPNetServiceComponent itest and verified code coverage
    
    Added Unit tests for PingNetServiceDiscoveryComponent
    
    Added PingNetServiceComponent itest and verified code coverage
    
    Implemented missing parts and cleaned up code

--- modules/plugins/netservices/src/main/java/org/rhq/plugins/netservices/HTTPNetServiceComponent.java
+++ modules/plugins/netservices/src/main/java/org/rhq/plugins/netservices/HTTPNetServiceComponent.java
@@ -1,6 +1,6 @@
 /*
  * RHQ Management Platform
- * Copyright (C) 2005-2008 Red Hat, Inc.
+ * Copyright (C) 2005-2013 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -19,14 +19,22 @@
 
 package org.rhq.plugins.netservices;
 
-import java.text.SimpleDateFormat;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.SimpleHttpConnectionManager;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.HeadMethod;
+import org.apache.commons.httpclient.util.DateUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -39,43 +47,114 @@ import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
 import org.rhq.core.pluginapi.measurement.MeasurementFacet;
-import org.rhq.core.pluginapi.operation.OperationFacet;
-import org.rhq.core.pluginapi.operation.OperationResult;
+import org.rhq.core.util.StringUtil;
 
 /**
  * Monitoring of HTTP Servers
  * 
  * @author Greg Hinkle
  */
-public class HTTPNetServiceComponent implements ResourceComponent, MeasurementFacet, OperationFacet {
-    private final Log log = LogFactory.getLog(this.getClass());
+public class HTTPNetServiceComponent implements ResourceComponent, MeasurementFacet {
 
-    public static final String CONFIG_URL = "url";
-    public static final String CONFIG_USER = "user";
-    public static final String CONFIG_PASSWORD = "password";
-    public static final String CONFIG_REALM = "realm";
+    public static final class ConfigKeys {
 
-    // One of { none, SSL, TLS }
-    public static final String CONFIG_SSL_PROTOCOL = "sslProtocol";
+        private ConfigKeys() {
+            // Defensive
+        }
 
-    // One of { HEAD, GET }
-    public static final String CONFIG_METHOD = "method";
-    public static final String CONFIG_FOLOW_REDIRECTS = "followRedirects";
-    public static final String CONFIG_RESPONSE_PATTERN = "responsePattern";
+        public static final String URL = "url";
+        public static final String USER = "user";
+        public static final String PASSWORD = "password";
+        public static final String REALM = "realm";
+        public static final String METHOD = "method";
+        public static final String FOLOW_REDIRECTS = "followRedirects";
+        public static final String VALIDATE_RESPONSE_CODE = "validateResponseCode";
+        public static final String VALIDATE_RESPONSE_PATTERN = "validateResponsePattern";
+
+    }
+
+    public static enum HttpMethod {
+        GET, HEAD
+    }
+
+    private static final Log LOG = LogFactory.getLog(HTTPNetServiceComponent.class);
 
     private ResourceContext resourceContext;
 
+    private Configuration pluginConfig;
+
+    private HTTPNetServiceComponentConfiguration componentConfig;
+
+    @Override
     public void start(ResourceContext resourceContext) throws InvalidPluginConfigurationException, Exception {
         this.resourceContext = resourceContext;
-        String url = resourceContext.getPluginConfiguration().getSimple(CONFIG_URL).getStringValue();
-        if (!url.startsWith("http:") && !url.startsWith("https")) {
-            throw new InvalidPluginConfigurationException("Url not valid. Must start with 'http:' or 'https:'");
+        pluginConfig = resourceContext.getPluginConfiguration();
+        componentConfig = createComponentConfiguration(pluginConfig);
+    }
+
+    /**
+     * Create a foram {@link HTTPNetServiceComponentConfiguration} instance with the supplied {@link Configuration}.
+     * May throw {@link InvalidPluginConfigurationException} if:
+     * <ul>
+     * <li>Url is empty, invalid, or pointing an non http/https resource</li>
+     * <li>Http method is not HEAD or GET</li>
+     * <li>If both content validation and HEAD method are configured</li>
+     * <li>If the content validation pattern is invalid</li>
+     * <ul>
+     *
+     * @param pluginConfig
+     * @return
+     * @throws InvalidPluginConfigurationException
+     */
+    static HTTPNetServiceComponentConfiguration createComponentConfiguration(Configuration pluginConfig) {
+
+        URL endPointUrl = null;
+        String configUrl = pluginConfig.getSimpleValue(ConfigKeys.URL, StringUtil.EMPTY_STRING);
+        if (StringUtil.isBlank(configUrl)) {
+            throw new InvalidPluginConfigurationException("Endpoint URL is not defined");
         }
+        try {
+            endPointUrl = new URL(configUrl);
+        } catch (MalformedURLException e) {
+            throw new InvalidPluginConfigurationException(configUrl + " is not a valid URL");
+        }
+        String protocol = endPointUrl.getProtocol();
+        if (!protocol.equals("http") && !protocol.equals("https")) {
+            throw new InvalidPluginConfigurationException(configUrl + "does not point to an http(s) resource");
+        }
+
+        HttpMethod httpMethod = null;
+        String configMethod = pluginConfig.getSimpleValue(ConfigKeys.METHOD, StringUtil.EMPTY_STRING);
+        try {
+            httpMethod = HttpMethod.valueOf(configMethod);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidPluginConfigurationException("Invalid http method: " + configMethod);
+        }
+
+        Pattern responseValidationPattern = null;
+        String configValidateResponsePattern = pluginConfig.getSimpleValue(ConfigKeys.VALIDATE_RESPONSE_PATTERN);
+        if (configValidateResponsePattern != null) {
+            if (httpMethod.equals(HttpMethod.HEAD)) {
+                throw new InvalidPluginConfigurationException("Cannot validate response content with HEAD request");
+            }
+            try {
+                responseValidationPattern = Pattern.compile(configValidateResponsePattern);
+            } catch (PatternSyntaxException e) {
+                throw new InvalidPluginConfigurationException("Invalid pattern: " + configValidateResponsePattern);
+            }
+        }
+
+        return new HTTPNetServiceComponentConfiguration(endPointUrl, httpMethod, responseValidationPattern);
     }
 
+    @Override
     public void stop() {
+        resourceContext = null;
+        pluginConfig = null;
+        componentConfig = null;
     }
 
+    @Override
     public AvailabilityType getAvailability() {
         try {
             return getValuesOrAvailability(null, null) ? AvailabilityType.UP : AvailabilityType.DOWN;
@@ -84,53 +163,60 @@ public class HTTPNetServiceComponent implements ResourceComponent, MeasurementFa
         }
     }
 
+    @Override
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) throws Exception {
         getValuesOrAvailability(report, metrics);
     }
 
-    public boolean getValuesOrAvailability(MeasurementReport report, Set<MeasurementScheduleRequest> metrics)
+    private boolean getValuesOrAvailability(MeasurementReport report, Set<MeasurementScheduleRequest> metrics)
         throws Exception {
+
+        SimpleHttpConnectionManager httpConnectionManager = new SimpleHttpConnectionManager();
+        HttpClient client = new HttpClient(httpConnectionManager);
+
+        String userName = pluginConfig.getSimpleValue(ConfigKeys.USER, StringUtil.EMPTY_STRING);
+        // Set credentials only if a user name is configured
+        if (StringUtil.isNotBlank(userName)) {
+            String password = pluginConfig.getSimpleValue(ConfigKeys.PASSWORD, StringUtil.EMPTY_STRING);
+            String realm = pluginConfig.getSimpleValue(ConfigKeys.REALM, AuthScope.ANY_REALM);
+            client.getState().setCredentials(
+                new AuthScope(componentConfig.getEndPointUrl().getHost(), componentConfig.getEndPointUrl().getPort(),
+                    realm), new UsernamePasswordCredentials(userName, password));
+        }
+
+        HttpMethodBase method = null;
+        switch (componentConfig.getHttpMethod()) {
+        case GET:
+            method = new GetMethod(componentConfig.getEndPointUrl().toExternalForm());
+            break;
+        case HEAD:
+            method = new HeadMethod(componentConfig.getEndPointUrl().toExternalForm());
+            break;
+        default:
+            throw new RuntimeException("Unsupported http method: '" + componentConfig.getHttpMethod() + "'");
+        }
+        Boolean followRedirects = pluginConfig.getSimple(ConfigKeys.FOLOW_REDIRECTS).getBooleanValue();
+        method.setFollowRedirects(followRedirects == null ? false : followRedirects.booleanValue());
 
         try {
 
-            Configuration config = resourceContext.getPluginConfiguration();
-
-            HttpClient httpClient = new HttpClient();
-
-            GetMethod method = new GetMethod(config.getSimple(CONFIG_URL).getStringValue());
-
-            method.setFollowRedirects(config.getSimple(CONFIG_FOLOW_REDIRECTS).getBooleanValue());
-
             long start = System.currentTimeMillis();
-            int responseCode = httpClient.executeMethod(method);
+            int responseCode = client.executeMethod(method);
             long connectTime = System.currentTimeMillis() - start;
 
-            boolean success = !config.getSimple("validateResponseCode").getBooleanValue()
+            // Availability may depend on reponse code value 
+            boolean success = !pluginConfig.getSimple(ConfigKeys.VALIDATE_RESPONSE_CODE).getBooleanValue()
                 || (responseCode >= 200 && responseCode <= 299);
-
-            String response = method.getResponseBodyAsString();
+            // Availability may depend on reponse content matching a pattern
+            success = success
+                && (componentConfig.getResponseValidationPattern() == null || componentConfig
+                    .getResponseValidationPattern().matcher(method.getResponseBodyAsString()).find());
 
             long readTime = (System.currentTimeMillis() - start);
 
-            // TODO: may need to allow plugin to configure the locale, but for this fixed string, make sure we
-            // ignore default locale, this works for english.
-            SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss zzz", Locale.ENGLISH);
             Header dateHeader = method.getResponseHeader("Date");
-            Date contentDate = dateHeader == null ? new Date(System.currentTimeMillis()) : sdf.parse(dateHeader.getValue());
-
-            //            System.out.println("Success: " + success);
-            //            System.out.println("Response: " + responseCode);
-            //            System.out.println("Connect Time: " + connectTime);
-            //            System.out.println("Read Time: " + readTime);
-            //            System.out.println("Content Length: " + response.length());
-            //            System.out.println("Content Date: " + contentDate);
-            //            System.out.println("Content Charset: " + method.getResponseCharSet());
-            //            System.out.println("Content Age: " + (System.currentTimeMillis() - contentDate.getTime()));
-
-            //            System.out.println("-----------------------");
-            //            for (Header header : method.getResponseHeaders()) {
-            //                System.out.println(header.getName() + " =  " + header.getValue());
-            //            }
+            Date contentDate = dateHeader == null ? new Date(System.currentTimeMillis()) : DateUtil
+                .parseDate(dateHeader.getValue());
 
             if (metrics != null) {
                 for (MeasurementScheduleRequest request : metrics) {
@@ -139,7 +225,7 @@ public class HTTPNetServiceComponent implements ResourceComponent, MeasurementFa
                     } else if (request.getName().equals("readTime")) {
                         report.addData(new MeasurementDataNumeric(request, (double) readTime));
                     } else if (request.getName().equals("contentLength")) {
-                        report.addData(new MeasurementDataNumeric(request, (double) response.length()));
+                        report.addData(new MeasurementDataNumeric(request, (double) method.getResponseContentLength()));
                     } else if (request.getName().equals("contentAge")) {
                         report.addData(new MeasurementDataNumeric(request,
                             (double) (System.currentTimeMillis() - contentDate.getTime())));
@@ -150,28 +236,14 @@ public class HTTPNetServiceComponent implements ResourceComponent, MeasurementFa
             return success;
 
         } catch (Exception e) {
-            // e.printStackTrace();
-            log.error(e);
+            LOG.error(e);
+        } finally {
+            // First release connection
+            method.releaseConnection();
+            // Then force close
+            httpConnectionManager.closeIdleConnections(0);
         }
         return false;
     }
 
-    // TODO GH: This really only makes sense to offer to go get the content if we can support long config content for responses bigger than 4k
-    public OperationResult invokeOperation(String name, Configuration parameters) throws InterruptedException,
-        Exception {
-        Configuration config = resourceContext.getPluginConfiguration();
-
-        HttpClient httpClient = new HttpClient();
-
-        GetMethod method = new GetMethod(config.getSimple(CONFIG_URL).getStringValue());
-
-        method.setFollowRedirects(config.getSimple(CONFIG_FOLOW_REDIRECTS).getBooleanValue());
-
-        int responseCode = httpClient.executeMethod(method);
-
-        String response = method.getResponseBodyAsString();
-
-        OperationResult result = new OperationResult(response);
-        return result;
-    }
 }

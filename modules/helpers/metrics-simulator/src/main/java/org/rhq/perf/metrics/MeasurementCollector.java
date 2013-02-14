1f4761ec133051b07acd783e173073ad12fe8933
John Sanda <jsanda@redhat.com>
2013-02-14 11:44:27 -0500

    initial commit for metrics-simulator module
    metrics-simulator will produce a program that stores and aggregates metrics. It
    provides a convenient, easy way to simulate load as well as way to observe data
    moving through the various metrics tables without having to run the program for
    extended periods of time. This will enable you to more easily observe performance
    and what not in the face of things like compaction, flushing of memtables, data
    distribution and so on.

---
+++ modules/helpers/metrics-simulator/src/main/java/org/rhq/perf/metrics/MeasurementCollector.java
@@ -0,0 +1,111 @@
+/*
+ *
+ *  * RHQ Management Platform
+ *  * Copyright (C) 2005-2012 Red Hat, Inc.
+ *  * All rights reserved.
+ *  *
+ *  * This program is free software; you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License, version 2, as
+ *  * published by the Free Software Foundation, and/or the GNU Lesser
+ *  * General Public License, version 2.1, also as published by the Free
+ *  * Software Foundation.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License and the GNU Lesser General Public License
+ *  * for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * and the GNU Lesser General Public License along with this program;
+ *  * if not, write to the Free Software Foundation, Inc.,
+ *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+package org.rhq.perf.metrics;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.core.domain.measurement.MeasurementDataNumeric;
+import org.rhq.server.metrics.MetricsServer;
+
+/**
+ * @author John Sanda
+ */
+public class MeasurementCollector implements Runnable {
+
+    private final Log log = LogFactory.getLog(MeasurementCollector.class);
+
+    private PriorityQueue<Schedule> queue;
+
+    private MetricsServer metricsServer;
+
+    private ReentrantLock queueLock;
+
+    public void setQueue(PriorityQueue<Schedule> queue) {
+        this.queue = queue;
+    }
+
+    public void setMetricsServer(MetricsServer metricsServer) {
+        this.metricsServer = metricsServer;
+    }
+
+    public void setQueueLock(ReentrantLock queueLock) {
+        this.queueLock = queueLock;
+    }
+
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        try {
+            log.info("Starting metrics collections...");
+            Set<Schedule> schedules = new HashSet<Schedule>();
+            try {
+                queueLock.lock();
+                Schedule first = queue.peek();
+                if (first != null && first.getNextCollection() <= System.currentTimeMillis()) {
+                    Schedule next = first;
+                    while (next != null && next.getNextCollection() == first.getNextCollection()) {
+                        schedules.add(queue.poll());
+                        next = queue.peek();
+                    }
+                }
+            } finally {
+                queueLock.unlock();
+            }
+
+            if (schedules.isEmpty()) {
+                log.debug("No schedules are ready for collections.");
+                return;
+            }
+            log.debug("There are " + schedules.size() + " schedules ready for collection.");
+
+            Set<MeasurementDataNumeric> data = new HashSet<MeasurementDataNumeric>(schedules.size());
+            for (Schedule schedule : schedules) {
+                data.add(new MeasurementDataNumeric(schedule.getNextCollection(), schedule.getId(),
+                    schedule.getNextValue()));
+                schedule.updateCollection();
+            }
+            metricsServer.addNumericData(data);
+
+            try {
+                queueLock.lock();
+                for (Schedule schedule : schedules) {
+                    queue.offer(schedule);
+                }
+            } finally {
+                queueLock.unlock();
+            }
+        } finally {
+            long endTime = System.currentTimeMillis();
+            log.info("Finished collecting and storing raw metric in " + (endTime - startTime) + " ms.");
+        }
+    }
+}
---
+++ modules/helpers/metrics-simulator/src/main/java/org/rhq/perf/metrics/Simulator.java
@@ -0,0 +1,149 @@
+/*
+ *
+ *  * RHQ Management Platform
+ *  * Copyright (C) 2005-2012 Red Hat, Inc.
+ *  * All rights reserved.
+ *  *
+ *  * This program is free software; you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License, version 2, as
+ *  * published by the Free Software Foundation, and/or the GNU Lesser
+ *  * General Public License, version 2.1, also as published by the Free
+ *  * Software Foundation.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License and the GNU Lesser General Public License
+ *  * for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * and the GNU Lesser General Public License along with this program;
+ *  * if not, write to the Free Software Foundation, Inc.,
+ *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+package org.rhq.perf.metrics;
+
+import static com.datastax.driver.core.ProtocolOptions.Compression.SNAPPY;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleAuthInfoProvider;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.cassandra.CassandraClusterManager;
+import org.rhq.cassandra.CassandraNode;
+import org.rhq.cassandra.ClusterInitService;
+import org.rhq.cassandra.DeploymentOptions;
+import org.rhq.cassandra.schema.SchemaManager;
+import org.rhq.server.metrics.MetricsServer;
+
+/**
+ * @author John Sanda
+ */
+public class Simulator {
+
+    private final Log log = LogFactory.getLog(Simulator.class);
+
+    public void run() throws Exception {
+        List<CassandraNode> nodes = deployCluster();
+        waitForClusterToInitialize(nodes);
+
+        createSchema(nodes);
+
+        Session session = createSession();
+
+        MetricsServer metricsServer = new MetricsServer();
+        metricsServer.setSession(session);
+
+        Set<Schedule> schedules = initSchedules();
+        PriorityQueue<Schedule> queue = new PriorityQueue<Schedule>(schedules);
+        ReentrantLock queueLock = new ReentrantLock();
+
+        MeasurementCollector measurementCollector = new MeasurementCollector();
+        measurementCollector.setMetricsServer(metricsServer);
+        measurementCollector.setQueue(queue);
+        measurementCollector.setQueueLock(queueLock);
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(3);
+        log.info("Starting executor service");
+        executorService.scheduleAtFixedRate(measurementCollector, 0, 500, TimeUnit.MILLISECONDS);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+        }
+        log.info("Shutting down executor service");
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    private List<CassandraNode> deployCluster() throws IOException {
+        File clusterDir = new File("target/cassandra");
+        log.info("Deploying cluster to " + clusterDir);
+        DeploymentOptions deploymentOptions = new DeploymentOptions();
+        deploymentOptions.setClusterDir(clusterDir.getAbsolutePath());
+        deploymentOptions.setNumNodes(2);
+        deploymentOptions.load();
+
+        CassandraClusterManager ccm = new CassandraClusterManager(deploymentOptions);
+        List<CassandraNode> nodes = ccm.createCluster();
+        ccm.startCluster();
+
+        return nodes;
+    }
+
+    private void waitForClusterToInitialize(List<CassandraNode> nodes) {
+        log.info("Waiting for cluster to initialize");
+        ClusterInitService clusterInitService = new ClusterInitService();
+        clusterInitService.waitForClusterToStart(nodes);
+    }
+
+    private void createSchema(List<CassandraNode> nodes) {
+        log.info("Creating schema");
+        SchemaManager schemaManager = new SchemaManager("rhqadmin", "rhqadmin", nodes);
+        schemaManager.createSchema();
+        schemaManager.updateSchema();
+    }
+
+    private Session createSession() throws NoHostAvailableException {
+        SimpleAuthInfoProvider authInfoProvider = new SimpleAuthInfoProvider();
+        authInfoProvider.add("username", "rhqadmin").add("password", "rhqadmin");
+
+        Cluster cluster = Cluster.builder()
+            .addContactPoints("127.0.0.1", "127.0.0.2")
+            .withAuthInfoProvider(authInfoProvider)
+            .withCompression(SNAPPY)
+            .build();
+
+        return cluster.connect("rhq");
+    }
+
+    private Set<Schedule> initSchedules() {
+        long nextCollection = System.currentTimeMillis();
+        Set<Schedule> schedules = new HashSet<Schedule>();
+        for (int i = 0; i < 10; ++i) {
+            Schedule schedule = new Schedule(i);
+            schedule.setInterval(500);
+            schedule.setNextCollection(nextCollection);
+            schedules.add(schedule);
+        }
+        return schedules;
+    }
+
+}

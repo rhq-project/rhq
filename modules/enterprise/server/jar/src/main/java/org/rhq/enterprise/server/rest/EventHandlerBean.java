9ba9f80aaa95d4f5ee3e209d26315d1cb70e12b8
Simeon Pinder <spinder@fulliautomatix.conchfritter.com>
2013-02-18 05:43:55 -0500

    Remove any paging changes as REST will handle paging differently.

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/rest/EventHandlerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/rest/EventHandlerBean.java
@@ -61,14 +61,11 @@ import org.rhq.core.domain.event.EventSource;
 import org.rhq.core.domain.resource.Resource;
 import org.rhq.core.domain.resource.ResourceType;
 import org.rhq.core.domain.util.PageControl;
-import org.rhq.core.domain.util.PageList;
 import org.rhq.enterprise.server.RHQConstants;
 import org.rhq.enterprise.server.event.EventManagerLocal;
 import org.rhq.enterprise.server.rest.domain.EventDefinitionRest;
 import org.rhq.enterprise.server.rest.domain.EventRest;
 import org.rhq.enterprise.server.rest.domain.EventSourceRest;
-import org.rhq.enterprise.server.util.CriteriaQuery;
-import org.rhq.enterprise.server.util.CriteriaQueryExecutor;
 
 /**
  * Handle event related things
@@ -308,16 +305,8 @@ public class EventHandlerBean extends AbstractRestBean {
 
 
     private Response.ResponseBuilder getEventsAsBuilderForCriteria(HttpHeaders headers, EventCriteria criteria) {
-        //Use CriteriaQuery to automatically chunk/page through criteria query results
-        CriteriaQueryExecutor<Event, EventCriteria> queryExecutor = new CriteriaQueryExecutor<Event, EventCriteria>() {
-            @Override
-            public PageList<Event> execute(EventCriteria criteria) {
-                return eventManager.findEventsByCriteria(caller, criteria);
-            }
-        };
-
-        CriteriaQuery<Event, EventCriteria> eventList = new CriteriaQuery<Event, EventCriteria>(criteria, queryExecutor);
-        List<EventRest> restEvents = new ArrayList<EventRest>();
+        List<Event> eventList = eventManager.findEventsByCriteria(caller, criteria);
+        List<EventRest> restEvents = new ArrayList<EventRest>(eventList.size());
         for (Event event : eventList) {
             restEvents.add(convertEvent(event));
         }

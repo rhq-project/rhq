8fdab8b44812964f3d4b11e3ff819bd3f5ab0d9d
Jay Shaughnessy <jshaughn@redhat.com>
2013-04-10 10:23:42 -0400

    Bug 950461 Update serialVersionUID for criteria classes
    These are additional, non-criteria, domain classes that need a
    serialization ID update due to recent changes.

--- modules/core/domain/src/main/java/org/rhq/core/domain/search/SearchSuggestion.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/search/SearchSuggestion.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class SearchSuggestion implements Serializable, Comparable<SearchSuggestion> {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
     public enum Kind {
         Unknown(""), //

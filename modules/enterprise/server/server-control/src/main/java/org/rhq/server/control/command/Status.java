dc69e64b584f0534f5af8e1efac9b29db95cffca
Jay Shaughnessy <jshaughn@redhat.com>
2013-04-30 10:17:51 -0400

    Fix 'rhqctl status' for windows, avoid throwing exception on aganet check

--- modules/enterprise/server/server-control/src/main/java/org/rhq/server/control/command/Status.java
+++ modules/enterprise/server/server-control/src/main/java/org/rhq/server/control/command/Status.java
@@ -31,6 +31,7 @@ import java.io.FileReader;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.PumpStreamHandler;
 
@@ -159,6 +160,15 @@ public class Status extends ControlCommand {
         Executor executor = new DefaultExecutor();
         executor.setWorkingDirectory(agentBinDir);
         executor.setStreamHandler(new PumpStreamHandler());
-        executor.execute(commandLine);
+        try {
+            executor.execute(commandLine);
+        } catch (ExecuteException e) {
+            // For windows the JSW exit code for a status check is expected to be a mask value and the agent wrapper
+            // .bat will return it explicitly.  We can ignore it and assume that the logged output is sufficient. 
+            // See http://wrapper.tanukisoftware.com/doc/english/launch-win.html#standalone-status
+            if (!isWindows()) {
+                throw e;
+            }
+        }
     }
 }

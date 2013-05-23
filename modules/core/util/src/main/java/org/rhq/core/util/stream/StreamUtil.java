8a0989ca54d012dfec6807e29b9e492a8a00c2c1
Jay Shaughnessy <jshaughn@redhat.com>
2013-05-23 17:08:27 -0400

    Fix test failure issues caused by unclosed streams. Also, make an anal retentive  change to StreamUtil to make absolutely sure we clean up the streams when closeStreams is true.

--- modules/core/util/src/main/java/org/rhq/core/util/stream/StreamUtil.java
+++ modules/core/util/src/main/java/org/rhq/core/util/stream/StreamUtil.java
@@ -240,16 +240,17 @@ public class StreamUtil {
             throw new IllegalArgumentException("Output stream is null.");
         }
 
+        BufferedInputStream bufInput = null;
         long numBytesCopied = 0;
         int bufferSize = 32768;
 
         try {
             // make sure we buffer the input
-            input = new BufferedInputStream(input, bufferSize);
+            bufInput = new BufferedInputStream(input, bufferSize);
 
             byte[] buffer = new byte[bufferSize];
 
-            for (int bytesRead = input.read(buffer); bytesRead != -1; bytesRead = input.read(buffer)) {
+            for (int bytesRead = bufInput.read(buffer); bytesRead != -1; bytesRead = bufInput.read(buffer)) {
                 if (htmlEscape) {
                     String htmlEncodedStr = forHTMLTag(new String(buffer, 0, bytesRead));
                     bytesRead = htmlEncodedStr.length();
@@ -266,15 +267,23 @@ public class StreamUtil {
         } finally {
             if (closeStreams) {
                 try {
-                    output.close();
+                    if (null != bufInput) {
+                        bufInput.close();
+                    }
                 } catch (IOException ioe2) {
-                    LOG.warn("Streams could not be closed", ioe2);
+                    LOG.warn("Buffered Input Stream could not be closed", ioe2);
                 }
 
                 try {
                     input.close();
                 } catch (IOException ioe2) {
-                    LOG.warn("Streams could not be closed", ioe2);
+                    LOG.warn("Input Stream could not be closed", ioe2);
+                }
+
+                try {
+                    output.close();
+                } catch (IOException ioe2) {
+                    LOG.warn("Output Stream could not be closed", ioe2);
                 }
             }
         }

fe019d24ded56cb8a10ee03ee8d215f3f8bb073a
Heiko W. Rupp <hwr@redhat.com>
2013-06-19 05:56:30 +0200

    BZ 918051 - Don't allow to pass arbitrary names for the resource.

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/components/lookup/ResourceLookupComboBoxItem.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/components/lookup/ResourceLookupComboBoxItem.java
@@ -1,6 +1,6 @@
 /*
  * RHQ Management Platform
- * Copyright (C) 2005-2010 Red Hat, Inc.
+ * Copyright (C) 2005-2013 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -79,6 +79,7 @@ public class ResourceLookupComboBoxItem extends ComboBoxItem {
         setPickListWidth(800);
         setTextMatchStyle(TextMatchStyle.SUBSTRING);
         setCompleteOnTab(true);
+        setAddUnknownValues(false);
 
     }
 }

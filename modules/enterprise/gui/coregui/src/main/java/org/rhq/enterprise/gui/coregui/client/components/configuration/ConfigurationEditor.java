495c35d7681ba12df56c7500bb110caf19fc4aff
Jirka Kremser <jkremser@redhat.com>
2013-07-29 14:38:14 +0200

    squashed commit: If there is just one group definition in ConfigurationEditor, normal form is used instead SectionStack component. If there are more than 3 items the combo-box component is used (instead 5).

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/components/configuration/ConfigurationEditor.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/enterprise/gui/coregui/client/components/configuration/ConfigurationEditor.java
@@ -427,8 +427,8 @@ public class ConfigurationEditor extends EnhancedVLayout {
                 layout.addMember(form);
             }
             if (groupDefinitions.size() == 1) {
-                propertyDefinitions.addAll(configurationDefinition.getPropertiesInGroup(groupDefinitions.get(0)
-                    .getName()));
+                propertyDefinitions = new ArrayList<PropertyDefinition>(
+                    configurationDefinition.getPropertiesInGroup(groupDefinitions.get(0).getName()));
                 DynamicForm groupForm = buildPropertiesForm(propertyDefinitions, configuration);
                 groupForm.setIsGroup(true);
                 groupForm.setGroupTitle(groupDefinitions.get(0).getDisplayName());
@@ -454,10 +454,10 @@ public class ConfigurationEditor extends EnhancedVLayout {
                 sectionStack.addSection(buildGroupSection(definition));
             }
 
-            if (groupDefinitions.size() > 1) {
+//            if (groupDefinitions.size() > 1) {
                 this.toolStrip = buildToolStrip(layout, sectionStack);
                 layout.addMember(toolStrip);
-            }
+//            }
             layout.addMember(sectionStack);
         }
 
@@ -1331,7 +1331,7 @@ public class ConfigurationEditor extends EnhancedVLayout {
                     valueItem = new ComboBoxItem();
                     ((ComboBoxItem) valueItem).setAddUnknownValues(true);
                 } else {
-                    if (valueOptions.size() > 5) {
+                    if (valueOptions.size() > 3) {
                         valueItem = new SelectItem();
                     } else {
                         valueItem = new RadioGroupItem();

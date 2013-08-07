089d80191b85565760a5097e2cc5336fe4e9e6bd
Thomas Segismont <tsegismo@redhat.com>
2013-08-07 18:53:44 +0200

    Bug 980639 - JBoss Web connectors are not discovered if host name contains dash/hyphen (-)
    Simplified the regular expression matching connector components.
    
    Side fix: servers started with -b [hostname] instead of -b [ip address] have connector with composed names: (myserver.com%2F127.0.0.1). It was not taken into account when trying to find the thread pool associated with it (in order to collect thread pool metrics).
    
    Tested with EAP 5.1.2 and 5.2.0

--- modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/ConnectorDiscoveryComponent.java
+++ modules/plugins/jboss-as-5/src/main/java/org/rhq/plugins/jbossas5/ConnectorDiscoveryComponent.java
@@ -1,27 +1,29 @@
 /*
- * Jopr Management Platform
- * Copyright (C) 2005-2009 Red Hat, Inc.
+ * RHQ Management Platform
+ * Copyright (C) 2005-2013 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation, and/or the GNU Lesser
- * General Public License, version 2.1, also as published by the Free
- * Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License and the GNU Lesser General Public License
- * for more details.
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * and the GNU Lesser General Public License along with this program;
- * if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
 package org.rhq.plugins.jbossas5;
+
+import static org.rhq.plugins.jbossas5.ConnectorComponent.ADDRESS_PROPERTY;
+import static org.rhq.plugins.jbossas5.ConnectorComponent.HOST_PROPERTY;
+import static org.rhq.plugins.jbossas5.ConnectorComponent.PORT_PROPERTY;
+import static org.rhq.plugins.jbossas5.ConnectorComponent.PROTOCOL_PROPERTY;
+import static org.rhq.plugins.jbossas5.ManagedComponentComponent.Config.COMPONENT_NAME;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -30,6 +32,11 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import org.jboss.deployers.spi.management.ManagementView;
+import org.jboss.managed.api.ComponentType;
+import org.jboss.managed.api.ManagedComponent;
+
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.domain.resource.ResourceType;
@@ -37,12 +44,8 @@ import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
 import org.rhq.plugins.jbossas5.helper.MoreKnownComponentTypes;
-import org.rhq.plugins.jbossas5.util.RegularExpressionNameMatcher;
 import org.rhq.plugins.jbossas5.util.ManagedComponentUtils;
-
-import org.jboss.deployers.spi.management.ManagementView;
-import org.jboss.managed.api.ComponentType;
-import org.jboss.managed.api.ManagedComponent;
+import org.rhq.plugins.jbossas5.util.RegularExpressionNameMatcher;
 
 /**
  * A component for discovering JBoss Web connectors.
@@ -52,18 +55,24 @@ import org.jboss.managed.api.ManagedComponent;
 public class ConnectorDiscoveryComponent
         implements ResourceDiscoveryComponent<JBossWebComponent>
 {
+    private static final Log LOG = LogFactory.getLog(ConnectorDiscoveryComponent.class);
+
     // A regex for the names of all MBean:WebRequestProcessor components,
     // e.g. "jboss.web:name=http-127.0.0.1-8080,type=GlobalRequestProcessor"
     private static final String WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_REGEX =
-            "jboss.web:name=([^\\-]+)-([^\\-]+)-([0-9]+),type=GlobalRequestProcessor";
+            "jboss.web:name=([^\\-]+)-(.+)-([0-9]+),type=GlobalRequestProcessor";
 
-    private final Log log = LogFactory.getLog(this.getClass());
+    private static final Pattern WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_PATTERN = Pattern
+        .compile(WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_REGEX);
+    
+    private static final Pattern HOSTADRESS_PATTERN = Pattern
+        .compile("(.*)(%2F)(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})");
 
     public Set<DiscoveredResourceDetails> discoverResources(
             ResourceDiscoveryContext<JBossWebComponent> discoveryContext) throws Exception
     {
         ResourceType resourceType = discoveryContext.getResourceType();
-        log.trace("Discovering " + resourceType.getName() + " Resources...");
+        LOG.trace("Discovering " + resourceType.getName() + " Resources...");
 
         JBossWebComponent jbossWebComponent = discoveryContext.getParentResourceComponent();
         ManagementView managementView = jbossWebComponent.getConnection().getManagementView();
@@ -78,16 +87,28 @@ public class ConnectorDiscoveryComponent
         {
             // Parse the component name, e.g. "jboss.web:name=http-127.0.0.1-8080,type=GlobalRequestProcessor", to
             // figure out the protocol, address, and port.
-            Pattern pattern = Pattern.compile(WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_REGEX);
-            Matcher matcher = pattern.matcher(webRequestProcessorComponent.getName());
+            Matcher matcher = WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_PATTERN.matcher(webRequestProcessorComponent
+                .getName());
             if (!matcher.matches())
             {
-                log.error("Component name '" + webRequestProcessorComponent.getName() + "' does not match regex '"
-                        + pattern + "'.");
+                LOG.error("Component name '" + webRequestProcessorComponent.getName() + "' does not match regex '"
+                        + WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_PATTERN + "'.");
                 continue;
             }
             String protocol = matcher.group(1);
-            String address = matcher.group(2);
+            String host = null;
+            String address = null;
+            String hostAddress = matcher.group(2);
+            Matcher hostAddressMatcher = HOSTADRESS_PATTERN.matcher(hostAddress);
+            if (hostAddressMatcher.matches()) {
+                // We have a composed host address string: my-server.com%2F127.0.0.98
+                host = hostAddressMatcher.group(1);
+                address = hostAddressMatcher.group(3);
+            } else {
+                // We only have an IP address
+                address = hostAddress;
+            }
+
             int port = Integer.valueOf(matcher.group(3));
 
             String resourceKey = protocol + "://" + address + ":" + port;
@@ -96,11 +117,11 @@ public class ConnectorDiscoveryComponent
             String resourceVersion = null;
 
             Configuration pluginConfig = discoveryContext.getDefaultPluginConfiguration();
-            pluginConfig.put(new PropertySimple(ManagedComponentComponent.Config.COMPONENT_NAME,
-                    webRequestProcessorComponent.getName()));
-            pluginConfig.put(new PropertySimple(ConnectorComponent.PROTOCOL_PROPERTY, protocol));
-            pluginConfig.put(new PropertySimple(ConnectorComponent.ADDRESS_PROPERTY, address));
-            pluginConfig.put(new PropertySimple(ConnectorComponent.PORT_PROPERTY, port));
+            pluginConfig.put(new PropertySimple(COMPONENT_NAME, webRequestProcessorComponent.getName()));
+            pluginConfig.put(new PropertySimple(PROTOCOL_PROPERTY, protocol));
+            pluginConfig.put(new PropertySimple(HOST_PROPERTY, host));
+            pluginConfig.put(new PropertySimple(ADDRESS_PROPERTY, address));
+            pluginConfig.put(new PropertySimple(PORT_PROPERTY, port));
 
             DiscoveredResourceDetails resource =
                     new DiscoveredResourceDetails(resourceType,
@@ -114,7 +135,7 @@ public class ConnectorDiscoveryComponent
             discoveredResources.add(resource);
         }
 
-        log.trace("Discovered " + discoveredResources.size() + " " + resourceType.getName() + " Resources.");
+        LOG.trace("Discovered " + discoveredResources.size() + " " + resourceType.getName() + " Resources.");
         return discoveredResources;
     }
 
@@ -122,8 +143,6 @@ public class ConnectorDiscoveryComponent
             throws Exception
     {
         ComponentType webRequestProcessorComponentType = MoreKnownComponentTypes.MBean.WebRequestProcessor.getType();
-        //return managementView.getMatchingComponents(WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_REGEX,
-        //        webRequestProcessorComponentType, new RegularExpressionNameMatcher());
         return ManagedComponentUtils.getManagedComponents(managementView, webRequestProcessorComponentType,
                 WEB_REQUEST_PROCESSOR_COMPONENT_NAMES_REGEX, new RegularExpressionNameMatcher());
     }

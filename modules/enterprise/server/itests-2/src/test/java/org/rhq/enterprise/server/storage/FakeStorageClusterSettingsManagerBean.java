714fb04be78b57d97ce53004290de503b044397d
John Sanda <jsanda@redhat.com>
2013-08-13 23:56:39 -0400

    adding test impl of StorageClusterSettingsManagerLocal

---
+++ modules/enterprise/server/itests-2/src/test/java/org/rhq/enterprise/server/storage/FakeStorageClusterSettingsManagerBean.java
@@ -0,0 +1,26 @@
+package org.rhq.enterprise.server.storage;
+
+import javax.ejb.Stateless;
+
+import org.rhq.core.domain.auth.Subject;
+
+/**
+ * @author John Sanda
+ */
+@Stateless
+public class FakeStorageClusterSettingsManagerBean implements StorageClusterSettingsManagerLocal {
+
+    @Override
+    public StorageClusterSettings getClusterSettings(Subject subject) {
+        StorageClusterSettings settings = new StorageClusterSettings();
+        settings.setGossipPort(7100);
+        settings.setCqlPort(9042);
+
+        return settings;
+    }
+
+    @Override
+    public void setClusterSettings(Subject subject, StorageClusterSettings clusterSettings) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}

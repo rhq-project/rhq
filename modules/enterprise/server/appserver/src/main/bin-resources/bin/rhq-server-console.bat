bf886d871f336211846d4852032f089a54b0b6c7
Jay Shaughnessy <jshaughn@redhat.com>
2013-10-10 11:16:23 -0400

    Bug 1006347 - Deprecate rhqserver scripts - updated scripts with better messaging - updated .sh scripts with better handling of stderr

--- modules/enterprise/server/appserver/src/main/bin-resources/bin/rhq-server-console.bat
+++ modules/enterprise/server/appserver/src/main/bin-resources/bin/rhq-server-console.bat
@@ -1,3 +1,6 @@
 @echo off
-echo This script is obsolete and no longer used. Use rhqctl instead.
+echo ERROR: %0 has been replaced by rhqctl
+echo Please use rhqctl. For help on the new command try:
+echo.
+echo     rhqctl --help
 exit /B 1

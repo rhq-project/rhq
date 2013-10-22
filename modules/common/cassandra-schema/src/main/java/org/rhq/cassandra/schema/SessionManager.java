94d9faba6fd4217954c4e454511ae0357aa59e0c
John Sanda <jsanda@redhat.com>
2013-10-21 21:26:58 -0400

    [BZ 1004050] do not require user to specify all storage nodes during install
    If you install multiple storage nodes before installing the server and fail to
    specifiy any of them in the rhq.storage.nodes property in rhq-server.properties,
    then you will run into problems at runtime as the nodes not specified will go
    through the deployment process. Any requests sent to those nodes during that
    deployment process will get rejected. This commit makes the install process a
    bit more robust by removing the requirement to specify all nodes. The server
    installer now queries the driver for all cluster nodes, and storage node
    entities will be created for all nodes reported by the driver.
    
    This involved a refactoring of the classes in the rhq-cassandra-schema maven
    module. A session is no longer created/destroyed for each operation, i.e.,
    install/update schema, version check, etc. Callers of SchemaManager need to
    invoke SchemaManager.shutdown to ensure that the cluster and session objects
    are properly shutdown.

---
+++ modules/common/cassandra-schema/src/main/java/org/rhq/cassandra/schema/SessionManager.java
@@ -0,0 +1,60 @@
+package org.rhq.cassandra.schema;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.ProtocolOptions;
+import com.datastax.driver.core.Session;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.rhq.cassandra.util.ClusterBuilder;
+
+/**
+ * @author John Sanda
+ */
+public class SessionManager {
+
+    private final Log log = LogFactory.getLog(SessionManager.class);
+
+
+    private Cluster cluster;
+
+    private Session session;
+
+    public void initSession(String username, String password, int cqlPort, String... nodes) {
+        if (session == null) {
+            log.info("Initializing session for [username: " + username + ", cqlPort: " + cqlPort + ", nodes: " +
+                Arrays.toString(nodes) + "]");
+            cluster = new ClusterBuilder().addContactPoints(nodes).withCredentialsObfuscated(username, password)
+                .withPort(cqlPort).withCompression(ProtocolOptions.Compression.NONE).build();
+            session = cluster.connect("system");
+        }
+    }
+
+    public void shutdownCluster() {
+        log.info("Shutting down cluster");
+        if (cluster != null) {
+            cluster.shutdown();
+            cluster = null;
+            session = null;
+        }
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public Set<String> getNodeAdresses() {
+        Set<String> nodes = new TreeSet<String>();
+        for (Host host : cluster.getMetadata().getAllHosts()) {
+            nodes.add(host.getAddress().getHostAddress());
+        }
+        return nodes;
+    }
+
+}

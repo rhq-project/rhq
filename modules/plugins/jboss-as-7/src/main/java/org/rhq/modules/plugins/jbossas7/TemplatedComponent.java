8880930a94be226844c581a2a96112729b2686a8
Simeon Pinder <spinder@fulliautomatix.conchfritter.com>
2013-11-14 10:42:55 -0500

    [BZ 997669] update previous fixes to better filter attribute list for templated components. (cherry picked from 3.2.x commit 268806173a4f96c53443d0ec944e549ae41c8450) (cherry picked from 3.2.x commit dd1f836e7df7b6be3773b6b15165baa85a7b703e) (cherry picked from 3.1.x commit 3da1ba24a8d33a5c2ae092bb4a84217974c864d0)

--- modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/TemplatedComponent.java
+++ modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/TemplatedComponent.java
@@ -20,14 +20,22 @@
 
 package org.rhq.modules.plugins.jbossas7;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.PropertySimple;
 import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
+import org.rhq.core.domain.configuration.definition.PropertyDefinition;
 import org.rhq.core.pluginapi.configuration.ConfigurationUpdateReport;
 import org.rhq.core.pluginapi.inventory.ResourceComponent;
+import org.rhq.modules.plugins.jbossas7.json.Operation;
+import org.rhq.modules.plugins.jbossas7.json.ReadResource;
+import org.rhq.modules.plugins.jbossas7.json.Result;
 
 /**
  * @author Stefan Negrea
+ * @author Simeon Pinder
  *
  */
 public class TemplatedComponent extends BaseComponent<ResourceComponent<?>> {
@@ -74,17 +82,66 @@ public class TemplatedComponent extends BaseComponent<ResourceComponent<?>> {
     public void updateResourceConfiguration(ConfigurationUpdateReport report) {
         ConfigurationDefinition configDef = context.getResourceType().getResourceConfigurationDefinition();
 
+        boolean templatedComponentUpdate = false;
+
         if (configDef.getDefaultTemplate().getConfiguration().get(TYPE_CONFIGURATION) != null) {
             //__type is a fake property, do not attempt to save it.
             configDef.getPropertyDefinitions().remove(TYPE_CONFIGURATION);
             report.getConfiguration().remove(TYPE_CONFIGURATION);
+            templatedComponentUpdate = true;
         } else if (configDef.getDefaultTemplate().getConfiguration().get(NAME_CONFIGURATION) != null) {
             //__name is a fake property, do not attempt to save it.
             configDef.getPropertyDefinitions().remove(NAME_CONFIGURATION);
             report.getConfiguration().remove(NAME_CONFIGURATION);
+            templatedComponentUpdate = true;
+        }
+
+        if (templatedComponentUpdate) {
+            //For templated resources we need to parse only the specific subset of attributes
+            //supported by this component
+            Map<String, Object> currentAttributeList = null;
+            Operation currentAttributesOp = new ReadResource(address);
+            Map<String, Object> additionalProperties = new HashMap<String, Object>();
+            //includes operation request attributes applicable to 6.0 & 6.1
+            additionalProperties.put("proxies", "true");
+            additionalProperties.put("include-runtime", "true");
+            additionalProperties.put("include-defaults", "true");
+            additionalProperties.put("attributes-only", "true");
+            currentAttributesOp.setAdditionalProperties(additionalProperties);
+            Result currentAttributes = getASConnection().execute(currentAttributesOp);
+            if ((currentAttributes!=null)&&(currentAttributes.isSuccess())) {
+                currentAttributeList = (Map<String, Object>) currentAttributes.getResult();
+            }
+
+            for (PropertyDefinition propDef : configDef.getNonGroupedProperties()) {
+                //with templated resources we should only parse the properties being used by this specific resource.
+                if (currentAttributeList != null) {
+                    //take care to strip of as7 plugin specific identifiers here when comparing attributes.
+                    if (!currentAttributeList.containsKey(removeAttributeMarkup(propDef.getName()))) {
+                        configDef.getPropertyDefinitions().remove(propDef.getName());
+                        report.getConfiguration().remove(propDef.getName());
+                    }
+                }
+            }
         }
 
         ConfigurationWriteDelegate delegate = new ConfigurationWriteDelegate(configDef, getASConnection(), address);
         delegate.updateResourceConfiguration(report);
+    }
+
+    /** Method removes attribute metadata mark up so that the attributes can be 
+     *  compared directly to results from stock as7/eap resources.
+     *  Ex. queue-length:expr -> queue-length.  
+     * 
+     * @param name : string to scrub metadata from.
+     * @return String minus known metadata markup.
+     */
+    private String removeAttributeMarkup(String name) {
+        int index = -1;
+        //Ex. Markup :pname,:key,:name,:expr,:collapsed,:nullable,:# .. where # represent [0-9]
+        if ((index = name.indexOf(":")) > -1) {
+            name = name.substring(0, index);
+        }
+        return name;
     }
 }

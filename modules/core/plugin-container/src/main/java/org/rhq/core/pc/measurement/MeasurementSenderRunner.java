306c7fc31bf020247ddd9c094c7322a00ffcdda7
Thomas Segismont <tsegismo@redhat.com>
2013-11-20 10:21:51 +0100

    Log avail and metric collections events at debug level

--- modules/core/plugin-container/src/main/java/org/rhq/core/pc/measurement/MeasurementSenderRunner.java
+++ modules/core/plugin-container/src/main/java/org/rhq/core/pc/measurement/MeasurementSenderRunner.java
@@ -1,24 +1,20 @@
 /*
  * RHQ Management Platform
- * Copyright (C) 2005-2008 Red Hat, Inc.
+ * Copyright (C) 2005-2013 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation, and/or the GNU Lesser
- * General Public License, version 2.1, also as published by the Free
- * Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License and the GNU Lesser General Public License
- * for more details.
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * and the GNU Lesser General Public License along with this program;
- * if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 package org.rhq.core.pc.measurement;
 
@@ -58,8 +54,10 @@ public class MeasurementSenderRunner implements Callable<MeasurementReport>, Run
         this.measurementManager.perMinuteItizeData(report);
 
         if (report.getDataCount() > 0) {
-            LOG.info("Measurement collection for [" + report.getDataCount() + "] metrics took "
-                + report.getCollectionTime() + "ms - sending report to Server...");
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Measurement collection for [" + report.getDataCount() + "] metrics took "
+                        + report.getCollectionTime() + "ms - sending report to Server...");
+            }
             this.measurementManager.sendMeasurementReport(report);
         } else {
             LOG.debug("Measurement report contains no data - not sending to Server.");

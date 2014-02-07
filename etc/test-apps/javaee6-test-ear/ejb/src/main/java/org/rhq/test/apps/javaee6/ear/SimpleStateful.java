27bb9a9edf397bd599ff4b2baa7cf4ca7014c5d7
Thomas Segismont <tsegismo@redhat.com>
2014-02-07 10:36:43 +0100

    Added new test app (javaee6 EAR)

---
+++ etc/test-apps/javaee6-test-ear/ejb/src/main/java/org/rhq/test/apps/javaee6/ear/SimpleStateful.java
@@ -0,0 +1,27 @@
+package org.rhq.test.apps.javaee6.ear;/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2013 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+import javax.ejb.Stateful;
+
+/**
+ * @author Thomas Segismont
+ */
+@Stateful
+public class SimpleStateful {
+}

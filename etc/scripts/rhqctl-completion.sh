05186a4299ff6b2d18c0d0cac1b2b1cd843045ee
Jirka Kremser <jkremser@redhat.com>
2014-03-07 00:24:56 +0100

    Updating the rhqctl-completion.sh script to reflect the current state.

--- etc/scripts/rhqctl-completion.sh
+++ etc/scripts/rhqctl-completion.sh
@@ -4,22 +4,24 @@
 # to auto load this script move this script to /etc/bash_completion.d/
 
 _rhqctl() {
-    local opts agentServerStorage storageInstallSubopts agentInstallSubopts serverInstallSubopts upgradeSubopts dataMingratorSubopts
+    local cur prev opts agentServerStorage agentServerStorageStart serverStorage dataMingratorSubopts \
+          storageInstallSubopts agentInstallSubopts serverInstallSubopts upgradeSubopts booleanSubopts trueFalse first second
     COMPREPLY=()
 
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="console install start status stop restart upgrade"
+    opts="console install restart start status stop upgrade --help"
     agentServerStorage="--agent --server --storage"
+    agentServerStorageStart="--agent --server --storage --start"
     serverStorage="--server --storage"
     dataMigratorSubopts="none estimate print-command do-it"
 
     # the spaces in the beginning and in the end are important here
-    storageInstallSubopts=" --storage-config --storage-data-root-dir "
-    agentInstallSubopts=" --agent-auto-start --agent-config --agent-preference "
-    serverInstallSubopts=" --server-config "
-    upgradeSubopts=" --from-agent-dir --from-server-dir --agent-auto-start --run-data-migrator --storage-config --storage-data-root-dir --use-remote-storage-node "
-    booleanSubopts=" --agent-auto-start --use-remote-storage-node "
+    storageInstallSubopts=" --storage-data-root-dir "
+    agentInstallSubopts=" --agent-config --agent-preference "
+    #serverInstallSubopts=" --server-config "
+    upgradeSubopts=" --from-agent-dir --from-server-dir --start --run-data-migrator --storage-config --storage-data-root-dir --use-remote-storage-node "
+    booleanSubopts=" --use-remote-storage-node "
     trueFalse="true false"
 
     if [[ "${COMP_LINE}" =~ ^\.?rhqctl[[:space:]]*$ ]] ; then
@@ -40,6 +42,9 @@ _rhqctl() {
           if [[ "x${prev}" == "x--run-data-migrator" ]] ; then
             COMPREPLY=( $(compgen -W "${dataMigratorSubopts}" -- ${cur}) )
             return 0
+          elif [[ "x${prev}" == "x--start" ]] ; then
+            COMPREPLY=( $(compgen -W "${upgradeSubopts}" -- ${cur}) )
+            return 0
           else
             checkForBooleanSubopt ${prev} && return 0
             completePath ${cur}
@@ -50,35 +55,34 @@ _rhqctl() {
         return 0
     elif [[ "x${first}" == "xinstall" ]] ; then
         second="${COMP_WORDS[2]}"
-        if [[ "x$second" == "x" ]] ; then
-            COMPREPLY=( $(compgen -W "${agentServerStorage}" -- ${cur}) )
+        if [[ "x$second" == "x" ]] || [[ "x${second}" == "x--" ]]; then
+            COMPREPLY=( $(compgen -W "${agentServerStorageStart}" -- ${cur}) )
             return 0
         fi
 
-        if [[ "x${second}" == "x--server" ]] ; then
-            if [[ "${serverInstallSubopts}" == *" ${prev} "* ]] ; then
-                completePath ${cur}
-                return 0
-            fi
-            COMPREPLY=( $(compgen -W "${serverInstallSubopts}" -- ${cur}) )
-            return 0
-        elif [[ "x${second}" == "x--agent" ]] ; then
+        #if [[ "x${second}" == "x--server" ]] ; then
+        #    if [[ "${serverInstallSubopts}" == *" ${prev} "* ]] ; then
+        #        completePath ${cur}
+        #        return 0
+        #    fi
+        #    COMPREPLY=( $(compgen -W "${serverInstallSubopts} --start" -- ${cur}) )
+        #    return 0
+        if [[ "x${second}" == "x--agent" ]] ; then
             if [[ "${agentInstallSubopts}" == *" ${prev} "* ]] ; then
-                checkForBooleanSubopt ${prev}  && return 0
                 completePath ${cur}
                 return 0
             fi
-            COMPREPLY=( $(compgen -W "${agentInstallSubopts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "${agentInstallSubopts} --start" -- ${cur}) )
             return 0
         elif [[ "x${second}" == "x--storage" ]] ; then
             if [[ "${storageInstallSubopts}" == *" ${prev} "* ]] ; then
                 completePath ${cur}
                 return 0
             fi
-            COMPREPLY=( $(compgen -W "${storageInstallSubopts}" -- ${cur}) )
+            COMPREPLY=( $(compgen -W "${storageInstallSubopts} --start" -- ${cur}) )
             return 0
         fi
-        COMPREPLY=( $(compgen -W "${agentServerStorage}" -- ${cur}) )
+        COMPREPLY=( $(compgen -W "${agentServerStorageStart}" -- ${cur}) )
         return 0
     # fallback
     elif [[ "${cur}" == * ]] ; then
@@ -88,12 +92,8 @@ _rhqctl() {
 }
 
 function completePath(){
-  if [[ "${cur}x" == *"/x" ]] ; then
-    toComplete="${cur}"
-  else
-    toComplete="${cur}*"
-  fi
-  COMPREPLY=( $(compgen -W "$( ls -1 ${toComplete} 2> /dev/null )" ) )
+  compopt -o nospace
+  COMPREPLY=( $(compgen -f "${cur}") )
 }
 
 function checkForBooleanSubopt(){

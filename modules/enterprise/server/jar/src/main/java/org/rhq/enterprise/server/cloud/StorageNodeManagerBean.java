919604ca697644ecf9fc8109395ecac9d21d781c
Jay Shaughnessy <jshaughn@redhat.com>
2014-03-31 16:29:06 -0400

    [1079027] Storage Node (un)deployment can cause deadlock in rhq_storage_node table Fix issue with new transactioning that left some entity changes uncommitted.

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cloud/StorageNodeManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cloud/StorageNodeManagerBean.java
@@ -204,6 +204,8 @@ public class StorageNodeManagerBean implements StorageNodeManagerLocal, StorageN
                 storageNode.setAddress(address);
                 storageNode.setResource(resource);
                 storageNode.setOperationMode(OperationMode.NORMAL);
+                storageNodeManager.linkExistingStorageNodeToResource(storageNode);
+
             } else {
                 StorageClusterSettings clusterSettings = storageClusterSettingsManager
                     .getClusterSettings(subjectManager.getOverlord());
@@ -224,6 +226,23 @@ public class StorageNodeManagerBean implements StorageNodeManagerLocal, StorageN
             throw new RuntimeException("Could not resolve address [" + address + "]. The resource " + resource
                 + " cannot be linked to a storage node", e);
         }
+    }
+
+    @Override
+    public StorageNode linkExistingStorageNodeToResource(StorageNode storageNode) {
+        StorageNode existingStorageNode = entityManager.find(StorageNode.class, storageNode.getId());
+        if (null != existingStorageNode) {
+            existingStorageNode.setAddress(storageNode.getAddress());
+            existingStorageNode.setResource(storageNode.getResource());
+            existingStorageNode.setOperationMode(storageNode.getOperationMode());
+            storageNode = entityManager.merge(existingStorageNode);
+
+        } else {
+            log.info("Storage node did not exist, could not link to Resource. Returning unpersisted Storage Node "
+                + storageNode);
+        }
+
+        return storageNode;
     }
 
     @Override

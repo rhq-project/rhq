6b86e62eed5d11cc1d91989935db12fc41abaa15
Jirka Kremser <jkremser@redhat.com>
2014-05-16 13:41:28 +0200

    Turning off our GWT based AuthN.

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/coregui/client/CoreGUI.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/coregui/client/CoreGUI.java
@@ -179,7 +179,8 @@ public class CoreGUI implements EntryPoint, ValueChangeHandler<String>, Event.Na
 
         messageCenter = new MessageCenter();
 
-        UserSessionManager.login();
+//        UserSessionManager.login();
+        UserSessionManager.login("rhqadmin", "rhqadmin");
     }
 
     public int getRpcTimeout() {

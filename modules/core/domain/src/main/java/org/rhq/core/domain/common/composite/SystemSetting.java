baa10480d9fc5ded3e46c3fabba878b9d5cf5fd2
Libor Zoubek <lzoubek@redhat.com>
2014-06-17 07:38:16 +0200

    Added proxy server settings to system settings
    this will need a change in jboss-software server plugin, which also has
    proxy server settings. Plugin will fallback to server settings.

--- modules/core/domain/src/main/java/org/rhq/core/domain/common/composite/SystemSetting.java
+++ modules/core/domain/src/main/java/org/rhq/core/domain/common/composite/SystemSetting.java
@@ -188,7 +188,15 @@ public enum SystemSetting {
      * This is a default password that can be used to connect to a remote SSH box.
      * Right now, it's used when remotely controlling/installing/uninstalling agents.
      */
-    REMOTE_SSH_PASSWORD_DEFAULT("REMOTE_SSH_PASSWORD_DEFAULT", PropertySimpleType.PASSWORD, false, false, true)
+    REMOTE_SSH_PASSWORD_DEFAULT("REMOTE_SSH_PASSWORD_DEFAULT", PropertySimpleType.PASSWORD, false, false, true),
+
+    HTTP_PROXY_SERVER_HOST("HTTP_PROXY_SERVER_HOST", PropertySimpleType.STRING, false, false, true),
+
+    HTTP_PROXY_SERVER_PORT("HTTP_PROXY_SERVER_PORT", PropertySimpleType.INTEGER, false, true, true),
+
+    HTTP_PROXY_SERVER_USERNAME("HTTP_PROXY_SERVER_USERNAME", PropertySimpleType.STRING, false, false, true),
+
+    HTTP_PROXY_SERVER_PASSWORD("HTTP_PROXY_SERVER_PASSWORD", PropertySimpleType.PASSWORD, false, false, true)
 
     ;
 

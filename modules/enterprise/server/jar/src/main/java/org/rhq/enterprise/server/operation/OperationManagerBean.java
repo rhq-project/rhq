d1be24fa474505640c396e0968dabd22dcaf2e62
Jay Shaughnessy <jshaughn@redhat.com>
2014-07-16 17:54:37 -0400

    [1111064] Some code changes to avoid a locking issue.  Presumably the issue has to so with passing an attached entity to an asynchronous slsb method.  A few things: - make sure we pass a deteched entity - notify the alert condition-cache prior to calling the storage operation hook - only call the storage operation hook for updates to existing history (not   at operation schedule-time) - remove an unnecessary outer Tx on runClusterMaintenance - add some logging
    Note: TODO: revert the hyped up scheduling of the cluster readrepair job

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/operation/OperationManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/operation/OperationManagerBean.java
@@ -987,7 +987,8 @@ public class OperationManagerBean implements OperationManagerLocal, OperationMan
 
         // The history item may have been created already, so find it in the database and
         // set the new state from our input
-        if (history.getId() != 0) {
+        boolean isNewHistory = (0 == history.getId());
+        if (!isNewHistory) {
             OperationHistory existingHistoryItem = entityManager.find(OperationHistory.class, history.getId());
             if (null == existingHistoryItem) {
                 throw new IllegalArgumentException(
@@ -1032,8 +1033,24 @@ public class OperationManagerBean implements OperationManagerLocal, OperationMan
         if (history.getParameters() != null) {
             history.getParameters().getId(); // eagerly reload the parameters
         }
-        storageNodeOperationsHandler.handleOperationUpdateIfNecessary(history);
+
+        // we can even alert on In-Progress (an operation just being scheduled) so we need to check the
+        // condition manager
         notifyAlertConditionCacheManager("updateOperationHistory", history);
+
+        // if this is not the initial create (i.e schedule-time of the operation) it means the
+        // operation status has likely been updated.  Notify the storage node to see if it needs
+        // to do anything in response to a storage node operation completion.  Don't pass an
+        // attached entity to an Asynchronous SLSB method that runs in its own transaction. That
+        // can cause locking with the current transaction.  Note we can't pass in just the id, because
+        // the updates to the history are not yet committed and the async method needs to see the updated
+        // history.
+        if (!isNewHistory) {
+            entityManager.flush();
+            entityManager.detach(history);
+            storageNodeOperationsHandler.handleOperationUpdateIfNecessary(history);
+        }
+
         return history;
     }
 

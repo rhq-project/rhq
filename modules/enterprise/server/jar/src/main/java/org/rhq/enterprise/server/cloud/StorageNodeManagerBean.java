6d39a26258d5bd2d5c6ec3a48a5d894344eb0682
Jay Shaughnessy <jshaughn@redhat.com>
2014-09-12 14:31:46 -0400

    [1138328] globally uncaught exception on storage metrics admin page Although I was still unable to reproduce this bug I did find a couple of places where we needed to shore things up, that perhaps contributed. - fix getResourceIdFromStorageNode utility to throw the correct exceptions,   this solves issues where caller catch definitions were not catching. - when uninventorying a Storage Node Resource we did not pro-actively unlink   it from the StorageNode entity.  We did the unlink in the async work but that   that left the bad resource linked until async job ran or until   it was re-linked to a newly imported SN resource.  Now unlinked up front   (which is unfortunate as it adds another db query per batch).
    (cherry picked from commit 58023b78baae102abdefa6d222a9a9d73dd69f0c)
    Signed-off-by: Jay Shaughnessy <jshaughn@redhat.com>

--- modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cloud/StorageNodeManagerBean.java
+++ modules/enterprise/server/jar/src/main/java/org/rhq/enterprise/server/cloud/StorageNodeManagerBean.java
@@ -772,11 +772,11 @@ public class StorageNodeManagerBean implements StorageNodeManagerLocal, StorageN
         if (storageNode.getResource() == null) {
             storageNode = entityManager.find(StorageNode.class, storageNode.getId());
             if (storageNode == null) { // no storage node with the specified id
-                throw new ResourceNotFoundException("There is no storage node with id [" + storageNodeId
+                throw new IllegalStateException("There is no storage node with id [" + storageNodeId
                     + "] stored in the database.");
             }
             if (storageNode.getResource() == null) { // no associated resource
-                throw new IllegalStateException("This storage node [" + storageNode.getId()
+                throw new ResourceNotFoundException("This storage node [" + storageNode.getId()
                     + "] has no associated resource.");
             }
         }
@@ -1277,7 +1277,7 @@ public class StorageNodeManagerBean implements StorageNodeManagerLocal, StorageN
                 if (REGULAR_SNAPSHOTS_SCHEDULE_DESCRIPTION.equals(schedule.getDescription())) {
                     log.info("Found operation schedule, unscheduling " + schedule);
                     operationManager.unscheduleResourceOperation(subject, schedule.getJobId().toString(), test.getId());
-                    // delete history items that have been scheduled but not yet started 
+                    // delete history items that have been scheduled but not yet started
                     ResourceOperationHistoryCriteria criteria = new ResourceOperationHistoryCriteria();
                     criteria.setPageControl(PageControl.getUnlimitedInstance());
                     criteria.addFilterJobId(schedule.getJobId());

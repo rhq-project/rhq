daddb445f25cb02b6cdb1d7fe6c61afbcff669ca
Stefan Negrea <snegrea@redhat.com>
2014-09-18 14:54:35 -0500

    [BZ 1141792] For backwards compatibility add the old script name back with a huge deprecation warning. The script will just run the newer functionality but the file name is kept intact.

---
+++ modules/enterprise/server/appserver/src/main/bin-resources/bin/rhq-encode-password.bat
@@ -0,0 +1,29 @@
+
+@echo off
+
+rem this script has been deprecated and will be removed in the next release
+
+echo WARNING! rhq-encode-password.bat has been deprecated. Please use rhq-encode-value.bat utility.
+echo.
+
+
+setlocal enabledelayedexpansion
+
+rem if debug variable is set, it is assumed to be on, unless its value is false
+if "%RHQ_CONTROL_DEBUG%" == "false" (
+   set RHQ_CONTROL_DEBUG=
+)
+
+set _SCRIPT_DIR=%~dp0
+if exist "%_SCRIPT_DIR%\rhq-server-env.bat" (
+   call "%_SCRIPT_DIR%\rhq-server-env.bat" %*
+) else (
+   if defined RHQ_CONTROL_DEBUG echo Failed to find rhq-server-env.bat. Continuing with current environment...
+)
+
+cd "%_SCRIPT_DIR%\internal"
+
+rem internal scripts assume they are running in the current working directory
+rhq-installer --encodevalue
+
+endlocal

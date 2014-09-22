0ee7953efebe337b74ca3065b7fd20b9c7da85a3
John Sanda <jsanda@redhat.com>
2014-09-22 17:50:25 -0400

    [BZ 1144238] fix compiler error and update dependencies

--- modules/enterprise/server/server-metrics/src/main/java/org/rhq/server/metrics/StorageResultSetFuture.java
+++ modules/enterprise/server/server-metrics/src/main/java/org/rhq/server/metrics/StorageResultSetFuture.java
@@ -44,10 +44,6 @@ public class StorageResultSetFuture implements ListenableFuture<ResultSet> {
         return wrapperFuture.isDone();
     }
 
-    public void setException(Throwable t) {
-        wrapperFuture.setException(t);
-    }
-
     /**
      * Delegates to {@link com.datastax.driver.core.ResultSetFuture#getUninterruptibly()}
      */

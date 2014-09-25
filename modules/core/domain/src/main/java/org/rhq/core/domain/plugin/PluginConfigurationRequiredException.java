bd1095fa5ac2f55544d5f8c4b45033b4050b94d9
Libor Zoubek <lzoubek@redhat.com>
2014-09-25 14:26:42 +0200

    Bug 1125423 - Fuse Fabric Groups Plugin cannot be enabled
    Generally when there is an attempt to enable server plugins we now first
    check it's configuration by looking for required properties (in ConfigDef)
    and making sure there's valid value in configuration. If something is
    missing, it means plugin is not configured and cannot be enabled.
    ServerPluginManagerBean thows this using
    PluginConfigurationRequiredException that is then presented to user.

---
+++ modules/core/domain/src/main/java/org/rhq/core/domain/plugin/PluginConfigurationRequiredException.java
@@ -0,0 +1,36 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2014 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.core.domain.plugin;
+
+public class PluginConfigurationRequiredException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public PluginConfigurationRequiredException(String message) {
+        super(message);
+    }
+
+    public PluginConfigurationRequiredException(String message, Throwable t) {
+        super(message, t);
+    }
+
+}

1101df820b973198d4026d7cfd8402ee9797540b
Thomas Segismont <tsegismo@redhat.com>
2014-09-25 18:49:30 +0200

    Bug 1140676 - Exception logged while inventorying EAP 6 host controller after setting http-interface
    Throw IPCE when security realm is not ready so that a proper resource
    error shows up in the GUI.
    
    (cherry picked from commit 9c1d53de54065ac9240df9c5d526fef04b09419c)
    Signed-off-by: Libor Zoubek <lzoubek@redhat.com>

---
+++ modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/json/SecurityRealmNotReadyException.java
@@ -0,0 +1,34 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2014 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+package org.rhq.modules.plugins.jbossas7.json;
+
+/**
+ * Denotes JBAS015135: The security realm is not ready to process requests. This usually indicates that no user is
+ * configured.
+ *
+ * @author Thomas Segismont
+ */
+public class SecurityRealmNotReadyException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public SecurityRealmNotReadyException(String message) {
+        super(message);
+    }
+}

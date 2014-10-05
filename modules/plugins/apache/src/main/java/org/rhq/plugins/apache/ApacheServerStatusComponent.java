d24d05d56a637158d3bfa64fecb36db4d883107c
Jeremie Lagarde <jer@printstacktrace.org>
2014-10-05 21:28:38 +0200

    BZ1103426 Avability for ApacheServerStatusComponent

--- modules/plugins/apache/src/main/java/org/rhq/plugins/apache/ApacheServerStatusComponent.java
+++ modules/plugins/apache/src/main/java/org/rhq/plugins/apache/ApacheServerStatusComponent.java
@@ -27,12 +27,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.domain.measurement.MeasurementDataNumeric;
 import org.rhq.core.domain.measurement.MeasurementReport;
@@ -49,9 +52,23 @@ import org.rhq.core.pluginapi.measurement.MeasurementFacet;
  */
 public class ApacheServerStatusComponent implements ResourceComponent<ApacheLocationComponent>, MeasurementFacet {
 
+    private static final Log LOG = LogFactory.getLog(ApacheServerStatusComponent.class);
+
     public static final String REGEXP_PROP = "regexp";
 
     private static final String SCOREBOARD = "Scoreboard";
+
+    private static final String METRIC_OPEN_SLOT_WITH_NO_CURRENT_PROCESS = "openSlotWithNoCurrentProcess";
+    private static final String METRIC_IDLE_CLEANUP_OF_WORKER = "idleCleanupOfWorker";
+    private static final String METRIC_GRACEFULLY_FINISHING = "gracefullyFinishing";
+    private static final String METRIC_LOGGING = "logging";
+    private static final String METRIC_CLOSING_CONNECTION = "closingConnection";
+    private static final String METRIC_DNS_LOOKUP = "dnsLookup";
+    private static final String METRIC_KEEPALIVE = "keepalive";
+    private static final String METRIC_SENDING_REPLY = "sendingReply";
+    private static final String METRIC_READING_REQUEST = "readingRequest";
+    private static final String METRIC_STARTING_UP = "startingUp";
+    private static final String METRIC_WAITING_FOR_CONNECTION = "waitingForConnection";
 
     ResourceContext<ApacheLocationComponent> resourceContext;
 
@@ -61,29 +78,71 @@ public class ApacheServerStatusComponent implements ResourceComponent<ApacheLoca
     }
 
     public void stop() {
+        resourceContext = null;
     }
 
     public AvailabilityType getAvailability() {
-        //TODO implement this
-        return AvailabilityType.UP;
+        AvailabilityType availability = resourceContext.getParentResourceComponent().getAvailability();
+        if (AvailabilityType.UP.equals(availability)) {
+            InputStream datas = null;
+            BufferedReader reader = null;
+            try {
+                datas = getDatas();
+                String line;
+                reader = new BufferedReader(new InputStreamReader(datas, "UTF-8"));
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains(SCOREBOARD))
+                        return AvailabilityType.UP;
+                }
+                return AvailabilityType.MISSING;
+            } catch (Exception e) {
+                LOG.error("Can't read the scoreboard : " + e, e);
+            } finally {
+                if (reader != null)
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        LOG.error("Can't close the scoreboard reader : " + e);
+                    }
+                if (datas != null)
+                    try {
+                        datas.close();
+                    } catch (IOException e) {
+                        LOG.error("Can't close the scoreboard data : " + e);
+                    }
+            }
+        }
+        return availability;
     }
 
     @Override
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> schedules) throws Exception {
+        InputStream datas = null;
+        try {
+            datas = getDatas();
+            Map<String, Double> result = parse(datas);
+            for (MeasurementScheduleRequest schedule : schedules) {
+                String metricName = schedule.getName();
+                if (result.containsKey(metricName)) {
+                    MeasurementDataNumeric metric = new MeasurementDataNumeric(schedule, result.get(metricName));
+                    report.addData(metric);
+                }
+            }
+        } finally {
+            if (datas != null)
+                datas.close();
+        }
+
+    }
+
+    private InputStream getDatas() throws MalformedURLException, IOException {
+        InputStream datas;
         URL url = new URL(resourceContext.getParentResourceComponent().getURL() + "?auto");
         URLConnection connection = url.openConnection();
         connection.setConnectTimeout(1000);
         connection.setReadTimeout(2000);
-        Map<String, Double> result = parse(connection.getInputStream());
-
-        for (MeasurementScheduleRequest schedule : schedules) {
-            String metricName = schedule.getName();
-            if(result.containsKey(metricName)) {
-                MeasurementDataNumeric metric = new MeasurementDataNumeric(schedule, result.get(metricName));
-                report.addData(metric);
-            }
-        }
-
+        datas = connection.getInputStream();
+        return datas;
     }
 
     private Map<String, Double> parse(InputStream statusPage) {
@@ -111,7 +170,7 @@ public class ApacheServerStatusComponent implements ResourceComponent<ApacheLoca
     }
 
     private void parseLine(Map<String, Double> result, String line) {
-        String[] datas = line.split(": ");        
+        String[] datas = line.split(": ");
         if (datas.length == 2) {
             if (datas[0].equals(SCOREBOARD)) {
                 char[] scoreboard = datas[1].toCharArray();
@@ -165,21 +224,23 @@ public class ApacheServerStatusComponent implements ResourceComponent<ApacheLoca
                         break;
                     }
                 }
-                result.put("waitingForConnection", waitingForConnection);
-                result.put("startingUp", startingUp);
-                result.put("readingRequest", readingRequest);
-                result.put("sendingReply", sendingReply);
-                result.put("keepalive", keepalive);
-                result.put("dnsLookup", dnsLookup);
-                result.put("closingConnection", closingConnection);
-                result.put("logging", logging);
-                result.put("gracefullyFinishing", gracefullyFinishing);
-                result.put("idleCleanupOfWorker", idleCleanupOfWorker);
-                result.put("openSlotWithNoCurrentProcess", openSlotWithNoCurrentProcess);
+                result.put(METRIC_WAITING_FOR_CONNECTION, waitingForConnection);
+                result.put(METRIC_STARTING_UP, startingUp);
+                result.put(METRIC_READING_REQUEST, readingRequest);
+                result.put(METRIC_SENDING_REPLY, sendingReply);
+                result.put(METRIC_KEEPALIVE, keepalive);
+                result.put(METRIC_DNS_LOOKUP, dnsLookup);
+                result.put(METRIC_CLOSING_CONNECTION, closingConnection);
+                result.put(METRIC_LOGGING, logging);
+                result.put(METRIC_GRACEFULLY_FINISHING, gracefullyFinishing);
+                result.put(METRIC_IDLE_CLEANUP_OF_WORKER, idleCleanupOfWorker);
+                result.put(METRIC_OPEN_SLOT_WITH_NO_CURRENT_PROCESS, openSlotWithNoCurrentProcess);
             } else {
-                result.put(datas[0].replace(" ", ""), Double.parseDouble(datas[1]));
+                try {
+                    result.put(datas[0].replace(" ", ""), Double.parseDouble(datas[1]));
+                } catch (NumberFormatException ex) {
+                }
             }
         }
     }
-
 }

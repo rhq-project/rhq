d5e65f48de47b1c3258277e5e3e7d16881b679fc
Thomas Segismont <tsegismo@redhat.com>
2014-10-27 17:02:29 +0100

    Bug 1154678 - [EAP6] - Newly created datasource is not enabled even when you have set Enabled=true -- user should be at least warned
    When the datasource was created, the 'enabled' attribute was supplied to the EAP6 management interface.
    But setting the attribute to true does not enable the datasource. An extra server reload is needed.
    To enable a datasource without a server reload, the 'enable' operation needs to be called.
    
    So now the DatasourcesComponent class:
    -> no longer supplies the 'enabled' attribute to the CreateResourceDelegate
    -> reads the state of the datasource after creation
    -> invokes the 'enable' or 'disable' operation (as needed) after creation

--- modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/DatasourcesComponent.java
+++ modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/DatasourcesComponent.java
@@ -24,7 +24,9 @@ import static org.rhq.core.domain.resource.CreateResourceStatus.FAILURE;
 import static org.rhq.core.domain.resource.CreateResourceStatus.INVALID_ARTIFACT;
 import static org.rhq.core.domain.resource.CreateResourceStatus.SUCCESS;
 import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.CONNECTION_PROPERTIES_ATTRIBUTE;
+import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.DISABLE_OPERATION;
 import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.ENABLED_ATTRIBUTE;
+import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.ENABLE_OPERATION;
 import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.XA_DATASOURCE_PROPERTIES_ATTRIBUTE;
 import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.getConnectionPropertiesAsMap;
 import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.isXADatasourceResource;
@@ -32,14 +34,17 @@ import static org.rhq.modules.plugins.jbossas7.DatasourceComponent.isXADatasourc
 import java.util.List;
 import java.util.Map;
 
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.configuration.ConfigurationUtility;
 import org.rhq.core.domain.configuration.PropertyList;
 import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.domain.configuration.definition.ConfigurationDefinition;
 import org.rhq.core.domain.resource.ResourceType;
 import org.rhq.core.pluginapi.inventory.CreateResourceReport;
 import org.rhq.modules.plugins.jbossas7.json.Address;
 import org.rhq.modules.plugins.jbossas7.json.CompositeOperation;
 import org.rhq.modules.plugins.jbossas7.json.Operation;
+import org.rhq.modules.plugins.jbossas7.json.ReadAttribute;
 import org.rhq.modules.plugins.jbossas7.json.Result;
 
 /**
@@ -50,24 +55,37 @@ import org.rhq.modules.plugins.jbossas7.json.Result;
 public class DatasourcesComponent extends BaseComponent<BaseComponent<?>> {
 
     @Override
-    public CreateResourceReport createResource(CreateResourceReport createResourceReport) {
-        ResourceType resourceType = createResourceReport.getResourceType();
+    public CreateResourceReport createResource(CreateResourceReport report) {
+        ResourceType resourceType = report.getResourceType();
+        ConfigurationDefinition configDef = resourceType.getResourceConfigurationDefinition().copy();
+        Configuration config = report.getResourceConfiguration().deepCopy(false);
 
-        List<String> validationErrors = ConfigurationUtility.validateConfiguration(
-            createResourceReport.getResourceConfiguration(), resourceType.getResourceConfigurationDefinition());
+        // Check if the users wants the datasource to be enabled
+        PropertySimple enabledProperty = config.getSimple(ENABLED_ATTRIBUTE);
+        // Let's assume the user wants the datasource enabled if the attribute is not present
+        Boolean enableDatasource = enabledProperty == null ? TRUE : enabledProperty.getBooleanValue();
+
+        // Remove this attribute which is manually managed
+        configDef.getPropertyDefinitions().remove(ENABLED_ATTRIBUTE);
+        config.remove(ENABLED_ATTRIBUTE);
+
+        List<String> validationErrors = ConfigurationUtility.validateConfiguration(config, configDef);
 
         if (!validationErrors.isEmpty()) {
-            createResourceReport.setErrorMessage(validationErrors.toString());
-            createResourceReport.setStatus(FAILURE);
-            return createResourceReport;
+            report.setErrorMessage(validationErrors.toString());
+            report.setStatus(FAILURE);
+            return report;
         }
 
-        final CreateResourceReport resourceReport = super.createResource(createResourceReport);
+        CreateResourceDelegate delegate = new CreateResourceDelegate(configDef, getASConnection(), getAddress());
+        report = delegate.createResource(report);
 
         // No success -> no point in continuing
-        if (resourceReport.getStatus() != SUCCESS) {
-            return resourceReport;
+        if (report.getStatus() != SUCCESS) {
+            return report;
         }
+
+        Address datasourceAddress = new Address(report.getResourceKey());
 
         // outer create resource did not cater for the connection or xa properties, so lets add them now
         String connPropAttributeNameOnServer, connPropPluginConfigPropertyName, keyName;
@@ -80,64 +98,55 @@ public class DatasourcesComponent extends BaseComponent<BaseComponent<?>> {
             connPropPluginConfigPropertyName = CONNECTION_PROPERTIES_ATTRIBUTE;
             keyName = "pname";
         }
-        PropertyList listPropertyWrapper = createResourceReport.getResourceConfiguration().getList(
-            connPropPluginConfigPropertyName);
+        PropertyList listPropertyWrapper = config.getList(connPropPluginConfigPropertyName);
         Map<String, String> connectionPropertiesAsMap = getConnectionPropertiesAsMap(listPropertyWrapper, keyName);
         // if no conn or xa props supplied in the create resource request, skip and continue
         if (!connectionPropertiesAsMap.isEmpty()) {
             CompositeOperation cop = new CompositeOperation();
             for (Map.Entry<String, String> connectionProperty : connectionPropertiesAsMap.entrySet()) {
-                Address propertyAddress = new Address(resourceReport.getResourceKey());
-                propertyAddress.add(connPropAttributeNameOnServer, connectionProperty.getKey());
-                Operation op = new Operation("add", propertyAddress);
+                datasourceAddress.add(connPropAttributeNameOnServer, connectionProperty.getKey());
+                Operation op = new Operation("add", datasourceAddress);
                 op.addAdditionalProperty("value", connectionProperty.getValue());
                 cop.addStep(op);
             }
             Result res = getASConnection().execute(cop);
             if (!res.isSuccess()) {
-                resourceReport.setErrorMessage("Datasource was added, but setting " + connPropAttributeNameOnServer
+                report.setErrorMessage("Datasource was added, but setting " + connPropAttributeNameOnServer
                     + " failed: " + res.getFailureDescription());
-                resourceReport.setStatus(INVALID_ARTIFACT);
-                return resourceReport;
+                report.setStatus(INVALID_ARTIFACT);
+                return report;
             }
         }
 
-        // Handle the 'enabled' property (it's a read-only attribute in management interface)
-        // See https://bugzilla.redhat.com/show_bug.cgi?id=854773
+        // Now enable/disable datasource as required
 
-        // What did the user say in the datasource creation form?
-        PropertySimple enabledProperty = createResourceReport.getResourceConfiguration().getSimple(ENABLED_ATTRIBUTE);
-        // Let's assume the user wants the datasource enabled if he/she said nothing
-        Boolean enabledPropertyValue = enabledProperty == null ? TRUE : enabledProperty.getBooleanValue();
+        ReadAttribute readEnabledAttribute = new ReadAttribute(datasourceAddress, ENABLED_ATTRIBUTE);
+        Result readEnabledAttributeResult = getASConnection().execute(readEnabledAttribute);
+        if (!readEnabledAttributeResult.isSuccess()) {
+            report.setStatus(INVALID_ARTIFACT);
+            report.setErrorMessage("Datasource was added but the agent could not read its configuration: "
+                + readEnabledAttributeResult.getFailureDescription());
+            return report;
+        }
+        Boolean enabledPropertyCurrentValue = (Boolean) readEnabledAttributeResult.getResult();
+        if (enabledPropertyCurrentValue != enableDatasource) {
+            if (enableDatasource == TRUE) {
+                Operation operation = new Operation(ENABLE_OPERATION, datasourceAddress);
+                Result result = getASConnection().execute(operation);
+                if (!result.isSuccess()) {
+                    report.setStatus(INVALID_ARTIFACT);
+                    report.setErrorMessage("Datasource was added but not enabled: " + result.getFailureDescription());
+                }
+            } else {
+                Operation operation = new Operation(DISABLE_OPERATION, datasourceAddress);
+                Result result = getASConnection().execute(operation);
+                if (!result.isSuccess()) {
+                    report.setStatus(INVALID_ARTIFACT);
+                    report.setErrorMessage("Datasource was added but not disabled: " + result.getFailureDescription());
+                }
+            }
+        }
 
-        DatasourceEnabledAttributeHelper
-            .on(new Address(resourceReport.getResourceKey()))
-            .with(getASConnection())
-            .setAttributeValue(enabledPropertyValue,
-                new DatasourceEnabledAttributeHelper.EnabledAttributeHelperCallbacks() {
-                    @Override
-                    public void onReadAttributeFailure(Result opResult) {
-                        resourceReport.setStatus(INVALID_ARTIFACT);
-                        resourceReport.setErrorMessage("Datasource was added, "
-                            + "but could not read its configuration after creation: "
-                            + opResult.getFailureDescription());
-                    }
-
-                    @Override
-                    public void onEnableOperationFailure(Result opResult) {
-                        resourceReport.setStatus(INVALID_ARTIFACT);
-                        resourceReport.setErrorMessage("Datasource was added but not enabled: "
-                            + opResult.getFailureDescription());
-                    }
-
-                    @Override
-                    public void onDisableOperationFailure(Result opResult) {
-                        resourceReport.setStatus(INVALID_ARTIFACT);
-                        resourceReport.setErrorMessage("Datasource was added but not disabled: "
-                            + opResult.getFailureDescription());
-                    }
-                });
-
-        return resourceReport;
+        return report;
     }
 }

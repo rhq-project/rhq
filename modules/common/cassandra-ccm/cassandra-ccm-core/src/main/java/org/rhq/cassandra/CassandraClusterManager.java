d5b50e9d939af70a0a0ef8ff553f7e005825fc2e
Stefan Negrea <snegrea@redhat.com>
2014-12-01 11:24:52 -0600

    Fix reactor order and remove circular dependencies issues.

--- modules/common/cassandra-ccm/cassandra-ccm-core/src/main/java/org/rhq/cassandra/CassandraClusterManager.java
+++ modules/common/cassandra-ccm/cassandra-ccm-core/src/main/java/org/rhq/cassandra/CassandraClusterManager.java
@@ -36,8 +36,10 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -45,7 +47,6 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.rhq.core.pluginapi.util.ProcessExecutionUtility;
 import org.rhq.core.system.OperatingSystemType;
 import org.rhq.core.system.ProcessExecution;
 import org.rhq.core.system.ProcessExecutionResults;
@@ -273,10 +274,10 @@ public class CassandraClusterManager {
 
         if (systemInfo.getOperatingSystemType() == OperatingSystemType.WINDOWS) {
             startScript = new File(binDir, "cassandra.bat");
-            startScriptExe = ProcessExecutionUtility.createProcessExecution(startScript);
+            startScriptExe = createProcessExecution(null, startScript);
         } else {
             startScript = new File(binDir, "cassandra");
-            startScriptExe = ProcessExecutionUtility.createProcessExecution(startScript);
+            startScriptExe = createProcessExecution(null, startScript);
             startScriptExe.addArguments(Arrays.asList("-p", "cassandra.pid"));
         }
 
@@ -356,5 +357,91 @@ public class CassandraClusterManager {
         StreamUtil.copy(new FileReader(new File(binDir, "cassandra.pid")), writer);
 
         return Long.parseLong(writer.getBuffer().toString());
+    }
+
+    /**
+     * Creates a ProcessExecution for the specified file for the current platform. If the current platform is Windows
+     * and the file name ends with ".bat" or ".cmd", the file will be assumed to be a Windows batch file, and the
+     * process execution will be initialized accordingly. Note, if the file is a UNIX script, its first line must be a
+     * valid #! reference to a script interpreter (e.g. #!/bin/sh), otherwise it will fail to execute. The returned
+     * ProcessExecution will have a non-null arguments list, an environment map that is a copy of the current process's
+     * environment, and a working directory set to its executable's parent directory.
+     *
+     * @param  prefix a prefix command line that should be prepended to the executable's command line
+     *                (e.g. "/usr/bin/nohup /usr/bin/sudo -u jboss -g jboss"). any files on the
+     *                command line should be absolute paths. if null, no prefix command line will be
+     *                prepended
+     * @param  file an executable or a batch file
+     *
+     * @return a process execution
+     */
+    public static ProcessExecution createProcessExecution(String prefix, File file) {
+        SystemInfo systemInfo = SystemInfoFactory.createSystemInfo();
+
+        ProcessExecution processExecution;
+
+        List<String> prefixArgs;
+        if (prefix != null) {
+            // TODO (ips, 04/27/10): Ideally, the prefix should be a String[], not a String.
+            prefixArgs = Arrays.asList(prefix.split("[ \t]+"));
+        } else {
+            prefixArgs = Collections.emptyList();
+        }
+        String executable;
+        List<String> args = new ArrayList<String>();
+        if (systemInfo.getOperatingSystemType() == OperatingSystemType.WINDOWS && isBatchFile(file)) {
+            // Windows batch files cannot be executed directly - they must be passed as arguments to cmd.exe, e.g.
+            // "C:\Windows\System32\cmd.exe /c C:\opt\jboss-as\bin\run.bat".
+            executable = getCmdExeFile().getPath();
+            args.add("/c");
+            args.addAll(prefixArgs);
+            args.add(file.getPath());
+        } else {
+            // UNIX
+            if (prefixArgs.isEmpty()) {
+                executable = file.getPath();
+            } else {
+                executable = prefixArgs.get(0);
+                if (prefixArgs.size() > 1) {
+                    args.addAll(prefixArgs.subList(1, prefixArgs.size()));
+                }
+                args.add(file.getPath());
+            }
+        }
+
+        processExecution = new ProcessExecution(executable);
+        processExecution.setArguments(args);
+
+        // Start out with a copy of our own environment, since Windows needs
+        // certain system environment variables to find DLLs, etc., and even
+        // on UNIX, many scripts will require certain environment variables
+        // (PATH, JAVA_HOME, etc.).
+        // TODO (ips, 04/27/12): We probably should not just do this by default.
+        Map<String, String> envVars = new LinkedHashMap<String, String>(System.getenv());
+        processExecution.setEnvironmentVariables(envVars);
+
+        // Many scripts (e.g. JBossAS scripts) assume their working directory is the directory containing the script.
+        processExecution.setWorkingDirectory(file.getParent());
+
+        return processExecution;
+    }
+
+    private static boolean isBatchFile(File file) {
+        return file.getName().matches(".*\\.((bat)|(cmd))$(?i)");
+    }
+
+    private static File getCmdExeFile() {
+        String cmdExe = System.getenv("COMSPEC");
+        if (cmdExe == null) {
+            throw new RuntimeException("COMSPEC environment variable is not defined.");
+            // TODO: Try to find cmd.exe by checking the various usual locations.
+        }
+
+        File cmdExeFile = new File(cmdExe);
+        if (!cmdExeFile.exists()) {
+            throw new RuntimeException("COMSPEC environment variable specifies a non-existent path: " + cmdExe);
+        }
+
+        return cmdExeFile;
     }
 }

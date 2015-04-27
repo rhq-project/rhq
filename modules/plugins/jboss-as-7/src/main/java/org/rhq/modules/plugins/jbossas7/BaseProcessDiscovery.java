rhq-project/rhq

commit f2dc89709b73b7506403f04ff36dc894a44232f5

Detect Managed AS through hosts.xml , start with deployments.

modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/BaseProcessDiscovery.java
@@ -1,3 +1,21 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.rhq.modules.plugins.jbossas7;
 
 import java.io.File;
@@ -58,12 +76,9 @@ public class BaseProcessDiscovery implements ResourceDiscoveryComponent
             String serverNameFull;
             String serverName;
             String psName = psr.getProcessScan().getName();
-            if (psName.equals("ProcessManager")) {
-                serverNameFull = "ProcessManager";
-                serverName = "ProcessManager";
-            } else if (psName.equals("ASManager")) {
-                serverName = "ASManager";
-                serverNameFull = "ASManager";
+            if (psName.equals("ProcessController")) {
+                serverNameFull = "ProcessController";
+                serverName = "ProcessController";
             } else if (psName.equals("HostController")) {
                 serverName = "HostController";
                 serverNameFull = "HostController";

modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/ASUploadConnection.java
@@ -1,0 +1,144 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.modules.plugins.jbossas7;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.REUtil;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.JsonNode;
+
+/**
+ * Connection for uploading of content.
+ * Partially taken from https://github.com/jbossas/jboss-as/blob/master/testsuite/smoke/src/test/java/org/jboss/as/test/surefire/servermodule/HttpDeploymentUploadUnitTestCase.java
+ *
+ * @author Jonathan Pearlin (of the original code)
+ * @author Heiko W. Rupp
+ */
+public class ASUploadConnection {
+
+    private static final String BOUNDARY = "-----------------------------261773107125236";
+
+    private static final String CRLF = "\r\n";
+
+    private static final String POST_REQUEST_METHOD = "POST";
+
+    private static final String UPLOAD_URL = "http://localhost:9990/domain-api/add-content";
+
+    private final Log log = LogFactory.getLog(ASUploadConnection.class);
+
+    BufferedOutputStream os = null;
+    BufferedInputStream is = null;
+    private HttpURLConnection connection;
+
+    public OutputStream getOutputStream(String fileName) {
+         try {
+            // Create the HTTP connection to the upload URL
+            connection = (HttpURLConnection) new URL(UPLOAD_URL).openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod(POST_REQUEST_METHOD);
+
+            // Grab the test WAR file and get a stream to its contents to be included in the POST.
+            os = new BufferedOutputStream(connection.getOutputStream());
+            os.write(buildPostRequestHeader(fileName));
+
+             return os;
+         }
+         catch (Exception e) {
+             e.printStackTrace();
+         }
+
+        return null;
+    }
+
+    public JsonNode finishUpload()  {
+        try {
+            os.write(buildPostRequestFooter());
+            os.flush();
+
+            is = new BufferedInputStream(connection.getInputStream());
+            // TODO read from IN
+
+        } catch (IOException e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+        finally {
+            closeQuietly(is);
+            closeQuietly(os);
+        }
+
+        return null;
+    }
+
+
+    private byte[] buildPostRequestHeader(String fileName) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(buildPostRequestHeaderSection("form-data; name=\"test1\"", "", "test1"));
+        builder.append(buildPostRequestHeaderSection("form-data; name=\"test2\"", "", "test2"));
+        builder.append(buildPostRequestHeaderSection("form-data; name=\"file\"; filename=\""+fileName+"\"", "application/octet-stream", ""));
+        return builder.toString().getBytes();
+    }
+
+    private String buildPostRequestHeaderSection(final String contentDisposition, final String contentType, final String content) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(BOUNDARY);
+        builder.append(CRLF);
+        if(contentDisposition != null && contentDisposition.length() > 0) {
+            builder.append(String.format("Content-Disposition: %s", contentDisposition));
+        }
+        builder.append(CRLF);
+        if(contentType != null && contentType.length() > 0) {
+            builder.append(String.format("Content-Type: %s", contentType));
+        }
+        builder.append(CRLF);
+        if(content != null && content.length() > 0) {
+            builder.append(content);
+        }
+        builder.append(CRLF);
+        return builder.toString();
+    }
+
+    private byte[] buildPostRequestFooter() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(CRLF);
+        builder.append(BOUNDARY);
+        builder.append("--");
+        builder.append(CRLF);
+        return builder.toString().getBytes();
+    }
+
+
+    private void closeQuietly(final Closeable closeable) {
+        if(closeable != null) {
+            try {
+                closeable.close();
+            } catch (final IOException e) {}
+        }
+    }
+}

modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/ManagedASDiscovery.java
@@ -1,0 +1,289 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.modules.plugins.jbossas7;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.configuration.PropertyList;
+import org.rhq.core.domain.configuration.PropertyMap;
+import org.rhq.core.domain.configuration.PropertySimple;
+import org.rhq.core.pluginapi.event.log.LogFileEventResourceComponentHelper;
+import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
+import org.rhq.core.pluginapi.inventory.ProcessScanResult;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
+import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
+import org.rhq.core.system.ProcessInfo;
+
+/**
+ * Discovery class for managed AS 7 instances.
+ *
+ * @author Heiko W. Rupp
+ */
+public class ManagedASDiscovery implements ResourceDiscoveryComponent
+
+{
+
+    static final String DJBOSS_SERVER_BASE_DIR = "-Djboss.server.base.dir=";
+    static final String DORG_JBOSS_BOOT_LOG_FILE = "-Dorg.jboss.boot.log.file=";
+    static final String DLOGGING_CONFIGURATION = "-Dlogging.configuration=";
+    static final int DEFAULT_MGMT_PORT = 9990;
+    private final Log log = LogFactory.getLog(this.getClass());
+    private Document hostXml;
+    private static final String DJBOSS_SERVER_HOME_DIR = "-Djboss.home.dir";
+
+    /**
+     * Run the auto-discovery
+     */
+    public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext discoveryContext) throws Exception {
+        Set<DiscoveredResourceDetails> discoveredResources = new HashSet<DiscoveredResourceDetails>();
+
+
+        List<ProcessScanResult> scans = discoveryContext.getAutoDiscoveredProcesses();
+
+        for (ProcessScanResult psr : scans) {
+
+            // get the HostController, as this is an indicator for managed AS
+            String psName = psr.getProcessScan().getName();
+            if (!psName.equals("HostController"))
+                continue;
+
+            // Now we have the host controller, lets get the host.xml file
+            // and obtain the servers from there.
+            ProcessInfo processInfo = psr.getProcessInfo();
+            readHostXml(processInfo);
+            String hostName = findHostName();
+            int port = getManagementPortFromHostXml();
+
+
+            List<ServerInfo> serverNames = getServersFromHostXml();
+            for (ServerInfo serverInfo : serverNames) {
+
+                Configuration config = discoveryContext.getDefaultPluginConfiguration();
+                config.put(new PropertySimple("domainHost",hostName));
+                config.put(new PropertySimple("group",serverInfo.group));
+                config.put(new PropertySimple("port",port));
+
+                // TODO this fails for the downed servers.
+                // get from the domain or other place as soon as the domain provides it.
+                initLogFile(scans, serverInfo.name, config, getHomeDirFromCommandLine(processInfo.getCommandLine()));
+
+                DiscoveredResourceDetails detail = new DiscoveredResourceDetails(
+                    discoveryContext.getResourceType(), // ResourceType
+                    hostName + "/" + serverInfo.name, // key
+                    serverInfo.name,  // Name
+                    null,  // TODO real version - get from Domain as soon as it is provided
+                    "Managed AS 7 instance", // Description
+                    config,
+                    null
+                );
+
+                // Add to return values
+                discoveredResources.add(detail);
+                log.info("Discovered new ...  " + discoveryContext.getResourceType() + ", " + serverInfo);
+            }
+        }
+        return discoveredResources;
+    }
+
+    /**
+     * Loop through the Process scans for ManagedAS and if found extract the logfile path.
+     * @param scans process scan results
+     * @param name server name to look for
+     * @param config config to put the info in
+     * @param basePath
+     */
+    private void initLogFile(List<ProcessScanResult> scans, String name, Configuration config, String basePath) {
+
+        for (ProcessScanResult psr : scans) {
+            if (!psr.getProcessScan().getName().equals("ManagedAS"))
+                continue;
+
+            String[] commandLine = psr.getProcessInfo().getCommandLine();
+
+            String logFile = basePath + File.separator +  getLogFileFromCommandLine(commandLine);
+
+            if (logFile.contains(name))
+                initLogEventSourcesConfigProp(logFile,config);
+        }
+    }
+
+    private int getManagementPortFromHostXml() {
+        Element host =  hostXml.getDocumentElement();
+        NodeList interfaceParent = host.getElementsByTagName("management-interfaces");
+        if (interfaceParent ==null || interfaceParent.getLength()==0) {
+            log.warn("No <management-interfaces> found in host.xml");
+            return DEFAULT_MGMT_PORT;
+        }
+        NodeList mgmtInterfaces = interfaceParent.item(0).getChildNodes();
+        if (mgmtInterfaces==null || mgmtInterfaces.getLength()==0) {
+            log.warn("No <*-interface> found in host.xml");
+            return DEFAULT_MGMT_PORT;
+        }
+        for (int i = 0 ; i < mgmtInterfaces.getLength(); i++) {
+            if (!(mgmtInterfaces.item(i) instanceof Element))
+                continue;
+
+            Element mgmtInterface = (Element) mgmtInterfaces.item(i);
+            if (mgmtInterface.getNodeName().equals("http-interface")) {
+                String tmp = mgmtInterface.getAttribute("port");
+                int port = Integer.valueOf(tmp);
+                return port;
+            }
+        }
+        return DEFAULT_MGMT_PORT;
+    }
+
+    private List<ServerInfo> getServersFromHostXml() {
+
+        Element host = hostXml.getDocumentElement();
+        NodeList serversElement = host.getElementsByTagName("servers");
+        if (serversElement ==null || serversElement.getLength()==0) {
+            log.warn("No <servers> found in host.xml");
+            return Collections.emptyList();
+        }
+        NodeList servers = serversElement.item(0).getChildNodes();
+        if (servers==null || servers.getLength()==0) {
+            log.warn("No <server> found in host.xml");
+            return Collections.emptyList();
+        }
+        List<ServerInfo> result = new ArrayList<ServerInfo>();
+        for (int i = 0 ; i < servers.getLength(); i++) {
+            if (!(servers.item(i) instanceof Element))
+                continue;
+
+            ServerInfo info = new ServerInfo();
+            Element server = (Element) servers.item(i);
+            info.name = server.getAttribute("name");
+            info.group = server.getAttribute("group");
+            String autoStart = server.getAttribute("autoStart");
+            if (autoStart==null || autoStart.isEmpty())
+                autoStart = "false";
+            info.autoStart = Boolean.getBoolean(autoStart);
+
+            result.add(info);
+        }
+
+        return result;
+    }
+
+
+    private String findHostName() {
+        String hostName = hostXml.getDocumentElement().getAttribute("name");
+        return hostName;
+    }
+
+    private void readHostXml(ProcessInfo processInfo) {
+        String hostXmlFile = getHostXmlFileLocation(processInfo);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            InputStream is = new FileInputStream(hostXmlFile);
+            hostXml = builder.parse(is);
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+    }
+
+    /**
+     * Get the location of the host definition file (host.xml in domain mode, standalone.xml
+     * in standalone mode.
+     *
+     * @param processInfo ProcessInfo structure containing the ENV variables
+     * @return The path to the definition file.
+     */
+    private String getHostXmlFileLocation(ProcessInfo processInfo) {
+
+        String home = getHomeDirFromCommandLine(processInfo.getCommandLine());
+        StringBuilder builder = new StringBuilder(home);
+        builder.append("/domain");
+        builder.append("/configuration");
+        builder.append("/host.xml");
+        return builder.toString();
+
+    }
+
+
+
+    String getHomeDirFromCommandLine(String[] commandLine) {
+            for (String line: commandLine) {
+                if (line.startsWith(DJBOSS_SERVER_HOME_DIR))
+                    return line.substring(DJBOSS_SERVER_HOME_DIR.length()+1);
+            }
+            return "";
+    }
+
+//-Dorg.jboss.boot.log.file=/devel/jbas7/jboss-as/build/target/jboss-7.0.0.Alpha2/domain/log/server-manager/boot.log
+//-Dlogging.configuration=file:/devel/jbas7/jboss-as/build/target/jboss-7.0.0.Alpha2/domain/configuration/logging.properties
+
+    String getLogFileFromCommandLine(String[] commandLine) {
+
+        for (String line: commandLine) {
+            if (line.startsWith(DORG_JBOSS_BOOT_LOG_FILE))
+                return line.substring(DORG_JBOSS_BOOT_LOG_FILE.length());
+        }
+        return "";
+    }
+
+    private void initLogEventSourcesConfigProp(String fileName, Configuration pluginConfiguration) {
+
+        PropertyList logEventSources = pluginConfiguration
+            .getList(LogFileEventResourceComponentHelper.LOG_EVENT_SOURCES_CONFIG_PROP);
+
+        if (logEventSources==null)
+            return;
+
+        File serverLogFile = new File(fileName);
+
+        if (serverLogFile.exists() && !serverLogFile.isDirectory()) {
+            PropertyMap serverLogEventSource = new PropertyMap("logEventSource");
+            serverLogEventSource.put(new PropertySimple(
+                LogFileEventResourceComponentHelper.LogEventSourcePropertyNames.LOG_FILE_PATH, serverLogFile));
+            serverLogEventSource.put(new PropertySimple(
+                LogFileEventResourceComponentHelper.LogEventSourcePropertyNames.ENABLED, Boolean.FALSE));
+            logEventSources.add(serverLogEventSource);
+        }
+    }
+
+    private class ServerInfo {
+        String name;
+        String group;
+        boolean autoStart;
+
+
+    }
+
+}

modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/ServerGroupComponent.java
@@ -1,0 +1,113 @@
+/*
+ * RHQ Management Platform
+ * Copyright (C) 2005-2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.rhq.modules.plugins.jbossas7;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.JsonNode;
+
+import org.rhq.core.domain.content.PackageType;
+import org.rhq.core.domain.content.transfer.DeployPackageStep;
+import org.rhq.core.domain.content.transfer.DeployPackagesResponse;
+import org.rhq.core.domain.content.transfer.RemovePackagesResponse;
+import org.rhq.core.domain.content.transfer.ResourcePackageDetails;
+import org.rhq.core.domain.resource.CreateResourceStatus;
+import org.rhq.core.pluginapi.content.ContentContext;
+import org.rhq.core.pluginapi.content.ContentFacet;
+import org.rhq.core.pluginapi.content.ContentServices;
+import org.rhq.core.pluginapi.inventory.CreateChildResourceFacet;
+import org.rhq.core.pluginapi.inventory.CreateResourceReport;
+
+/**
+ * Component dealing with server group specific things
+ * @author Heiko W. Rupp
+ */
+public class ServerGroupComponent extends DomainComponent implements ContentFacet, CreateChildResourceFacet {
+
+    private final Log log = LogFactory.getLog(ServerGroupComponent.class);
+
+    @Override
+    public List<DeployPackageStep> generateInstallationSteps(ResourcePackageDetails packageDetails) {
+        return null;  // TODO: Customise this generated block
+    }
+
+    @Override
+    public DeployPackagesResponse deployPackages(Set<ResourcePackageDetails> packages,
+                                                 ContentServices contentServices) {
+
+        ContentContext cctx = context.getContentContext();
+
+        String uploadUrl = "http://localhost:9990/domain-api/add-content";
+
+        for (ResourcePackageDetails details : packages) {
+            ASUploadConnection uploadConnection = new ASUploadConnection();
+            OutputStream out = uploadConnection.getOutputStream(details.getFileName());
+            contentServices.downloadPackageBits(cctx, details.getKey(), out, false);
+            JsonNode result = uploadConnection.finishUpload();
+        }
+
+
+        return null;  // TODO: Customise this generated block
+    }
+
+    @Override
+    public RemovePackagesResponse removePackages(Set<ResourcePackageDetails> packages) {
+        return null;  // TODO: Customise this generated block
+    }
+
+    @Override
+    public Set<ResourcePackageDetails> discoverDeployedPackages(PackageType type) {
+        return null;  // TODO: Customise this generated block
+    }
+
+    @Override
+    public InputStream retrievePackageBits(ResourcePackageDetails packageDetails) {
+        return null;  // TODO: Customise this generated block
+    }
+
+    @Override
+    public CreateResourceReport createResource(CreateResourceReport report) {
+
+
+        ResourcePackageDetails details = report.getPackageDetails();
+
+        ContentContext cctx = context.getContentContext();
+        ContentServices contentServices = cctx.getContentServices();
+        String resourceTypeName = report.getResourceType().getName();
+
+        ASUploadConnection connection = new ASUploadConnection();
+        OutputStream out = connection.getOutputStream(details.getFileName());
+//        contentServices.downloadPackageBits(cctx,details.getKey(),out,false);
+        contentServices.downloadPackageBitsForChildResource(cctx, resourceTypeName, details.getKey(), out);
+
+        JsonNode result = connection.finishUpload();
+        System.out.println(result);
+
+
+        report.setStatus(CreateResourceStatus.SUCCESS)       ;
+
+        return report;
+
+    }
+}

modules/plugins/jboss-as-7/src/main/resources/META-INF/rhq-plugin.xml
@@ -71,7 +71,7 @@
             &logSources;
         </plugin-configuration>
 
-        <process-scan name="ProcessManager" query="process|basename|match=^java.*,arg|org.jboss.as.process-controller|match=.*"/>
+        <process-scan name="ProcessController" query="process|basename|match=^java.*,arg|org.jboss.as.process-controller|match=.*"/>
 
 
         <operation name="dummyOperation">
@@ -93,7 +93,7 @@
             &logSources;
         </plugin-configuration>
 
-        <process-scan name="ProcessManager" query="process|basename|match=^java.*,arg|org.jboss.as.host-controller|match=.*"/>
+        <process-scan name="HostController" query="process|basename|match=^java.*,arg|org.jboss.as.host-controller|match=.*"/>
 
 
         <operation name="dummyOperation">
@@ -132,16 +132,25 @@
         <server name="ServerGroup"
                 description="Server groups on this domain"
                 discovery="SubsystemDiscovery"
-                class="ServerGroupComponent">
+                class="ServerGroupComponent"
+                >
+            <!-- TODO move operation to createDeletePolicy="create-only" -->
+
             <plugin-configuration>
                 <c:simple-property name="path" default="/server-group/*" readOnly="true"/>
             </plugin-configuration>
+
+
+            <content name="deployment" category="deployable" isCreationType="true" description="Deployments on this domain">
+            </content>
+
             <resource-configuration>
                 <c:simple-property name="profile" />
                 <c:simple-property name="socket-binding-group"/>
                 <c:simple-property name="jvm"/>
                 <c:simple-property name="deployment" required="false"/> <!-- TODO change structure. Or put below as resource? -->
             </resource-configuration>
+            <service name="Deployment" class="BaseComponent" discovery="SubsystemDiscovery" createDeletePolicy="create-only"/>
         </server>
         <server name="Host"
                 description="Host involved in this domain"
@@ -171,9 +180,11 @@
                 </results>
             </operation>
 
+<!--
             <resource-configuration>
                 <c:list-property name="server-config" />
             </resource-configuration>
+-->
 
         </server>
 
@@ -216,16 +227,20 @@
     </server>
 
     <server name="JBossAS-Managed"
-            discovery="BaseProcessDiscovery"
+            discovery="ManagedASDiscovery"
             class="DomainComponent"
             >
         <plugin-configuration>
             <c:simple-property name="hostname" default="localhost" required="true"/>
             <c:simple-property name="port" default="9990" type="integer" required="true"/>
+            <c:simple-property name="domainHost" readOnly="true" description="Hostname in the domain"/>
+            <c:simple-property name="group" readOnly="true" displayName="Server Group" description="Server Group this instance belongs to."/>
 
             &logSources;
         </plugin-configuration>
 
+        <!-- Scan for host controller is intentional -->
+        <process-scan name="HostController" query="process|basename|match=^java.*,arg|org.jboss.as.host-controller|match=.*"/>
         <process-scan name="ManagedAS" query="process|basename|match=^java.*,arg|org.jboss.as.server|match=.*"/>
 
         <operation name="server:start" description="Start this server instance." displayName="Start">
@@ -330,7 +345,7 @@
                  class="BaseComponent"
                 >
             <plugin-configuration>
-                <c:simple-property name="path" readOnly="true" default="/queue/"/>
+                <c:simple-property name="path" readOnly="true" default="/queue/*"/>
             </plugin-configuration>
         </service>
     </server>
@@ -383,7 +398,9 @@
 
             <resource-configuration>
                 <c:simple-property name="access-log"/>
-                <c:list-property name="alias" description="The virtual server aliases"/>
+                <c:list-property name="alias" description="The virtual server aliases">
+                    <c:simple-property name="alias"/>
+                </c:list-property>
                 <c:simple-property name="rewrite"/>
             </resource-configuration>
 

commit dacab80cb014a34a23ad32a61643fbe26b4fa5dc

Incorrect copyright date introduced by commit f2dc897
Corrected copyright start date. This class was not introduced until
2011.

modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/BaseProcessDiscovery.java
@@ -1,6 +1,6 @@
 /*
  * RHQ Management Platform
- * Copyright (C) 2005-2012 Red Hat, Inc.
+ * Copyright (C) 2011-2012 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -13,8 +13,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 package org.rhq.modules.plugins.jbossas7;
 
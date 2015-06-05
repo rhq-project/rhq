24bd7c37e5a6495025c556779a800566ebda2008
Libor Zoubek <lzoubek@redhat.com>
2015-06-05 14:01:24 +0200

    Bug 1212933 - Resource start operation leads to broken managed server
    Close output and input streams in case of Error, not just Exception is
    thrown.

--- modules/core/util/src/main/java/org/rhq/core/util/exec/StreamRedirectorRunnable.java
+++ modules/core/util/src/main/java/org/rhq/core/util/exec/StreamRedirectorRunnable.java
@@ -1,30 +1,33 @@
  /*
-  * RHQ Management Platform
-  * Copyright (C) 2005-2008 Red Hat, Inc.
-  * All rights reserved.
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License, version 2, as
-  * published by the Free Software Foundation, and/or the GNU Lesser
-  * General Public License, version 2.1, also as published by the Free
-  * Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License and the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * and the GNU Lesser General Public License along with this program;
-  * if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  */
+ * RHQ Management Platform
+ * Copyright (C) 2005-2015 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.rhq.core.util.exec;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Redirects data coming in from one (input) stream into another (output) stream.
@@ -43,6 +46,8 @@ public class StreamRedirectorRunnable implements Runnable {
     private final OutputStream m_output;
 
     private final String m_name;
+
+    private static final Log LOG = LogFactory.getLog(StreamRedirectorRunnable.class);
 
     /**
      * Constructor for {@link StreamRedirectorRunnable} that takes an input stream where we read data in and an output stream
@@ -71,39 +76,42 @@ public class StreamRedirectorRunnable implements Runnable {
      */
     @Override
     public void run() {
-        Thread t = Thread.currentThread();
-        t.setName(m_name);
+        Thread thread = Thread.currentThread();
+        thread.setName(m_name);
+
 
         final int bufferSize = 4096;
         byte[] buffer = new byte[bufferSize];
-        boolean keepGoing = true;
 
         try {
-            while (keepGoing) {
+            while (Thread.interrupted() != true) {
                 int read = m_input.read(buffer, 0, bufferSize);
-                if (read > 0) {
-                    if (m_output != null) {
-                        m_output.write(buffer, 0, read);
-                    }
-                } else {
-                    keepGoing = false;
+                // check for EOF
+                if (read < 0) {
+                    LOG.debug("Reached EOF on input stream"); //$NON-NLS-1$
+                    break;
+                }
+                // do redirection
+                if (read > 0 && m_output != null) {
+                    m_output.write(buffer, 0, read);
                 }
             }
-        } catch (Exception e) {
-            // just abort the while loop and close the streams
-        }
-
-        // finished reading the input, so we can now close the streams and exit the thread
-        try {
-            m_input.close();
-        } catch (IOException e) {
-        }
-
-        try {
-            if (m_output != null) {
-                m_output.close();
+        } catch (Throwable t) {
+            LOG.warn("An unexpected error occurred while redirecting stream output: " + t.getMessage(), t); //$NON-NLS-1$
+        } finally {
+            // finished reading the input, close the streams and exit the thread
+            try {
+                m_input.close();
+            } catch (IOException e) {
+                // we do not care if close fails as there is nothing we can do
             }
-        } catch (IOException e) {
+            try {
+                if (m_output != null) {
+                    m_output.close();
+                }
+            } catch (IOException e) {
+                // we do not care if close fails as there is nothing we can do
+            }
         }
 
         return;

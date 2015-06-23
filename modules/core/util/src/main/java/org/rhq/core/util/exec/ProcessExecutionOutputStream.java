6e6149c9eb0a2571ded8853a08cfda393464ae3c
Libor Zoubek <lzoubek@redhat.com>
2015-06-23 17:37:30 +0200

    Bug 1212878 - Cannot obtain client connection to the RHQ app server while upgrading JON server
    When checking whether server is up and we're able to talk to it we now
    ignore all output for first 10 checks (out of 30) and only care about
    connection test exit code. For this purpose ProcessExecutionOutputStream has
    been moved from rhq-core-native-system to rhq-core-util.

--- modules/core/util/src/main/java/org/rhq/core/util/exec/ProcessExecutionOutputStream.java
+++ modules/core/util/src/main/java/org/rhq/core/util/exec/ProcessExecutionOutputStream.java
@@ -20,7 +20,7 @@
  * if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package org.rhq.core.system;
+package org.rhq.core.util.exec;
 
 import java.io.ByteArrayOutputStream;
 
--- modules/enterprise/server/server-control/src/main/java/org/rhq/server/control/util/ExecutorAssist.java
+++ modules/enterprise/server/server-control/src/main/java/org/rhq/server/control/util/ExecutorAssist.java
@@ -1,3 +1,27 @@
+/*
+ *
+ *  * RHQ Management Platform
+ *  * Copyright (C) 2005-2015 Red Hat, Inc.
+ *  * All rights reserved.
+ *  *
+ *  * This program is free software; you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License, version 2, as
+ *  * published by the Free Software Foundation, and/or the GNU Lesser
+ *  * General Public License, version 2.1, also as published by the Free
+ *  * Software Foundation.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License and the GNU Lesser General Public License
+ *  * for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * and the GNU Lesser General Public License along with this program;
+ *  * if not, write to the Free Software Foundation, Inc.,
+ *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
 package org.rhq.server.control.util;
 
 import java.io.File;
@@ -14,16 +38,49 @@ import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.PumpStreamHandler;
 
+import org.rhq.core.util.exec.ProcessExecutionOutputStream;
+
+@SuppressWarnings("rawtypes")
 public class ExecutorAssist {
 
-    private static Executor executor;
+    private static ExecutorAssist defaultAssit;
+    private static ExecutorAssist silentAssist;
+    /**
+     * @return default executor assist
+     */
+    public static ExecutorAssist getDefault() {
+        if (defaultAssit == null) {
+            defaultAssit = new ExecutorAssist();
+        }
+        return defaultAssit;
+    }
 
-    static {
+    /**
+     * @return silent executor assist (does not capture child process output streams)
+     */
+    public static ExecutorAssist getSilent() {
+        if (silentAssist == null) {
+            silentAssist = new ExecutorAssist();
+            silentAssist.executor.setStreamHandler(
+                new PumpStreamHandler(
+                    new ProcessExecutionOutputStream(0, false),
+                    new ProcessExecutionOutputStream(0, false)));
+        }
+        return silentAssist;
+    }
+
+    private final Executor executor;
+
+    private ExecutorAssist() {
         executor = new DefaultExecutor();
         executor.setStreamHandler(new PumpStreamHandler());
     }
 
-    public static int execute(final File workingDir, final CommandLine commandLine, @SuppressWarnings("rawtypes") final Map environment) throws IOException {
+    public int exec(final File workingDir, final CommandLine commandLine) throws IOException {
+        return exec(workingDir, commandLine, null);
+    }
+
+    public int exec(final File workingDir, final CommandLine commandLine, final Map environment) throws IOException {
         int rValue = 0;
         try {
             // Synchronized to prevent two threads from setting different workingDirectory at the same time..
@@ -39,17 +96,30 @@ public class ExecutorAssist {
         return rValue;
     }
 
-    public static Future<Integer> executeAsync(final File workingDir, final CommandLine commandLine, final Map environment) {
+    // static methods for compatibility (using default ExecutorAssist)
+
+    public Future<Integer> execAsync(final File workingDir, final CommandLine commandLine, final Map environment) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         return executorService.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
-                return execute(workingDir, commandLine, environment);
+                return exec(workingDir, commandLine, environment);
             }
         });
     }
 
-    public static int execute(final File workingDir, final CommandLine commandLine) throws IOException {
-        return execute(workingDir, commandLine, null);
+    public static int execute(final File workingDir, final CommandLine commandLine, final Map environment)
+        throws IOException {
+        return getDefault().exec(workingDir, commandLine, environment);
     }
+
+    public static Future<Integer> executeAsync(final File workingDir, final CommandLine commandLine,
+        final Map environment) {
+        return getDefault().execAsync(workingDir, commandLine, environment);
+    }
+
+    public static int execute(final File workingDir, final CommandLine commandLine) throws IOException {
+        return getDefault().exec(workingDir, commandLine);
+    }
+
 }

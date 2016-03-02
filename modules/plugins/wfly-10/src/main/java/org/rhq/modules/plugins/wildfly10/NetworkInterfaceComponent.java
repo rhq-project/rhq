2f0b11e407d19783f74f7beae7554051f2dc12c1
Michael Burman <miburman@redhat.com>
2016-03-02 15:27:46 +0200

    [BZ 1312389] Remove any-ipv4-address and any-ipv6-address as options

--- modules/plugins/wfly-10/src/main/java/org/rhq/modules/plugins/wildfly10/NetworkInterfaceComponent.java
+++ modules/plugins/wfly-10/src/main/java/org/rhq/modules/plugins/wildfly10/NetworkInterfaceComponent.java
@@ -32,29 +32,7 @@ import org.rhq.core.pluginapi.configuration.ConfigurationUpdateReport;
 public class NetworkInterfaceComponent extends BaseComponent<NetworkInterfaceComponent> implements
          ConfigurationFacet {
 
-    private static final String[] wildCards = {"any-address","any-ipv4-address","any-ipv6-address"};
-    private static final String WILDCARD_IGNORE = "wildcard:ignore";
-
-    @Override
-    public Configuration loadResourceConfiguration() throws Exception {
-        Configuration configuration = super.loadResourceConfiguration();
-
-
-
-/* TODO revisit later BZ 825169
-        // We now need to get the any*address properties separately
-        for (String wildcard : wildCards) {
-            Boolean val = readAttribute(getAddress(),wildcard,Boolean.class);
-            if (val) {
-                PropertySimple wildCardProp = new PropertySimple(WILDCARD_IGNORE,wildcard);
-                configuration.put(wildCardProp);
-                break;
-            }
-        }
-*/
-
-        return configuration;
-    }
+    private static final String[] wildCards = {"any-address"};
 
     @Override
     public void updateResourceConfiguration(ConfigurationUpdateReport report) {
@@ -70,7 +48,7 @@ public class NetworkInterfaceComponent extends BaseComponent<NetworkInterfaceCom
             if (isWildcard) {
                 // we couldn't know what user wants ... if setting inet-addr or one of wildcard props
                 report
-                    .setErrorMessage("When setting any-address or any-ipv4-address or any-ipv6-address to true inet-address must be unset");
+                    .setErrorMessage("When setting any-address to true inet-address must be unset");
                 report.setStatus(ConfigurationUpdateStatus.FAILURE);
                 return;
             }
@@ -78,10 +56,9 @@ public class NetworkInterfaceComponent extends BaseComponent<NetworkInterfaceCom
             for (String wildcard : wildCards) {
                 config.getSimple(wildcard).setValue(null);
             }
-
         } else if (!isWildcard) {
             report
-                .setErrorMessage("You need to enable either any-address or any-ipv4-address or any-ipv6-address when inet-address is disabled");
+                .setErrorMessage("You need to enable either any-address when inet-address is disabled");
             report.setStatus(ConfigurationUpdateStatus.FAILURE);
             return;
         }
@@ -89,31 +66,10 @@ public class NetworkInterfaceComponent extends BaseComponent<NetworkInterfaceCom
     }
 
     public static void preProcessCreateChildConfiguration(Configuration configuration) {
-
-        // Server is too stupid to accept all three any*address props for :add
-        // [standalone@localhost:9999 interface=test] :add(any-address=true,any-ipv4-address=false,any-ipv6-address=false)
-        // {
-        //    "outcome" => "failed",
-        //    "failure-description" => "JBAS014690: any-address is invalid",
-        //    "rolled-back" => true
-        //}
-        // So we need to filter the false ones
-
         for (String wildCard: wildCards) {
             PropertySimple ps = configuration.getSimple(wildCard);
             if (ps!=null && ps.getStringValue().equals("false"))
                 configuration.remove(wildCard);
         }
-
-
-/* TODO revisit later BZ 825169
-        PropertySimple wildCardProp = configuration.getSimple(WILDCARD_IGNORE);//, "any-address");
-        String tmp = wildCardProp.getStringValue();
-        for (String wildCard : wildCards) {
-            boolean val = wildCard.equals(tmp);
-            PropertySimple prop = new PropertySimple(wildCard,val);
-            configuration.put(prop);
-        }
-*/
     }
 }

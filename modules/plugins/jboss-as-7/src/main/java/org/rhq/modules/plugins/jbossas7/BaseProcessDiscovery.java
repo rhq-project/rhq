264870d0caefc0f6682552e039b53fc0b5ce87fa
Michael Burman <miburman@redhat.com>
2016-03-04 15:33:41 +0200

    [BZ 1314782] Add missing null check for HostXmlFile

--- modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/BaseProcessDiscovery.java
+++ modules/plugins/jboss-as-7/src/main/java/org/rhq/modules/plugins/jbossas7/BaseProcessDiscovery.java
@@ -523,7 +523,7 @@ public abstract class BaseProcessDiscovery implements ResourceDiscoveryComponent
         HostConfiguration hostConfig = null;
         // load hostConfiguration just once - we need it several times
         File hostXmlFile = serverPluginConfiguration.getHostConfigFile();
-        if (hostXmlFile.exists()) {
+        if (hostXmlFile != null && hostXmlFile.exists()) {
             try {
                 hostConfig = loadHostConfiguration(hostXmlFile);
             } catch (Exception e) {

9b2d5e073d701610b5684a48f5e170350f937760
Josejulio Martínez <jmartine@redhat.com>
2017-01-12 23:45:07 -0600

    Bug 1368107 - Increase the amount of items per page.
    This increases the number of items per page on every gui/portal-war
    that uses the PageControlSettingsUIBean.

--- modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/common/paging/PageControlSettingsUIBean.java
+++ modules/enterprise/gui/portal-war/src/main/java/org/rhq/enterprise/gui/common/paging/PageControlSettingsUIBean.java
@@ -24,8 +24,30 @@ import javax.faces.model.SelectItem;
  * @author Joseph Marques
  */
 public class PageControlSettingsUIBean {
-    private SelectItem[] pageSizes = new SelectItem[] { new SelectItem("15", "15"), new SelectItem("30", "30"),
-        new SelectItem("45", "45") };
+    private SelectItem[] pageSizes;
+    private static String MAX_ITEMS_PER_PAGE = "rhq.server.gui.max-items-per-page";
+
+    public PageControlSettingsUIBean() {
+        try {
+            String maxItemsPerPage = System.getProperty(MAX_ITEMS_PER_PAGE);
+            int maxItemsPerPageInt;
+            if (maxItemsPerPage != null && (maxItemsPerPageInt = Integer.parseInt(maxItemsPerPage)) >= 45) {
+                // Scale default page sizes
+                String tier0 = String.valueOf(Math.max(15, (int) (maxItemsPerPageInt * 0.25)));
+                String tier1 = String.valueOf(Math.max(30, (int) (maxItemsPerPageInt * 0.50)));
+                String tier2 = String.valueOf(Math.max(45, (int) (maxItemsPerPageInt * 0.75)));
+                pageSizes = new SelectItem[] { new SelectItem(tier0, tier0), new SelectItem(tier1, tier1),
+                        new SelectItem(tier2, tier2), new SelectItem(maxItemsPerPage, maxItemsPerPage) };
+            }
+        } catch (NumberFormatException nfe) {
+            // Ignore this exception.
+        }
+        if (pageSizes == null) {
+            pageSizes = new SelectItem[]{new SelectItem("15", "15"), new SelectItem("30", "30"),
+                    new SelectItem("45", "45")};
+        }
+
+    }
 
     public SelectItem[] getPageSizes() {
         return pageSizes;

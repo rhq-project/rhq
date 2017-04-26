21192c2a94454e1b07154f3dccdc34e9cdfcc9e6
Michael Burman <miburman@redhat.com>
2017-04-26 13:55:37 +0300

    [BZ 1387296] Add server connection settings to manage deploymentTimeouts

--- modules/plugins/wfly-10/src/main/java/org/rhq/modules/plugins/wildfly10/helper/ServerPluginConfiguration.java
+++ modules/plugins/wfly-10/src/main/java/org/rhq/modules/plugins/wildfly10/helper/ServerPluginConfiguration.java
@@ -44,6 +44,7 @@ public class ServerPluginConfiguration {
         public static final String USER = "user";
         public static final String PASSWORD = "password";
         public static final String MANAGEMENT_CONNECTION_TIMEOUT = "managementConnectionTimeout";
+        public static final String DEPLOYMENT_CONNECTION_TIMEOUT = "deploymentConnectionTimeout";
         public static final String HOME_DIR = "homeDir";
         public static final String BASE_DIR = "baseDir";
         public static final String CONFIG_DIR = "configDir";
@@ -144,6 +145,13 @@ public class ServerPluginConfiguration {
 
     public void setPassword(String password) {
         this.pluginConfig.setSimpleValue(Property.PASSWORD, password);
+    }
+
+    public Long getDeploymentConnectionTimeout() {
+        if(pluginConfig.getSimple(Property.DEPLOYMENT_CONNECTION_TIMEOUT) != null) {
+            return pluginConfig.getSimple(Property.DEPLOYMENT_CONNECTION_TIMEOUT).getLongValue();
+        }
+        return null;
     }
 
     public Long getManagementConnectionTimeout() {
